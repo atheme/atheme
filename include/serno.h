32c0118b35228b05c6a8218160b049aa901be4d6
pfish <devnull@localhost>
2005-10-02 19:10:24 -0400

    [svn] Add NICKSERV RESETPASS. Opers can generate a random password for a nickname.

--- include/serno.h
+++ include/serno.h
@@ -1,1 +1,1 @@
-#define SERNO "20051002-2503"
+#define SERNO "20051002-2505"
---
+++ modules/nickserv/resetpass.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2005 Patrick Fish, et al.
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * This file contains code for NickServ RESETPASS
+ *
+ * $Id: resetpass.c 2507 2005-10-02 23:10:24Z pfish $
+ */
+
+#include "atheme.h"
+
+DECLARE_MODULE_V1
+(
+	"nickserv/resetpass", FALSE, _modinit, _moddeinit,
+	"$Id: resetpass.c 2507 2005-10-02 23:10:24Z pfish $",
+	"Atheme Development Group <http://www.atheme.org>"
+);
+
+static void ns_cmd_resetpass(char *origin);
+
+command_t ns_resetpass = { "RESETPASS", "Resets a nickname password.",
+                        AC_IRCOP, ns_cmd_resetpass };
+                                                                                   
+list_t *ns_cmdtree;
+
+void _modinit(module_t *m)
+{
+	ns_cmdtree = module_locate_symbol("nickserv/main", "ns_cmdtree");
+        command_add(&ns_resetpass, ns_cmdtree);
+}
+
+void _moddeinit()
+{
+	command_delete(&ns_resetpass, ns_cmdtree);
+}
+
+static void ns_cmd_resetpass(char *origin)
+{
+	myuser_t *mu;
+//	node_t *n, *tn;
+	metadata_t *md;
+	char *name = strtok(NULL, " ");
+	char *newpass = gen_pw(12);
+
+	if (!name)
+	{
+		notice(nicksvs.nick, origin, "Invalid parameters specified for \2RESETPASS\2.");
+		notice(nicksvs.nick, origin, "Syntax: RESETPASS <nickname>");
+		return;
+	}
+
+	if (!(mu = myuser_find(name)))
+	{
+		notice(nicksvs.nick, origin, "\2%s\2 is not registered.", name);
+		return;
+	}
+
+	if (md = metadata_find(mu, METADATA_USER, "private:mark:setter"))
+	{
+		notice(nicksvs.nick, origin, "This operation cannot be performed on %s, because the nickname has been marked by %s.", name, md->value);
+		return;
+	}
+
+	notice(nicksvs.nick, origin, "The password for the nickname %s has been changed to %s.", name, newpass);
+	strlcpy(mu->pass, newpass, NICKLEN);
+
+	snoop("RESETPASS: \2%s\2 reset the password for \2%s\2", origin, name);
+
+}
