68288d20b551eafb60d95d75bf48ed7da304a704
jilles <devnull@localhost>
2006-10-25 18:53:31 -0400

    [svn] chanserv unban: give an error message if the given mask is a valid n!u@h mask but is not on the channel ban list (previously this silently did nothing).

--- include/serno.h
+++ include/serno.h
@@ -1,1 +1,1 @@
-#define SERNO "20061025-6947"
+#define SERNO "20061025-6949"
--- modules/chanserv/ban.c
+++ modules/chanserv/ban.c
@@ -4,7 +4,7 @@
  *
  * This file contains code for the CService BAN/UNBAN function.
  *
- * $Id: ban.c 6727 2006-10-20 18:48:53Z jilles $
+ * $Id: ban.c 6951 2006-10-25 22:53:31Z jilles $
  */
 
 #include "atheme.h"
@@ -12,7 +12,7 @@
 DECLARE_MODULE_V1
 (
 	"chanserv/ban", FALSE, _modinit, _moddeinit,
-	"$Id: ban.c 6727 2006-10-20 18:48:53Z jilles $",
+	"$Id: ban.c 6951 2006-10-25 22:53:31Z jilles $",
 	"Atheme Development Group <http://www.atheme.org>"
 );
 
@@ -222,6 +222,8 @@ static void cs_cmd_unban(sourceinfo_t *si, int parc, char *parv[])
 			if (!chanuser_find(mc->chan, si->su))
 				command_success_nodata(si, "Unbanned \2%s\2 on \2%s\2.", target, channel);
 		}
+		else
+			command_fail(si, fault_nosuch_key, "No such ban \2%s\2 on \2%s\2.", target, channel);
 
 		return;
 	}
