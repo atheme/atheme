7c8ab226033395f077765c8142442f654c89cd07
jilles <devnull@localhost>
2007-02-09 14:20:38 -0500

    [svn] Add /ns fverify to nickserv/verify, allows admins with user:admin privilege to verify any account without knowing the verification key from the email.

--- include/serno.h
+++ include/serno.h
@@ -1,1 +1,1 @@
-#define SERNO "20070208-7619"
+#define SERNO "20070208-7621"
--- modules/nickserv/verify.c
+++ modules/nickserv/verify.c
@@ -4,7 +4,7 @@
  *
  * This file contains code for the NickServ VERIFY function.
  *
- * $Id: verify.c 7063 2006-11-04 19:26:52Z jilles $
+ * $Id: verify.c 7623 2007-02-09 19:20:38Z jilles $
  */
 
 #include "atheme.h"
@@ -12,13 +12,15 @@
 DECLARE_MODULE_V1
 (
 	"nickserv/verify", FALSE, _modinit, _moddeinit,
-	"$Id: verify.c 7063 2006-11-04 19:26:52Z jilles $",
+	"$Id: verify.c 7623 2007-02-09 19:20:38Z jilles $",
 	"Atheme Development Group <http://www.atheme.org>"
 );
 
 static void ns_cmd_verify(sourceinfo_t *si, int parc, char *parv[]);
+static void ns_cmd_fverify(sourceinfo_t *si, int parc, char *parv[]);
 
 command_t ns_verify = { "VERIFY", "Verifies a nickname registration.", AC_NONE, 3, ns_cmd_verify };
+command_t ns_fverify = { "FVERIFY", "Forcefully verifies a nickname registration.", PRIV_USER_ADMIN, 2, ns_cmd_fverify };
 
 list_t *ns_cmdtree, *ns_helptree;
 
@@ -29,12 +31,16 @@ void _modinit(module_t *m)
 
 	command_add(&ns_verify, ns_cmdtree);
 	help_addentry(ns_helptree, "VERIFY", "help/nickserv/verify", NULL);
+	command_add(&ns_fverify, ns_cmdtree);
+	help_addentry(ns_helptree, "FVERIFY", "help/nickserv/fverify", NULL);
 }
 
 void _moddeinit()
 {
 	command_delete(&ns_verify, ns_cmdtree);
 	help_delentry(ns_helptree, "VERIFY");
+	command_delete(&ns_fverify, ns_cmdtree);
+	help_delentry(ns_helptree, "FVERIFY");
 }
 
 static void ns_cmd_verify(sourceinfo_t *si, int parc, char *parv[])
@@ -142,6 +148,83 @@ static void ns_cmd_verify(sourceinfo_t *si, int parc, char *parv[])
 	{
 		command_fail(si, fault_badparams, "Invalid operation specified for \2VERIFY\2.");
 		command_fail(si, fault_badparams, "Please double-check your verification e-mail.");
+		return;
+	}
+}
+
+static void ns_cmd_fverify(sourceinfo_t *si, int parc, char *parv[])
+{
+	myuser_t *mu;
+	metadata_t *md;
+	node_t *n;
+	char *op = parv[0];
+	char *nick = parv[1];
+
+	if (!op || !nick)
+	{
+		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "FVERIFY");
+		command_fail(si, fault_needmoreparams, "Syntax: FVERIFY <operation> <nickname>");
+		return;
+	}
+
+	if (!(mu = myuser_find(nick)))
+	{
+		command_fail(si, fault_nosuch_target, "\2%s\2 is not registered.", nick);
+		return;
+	}
+
+	if (!strcasecmp(op, "REGISTER"))
+	{
+		if (!(mu->flags & MU_WAITAUTH) || !(md = metadata_find(mu, METADATA_USER, "private:verify:register:key")))
+		{
+			command_fail(si, fault_badparams, "\2%s\2 is not awaiting authorization.", nick);
+			return;
+		}
+
+		mu->flags &= ~MU_WAITAUTH;
+
+		snoop("REGISTER:VS: \2%s\2 for \2%s\2 by \2%s\2", mu->email, mu->name, get_source_name(si));
+		logcommand(si, CMDLOG_REGISTER, "FVERIFY REGISTER %s (email: %s)", mu->name, mu->email);
+
+		metadata_delete(mu, METADATA_USER, "private:verify:register:key");
+		metadata_delete(mu, METADATA_USER, "private:verify:register:timestamp");
+
+		command_success_nodata(si, "\2%s\2 has now been verified.", mu->name);
+		LIST_FOREACH(n, mu->logins.head)
+		{
+			user_t *u = n->data;
+			ircd_on_login(u->nick, mu->name, NULL);
+		}
+
+		return;
+	}
+	else if (!strcasecmp(op, "EMAILCHG"))
+	{
+		if (!(md = metadata_find(mu, METADATA_USER, "private:verify:emailchg:key")))
+		{
+			command_fail(si, fault_badparams, "\2%s\2 is not awaiting authorization.", nick);
+			return;
+		}
+
+		md = metadata_find(mu, METADATA_USER, "private:verify:emailchg:newemail");
+
+		strlcpy(mu->email, md->value, EMAILLEN);
+
+		snoop("SET:EMAIL:VS: \2%s\2 for \2%s\2 by \2%s\2", mu->email, mu->name, get_source_name(si));
+		logcommand(si, CMDLOG_REGISTER, "FVERIFY EMAILCHG %s (email: %s)", mu->name, mu->email);
+
+		metadata_delete(mu, METADATA_USER, "private:verify:emailchg:key");
+		metadata_delete(mu, METADATA_USER, "private:verify:emailchg:newemail");
+		metadata_delete(mu, METADATA_USER, "private:verify:emailchg:timestamp");
+
+		command_success_nodata(si, "\2%s\2 has now been verified.", mu->email);
+
+		return;
+	}
+	else
+	{
+		command_fail(si, fault_badparams, "Invalid operation specified for \2FVERIFY\2.");
+		command_fail(si, fault_badparams, "Valid operations are REGISTER and EMAILCHG.");
 		return;
 	}
 }
