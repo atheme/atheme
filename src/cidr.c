e3836c81ebe715f9c49dfb2dab5edc646633d433
William Pitcock <nenolod@atheme.org>
2007-06-14 20:44:13 -0500

    atheme core: BSD license in boilerplate text.

--- src/cidr.c
+++ src/cidr.c
@@ -1,17 +1,40 @@
 /*
- * cidr.c: CIDR matching
- *
- * Copyright (c) 1996-2002 Hybrid Development Team
- * Copyright (c) 2002-2005 ircd-ratbox development team
- * Copyright (c) 2006 Atheme Development Group
- *
- * See doc/LICENSE for licensing information.
+ * atheme-services: A collection of minimalist IRC services   
+ * cidr.c: CIDR matching.
  *
  * Most code in this file has been copied from ratbox, src/match.c and
  * src/irc_string.c. It provides CIDR matching for IPv4 and IPv6 without
  * special OS support.
  *
- * $Id: cidr.c 7779 2007-03-03 13:55:42Z pippijn $
+ * Copyright (c) 1996-2002 Hybrid Development Team
+ * Copyright (c) 2002-2005 ircd-ratbox development team
+ * Copyright (c) 2005-2007 Atheme Project (http://www.atheme.org)           
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "atheme.h"
--- src/confparse.c
+++ src/confparse.c
@@ -1,12 +1,34 @@
 /*
- * Copyright (c) 2005 Atheme Development Group
+ * atheme-services: A collection of minimalist IRC services   
+ * confparse.c: Initialization and startup of the services system
  *
- * Rights to this code are documented in doc/LICENSE.
+ * Copyright (c) 2005-2007 Atheme Project (http://www.atheme.org)           
  *
- * This file contains config file parsing routines.
- * This code was taken from Sentinel: copyright W. Campbell.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
  *
- * $Id: confparse.c 8297 2007-05-20 08:56:59Z nenolod $
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "atheme.h"
