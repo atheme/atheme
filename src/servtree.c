16bb6a4e8ab4439909f7d6ed27226f023f9caa37
William Pitcock <nenolod@atheme.org>
2007-09-05 06:38:27 -0500

    licensing cleanup: relicense most of the atheme core under the simplied BSD license (also known as the ISC license), which is functionally the same thing as the BSD license that we were using. the old Atheme dual-licensing option was still mentioned in version.sh, this has not been relevant since v2.2, change that text to the new Atheme license

--- src/servtree.c
+++ src/servtree.c
@@ -1,10 +1,23 @@
 /*
  * Copyright (c) 2005-2006 Atheme Development Group
- * Rights to this code are as documented in doc/LICENSE.
+ * servtree.c: Services binary tree manipulation. (add_service, 
+ *    del_service, et al.)
  *
- * Services binary tree manipulation. (add_service, del_service, et al.)
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * $Id: servtree.c 7839 2007-03-06 00:09:30Z pippijn $
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "atheme.h"
