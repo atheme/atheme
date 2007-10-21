e9d5fd6930aae4263436ddda552845e87e4e4ce8
Jilles Tjoelker <jilles@stack.nl>
2007-10-21 22:45:49 +0200

    nickserv: do not mention expiry if it is set to 0.

--- modules/nickserv/help.c
+++ modules/nickserv/help.c
@@ -48,16 +48,26 @@ void ns_cmd_help(sourceinfo_t *si, int parc, char *parv[])
 		if (nicksvs.no_nick_ownership)
 		{
 			command_success_nodata(si, _("\2%s\2 allows users to \2'register'\2 an account for use with"), nicksvs.nick);
-			command_success_nodata(si, _("\2%s\2. If a registered account is not used by the owner for %d days,"), chansvs.nick, (nicksvs.expiry / 86400));
-			command_success_nodata(si, _("\2%s\2 will drop the account, allowing it to be reregistered."), nicksvs.nick);
+			if (nicksvs.expiry > 0)
+			{
+				command_success_nodata(si, _("\2%s\2. If a registered account is not used by the owner for %d days,"), chansvs.nick, (nicksvs.expiry / 86400));
+				command_success_nodata(si, _("\2%s\2 will drop the account, allowing it to be reregistered."), nicksvs.nick);
+			}
+			else
+				command_success_nodata(si, _("\2%s\2."), chansvs.nick);
 		}
 		else
 		{
 			command_success_nodata(si, _("\2%s\2 allows users to \2'register'\2 a nickname, and stop"), nicksvs.nick);
 			command_success_nodata(si, _("others from using that nick. \2%s\2 allows the owner of a"), nicksvs.nick);
 			command_success_nodata(si, _("nickname to disconnect a user from the network that is using"), nicksvs.nick);
-			command_success_nodata(si, _("their nickname. If a registered nick is not used by the owner for %d days,"), (nicksvs.expiry / 86400));
-			command_success_nodata(si, _("\2%s\2 will drop the nickname, allowing it to be reregistered."), nicksvs.nick);
+			if (nicksvs.expiry > 0)
+			{
+				command_success_nodata(si, _("their nickname. If a registered nick is not used by the owner for %d days,"), (nicksvs.expiry / 86400));
+				command_success_nodata(si, _("\2%s\2 will drop the nickname, allowing it to be reregistered."), nicksvs.nick);
+			}
+			else
+				command_success_nodata(si, _("their nickname."));
 		}
 		command_success_nodata(si, " ");
 		command_success_nodata(si, _("For more information on a command, type:"));
