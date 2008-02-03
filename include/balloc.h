c6d7afb013cf3ca488d779c85d6b225a491b6318
Jilles Tjoelker <jilles@stack.nl>
2008-02-03 13:25:47 +0100

    blockheap: move struct definitions to balloc.c

--- include/balloc.h
+++ include/balloc.h
@@ -2,7 +2,7 @@
  * Copyright (C) 2005 William Pitcock, et al.
  * Rights to this code are as documented in doc/LICENSE.
  *
- * Data structures for the block allocator.
+ * The block allocator.
  *
  * $Id: balloc.h 7779 2007-03-03 13:55:42Z pippijn $
  */
@@ -10,36 +10,7 @@
 #ifndef BALLOC_H
 #define BALLOC_H
 
-struct Block
-{
-  size_t alloc_size;
-  struct Block *next;     /* Next in our chain of blocks */
-  void *elems;            /* Points to allocated memory */
-  list_t free_list;
-};
-typedef struct Block Block;
-
-struct MemBlock
-{
-#ifdef DEBUG_BALLOC
-  unsigned long magic;
-#endif
-  node_t self;
-  Block *block;           /* Which block we belong to */
-};
-
-typedef struct MemBlock MemBlock;
-
-/* information for the root node of the heap */
-struct BlockHeap
-{
-  node_t hlist;
-  size_t elemSize;        /* Size of each element to be stored */
-  unsigned long elemsPerBlock;    /* Number of elements per block */
-  unsigned long blocksAllocated;  /* Number of blocks allocated */
-  unsigned long freeElems;                /* Number of free elements */
-  Block *base;            /* Pointer to first block */
-};
+struct BlockHeap;
 typedef struct BlockHeap BlockHeap;
 
 E int BlockHeapFree(BlockHeap *bh, void *ptr);
--- src/balloc.c
+++ src/balloc.c
@@ -38,6 +38,36 @@
 # define PROT_WRITE 0
 #endif
 
+struct Block
+{
+  size_t alloc_size;
+  struct Block *next;     /* Next in our chain of blocks */
+  void *elems;            /* Points to allocated memory */
+  list_t free_list;
+};
+typedef struct Block Block;
+
+struct MemBlock
+{
+#ifdef DEBUG_BALLOC
+  unsigned long magic;
+#endif
+  node_t self;
+  Block *block;           /* Which block we belong to */
+};
+
+typedef struct MemBlock MemBlock;
+
+/* information for the root node of the heap */
+struct BlockHeap
+{
+  node_t hlist;
+  size_t elemSize;        /* Size of each element to be stored */
+  unsigned long elemsPerBlock;    /* Number of elements per block */
+  unsigned long blocksAllocated;  /* Number of blocks allocated */
+  unsigned long freeElems;                /* Number of free elements */
+  Block *base;            /* Pointer to first block */
+};
 static int newblock(BlockHeap *bh);
 static int BlockHeapGarbageCollect(BlockHeap *);
 static void block_heap_gc(void *unused);
