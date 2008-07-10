ac3d157139b97991d4e62ad9cb9e89e61c5670b1
William Pitcock <nenolod@atheme.org>
2008-07-09 21:00:05 -0500

    Fix module ID.

--- contrib/cs_babbler.c
+++ contrib/cs_babbler.c
@@ -10,7 +10,7 @@
 
 DECLARE_MODULE_V1
 (
-	"chanserv/kickdots", FALSE, _modinit, _moddeinit,
+	"chanserv/babbler", FALSE, _modinit, _moddeinit,
 	"$Id: cs_kickdots.c 7785 2007-03-03 15:54:32Z pippijn $",
 	"William Pitcock <nenolod -at- nenolod.net>"
 );
