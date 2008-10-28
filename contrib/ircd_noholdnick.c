572238bb73128fa1a3e7db0f9c18c9421ff161b2
Jilles Tjoelker <jilles@stack.nl>
2008-10-28 21:08:56 +0100

    Add a contrib module to force client enforcers, mainly for debugging.

---
+++ contrib/ircd_noholdnick.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2008 Jilles Tjoelker
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * Module to disable holdnick enforcers.
+ */
+
+#include "atheme.h"
+
+DECLARE_MODULE_V1
+(
+	"ircd_noholdnick", FALSE, _modinit, _moddeinit,
+	"$Id$",
+	"Atheme Development Group <http://www.atheme.org>"
+);
+
+int oldflag;
+
+void _modinit(module_t *m)
+{
+
+	if (ircd == NULL)
+	{
+		slog(LG_ERROR, "Module %s must be loaded after a protocol module.", m->name);
+		m->mflags = MODTYPE_FAIL;
+		return;
+	}
+	oldflag = ircd->flags & IRCD_HOLDNICK;
+	ircd->flags &= ~IRCD_HOLDNICK;
+}
+
+void _moddeinit()
+{
+
+	ircd->flags |= oldflag;
+}
+
+/* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
+ * vim:ts=8
+ * vim:sw=8
+ * vim:noexpandtab
+ */
