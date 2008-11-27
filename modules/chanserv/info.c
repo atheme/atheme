565bb504bb300b5f84749396872242d9aa1cd6fa
Jilles Tjoelker <jilles@stack.nl>
2008-11-27 22:54:31 +0100

    Fix inverted privilege check in /cs info successor display.

--- modules/chanserv/info.c
+++ modules/chanserv/info.c
@@ -91,7 +91,7 @@ static void cs_cmd_info(sourceinfo_t *si, int parc, char *parv[])
 		command_success_nodata(si, _("Founder    : %s"), mychan_founder_names(mc));
 
 	if (chanacs_source_has_flag(mc, si, CA_ACLVIEW) ||
-		!has_priv(si, PRIV_CHAN_AUSPEX))
+		has_priv(si, PRIV_CHAN_AUSPEX))
 	{
 		mu = mychan_pick_successor(mc);
 		if (mu != NULL)
