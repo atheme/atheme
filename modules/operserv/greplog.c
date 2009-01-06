3d5408189fc1d3aa77ce9bdd4368dffa1da93728
Jilles Tjoelker <jilles@stack.nl>
2009-01-06 01:04:29 +0100

    Change a FALSE to false.

--- modules/operserv/greplog.c
+++ modules/operserv/greplog.c
@@ -11,7 +11,7 @@
 
 DECLARE_MODULE_V1
 (
-	"operserv/greplog", FALSE, _modinit, _moddeinit,
+	"operserv/greplog", false, _modinit, _moddeinit,
 	"$Id: os_greplog.c 54 2008-09-30 23:07:51Z jilles $",
 	"Atheme Development Group <http://www.atheme.org>"
 );
