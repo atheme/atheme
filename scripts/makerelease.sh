05906c86c5dd12771d827e6d8948060754169c7d
William Pitcock <nenolod@atheme.org>
2009-01-29 22:19:26 -0600

    Fix makerelease.

--- scripts/makerelease.sh
+++ scripts/makerelease.sh
@@ -85,8 +85,8 @@ if [ "x$AUTOMATIC" != "xyes" ]; then
 fi
 
 if [ "x$PUBLISH" = "xyes" ]; then
-	scp $RELEASENAME.tgz distfiles-master.atheme.org:/srv/distfiles
-	scp $RELEASENAME.tbz2 distfiles-master.atheme.org:/srv/distfiles
+	scp $RELEASENAME.tgz hg.atheme.org:/srv/distfiles
+	scp $RELEASENAME.tbz2 hg.atheme.org:/srv/distfiles
 
 	echo
 	echo "The releases have been published, and will be available to the entire"
