66195a4c12528bffb4db29f4c31a3884de3b5ba7
Jilles Tjoelker <jilles@stack.nl>
2009-02-27 01:57:55 +0100

    Apparently that inspircd12 change was not correct yet.

--- modules/protocol/inspircd12.c
+++ modules/protocol/inspircd12.c
@@ -812,7 +812,7 @@ static void m_uid(sourceinfo_t *si, int parc, char *parv[])
 		u = user_add(parv[2], parv[5], parv[3], parv[4], parv[6], parv[0], parv[parc - 1], si->s, atol(parv[1]));
 		if (u == NULL)
 			return;
-		user_mode(u, parv[9]);
+		user_mode(u, parv[8]);
 
 		/* If server is not yet EOB we will do this later.
 		 * This avoids useless "please identify" -- jilles */
