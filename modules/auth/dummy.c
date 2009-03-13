b949c5f4264fdc3bd631d40d63ce86dab1262be0
Jilles Tjoelker <jilles@stack.nl>
2009-03-14 00:46:49 +0100

    Add LDAP authentication module. This is compiled when OpenLDAP client libraries can be found. Only the module itself is linked to these libraries. The module is configured in atheme.conf. There is also a dummy authentication module, needed to keep the build system working in a simple manner.

---
+++ modules/auth/dummy.c
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2009 Atheme Development Group
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * Dummy authentication, always returns failure.
+ * This really only serves to have an auth module that always compiles.
+ */
+
+#include "atheme.h"
+
+DECLARE_MODULE_V1
+(
+	"auth/dummy", false, _modinit, _moddeinit,
+	"$Id$",
+	"Atheme Development Group <http://www.atheme.org>"
+);
+
+static bool dummy_auth_user(myuser_t *mu, const char *password)
+{
+	return false;
+}
+
+void _modinit(module_t *m)
+{
+	auth_user_custom = &dummy_auth_user;
+
+	auth_module_loaded = true;
+}
+
+void _moddeinit(void)
+{
+	auth_user_custom = NULL;
+
+	auth_module_loaded = false;
+}
+
+/* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
+ * vim:ts=8
+ * vim:sw=8
+ * vim:noexpandtab
+ */
---
+++ modules/auth/ldap.c
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2009 Atheme Development Group
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * LDAP authentication.
+ */
+
+#include "atheme.h"
+
+#define LDAP_DEPRECATED 1
+#include <ldap.h>
+
+DECLARE_MODULE_V1
+(
+	"auth/ldap", false, _modinit, _moddeinit,
+	"$Id$",
+	"Atheme Development Group <http://www.atheme.org>"
+);
+
+list_t conf_ldap_table;
+struct
+{
+	char *url;
+	char *dnformat;
+} ldap_config;
+LDAP *ldap_conn;
+
+static void ldap_config_ready(void *unused)
+{
+	int res;
+	char *p;
+	static time_t lastwarning;
+
+	if (ldap_conn != NULL)
+		ldap_unbind_ext_s(ldap_conn, NULL, NULL);
+	ldap_conn = NULL;
+	if (ldap_config.url == NULL || ldap_config.dnformat == NULL)
+	{
+		slog(LG_ERROR, "ldap_config_ready(): ldap {} block missing or invalid");
+		return;
+	}
+	p = strchr(ldap_config.dnformat, '%');
+	if (p == NULL || p[1] != 's' || strchr(p + 1, '%'))
+	{
+		slog(LG_ERROR, "ldap_config_ready(): dnformat must contain exactly one %%s and no other %%");
+		return;
+	}
+	ldap_set_option(NULL, LDAP_OPT_PROTOCOL_VERSION, &(const int){ 3 });
+	res = ldap_initialize(&ldap_conn, ldap_config.url);
+	if (res != LDAP_SUCCESS)
+	{
+		slog(LG_ERROR, "ldap_config_ready(): ldap_initialize(%s) failed: %s",
+				ldap_config.url, ldap_err2string(res));
+		if (CURRTIME > lastwarning + 300)
+		{
+			snoop("LDAP:ERROR: %s", ldap_err2string(res));
+			wallops("Problem with LDAP server: %s", ldap_err2string(res));
+			lastwarning = CURRTIME;
+		}
+		return;
+	}
+	/* short timeouts, because this blocks atheme as a whole */
+	ldap_set_option(ldap_conn, LDAP_OPT_TIMEOUT, &(const struct timeval){ 1, 0 });
+	ldap_set_option(ldap_conn, LDAP_OPT_NETWORK_TIMEOUT, &(const struct timeval){ 1, 0 });
+	ldap_set_option(ldap_conn, LDAP_OPT_DEREF, &(const int){ false });
+	ldap_set_option(ldap_conn, LDAP_OPT_REFERRALS, &(const int){ false });
+}
+
+static bool ldap_auth_user(myuser_t *mu, const char *password)
+{
+	int res;
+	char dn[512];
+	static time_t lastwarning;
+
+	if (ldap_conn == NULL || ldap_config.dnformat == NULL)
+		return false;
+
+	if (strchr(mu->name, ' ') || strchr(mu->name, ',') ||
+			strchr(mu->name, '/'))
+		return false;
+
+	snprintf(dn, sizeof dn, ldap_config.dnformat, mu->name);
+	res = ldap_simple_bind_s(ldap_conn, dn, password);
+	switch (res)
+	{
+		case LDAP_SUCCESS:
+			return true;
+		case LDAP_INVALID_CREDENTIALS:
+			return false;
+	}
+	slog(LG_INFO, "ldap_auth_user(): ldap_bind_s(%s) failed: %s, reconnecting and retrying",
+			dn, ldap_err2string(res));
+	ldap_config_ready(NULL);
+	if (ldap_conn == NULL)
+		return false;
+	res = ldap_simple_bind_s(ldap_conn, dn, password);
+	switch (res)
+	{
+		case LDAP_SUCCESS:
+			return true;
+		case LDAP_INVALID_CREDENTIALS:
+			return false;
+	}
+	slog(LG_ERROR, "ldap_auth_user(): ldap_bind_s(%s) failed: %s",
+			dn, ldap_err2string(res));
+	if (CURRTIME > lastwarning + 300)
+	{
+		snoop("LDAP:ERROR: %s", ldap_err2string(res));
+		wallops("Problem with LDAP server: %s", ldap_err2string(res));
+		lastwarning = CURRTIME;
+	}
+	return false;
+}
+
+void _modinit(module_t *m)
+{
+        hook_add_event("config_ready");
+        hook_add_hook("config_ready", ldap_config_ready);
+
+	add_subblock_top_conf("LDAP", &conf_ldap_table);
+	add_dupstr_conf_item("URL", &conf_ldap_table, &ldap_config.url);
+	add_dupstr_conf_item("DNFORMAT", &conf_ldap_table, &ldap_config.dnformat);
+
+	auth_user_custom = &ldap_auth_user;
+
+	auth_module_loaded = true;
+}
+
+void _moddeinit(void)
+{
+	auth_user_custom = NULL;
+
+	auth_module_loaded = false;
+
+	if (ldap_conn != NULL)
+		ldap_unbind_ext_s(ldap_conn, NULL, NULL);
+
+	hook_del_hook("config_ready", ldap_config_ready);
+	del_conf_item("URL", &conf_ldap_table);
+	del_conf_item("DNFORMAT", &conf_ldap_table);
+	del_top_conf("LDAP");
+}
+
+/* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
+ * vim:ts=8
+ * vim:sw=8
+ * vim:noexpandtab
+ */
