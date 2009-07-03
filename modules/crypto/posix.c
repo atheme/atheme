d54d7e9248ee8b4d02ca6ec12a03e69b8e9a0cdb
Jilles Tjoelker <jilles@stack.nl>
2009-07-04 01:23:16 +0200

    Try to detect MacOS X crypt(3) breakage and generate a DES based hash.

--- modules/crypto/posix.c
+++ modules/crypto/posix.c
@@ -18,7 +18,22 @@ DECLARE_MODULE_V1
 
 static const char *posixc_crypt_string(const char *key, const char *salt)
 {
-	return crypt(key, salt);
+	const char *result;
+	char salt2[3];
+	static int warned;
+
+	result = crypt(key, salt);
+	if (!strncmp(salt, "$1$", 3) && strncmp(result, "$1$", 3))
+	{
+		if (!warned)
+			slog(LG_INFO, "posixc_crypt_string(): broken crypt() detected, falling back to DES");
+		warned = 1;
+		salt2[0] = salt[3];
+		salt2[1] = salt[4];
+		salt2[2] = '\0';
+		result = crypt(key, salt2);
+	}
+	return result;
 }
 
 void _modinit(module_t *m)
