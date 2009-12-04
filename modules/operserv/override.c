3795789d380c439cfe2f0bb2dadd1d166c0ea8b2
William Pitcock <nenolod@atheme.org>
2009-12-04 17:56:26 -0600

    operserv/override: Fix the memoserv block.

--- modules/operserv/override.c
+++ modules/operserv/override.c
@@ -217,7 +217,7 @@ static void os_cmd_override(sourceinfo_t *si, int parc, char *parv[])
 		return;
 	}
 
-	if (!irccasecmp(parv[1], memosvs.name))
+	if (!irccasecmp(parv[1], memosvs.nick))
 	{
 		command_fail(si, fault_noprivs, _("\2%s\2 cannot be used as an override service."), parv[1]);
 		return;
