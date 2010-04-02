01b2c47ce427d9749d532cca11f7f70f73bf5727
J Beshir <namegduf@tellaerad.net>
2010-04-02 02:56:05 +0100

    Add AUTHCOOKIE authentication mechanism to SaslServ.

---
+++ modules/saslserv/authcookie.c
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2006 Atheme Development Group
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * AUTHCOOKIE mechanism provider
+ *
+ * $Id: authcookie.c 2010 JBeshir $
+ */
+
+#include "atheme.h"
+#include "authcookie.h"
+
+DECLARE_MODULE_V1
+(
+	"saslserv/authcookie", false, _modinit, _moddeinit,
+	"$Id: authcookie.c 2010 JBeshir $",
+	"Atheme Development Group <http://www.atheme.org>"
+);
+
+list_t *mechanisms;
+node_t *mnode;
+static int mech_start(sasl_session_t *p, char **out, int *out_len);
+static int mech_step(sasl_session_t *p, char *message, int len, char **out, int *out_len);
+static void mech_finish(sasl_session_t *p);
+sasl_mechanism_t mech = {"AUTHCOOKIE", &mech_start, &mech_step, &mech_finish};
+
+void _modinit(module_t *m)
+{
+	MODULE_USE_SYMBOL(mechanisms, "saslserv/main", "sasl_mechanisms");
+	mnode = node_create();
+	node_add(&mech, mnode, mechanisms);
+}
+
+void _moddeinit()
+{
+	node_del(mnode, mechanisms);
+}
+
+static int mech_start(sasl_session_t *p, char **out, int *out_len)
+{
+	return ASASL_MORE;
+}
+
+static int mech_step(sasl_session_t *p, char *message, int len, char **out, int *out_len)
+{
+	char auth[256];
+	char cookie[256];
+	myuser_t *mu;
+
+	/* Skip the authzid entirely */
+	len -= strlen(message) + 1;
+	if(len <= 0)
+		return ASASL_FAIL;
+	message += strlen(message) + 1;
+
+	/* Copy the authcid */
+	if(strlen(message) > 255)
+		return ASASL_FAIL;
+	len -= strlen(message) + 1;
+	if(len <= 0)
+		return ASASL_FAIL;
+	strcpy(auth, message);
+	message += strlen(message) + 1;
+
+	/* Copy the authcookie */
+	if(strlen(message) > 255)
+		return ASASL_FAIL;
+	strlcpy(cookie, message, len + 1);
+
+	/* Done dissecting, now check. */
+	if(!(mu = myuser_find(auth)))
+		return ASASL_FAIL;
+
+	p->username = strdup(auth);
+	return authcookie_find(cookie, mu) != NULL ? ASASL_DONE : ASASL_FAIL;
+}
+
+static void mech_finish(sasl_session_t *p)
+{
+}
+
+/* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
+ * vim:ts=8
+ * vim:sw=8
+ * vim:noexpandtab
+ */
