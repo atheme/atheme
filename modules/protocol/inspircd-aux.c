b2273d10f132cd57133abc68535222c53e0ab265
William Pitcock <nenolod@atheme.org>
2010-05-23 14:43:36 -0500

    inspircd: Set a custom ignore_mode_list size.

--- modules/protocol/inspircd-aux.c
+++ modules/protocol/inspircd-aux.c
@@ -1388,6 +1388,7 @@ void _modinit(module_t * m)
 	status_mode_list = inspircd_status_mode_list;
 	prefix_mode_list = inspircd_prefix_mode_list;
 	user_mode_list = inspircd_user_mode_list;
+	ignore_mode_list_size = ARRAY_SIZE(inspircd_ignore_mode_list);
 
 	ircd = &InspIRCd;
 
