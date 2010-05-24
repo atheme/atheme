c942834234034014914d9f61f147e0f6268f96e5
William Pitcock <nenolod@atheme.org>
2010-05-23 21:22:00 -0500

    Add nickserv/cracklib module.

---
+++ modules/nickserv/cracklib.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2010 William Pitcock <nenolod@atheme.org>
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * This file contains code for checking password security with cracklib.
+ */
+
+#include "atheme.h"
+#include <crack.h>
+
+DECLARE_MODULE_V1
+(
+	"nickserv/cracklib", false, _modinit, _moddeinit,
+        "$Id$",
+        "Atheme Development Group <http://www.atheme.org>"
+);
+
+void
+cracklib_hook(hook_user_register_check_t *hdata)
+{
+	const char *cracklib_reason;
+
+	return_if_fail(hdata != NULL);
+	return_if_fail(hdata->si != NULL);
+	return_if_fail(hdata->password != NULL);
+
+	if ((cracklib_reason = FascistCheck(hdata->password, NULL)) != NULL)
+	{
+		command_fail(hdata->si, fault_badparams, _("The password provided is insecure: %s"), cracklib_reason);
+		hdata->approved++;
+	}
+}
+
+void
+_modinit(module_t *m)
+{
+	hook_add_event("user_can_register");
+	hook_add_user_can_register(cracklib_hook);
+}
+
+void
+_moddeinit(void)
+{
+	hook_del_user_can_register(cracklib_hook);
+}
