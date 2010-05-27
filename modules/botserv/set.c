fb8e720539d99269b7eb553aa55dcc401f52e9a5
JD Horelick <jdhore1@gmail.com>
2010-05-27 15:00:35 -0400

    Move BS SET NOBOT to its own module.

--- modules/botserv/set.c
+++ modules/botserv/set.c
@@ -21,16 +21,13 @@ DECLARE_MODULE_V1
 static void bs_help_set(sourceinfo_t *si);
 static void bs_cmd_set(sourceinfo_t *si, int parc, char *parv[]);
 
-static void bs_cmd_set_nobot(sourceinfo_t *si, int parc, char *parv[]);
 static void bs_cmd_set_private(sourceinfo_t *si, int parc, char *parv[]);
 
 command_t bs_set = { "SET", N_("Configures bot options."), AC_NONE, 3, bs_cmd_set };
 
-command_t bs_set_nobot          = { "NOBOT",          N_("Prevent a bot from being assigned to a channel."),         PRIV_CHAN_ADMIN, 2, bs_cmd_set_nobot };
 command_t bs_set_private        = { "PRIVATE",        N_("Prevent a bot from being assigned by non IRC operators."), PRIV_CHAN_ADMIN, 2, bs_cmd_set_private };
 
 command_t *bs_set_commands[] = {
-	&bs_set_nobot,
 	&bs_set_private,
 	NULL
 };
@@ -56,7 +53,6 @@ void _modinit(module_t *m)
 
 
 	help_addentry(bs_helptree, "SET", NULL, bs_help_set);
-	help_addentry(bs_helptree, "SET NOBOT", "help/botserv/set_nobot", NULL);
 	help_addentry(bs_helptree, "SET PRIVATE", "help/botserv/set_private", NULL);
 }
 
@@ -65,7 +61,6 @@ void _moddeinit()
 	command_delete(&bs_set, bs_cmdtree);
 
 	help_delentry(bs_helptree, "SET");
-	help_delentry(bs_helptree, "SET NOBOT");
 	help_delentry(bs_helptree, "SET PRIVATE");
 }
 
@@ -106,69 +101,6 @@ static void bs_cmd_set(sourceinfo_t *si, int parc, char *parv[])
 
 	parv[1] = dest;
 	command_exec(si->service, si, c, parc - 1, parv + 1);
-}
-
-static void bs_cmd_set_nobot(sourceinfo_t *si, int parc, char *parv[])
-{
-	char *channel = parv[0];
-	char *option = parv[1];
-	mychan_t *mc;
-	metadata_t *md;
-
-	if (parc < 2 || !channel || !option)
-	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "SET NOBOT");
-		command_fail(si, fault_needmoreparams, _("Syntax: SET <#channel> NOBOT {ON|OFF}"));
-		return;
-	}
-
-	mc = mychan_find(channel);
-	if (!mc)
-	{
-		command_fail(si, fault_nosuch_target, _("Channel \2%s\2 is not registered."), channel);
-		return;
-	}
-
-	if (!si->smu)
-	{
-		command_fail(si, fault_noprivs, _("You are not logged in."));
-		return;
-	}
-
-	if (!has_priv(si, PRIV_CHAN_ADMIN))
-	{
-		command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
-		return;
-	}
-
-	if (!irccasecmp(option, "ON"))
-	{
-		metadata_add(mc, "private:botserv:no-bot", "ON");
-		if ((md = metadata_find(mc, "private:botserv:bot-assigned")) != NULL)
-		{
-			if (mc->flags & MC_GUARD &&
-					(!config_options.leave_chans ||
-					 (mc->chan != NULL &&
-					  LIST_LENGTH(&mc->chan->members) > 1)))
-				join(mc->name, chansvs.nick);
-			part(mc->name, md->value);
-			metadata_delete(mc, "private:botserv:bot-assigned");
-			metadata_delete(mc, "private:botserv:bot-handle-fantasy");
-		}
-		logcommand(si, CMDLOG_SET, "SET:NOBOT:ON: \2%s\2", mc->name);
-		command_success_nodata(si, _("No Bot mode is now \2ON\2 on channel %s."), mc->name);
-	}
-	else if(!irccasecmp(option, "OFF"))
-	{
-		metadata_delete(mc, "private:botserv:no-bot");
-		logcommand(si, CMDLOG_SET, "SET:NOBOT:OFF: \2%s\2", mc->name);
-		command_success_nodata(si, _("No Bot mode is now \2OFF\2 on channel %s."), mc->name);
-	}
-	else
-	{
-		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "SET NOBOT");
-		command_fail(si, fault_badparams, _("Syntax: SET <#channel> NOBOT {ON|OFF}"));
-	}
 }
 
 static void bs_cmd_set_private(sourceinfo_t *si, int parc, char *parv[])
