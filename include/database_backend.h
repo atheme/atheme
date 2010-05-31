918c008d248bcfd70f0eda4039c573ad85062ea2
William Pitcock <nenolod@atheme.org>
2010-05-31 03:38:06 -0500

    Add type handler management.

--- include/database_backend.h
+++ include/database_backend.h
@@ -55,4 +55,10 @@ E bool db_write_multiword(database_handle_t *db, const char *str);
 E bool db_write_int(database_handle_t *db, int num);
 E bool db_commit_row(database_handle_t *db);
 
+typedef void (*database_handler_f)(database_handle_t *db, const char *type);
+
+E void db_register_type_handler(database_handle_t *db, const char *type, database_handler_f fun);
+E void db_unregister_type_handler(database_handle_t *db, const char *type);
+E void db_init(void);
+
 #endif
