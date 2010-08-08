0a4fef09903e74491f8da7474b6fad35a674af55
JD Horelick <jdhore1@gmail.com>
2010-08-08 12:52:48 -0400

    Taint on more Inspircd 2.0/2.1 modules that are like in-ircd services and/or would be a *HUGE* pain to implement. Possibly requiring rewriting half of Atheme to support.

--- modules/protocol/inspircd-aux.c
+++ modules/protocol/inspircd-aux.c
@@ -1382,6 +1382,9 @@ static void m_capab(sourceinfo_t *si, int parc, char *parv[])
 		TAINT_ON(strstr(parv[1], "m_invisible.so") != NULL, "invisible is not presently supported correctly in atheme, and won't be due to ethical obligations");
 		TAINT_ON(strstr(parv[1], "m_serverbots.so") != NULL, "inspircd built-in services are not compatible with atheme");
 		TAINT_ON(strstr(parv[1], "m_chanacl.so") != NULL, "inspircd built-in services are not compatible with atheme");
+		TAINT_ON(strstr(parv[1], "m_chanregister.so") != NULL, "inspircd built-in services are not compatible with atheme");
+		TAINT_ON(strstr(parv[1], "m_namedmodes.so") != NULL, "namedmodes are unsupported in Atheme due to the fact that any network can change modes around thus possibly breaking mlocks");
+		TAINT_ON(strstr(parv[1], "m_opflags.so") != NULL, "inspircd built-in services are not compatible with atheme");
 	}
 	else if (strcasecmp(parv[0], "END") == 0)
 	{
