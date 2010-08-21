4e8000e4199dcb545740f597ea8a05d9dc41ea73
William Pitcock <nenolod@atheme.org>
2010-08-21 18:06:17 -0500

    groupserv: Add an owner field to mygroup_t.

--- modules/groupserv/groupserv.c
+++ modules/groupserv/groupserv.c
@@ -18,20 +18,26 @@ void mygroup_delete(mygroup_t *mg)
 	/* XXX nothing */
 }
 
-mygroup_t *mygroup_add(const char *name)
+mygroup_t *mygroup_add(myuser_t *owner, const char *name)
 {
 	mygroup_t *mg;
 
 	mg = BlockHeapAlloc(mygroup_heap);
 	object_init(object(mg), NULL, (destructor_t) mygroup_delete);
+
 	strlcpy(entity(mg)->name, name, sizeof(entity(mg)->name));
 	myentity_put(entity(mg));
+
+	mg->owner = owner;
+
 	return mg;
 }
 
 mygroup_t *mygroup_find(const char *name)
 {
 	myentity_t *me = myentity_find(name);
+
 	return_val_if_fail(isgroup(me), NULL);
+
 	return group(me);
 }
--- modules/groupserv/groupserv.h
+++ modules/groupserv/groupserv.h
@@ -12,10 +12,12 @@ typedef struct mygroup_ mygroup_t;
 struct mygroup_ {
 	myentity_t ent;
 	list_t acs;
+
+	myuser_t *owner;
 };
 
 E void mygroups_init(void);
-E mygroup_t *mygroup_add(const char *name);
+E mygroup_t *mygroup_add(myuser_t *owner, const char *name);
 E mygroup_t *mygroup_find(const char *name);
 
 #endif /* !GROUPSERV_H */
