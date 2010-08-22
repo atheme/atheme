ec35cf76f97b56f69931fd31e5d97e4b0ff86cfb
William Pitcock <nenolod@atheme.org>
2010-08-21 19:32:32 -0500

    groupserv: Add REGISTER command skeleton.

--- modules/groupserv/basecmds.c
+++ modules/groupserv/basecmds.c
@@ -29,15 +29,59 @@ void gs_cmd_help(sourceinfo_t *si, int parc, char *parv[])
 	help_display(si, si->service, parv[0], &gs_helptree);
 }
 
+static void gs_cmd_register(sourceinfo_t *si, int parc, char *parv[]);
+
+command_t gs_register = { "REGISTER", N_("Registers a group."), AC_NONE, 2, gs_cmd_register };
+
+static void gs_cmd_register(sourceinfo_t *si, int parc, char *parv[])
+{
+	mygroup_t *mg;
+
+	if (!parv[0])
+	{
+		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "REGISTER");
+		command_fail(si, fault_needmoreparams, _("To register a group: REGISTER <!groupname>"));
+		return;
+	}
+
+	if (*parv[0] != '!')
+	{
+		command_fail(si, fault_needmoreparams, STR_INVALID_PARAMS, "REGISTER");
+		command_fail(si, fault_needmoreparams, _("To register a group: REGISTER <!groupname>"));
+		return;
+	}
+
+	if ((mg = mygroup_find(parv[0])) != NULL)
+	{
+		command_fail(si, fault_alreadyexists, _("The group \2%s\2 already exists."), parv[0]);
+		return;
+	}
+
+	if (si->smu == NULL)
+	{
+		command_fail(si, fault_noprivs, _("You are not logged in."));
+		return;
+	}
+
+	/* XXX: need to check registration limits here. */
+	mg = mygroup_add(si->smu, parv[0]);
+
+	command_success_nodata(si, _("The group \2%s\2 has been registered to \2%s\2."), entity(mg)->name, entity(si->smu)->name);
+}
+
 void basecmds_init(void)
 {
 	command_add(&gs_help, &gs_cmdtree);
+	command_add(&gs_register, &gs_cmdtree);
+
 	help_addentry(&gs_helptree, "HELP", "help/help", NULL);
 }
 
 void basecmds_deinit(void)
 {
 	command_delete(&gs_help, &gs_cmdtree);
+	command_delete(&gs_register, &gs_cmdtree);
+
 	help_delentry(&gs_helptree, "HELP");
 }
 
