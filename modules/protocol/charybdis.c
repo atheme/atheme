90358759c17b9ca229aea88d36366d418b7bfb9f
William Pitcock <nenolod@atheme.org>
2010-08-24 14:07:58 -0500

    protocol/charybdis: Convert a myuser_t into a myentity_t for chanacs.

--- modules/protocol/charybdis.c
+++ modules/protocol/charybdis.c
@@ -120,7 +120,7 @@ static bool check_forward(const char *value, channel_t *c, mychan_t *mc, user_t 
 			return true;
 	}
 	else if (mu != NULL)
-		if (chanacs_find(target_mc, mu, CA_SET))
+		if (chanacs_find(target_mc, entity(mu), CA_SET))
 			return true;
 	return false;
 }
