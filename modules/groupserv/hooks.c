b0fb38233da35b34e8c72cdedf7dc1d7ca34f1ce
William Pitcock <nenolod@atheme.org>
2010-08-27 02:05:45 -0500

    groupserv: Clean up ACL entries when a myuser gets deleted.

--- modules/groupserv/hooks.c
+++ modules/groupserv/hooks.c
@@ -34,12 +34,33 @@ static void user_info_hook(hook_user_req_t *req)
 	command_success_nodata(req->si, _("Groups     : %s"), buf);
 }
 
+static void myuser_delete_hook(myuser_t *mu)
+{
+	myentity_t *mt;
+	myentity_iteration_state_t state;
+
+	/* XXX: we need a way to stash a list in myuser showing the list of
+	 * groups we're in.  it would speed this up dramatically.  --nenolod
+	 */
+	MYENTITY_FOREACH_T(mt, &state, ENT_GROUP)
+	{
+		mygroup_t *mg;
+
+		mg = group(mt);
+		continue_if_fail(mg != NULL);
+
+		groupacs_delete(mg, mu);
+	}
+}
+
 void gs_hooks_init(void)
 {
 	hook_add_user_info(user_info_hook);
+	hook_add_myuser_delete(myuser_delete_hook);
 }
 
 void gs_hooks_deinit(void)
 {
 	hook_del_user_info(user_info_hook);
+	hook_del_myuser_delete(myuser_delete_hook);
 }
