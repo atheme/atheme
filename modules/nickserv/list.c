770b0c24045c12c415bb90787a766cc20b1e700f
William Pitcock <nenolod@atheme.org>
2010-09-06 02:29:50 -0500

    nickserv/list: Same enhancements as chanserv/list.

--- modules/nickserv/list.c
+++ modules/nickserv/list.c
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) 2005-2007 Robin Burchell, et al.
+ * Copyright (c) 2010 William Pitcock <nenolod@atheme.org>.
  * Rights to this code are as documented in doc/LICENSE.
  *
  * This file contains code for the NickServ LIST function.
  * Based on Alex Lambert's LISTEMAIL.
- *
  */
 
 #include "atheme.h"
@@ -18,7 +18,7 @@ DECLARE_MODULE_V1
 
 static void ns_cmd_list(sourceinfo_t *si, int parc, char *parv[]);
 
-command_t ns_list = { "LIST", N_("Lists nicknames registered matching a given pattern."), PRIV_USER_AUSPEX, 1, ns_cmd_list };
+command_t ns_list = { "LIST", N_("Lists nicknames registered matching a given pattern."), PRIV_USER_AUSPEX, 10, ns_cmd_list };
 
 list_t *ns_cmdtree, *ns_helptree;
 
@@ -35,6 +35,110 @@ void _moddeinit()
 {
 	command_delete(&ns_list, ns_cmdtree);
 	help_delentry(ns_helptree, "LIST");
+}
+
+typedef enum {
+	OPT_BOOL,
+	OPT_INT,
+	OPT_STRING,
+	OPT_FLAG,
+	OPT_AGE,
+} list_opttype_t;
+
+typedef struct {
+	char *option;
+	list_opttype_t opttype;
+	union {
+		bool *boolval;
+		int *intval;
+		char **strval;
+		unsigned int *flagval;
+		time_t *ageval;
+	} optval;
+	unsigned int flag;
+} list_option_t;
+
+static time_t parse_age(char *s)
+{
+	time_t duration;
+
+	duration = (atol(s) * 60);
+	while (isdigit(*s))
+		s++;
+
+	if (*s == 'h' || *s == 'H')
+		duration *= 60;
+	else if (*s == 'd' || *s == 'D')
+		duration *= 1440;
+	else if (*s == 'w' || *s == 'W')
+		duration *= 10080;
+	else if (*s == '\0')
+		;
+	else
+		duration = 0;
+
+	return duration;
+}
+
+static void process_parvarray(list_option_t *opts, size_t optsize, int parc, char *parv[])
+{
+	int i, j;
+
+	for (i = 0; i < parc; i++)
+	{
+		for (j = 0; j < optsize; j++)
+		{
+			if (!strcasecmp(opts[j].option, parv[i]))
+			{
+				switch(opts[j].opttype)
+				{
+				case OPT_BOOL:
+					*opts[j].optval.boolval = true;
+					break;
+				case OPT_INT:
+					if (i + 1 < parc)
+					{
+						*opts[j].optval.intval = atoi(parv[i + 1]);
+						i++;
+					}
+					break;
+				case OPT_STRING:
+					if (i + 1 < parc)
+					{
+						*opts[j].optval.strval = parv[i + 1];
+						i++;
+					}
+					break;
+				case OPT_FLAG:
+					*opts[j].optval.flagval |= opts[j].flag;
+					break;
+				case OPT_AGE:
+					if (i + 1 < parc)
+					{
+						*opts[j].optval.ageval = parse_age(parv[i + 1]);
+						i++;
+					}
+					break;					
+				default:
+					break;
+				}
+			}
+		}
+	}
+}
+
+static void build_criteriastr(char *buf, int parc, char *parv[])
+{
+	int i;
+
+	return_if_fail(buf != NULL);
+
+	*buf = 0;
+	for (i = 0; i < parc; i++)
+	{
+		strlcat(buf, parv[i], BUFSIZE);
+		strlcat(buf, " ", BUFSIZE);
+	}
 }
 
 static void list_one(sourceinfo_t *si, myuser_t *mu, mynick_t *mn)
@@ -78,7 +182,8 @@ static void list_one(sourceinfo_t *si, myuser_t *mu, mynick_t *mn)
 
 static void ns_cmd_list(sourceinfo_t *si, int parc, char *parv[])
 {
-	char pat[512], *nickpattern = NULL, *hostpattern = NULL, *p;
+	char criteriastr[BUFSIZE];
+	char pat[512], *pattern, *nickpattern = NULL, *hostpattern = NULL, *p;
 	bool hostmatch;
 	mowgli_patricia_iteration_state_t state;
 	myentity_iteration_state_t mestate;
@@ -87,29 +192,54 @@ static void ns_cmd_list(sourceinfo_t *si, int parc, char *parv[])
 	mynick_t *mn;
 	metadata_t *md;
 	int matches = 0;
+	bool frozen = false, marked = false;
+	unsigned int flagset = 0;
+	time_t age = 0, lastlogin = 0;
 
-	if (parc < 1)
+	list_option_t optstable[] = {
+		{"pattern",	OPT_STRING,	{.strval = &pattern}},
+		{"noexpire",	OPT_FLAG,	{.flagval = &flagset}, MU_HOLD},
+		{"held",	OPT_FLAG,	{.flagval = &flagset}, MU_HOLD},
+		{"hold",	OPT_FLAG,	{.flagval = &flagset}, MU_HOLD},
+		{"noop",	OPT_FLAG,	{.flagval = &flagset}, MU_NOOP},
+		{"neverop",	OPT_FLAG,	{.flagval = &flagset}, MU_NEVEROP},
+		{"waitauth",	OPT_FLAG,	{.flagval = &flagset}, MU_WAITAUTH},
+		{"hidemail",	OPT_FLAG,	{.flagval = &flagset}, MU_HIDEMAIL},
+		{"nomemo",	OPT_FLAG,	{.flagval = &flagset}, MU_NOMEMO},
+		{"emailmemos",	OPT_FLAG,	{.flagval = &flagset}, MU_EMAILMEMOS},
+		{"use-privmsg",	OPT_FLAG,	{.flagval = &flagset}, MU_USE_PRIVMSG},
+		{"private",	OPT_FLAG,	{.flagval = &flagset}, MU_PRIVATE},
+		{"quietchg",	OPT_FLAG,	{.flagval = &flagset}, MU_QUIETCHG},
+		{"nogreet",	OPT_FLAG,	{.flagval = &flagset}, MU_NOGREET},
+		{"regnolimit",	OPT_FLAG,	{.flagval = &flagset}, MU_REGNOLIMIT},
+		{"frozen",	OPT_BOOL,	{.boolval = &frozen}},
+		{"marked",	OPT_BOOL,	{.boolval = &marked}},
+		{"registered",	OPT_AGE,	{.ageval = &age}},
+		{"lastlogin",	OPT_AGE,	{.ageval = &lastlogin}},
+	};
+
+	process_parvarray(optstable, ARRAY_SIZE(optstable), parc, parv);
+	build_criteriastr(criteriastr, parc, parv);
+
+	if (pattern != NULL)
 	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "LIST");
-		command_fail(si, fault_needmoreparams, _("Syntax: LIST <pattern>[!<user@host>]"));
-		return;
+		strlcpy(pat, parv[0], sizeof pat);
+		p = strrchr(pat, ' ');
+		if (p == NULL)
+			p = strrchr(pat, '!');
+		if (p != NULL)
+		{
+			*p++ = '\0';
+			nickpattern = pat;
+			hostpattern = p;
+		}
+		else if (strchr(pat, '@'))
+			hostpattern = pat;
+		else
+			nickpattern = pat;
+		if (nickpattern && !strcmp(nickpattern, "*"))
+			nickpattern = NULL;
 	}
-	strlcpy(pat, parv[0], sizeof pat);
-	p = strrchr(pat, ' ');
-	if (p == NULL)
-		p = strrchr(pat, '!');
-	if (p != NULL)
-	{
-		*p++ = '\0';
-		nickpattern = pat;
-		hostpattern = p;
-	}
-	else if (strchr(pat, '@'))
-		hostpattern = pat;
-	else
-		nickpattern = pat;
-	if (nickpattern && !strcmp(nickpattern, "*"))
-		nickpattern = NULL;
 
 	if (nicksvs.no_nick_ownership)
 	{
@@ -130,6 +260,22 @@ static void ns_cmd_list(sourceinfo_t *si, int parc, char *parv[])
 				if (!hostmatch)
 					continue;
 			}
+
+			if (marked && !metadata_find(mu, "private:mark:setter"))
+				continue;
+
+			if (frozen && !metadata_find(mu, "private:freeze:freezer"))
+				continue;
+
+			if (flagset && !(mu->flags & flagset))
+				continue;
+
+			if (age && (CURRTIME - mu->registered) < age)
+				continue;
+
+			if (lastlogin && (CURRTIME - mu->lastlogin) < lastlogin)
+				continue;
+
 			list_one(si, mu, NULL);
 			matches++;
 		}
@@ -153,16 +299,32 @@ static void ns_cmd_list(sourceinfo_t *si, int parc, char *parv[])
 				if (!hostmatch)
 					continue;
 			}
+
+			if (marked && !metadata_find(mu, "private:mark:setter"))
+				continue;
+
+			if (frozen && !metadata_find(mu, "private:freeze:freezer"))
+				continue;
+
+			if (flagset && !(mu->flags & flagset))
+				continue;
+
+			if (age && (CURRTIME - mu->registered) < age)
+				continue;
+
+			if (lastlogin && (CURRTIME - mu->lastlogin) < lastlogin)
+				continue;
+
 			list_one(si, NULL, mn);
 			matches++;
 		}
 	}
 
-	logcommand(si, CMDLOG_ADMIN, "LIST: \2%s\2 (\2%d\2 matches)", parv[0], matches);
+	logcommand(si, CMDLOG_ADMIN, "LIST: \2%s\2 (\2%d\2 matches)", criteriastr, matches);
 	if (matches == 0)
-		command_success_nodata(si, _("No nicknames matched pattern \2%s\2"), parv[0]);
+		command_success_nodata(si, _("No nicknames matched criteria \2%s\2"), criteriastr);
 	else
-		command_success_nodata(si, ngettext(N_("\2%d\2 match for pattern \2%s\2"), N_("\2%d\2 matches for pattern \2%s\2"), matches), matches, parv[0]);
+		command_success_nodata(si, ngettext(N_("\2%d\2 match for criteria \2%s\2"), N_("\2%d\2 matches for criteria \2%s\2"), matches), matches, criteriastr);
 }
 
 /* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
