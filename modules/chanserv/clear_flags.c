de3b425644db034ab538c2c266e2260daeb82a63
JD Horelick <jdhore1@gmail.com>
2010-09-12 14:36:59 -0400

    chanserv: Add CLEAR FLAGS command. Removes channel access from all users with channel access, except for founders.

---
+++ modules/chanserv/clear_flags.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2010 Atheme Development Group
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * This file contains code for the CService CLEAR FLAGS functions.
+ *
+ */
+
+#include "atheme.h"
+
+DECLARE_MODULE_V1
+(
+	"chanserv/clear_flags", false, _modinit, _moddeinit,
+	PACKAGE_STRING,
+	"Atheme Development Group <http://www.atheme.org>"
+);
+
+static void cs_cmd_clear_flags(sourceinfo_t *si, int parc, char *parv[]);
+
+command_t cs_clear_flags = { "FLAGS", "Clears all channel flags.", AC_NONE, 2, cs_cmd_clear_flags };
+
+list_t *cs_clear_cmds;
+list_t *cs_helptree;
+
+void _modinit(module_t *m)
+{
+	MODULE_USE_SYMBOL(cs_clear_cmds, "chanserv/clear", "cs_clear_cmds");
+	MODULE_USE_SYMBOL(cs_helptree, "chanserv/main", "cs_helptree");
+
+        command_add(&cs_clear_flags, cs_clear_cmds);
+	help_addentry(cs_helptree, "CLEAR FLAGS", "help/cservice/clear_flags", NULL);
+}
+
+void _moddeinit()
+{
+	command_delete(&cs_clear_flags, cs_clear_cmds);
+	help_delentry(cs_helptree, "CLEAR FLAGS");
+}
+
+static void cs_cmd_clear_flags(sourceinfo_t *si, int parc, char *parv[])
+{
+	mychan_t *mc;
+	node_t *n, *tn;
+	chanacs_t *ca;
+	char *name = parv[0];
+
+	if (!name)
+	{
+		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "CLEAR FLAGS");
+		command_fail(si, fault_needmoreparams, "Syntax: CLEAR <#channel> FLAGS");
+		return;
+	}
+
+	if (!(mc = mychan_find(name)))
+	{
+		command_fail(si, fault_nosuch_target, "\2%s\2 is not registered.", name);
+		return;
+	}
+	
+	if (metadata_find(mc, "private:close:closer"))
+	{
+		command_fail(si, fault_noprivs, "\2%s\2 is closed.", name);
+		return;
+	}
+
+	if (!mc->chan)
+	{
+		command_fail(si, fault_nosuch_target, "\2%s\2 does not exist.", name);
+		return;
+	}
+
+	if (!chanacs_source_has_flag(mc, si, CA_FOUNDER))
+	{
+		command_fail(si, fault_noprivs, "You are not authorized to perform this operation.");
+		return;
+	}
+
+
+	LIST_FOREACH_SAFE(n, tn, mc->chanacs.head)
+	{
+		ca = n->data;
+
+		if (ca->level & CA_FOUNDER)
+			continue;
+
+		object_unref(ca);
+	}
+
+	logcommand(si, CMDLOG_DO, "CLEAR:FLAGS: \2%s\2", mc->name);
+	command_success_nodata(si, _("Cleared flags in \2%s\2."), name);
+}
+
+/* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
+ * vim:ts=8
+ * vim:sw=8
+ * vim:noexpandtab
+ */
