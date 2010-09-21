7f0d8f732b4f6a0b30340bc71be4ed497d4a07ee
JD Horelick <jdhore1@gmail.com>
2010-09-21 19:55:32 -0400

    Fix a custom prefix not showing up in INFO when one is set. Forgot to change one usage after I changed the metadata key.

--- modules/chanserv/info.c
+++ modules/chanserv/info.c
@@ -235,7 +235,7 @@ static void cs_cmd_info(sourceinfo_t *si, int parc, char *parv[])
 	if (*buf)
 		command_success_nodata(si, _("Flags      : %s"), buf);
 
-	if ((md = metadata_find(mc, "prefix")))
+	if ((md = metadata_find(mc, "private:prefix")))
 		command_success_nodata(si, _("Prefix     : %s"), md->value);
 	else
 		command_success_nodata(si, _("Prefix     : %s (default)"), chansvs.trigger);
