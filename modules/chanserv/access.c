508504f99cb81c56aa1b3e924379e5b7e39b6981
William Pitcock <nenolod@atheme.org>
2010-09-28 21:50:53 -0500

    chanserv/access: Separate the core command handlers from the actual logic implementation.

--- modules/chanserv/access.c
+++ modules/chanserv/access.c
@@ -37,6 +37,59 @@ void _moddeinit()
 	mowgli_patricia_destroy(cs_access_cmds, NULL, NULL);
 }
 
+static void cs_help_access(sourceinfo_t *si, char *subcmd)
+{
+	if (!subcmd)
+	{
+		command_success_nodata(si, _("***** \2%s Help\2 *****"), chansvs.me->disp);
+		command_success_nodata(si, _("Help for \2ACCESS\2:"));
+		command_success_nodata(si, " ");
+		command_help(si, cs_access_cmds);
+		command_success_nodata(si, " ");
+		command_success_nodata(si, _("For more information, use \2/msg %s HELP ACCESS \37command\37\2."), chansvs.me->disp);
+		command_success_nodata(si, _("***** \2End of Help\2 *****"));
+	}
+	else
+		help_display(si, si->service, subcmd, cs_access_cmds);
+}
+
+static void cs_cmd_access(sourceinfo_t *si, int parc, char *parv[])
+{
+	char *chan;
+	char *cmd;
+	command_t *c;
+
+	if (parc < 2)
+	{
+		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "ACCESS");
+		command_fail(si, fault_needmoreparams, _("Syntax: ACCESS <#channel> <command> [parameters]"));
+		return;
+	}
+	
+	if (parv[0][0] == '#')
+		chan = parv[0], cmd = parv[1];
+	else if (parv[1][0] == '#')
+		cmd = parv[0], chan = parv[1];
+	else
+	{
+		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "ACCESS");
+		command_fail(si, fault_badparams, _("Syntax: ACCESS <#channel> <command> [parameters]"));
+		return;
+	}
+
+	c = command_find(cs_access_cmds, cmd);
+	if (c == NULL)
+	{
+		command_fail(si, fault_badparams, _("Invalid command. Use \2/%s%s help\2 for a command listing."), (ircd->uses_rcommand == false) ? "msg " : "", chansvs.me->disp);
+		return;
+	}
+
+	parv[1] = chan;
+	command_exec(si->service, si, c, parc - 1, parv + 1);
+}
+
+/***********************************************************************************************/
+
 static const char *get_template_name_fuzzy(mychan_t *mc, unsigned int level)
 {
 	metadata_t *md;
@@ -148,57 +201,6 @@ static const char *get_role_name(mychan_t *mc, unsigned int level)
 		return role;
 
 	return get_template_name_fuzzy(mc, level);
-}
-
-static void cs_help_access(sourceinfo_t *si, char *subcmd)
-{
-	if (!subcmd)
-	{
-		command_success_nodata(si, _("***** \2%s Help\2 *****"), chansvs.me->disp);
-		command_success_nodata(si, _("Help for \2ACCESS\2:"));
-		command_success_nodata(si, " ");
-		command_help(si, cs_access_cmds);
-		command_success_nodata(si, " ");
-		command_success_nodata(si, _("For more information, use \2/msg %s HELP ACCESS \37command\37\2."), chansvs.me->disp);
-		command_success_nodata(si, _("***** \2End of Help\2 *****"));
-	}
-	else
-		help_display(si, si->service, subcmd, cs_access_cmds);
-}
-
-static void cs_cmd_access(sourceinfo_t *si, int parc, char *parv[])
-{
-	char *chan;
-	char *cmd;
-	command_t *c;
-
-	if (parc < 2)
-	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "ACCESS");
-		command_fail(si, fault_needmoreparams, _("Syntax: ACCESS <#channel> <command> [parameters]"));
-		return;
-	}
-	
-	if (parv[0][0] == '#')
-		chan = parv[0], cmd = parv[1];
-	else if (parv[1][0] == '#')
-		cmd = parv[0], chan = parv[1];
-	else
-	{
-		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "ACCESS");
-		command_fail(si, fault_badparams, _("Syntax: ACCESS <#channel> <command> [parameters]"));
-		return;
-	}
-
-	c = command_find(cs_access_cmds, cmd);
-	if (c == NULL)
-	{
-		command_fail(si, fault_badparams, _("Invalid command. Use \2/%s%s help\2 for a command listing."), (ircd->uses_rcommand == false) ? "msg " : "", chansvs.me->disp);
-		return;
-	}
-
-	parv[1] = chan;
-	command_exec(si->service, si, c, parc - 1, parv + 1);
 }
 
 /* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
