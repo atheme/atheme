4fd2b9a0cac24df08b994d90ea2231f086d2dae2
William Pitcock <nenolod@atheme.org>
2010-10-08 21:16:23 -0500

    Remove LIST_FOREACH_* macros.

--- include/dlink.h
+++ include/dlink.h
@@ -13,13 +13,7 @@ typedef struct node_ node_t;
 typedef struct list_ list_t;
 
 /* macros for linked lists */
-#define LIST_FOREACH(n, head) for (n = (head); n; n = n->next)  
-#define LIST_FOREACH_NEXT(n, head) for (n = (head); n->next; n = n->next)
-#define LIST_FOREACH_PREV(n, tail) for (n = (tail); n; n = n->prev)
-
 #define LIST_LENGTH(list) (list)->count
-
-#define LIST_FOREACH_SAFE(n, tn, head) for (n = (head), tn = n ? n->next : NULL; n != NULL; n = tn, tn = n ? n->next : NULL)
 
 /* list node struct */
 struct node_
