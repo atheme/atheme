bdb54519c2b42ba06a28ffd496c5ec45deb82961
JD Horelick <jdhore1@gmail.com>
2010-11-02 04:29:14 -0400

    contrib/ns_guestnoreg: Switch from using a single string conf item to a block so you can specify many guest nicks.

--- modules/contrib/ns_guestnoreg.c
+++ modules/contrib/ns_guestnoreg.c
@@ -5,9 +5,12 @@
  * This file contains code for blocking registrations of guest nicks.
  * Particularly for use with webchat clients.
  *
- * To actually use this, add a line like the following to 
+ * To actually use this, add a something like the following to 
  * the nickserv {} block of your atheme.conf:
- * guestnick = "mib_";
+ * guestnicks {
+ *      "mib_";
+ *      "WebUser";
+ *  };
  *
  */
 
@@ -21,35 +24,73 @@ DECLARE_MODULE_V1
         "Atheme Development Group <http://www.atheme.org>"
 );
 
-static char *guestnick = NULL;
+static mowgli_list_t guestnicks = { NULL, NULL, 0 };
 
 static void guestnoreg_hook(hook_user_register_check_t *hdata)
 {
+        mowgli_node_t *n;
+
 	return_if_fail(hdata != NULL);
 	return_if_fail(hdata->si != NULL);
 
-        if (!irccasecmp(guestnick, hdata->account))
+        MOWGLI_ITER_FOREACH(n, guestnicks.head)
         {
-                command_fail(hdata->si, fault_badparams, _("Registering of guest nicknames is disallowed."));
-                hdata->approved++;
+                char *nick = n->data;
+                int nicklen = strlen(nick);
+
+                if (!strncasecmp(hdata->account, nick, nicklen))
+                {
+                        command_fail(hdata->si, fault_badparams, _("Registering of guest nicknames is disallowed."));
+                        hdata->approved++;
+                }
         }
 
+}
+
+static int guestnoreg_config_handler(config_entry_t *ce)
+{
+        config_entry_t *cce;
+
+        for (cce = ce->ce_entries; cce != NULL; cce = cce->ce_next)
+        {
+                char *nick = sstrdup(cce->ce_varname);
+                mowgli_node_add(nick, mowgli_node_create(), &guestnicks);
+        }
+
+        return 0;
+}
+
+static void guestnoreg_config_purge(void *unused)
+{
+        mowgli_node_t *n, *tn;
+
+        MOWGLI_ITER_FOREACH_SAFE(n, tn, guestnicks.head)
+        {
+                char *nick = n->data;
+
+                free(nick);
+                mowgli_node_delete(n, &guestnicks);
+                mowgli_node_free(n);
+        }
 }
 
 void
 _modinit(module_t *m)
 {
+        hook_add_event("config_purge");
+        hook_add_config_purge(guestnoreg_config_purge);
 
 	hook_add_event("user_can_register");
 	hook_add_user_can_register(guestnoreg_hook);
 
-	add_dupstr_conf_item("GUESTNICK", &conf_ni_table, 0, &guestnick, NULL);
+        add_conf_item("GUESTNICKS", &conf_ni_table, guestnoreg_config_handler);
 }
 
 void
 _moddeinit(void)
 {
 	hook_del_user_can_register(guestnoreg_hook);
+        hook_del_config_purge(guestnoreg_config_purge);
 
-	del_conf_item("GUESTNICK", &conf_ni_table);
+        del_conf_item("GUESTNICKS", &conf_ni_table);
 }
