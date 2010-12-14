356a7c779e3ae04627a6a5149dbe226c09529dd2
William Pitcock <nenolod@atheme.org>
2010-12-14 03:16:31 -0600

    Add missing stub.

---
+++ src/services/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2005-2010 William Pitcock <nenolod@atheme.org>.
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * Initialization stub for libathemecore.
+ */
+
+extern int atheme_main(int argc, const char *argv[]);
+
+int main(int argc, const char *argv[])
+{
+	return atheme_main(argc, argv);
+}
