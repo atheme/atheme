d86780e6891ecac7d0cb8fb27ba5c82310225472
JD Horelick <jdhore1@gmail.com>
2010-12-27 13:39:52 -0500

    groupserv: Add LISTGROUPS command for listing groups you have access in (and what your access in them is).

--- modules/groupserv/basecmds.c
+++ modules/groupserv/basecmds.c
@@ -218,6 +218,89 @@ static void gs_cmd_list(sourceinfo_t *si, int parc, char *parv[])
 	logcommand(si, CMDLOG_ADMIN, "LIST: \2%s\2 (\2%d\2 matches)", pattern, matches);
 }
 
+static void gs_cmd_listgroups(sourceinfo_t *si, int parc, char *parv[]);
+
+command_t gs_listgroups = { "LISTGROUPS", N_("Lists groups that you have access to."), AC_NONE, 1, gs_cmd_listgroups, { .path = "groupserv/listgroups" } };
+
+static void gs_cmd_listgroups(sourceinfo_t *si, int parc, char *parv[])
+{
+	myuser_t *mu;
+	mowgli_node_t *n;
+	mowgli_list_t *l;
+	unsigned int bans = 0, i;
+
+	/* Optional target */
+	char *target = parv[0];
+
+	if (target)
+	{
+		if (!has_priv(si, PRIV_GROUP_AUSPEX))
+		{
+			command_fail(si, fault_noprivs, _("You are not authorized to use the target argument."));
+			return;
+		}
+
+		mu = myuser_find_ext(target);
+
+		if (mu == NULL)
+		{
+			command_fail(si, fault_nosuch_target, _("\2%s\2 is not registered."), target);
+			return;
+		}
+	}
+	else
+	{
+		mu = si->smu;
+		if (mu == NULL)
+		{
+			command_fail(si, fault_noprivs, _("You are not logged in."));
+			return;
+		}
+	}
+
+	if (mu != si->smu)
+	{	/* must have been an oper */
+		logcommand(si, CMDLOG_ADMIN, "LISTGROUPS: \2%s\2", entity(mu)->name);
+	}
+	else
+	{	/* just a user, or oper is listing himself */
+		logcommand(si, CMDLOG_GET, "LISTGROUPS");
+	}
+
+	l = myuser_get_membership_list(mu);
+
+	if (MOWGLI_LIST_LENGTH(l) == 0)
+	{
+		command_success_nodata(si, _("No group access was found for the nickname \2%s\2."), entity(mu)->name);
+		return;
+	}
+
+	MOWGLI_ITER_FOREACH(n, l->head)
+	{
+		groupacs_t *ga = n->data;
+
+		if (ga->mu != mu)
+			continue;
+
+		if (groupacs_find(ga->mg, mu, GA_BAN) != NULL)
+		{
+			bans++;
+			continue;
+		}
+
+		command_success_nodata(si, _("Access flag(s) %s in %s"), gflags_tostr(ga_flags, ga->flags), entity(ga->mg)->name);
+	}
+
+	i = MOWGLI_LIST_LENGTH(l) - bans;
+
+	if (i == 0)
+		command_success_nodata(si, _("No group access was found for the nickname \2%s\2."), entity(mu)->name);
+	else
+		command_success_nodata(si, ngettext(N_("\2%d\2 group access match for the nickname \2%s\2"),
+						    N_("\2%d\2 group access matches for the nickname \2%s\2"), i),
+						    i, entity(mu)->name);
+}
+
 static void gs_cmd_drop(sourceinfo_t *si, int parc, char *parv[]);
 
 command_t gs_drop = { "DROP", N_("Drops a group registration."), AC_AUTHENTICATED, 2, gs_cmd_drop, { .path = "groupserv/drop" } };
@@ -781,6 +864,7 @@ void basecmds_init(void)
 	service_bind_command(groupsvs, &gs_register);
 	service_bind_command(groupsvs, &gs_info);
 	service_bind_command(groupsvs, &gs_list);
+	service_bind_command(groupsvs, &gs_listgroups);
 	service_bind_command(groupsvs, &gs_drop);
 	service_bind_command(groupsvs, &gs_flags);
 	service_bind_command(groupsvs, &gs_regnolimit);
@@ -796,6 +880,7 @@ void basecmds_deinit(void)
 	service_unbind_command(groupsvs, &gs_register);
 	service_unbind_command(groupsvs, &gs_info);
 	service_unbind_command(groupsvs, &gs_list);
+	service_unbind_command(groupsvs, &gs_listgroups);
 	service_unbind_command(groupsvs, &gs_drop);
 	service_unbind_command(groupsvs, &gs_flags);
 	service_unbind_command(groupsvs, &gs_regnolimit);
