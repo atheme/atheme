edfca431f91033f501abfc6d3b961d25efe79746
William Pitcock <nenolod@atheme.org>
2010-12-30 23:35:40 -0600

    gameserv: Port and clean up Darkmyst's old dice calculator. At least it's not as bad as m_roll.

--- modules/gameserv/dice.c
+++ modules/gameserv/dice.c
@@ -11,6 +11,8 @@
 #include "atheme.h"
 #include "gameserv_common.h"
 
+#include <math.h>
+
 DECLARE_MODULE_V1
 (
 	"gameserv/dice", false, _modinit, _moddeinit,
@@ -20,7 +22,7 @@ DECLARE_MODULE_V1
 
 static void command_dice(sourceinfo_t *si, int parc, char *parv[]);
 
-command_t cmd_dice = { "ROLL", N_("Rolls one or more dice."), AC_NONE, 2, command_dice, { .path = "gameserv/roll" } };
+command_t cmd_dice = { "ROLL", N_("Rolls one or more dice."), AC_NONE, 1, command_dice, { .path = "gameserv/roll" } };
 
 void _modinit(module_t * m)
 {
@@ -36,12 +38,495 @@ void _moddeinit(module_unload_intent_t intent)
 	service_named_unbind_command("chanserv", &cmd_dice);
 }
 
+#if 0
+static void do_calc(User *u) {
+ 
+    char *target = strtok(NULL, "");
+    char *calcstr;
+
+    ChannelInfo *ci;
+
+    if (!target) {
+        syntax_error(s_GameServ, u, "CALC", GAME_CALC_SYNTAX);
+	return;
+    } else if ( *target == '#' ) {
+	target = strtok(target, " ");
+	calcstr = strtok(NULL, "");
+
+        write_log("%s is using the calc command: %s", u->nick, calcstr);
+
+	if (!(ci = cs_findchan(target))) {
+	    notice_lang(s_GameServ, u, CHAN_X_NOT_REGISTERED, target);
+	    return;
+	} else if (ci->flags & CI_VERBOTEN) {
+	    notice_lang(s_GameServ, u, CHAN_X_FORBIDDEN, target);
+	    return;
+	} else if (!ci->founder) {
+	    notice_lang(s_GameServ, u, CHAN_X_NOT_REGISTERED, target);
+	    return;
+	} else if (!(ci->flags & CI_GAMESERV)) {
+	    notice_lang(s_GameServ, u, GAME_DISABLED, target);
+	    return;
+	} else if (!(ci->flags & CI_CALC)) {
+	    notice_lang(s_GameServ, u, GAME_CALC_DISABLED, target);
+	    return;
+	} else if (!is_on_chan(u, target)) {
+	    notice_lang(s_GameServ, u, MUST_JOIN_CHANNEL, target);
+	    return;
+	}
+    } else {
+	calcstr = target;
+	target = u->nick;
+    }
+ 
+    calcstr = process_calc(calcstr);
+
+    if (strncmp(calcstr, "Error:", 6) == 0) {
+	// Error Message... skip the prefix and send it.
+	send_cmd(s_GameServ, "PRIVMSG %s :%s: %s", target, u->nick, calcstr+7);
+	return;
+    }
+
+    // Show the result...
+    send_cmd(s_GameServ, "PRIVMSG %s :%s: %s", target, u->nick, calcstr);
+    return;
+}
+#endif
+
+#define CALC_MAX_STACK		(128)
+
+#define DICE_MAX_DICE		(100)
+#define DICE_MAX_SIDES		(100)
+
+int do_calc_expr(sourceinfo_t *si, char *expr, char *errmsg, long double *presult);
+int do_calc_eval(sourceinfo_t *si, long double lhs, char oper, long double rhs,
+				 long double *out, char *errmsg);
+int is_calcoper(char oper);
+
+//
+// <expr>
+//   expr = {<op>(<expr>)}|{<op><expr>}
+//   op   = { ~ ! * / % \ ^ + - & $ | }
+//
+// [Rank 1]                        |  [Rank 4]
+// ~   = One's compliment          |  + - = Add / Subtract
+// !   = Logical NOT               |
+//                                 |  [Rank 5]
+// [Rank 2]                        |  &   = Bitwise AND
+// ^   = Power                     |
+//                                 |  [Rank 6]
+// [Rank 3]                        |  $   = Bitwise XOR (eXclusive OR)
+// * / = Multiply / Divide         |
+// % \ = Modulus / Integer-divide  |  [Rank 7]
+//                                 |  |   = Bitwise inclusive OR
+//
+static void eval_calc(sourceinfo_t *si, char *s_input)
+{
+	static char buffer[1024];
+
+	char *ci = s_input;
+
+	int err, braces = 0;
+	long double expr;
+
+	
+	if (s_input == NULL)
+	{
+		command_fail(si, fault_badparams, _("Error: You typed an invalid expression."));
+		return;
+	}
+
+	// Skip leading whitespace
+	while( *ci && isspace(*ci) ) ci++;
+
+	if (!*ci) {
+		command_fail(si, fault_badparams, _("Error: You typed an invalid expression."));
+		return;
+	}
+
+	// Validate braces
+	while( *ci ) {
+		if ( *ci == '(' ) {
+			braces++;
+		} else if ( *ci == ')' ) {
+			if ( --braces < 0 )
+				break; // mismatched!
+		} else if ( !isspace(*ci) && !isdigit(*ci) && *ci != '.' && !is_calcoper(*ci) ) {
+			command_fail(si, fault_badparams, _("Error: You typed an invalid expression."));
+			return;
+		}
+		ci++;
+	}
+
+	if (braces != 0)
+	{
+		command_fail(si, fault_badparams, _("Error: Mismatched braces '( )' in expression."));
+		return;
+	}
+
+	// Calculate the expression
+	//
+	//==> err=0: calc succeeded / err!=0: calc failed - errmsg in 'buffer'
+	//
+
+	err = do_calc_expr(si, s_input, buffer, &expr);
+
+	if ( !err ) {
+		if (strlen(s_input) > 250) {
+			strncpy(buffer, s_input, 150);
+			sprintf(&buffer[150],"...%s = %.8Lg", &s_input[strlen(s_input)-10], expr);
+		} else {
+			sprintf(buffer, "%s = %.8Lg", s_input, expr);
+		}
+	}
+
+	gs_command_report(si, "%s", buffer);
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+
+enum {
+	CALCEXPR_VALUE,
+	CALCEXPR_OPER
+};
+
+typedef struct _tagCalcStack {
+	long double _value;
+	char _oper;
+	int _rank;
+	int _brace;
+} CalcStack;
+
+int do_calc_expr(sourceinfo_t *si, char *expr, char *errmsg, long double *presult)
+{
+	int expect = CALCEXPR_VALUE, lastrank, currank;
+	char *cur = expr, *endptr, lastop, curop = ' ';
+	long double total, curval = 0;
+	static int nStack = 0; // Current stack position
+	static CalcStack pStack[CALC_MAX_STACK]; // Value stack
+
+	nStack = 0;
+
+	total = 0;
+	lastrank = is_calcoper(lastop = '+');
+
+	while ( *cur ) {
+		printf("%i : %s\n",expect,cur);
+		switch(expect) {
+			case CALCEXPR_VALUE: {
+				curval = strtol(cur, &endptr, 10);
+				if ( *endptr == '.' || (curval < -2000000000 || curval > 2000000000))
+					curval = strtod(cur, &endptr);
+				if ( cur == endptr ) { // didn't go anywhere! o.O
+					if ( *cur == '(' ) {
+						if (curop != ' ') {
+							command_fail(si, fault_badparams, _("Error: Missing expected value in expression."));
+							return 1;
+						}
+						cur++; // skip it
+						pStack[nStack]._value = total;
+						pStack[nStack]._oper = lastop;
+						pStack[nStack]._rank = lastrank;
+						pStack[nStack++]._brace = 1;
+						total = 0;
+						lastrank = is_calcoper(lastop = '+');
+					} else if ( ( currank = is_calcoper(curop = *cur) ) ) {
+						if ( currank != 1 ) { // Unary: ~!
+							command_fail(si, fault_badparams, _("Error: Missing expected value in expression."));
+							return 1;
+						}
+						cur++; // skip unary op
+					} else {
+						command_fail(si, fault_badparams, _("Error: You typed an invalid expression."));
+						return 1;
+					}
+				} else {
+					// got value... grab next operator
+					if ( curop != ' ' ) {
+						// LHS ignored -- unary ops only have RHS :)
+						if ( do_calc_eval(si, 0, curop, curval, &curval, errmsg) ) {
+							return 1; // error
+						}
+					}
+					cur = endptr;
+					expect = CALCEXPR_OPER;
+				}
+				break;
+			}
+			case CALCEXPR_OPER: {
+				currank = is_calcoper(curop = *cur);
+				if (curop == '(') {
+					cur--;
+					currank = is_calcoper(curop = '*');
+				}
+				if ( currank == 0 ) {
+					if ( curop == ')' ) {
+						do {
+							if ( do_calc_eval(si, total, lastop, curval, &curval, errmsg) ) {
+								return 1; // error
+							}
+							total = pStack[--nStack]._value;
+							lastop = pStack[nStack]._oper;
+							lastrank = pStack[nStack]._rank;
+						} while ( !pStack[nStack]._brace );
+						expect = CALCEXPR_OPER;
+					} else {
+						command_fail(si, fault_badparams, _("Error: Missing expected operator in expression."));
+						return 1;
+					}
+				} else if ( currank < lastrank ) { // operator precedence (new over old!)
+					pStack[nStack]._value = total;
+					pStack[nStack]._oper = lastop;
+					pStack[nStack]._rank = lastrank;
+					pStack[nStack++]._brace = 0;
+					total = curval;
+					lastop = curop;
+					lastrank = currank;
+					currank = is_calcoper(curop = ' ');
+					expect = CALCEXPR_VALUE;
+				} else { // previous opers have precedence -- work backwards (until start/brace)
+					while ( currank >= lastrank ) {
+						if ( do_calc_eval(si, total, lastop, curval, &curval, errmsg) ) {
+							return 1; // error
+						}
+						if ( nStack == 0 )
+							break;
+						if ( pStack[nStack-1]._brace || currank < pStack[nStack-1]._rank )
+							break;
+						total = pStack[--nStack]._value;
+						lastop = pStack[nStack]._oper;
+						lastrank = pStack[nStack]._rank;
+					}
+					total = curval;
+					lastop = curop;
+					lastrank = currank;
+					currank = is_calcoper(curop = ' ');
+					expect = CALCEXPR_VALUE;
+				}
+				cur++;
+				break;
+			}
+		}
+		if ( nStack >= CALC_MAX_STACK ) {
+			command_fail(si, fault_badparams, _("Error: Expression is too deeply nested."));
+			return 1;
+		}
+		// skip whitespace
+		while( *cur && isspace(*cur) ) cur++;
+	}
+
+	// Did we end on an operator instead of a value?
+	if ( expect == CALCEXPR_VALUE ) {
+		command_fail(si, fault_badparams, _("Error: Missing expected value in expression."));
+		return 1;
+	}
+
+	// Wind back up the stack, if it exists (including last value!)
+	while ( nStack > -1 ) {
+		if ( do_calc_eval(si, total, lastop, curval, &curval, errmsg) ) {
+			return 1; // error
+		}
+		if ( nStack == 0 )
+			break;
+		total = pStack[--nStack]._value;
+		lastop = pStack[nStack]._oper;
+		// rank doesn't matter now
+	}
+
+	*presult = curval;
+	return 0; // no error
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+
+int do_calc_eval(sourceinfo_t *si, long double lhs, char oper, long double rhs, long double *out, char *errmsg)
+{
+	switch(oper)
+	{
+		case '~': // 1's compliment (unary)
+			*out = (long double)(~(long long)rhs);
+			break;
+		case '!': // NOT (unary)
+			*out = !rhs;
+			break;
+		case '*': // multiplication
+			*out = lhs * rhs;
+			break;
+		case '/': // division
+		case '%': // MOD
+		case '\\': // DIV
+			if (rhs == 0 || (oper=='%' && ((long long)rhs == 0)))
+			{
+				command_fail(si, fault_badparams, _("Error: Cannot perform modulus or division by zero."));
+				return 1;
+			}
+
+			switch (oper)
+			{
+				case '/':
+					*out = lhs / rhs;
+					break;
+				case '%':
+					*out = (long double)((long long)lhs % (long long)rhs);
+					break;
+				case '\\':
+					lhs /= rhs;
+					*out = (lhs < 0) ? ceil(lhs) : floor(lhs);
+					break;
+			}
+			break;
+		case '^': // power-of
+			*out = powl(lhs, rhs);
+			break;
+		case '+': // addition
+			*out = lhs + rhs;
+			break;
+		case '-': // subtraction
+			*out = lhs - rhs;
+			break;
+		case '&': // AND (bitwise)
+			*out = (long double)((long long)lhs & (long long)rhs);
+			break;
+		case '$': // XOR (bitwise)
+			*out = (long double)((long long)lhs ^ (long long)rhs);
+			break;
+		case '|': // OR (bitwise)
+			*out = (long double)((long long)lhs | (long long)rhs);
+			break;
+		default:
+			command_fail(si, fault_unimplemented, _("Error: Unknown mathematical operator %c."), oper);
+			return 1;
+	}
+
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+// Is 'oper' an operator? If so, return its 'rank', else '0'.
+//
+int is_calcoper(char oper)
+{
+	static char *opers = "~! ^ */%\\ +- & $ |";
+	char *c = opers;
+	int rank = 1;
+
+	while( *c && *c != oper ) {
+		if ( *c == ' ' ) rank++;
+		c++;
+	}
+
+	return (*c ? rank : 0);
+}
+
+/*************************************************************************************/
+
+void eval_dice(sourceinfo_t *si, char *s_input)
+{
+	static char buffer[1024], result[32];
+
+	char op = '\0', *c = s_input;
+	unsigned int dice, roll, x, y, z = 0;
+	double total;
+
+	while( *c && isspace(*c) ) ++c;
+	if ( !*c || !isdigit(*c) ) {
+		gs_command_report(si, _("Syntax: XdY [ {-|+|*|/} Z ]"));
+		return;
+	}
+
+	x = strtoul(c, &c, 10);
+	if (x == 0 || c == NULL || ToLower(*c++) != 'd' || !isdigit(*c)) 
+	{
+		if (x < 1 || x > DICE_MAX_DICE)
+		{
+			gs_command_report(si, _("Only 1-100 dice may be thrown at once."));
+			return;
+		}
+
+		gs_command_report(si, _("Syntax: XdY [ {-|+|*|/} Z ]"));
+		return;
+	}
+
+	y = strtoul(c, &c, 10);
+	if (c != NULL)
+	{
+		while (*c && isspace(*c))
+			++c;
+
+		if ( *c && strchr("-+*/", *c) == NULL ) {
+			gs_command_report(si, _("Syntax: XdY [ {-|+|*|/} Z ]"));
+			return;
+		}
+	}
+
+	if (x < 1 || x > 100)
+	{
+		gs_command_report(si, _("Syntax: XdY [ {-|+|*|/} Z ]"));
+		return;
+	}
+
+	if (y < 1 || y > DICE_MAX_SIDES)
+	{
+		gs_command_report(si, _("Only 1-100 sides may be used on a dice."));
+		return;
+	}
+
+	if (*c)
+	{
+		op = *c++;
+
+		z = strtoul(c, &c, 10);
+
+		while (*c && isspace(*c))
+			++c;
+
+		if (*c)
+		{
+			gs_command_report(si, _("Syntax: XdY [ {-|+|*|/} Z ]"));
+			return;
+		}
+		else if (op == '/' && z == 0)
+		{
+			gs_command_report(si, _("Can't divide by zero."));
+			return;
+		}
+	}
+
+	total = 0.0;
+	snprintf(buffer, 1024, "\2%s\2 rolled %lud%lu: ", si->su->nick, x, y);
+	for (roll = 0; roll < x ; ++roll)
+	{
+		snprintf(result, 32, "%d ", dice = (1 + (arc4random() % y)));
+		strlcat(buffer, result, sizeof(buffer));
+		total += dice;
+	}
+
+	if (op == '\0')
+		snprintf(result, 32, " <Total: %g>", total);
+	else {
+		snprintf(result, 32, " <Total: %g(%c%lu) = ", total, op, z);
+		strlcat(buffer, result, sizeof(buffer));
+		switch(op) {
+			case '+':	total += z; break;
+			case '-':	total -= z; break;
+			case '/':	total /= z; break;
+			case '*':	total *= z; break;
+			default:	break;
+		}
+		snprintf(result, 32, "%g>", total);
+	}
+	strlcat(buffer, result, sizeof(buffer));
+
+	gs_command_report(si, "%s", buffer);
+}
+
 static void command_dice(sourceinfo_t *si, int parc, char *parv[])
 {
 	char *arg;
 	mychan_t *mc;
-	int dice = 0, sides = 0, i = 0, roll = 0, modifier = 0;
-	char buf[BUFSIZE];
 
 	if (!gs_do_parameters(si, &parc, &parv, &mc))
 		return;
@@ -53,58 +538,25 @@ static void command_dice(sourceinfo_t *si, int parc, char *parv[])
 	}
 	arg = parv[0];
 
-	sscanf(arg, "%dd%d+%d", &dice, &sides, &modifier);
-
-	if (dice <= 0)
-	{
-		modifier = 0;
-		sscanf(arg, "%dd%d", &dice, &sides);
-
-		if (dice <= 0)
-		{
-			dice = 1;
-
-			sscanf(arg, "d%d+%d", &sides, &modifier);
-
-			if (sides <= 0)
-				sscanf(arg, "d%d", &sides);
-		}
-	}
-
-	if (dice > 100)
-		dice = 100;
-	if (sides > 100)
-		sides = 100;
-
-	if (dice <= 0 || sides <= 0)
-	{
-		dice = 1;
-		sides = 1;
-	}
-
-	*buf = '\0';
-
-	for (i = 0; i < dice; i++)
-	{
-		unsigned int y = 1 + arc4random() % sides;
-		char buf2[BUFSIZE];
-
-		if (*buf != '\0')
-		{
-			snprintf(buf2, BUFSIZE, ", %d", y);
-			strlcat(buf, buf2, BUFSIZE);
-		}
-		else
-		{
-			snprintf(buf2, BUFSIZE, "%d", y);
-			strlcpy(buf, buf2, BUFSIZE);
-		}
-
-		roll += y;
-	}
-
-	if (modifier != 0)
-		gs_command_report(si, "(%s) + %d == \2%d\2", buf, modifier, (roll + modifier));
-	else
-		gs_command_report(si, "%s == \2%d\2", buf, roll);
+	return eval_dice(si, arg);
 }
+
+static void command_calc(sourceinfo_t *si, int parc, char *parv[])
+{
+	char *arg;
+	mychan_t *mc;
+
+	if (!gs_do_parameters(si, &parc, &parv, &mc))
+		return;
+	if (parc < 1)
+	{
+		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "CALC");
+		command_fail(si, fault_needmoreparams, _("Syntax: CALC <expression>"));
+		return;
+	}
+	arg = parv[0];
+
+	return eval_calc(si, arg);
+}
+
+//////////////////////////////////////////////////////////////////////////
