c6978a682e0327fd2852b56c75e2d039ec2ef9ff
William Pitcock <nenolod@atheme.org>
2011-03-27 14:42:08 -0500

    contrib/os_resolve: demonstrate how to use the suspendable commands API.

--- modules/contrib/os_resolve.c
+++ modules/contrib/os_resolve.c
@@ -28,10 +28,16 @@ void _moddeinit(module_unload_intent_t intent)
 	service_named_unbind_command("operserv", &cmd_resolve);
 }
 
+typedef struct {
+	dns_query_t dns_query;
+	sourceinfo_t *si;
+} resolve_req_t;
+
+mowgli_heap_t *request_heap = NULL;
+
 static void resolve_cb(void *vptr, dns_reply_t *reply)
 {
-	service_t *svs;
-	user_t *u = vptr;
+	resolve_req_t *req = vptr;
 	char buf[BUFSIZE];
 
 	return_if_fail(vptr != NULL);
@@ -42,24 +48,30 @@ static void resolve_cb(void *vptr, dns_reply_t *reply)
 
 	inet_ntop(reply->addr.saddr.sa.sa_family, &reply->addr.saddr.sin.sin_addr, buf, reply->addr.saddr_len);
 
-	svs = service_find("operserv");
-	notice(svs->nick, u->nick, "Result is %s", buf);
+	mowgli_heap_free(request_heap, req);
+	object_unref(req->si);
 }
-
-dns_query_t dns_query;
 
 static void os_cmd_resolve(sourceinfo_t *si, int parc, char *parv[])
 {
+	resolve_req_t *req;
+
+	if (request_heap == NULL)
+		request_heap = mowgli_heap_create(sizeof(resolve_req_t), 32, BH_LAZY);
+
 	if (!parv[0])
 	{
 		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "RESOLVE");
 		return;
 	}
 
-	memset(&dns_query, '\0', sizeof(dns_query_t));
+	req = mowgli_heap_alloc(request_heap);
 
-	dns_query.ptr = si->su;
-	dns_query.callback = resolve_cb;
+	req->si = si;
+	req->dns_query.ptr = req;
+	req->dns_query.callback = resolve_cb;
 
-	gethost_byname_type(parv[0], &dns_query, T_A);
+	gethost_byname_type(parv[0], &req->dns_query, T_A);
+
+	object_ref(req->si);
 }
