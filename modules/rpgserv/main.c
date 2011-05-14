6952e8f7d203a64380b04e0b2cac38d0d37052ca
William Pitcock <nenolod@atheme.org>
2011-05-14 12:55:09 -0500

    rpgserv: add keys for pretty names

--- modules/rpgserv/main.c
+++ modules/rpgserv/main.c
@@ -17,6 +17,14 @@ static const char *rpg_rating_names[RPG_RATING_COUNT] = {
 	[RPG_RATING_ADULT]	= "Adult",
 };
 
+static const char *rpg_rating_keys[RPG_RATING_COUNT] = {
+	[RPG_RATING_G]		= "G",
+	[RPG_RATING_PG]		= "PG",
+	[RPG_RATING_PG13]	= "PG13",
+	[RPG_RATING_R]		= "R",
+	[RPG_RATING_ADULT]	= "ADULT",
+};
+
 static const char *rpg_ruleset_names[RPG_RULESET_COUNT] = {
 	[RPG_RULESET_ADND]	= "ADnD",
 	[RPG_RULESET_DND3]	= "DnD 3.0",
@@ -29,6 +37,18 @@ static const char *rpg_ruleset_names[RPG_RULESET_COUNT] = {
 	[RPG_RULESET_OTHER]	= "Other",
 };
 
+static const char *rpg_ruleset_keys[RPG_RULESET_COUNT] = {
+	[RPG_RULESET_ADND]	= "ADND",
+	[RPG_RULESET_DND3]	= "DND3.0",
+	[RPG_RULESET_DND35]	= "DND3.5",
+	[RPG_RULESET_DND4]	= "DND4.0",
+	[RPG_RULESET_FREEFORM]	= "FREEFORM",
+	[RPG_RULESET_HOMEBREW]	= "HOMEBREW",
+	[RPG_RULESET_NWOD]	= "NWOD",
+	[RPG_RULESET_OWOD]	= "OWOD",
+	[RPG_RULESET_OTHER]	= "Other",
+};
+
 static const char *rpg_period_names[RPG_PERIOD_COUNT] = {
 	[RPG_PERIOD_ANTIQUITY]		= "Antiquity",
 	[RPG_PERIOD_EARLYMODERN]	= "Early-modern",
@@ -36,6 +56,15 @@ static const char *rpg_period_names[RPG_PERIOD_COUNT] = {
 	[RPG_PERIOD_MIDDLEAGES]		= "Middle ages",
 	[RPG_PERIOD_MODERN]		= "Modern",
 	[RPG_PERIOD_PREHISTORIC]	= "Pre-historic",
+};
+
+static const char *rpg_period_keys[RPG_PERIOD_COUNT] = {
+	[RPG_PERIOD_ANTIQUITY]		= "ANTIQUITY",
+	[RPG_PERIOD_EARLYMODERN]	= "EARLYMODERN",
+	[RPG_PERIOD_FUTURE]		= "FUTURE",
+	[RPG_PERIOD_MIDDLEAGES]		= "MIDDLEAGES",
+	[RPG_PERIOD_MODERN]		= "MODERN",
+	[RPG_PERIOD_PREHISTORIC]	= "PREHISTORIC",
 };
 
 static const char *rpg_genre_names[RPG_GENRE_COUNT] = {
@@ -49,6 +78,19 @@ static const char *rpg_genre_names[RPG_GENRE_COUNT] = {
 	[RPG_GENRE_REALISTIC]		= "Realistic",
 	[RPG_GENRE_SCIFI]		= "Sci-fi",
 	[RPG_GENRE_STEAMPUNK]		= "Steampunk",
+};
+
+static const char *rpg_genre_keys[RPG_GENRE_COUNT] = {
+	[RPG_GENRE_APOCALYPSE]		= "APOCALYPSE",
+	[RPG_GENRE_ANIME]		= "ANIME",
+	[RPG_GENRE_ANTROPOMORPH]	= "ANTROPOMORPH",
+	[RPG_GENRE_CYBERPUNK]		= "CYBERPUNK",
+	[RPG_GENRE_FANTASY]		= "FANTASY",
+	[RPG_GENRE_HORROR]		= "HORROR",
+	[RPG_GENRE_MULTIGENRE]		= "MULTIGENRE",
+	[RPG_GENRE_REALISTIC]		= "REALISTIC",
+	[RPG_GENRE_SCIFI]		= "SCIFI",
+	[RPG_GENRE_STEAMPUNK]		= "STEAMPUNK",
 };
 
 static unsigned int lookup_flag(const char **flags, unsigned int count, const char *name)
