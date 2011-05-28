a817416f962c877e1d71f0b9efcb372d2d129e98
Alyx <alyx@sporksmoo.net>
2011-05-28 16:12:32 -0500

    Extra period.

--- modules/statserv/main.c
+++ modules/statserv/main.c
@@ -40,7 +40,7 @@ void ss_cmd_help(sourceinfo_t *si, int parc, char *parv[])
     if (!command)
     {
 		command_success_nodata(si, _("***** \2%s Help\2 *****"), si->service->nick);
-		command_success_nodata(si, _("\2%s\2 records various network statistics.."), si->service->nick);
+		command_success_nodata(si, _("\2%s\2 records various network statistics."), si->service->nick);
 		command_success_nodata(si, " ");
 		command_success_nodata(si, _("For more information on a command, type:"));
 		command_success_nodata(si, "\2/%s%s help <command>\2", (ircd->uses_rcommand == false) ? "msg " : "", si->service->disp);
