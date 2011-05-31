c7cdff13c2f6602c97c006d23e917d287c66261e
Alyx <alyx@sporksmoo.net>
2011-05-31 13:00:28 -0700

    count should be the same type of variable as the return value.

--- modules/statserv/server.c
+++ modules/statserv/server.c
@@ -81,7 +81,6 @@ static void ss_cmd_server_list(sourceinfo_t * si, int parc, char *parv[])
 
 static void ss_cmd_server_count(sourceinfo_t * si, int parc, char *parv[])
 {
-	int count;
-	count = mowgli_patricia_size(servlist);
+	unsigned int count = mowgli_patricia_size(servlist);
 	command_success_nodata(si, _("Network size: %d servers"), count);
 }
