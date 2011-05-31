4732935af348e640a3a73f6234ddbad60762944d
Elizabeth Jennifer Myers <elizabeth@sporksirc.net>
2011-05-31 14:42:18 -0400

    Fix indentation stuff per atheme standards.

--- modules/statserv/channel.c
+++ modules/statserv/channel.c
@@ -8,90 +8,96 @@
 
 #include "atheme.h"
 
-DECLARE_MODULE_V1(
-    "statserv/channel", false, _modinit, _moddeinit,
-    PACKAGE_STRING,
-    "Alexandria Wolcott <alyx@sporksmoo.net>"
-);
+DECLARE_MODULE_V1("statserv/channel", false, _modinit, _moddeinit,
+		  PACKAGE_STRING, "Alexandria Wolcott <alyx@sporksmoo.net>");
 
-static void ss_cmd_channel(sourceinfo_t *si, int parc, char *parv[]);
-static void ss_cmd_channel_topic(sourceinfo_t *si, int parc, char *parv[]);
+static void ss_cmd_channel(sourceinfo_t * si, int parc, char *parv[]);
+static void ss_cmd_channel_topic(sourceinfo_t * si, int parc, char *parv[]);
 
-command_t ss_channel = { "CHANNEL", N_("Obtain various information about clones"), AC_NONE, 2, ss_cmd_channel, { .path = "statserv/channels" } };
-command_t ss_channel_topic = { "TOPIC", N_("Obtain the topic for a given channel."), AC_NONE, 1, ss_cmd_channel_topic, { .path = "" } };
+command_t ss_channel =
+	{ "CHANNEL", N_("Obtain various information about clones"), AC_NONE, 2, ss_cmd_channel,
+ {.path = "statserv/channels"} };
+command_t ss_channel_topic =
+	{ "TOPIC", N_("Obtain the topic for a given channel."), AC_NONE, 1, ss_cmd_channel_topic,
+{.path = ""} };
 
 mowgli_patricia_t *ss_channel_cmds;
 
-void _modinit(module_t *m)
+void _modinit(module_t * m)
 {
-    service_named_bind_command("statserv", &ss_channel);
+	service_named_bind_command("statserv", &ss_channel);
 
-    ss_channel_cmds = mowgli_patricia_create(strcasecanon);
+	ss_channel_cmds = mowgli_patricia_create(strcasecanon);
 
-    command_add(&ss_channel_topic, ss_channel_cmds);
+	command_add(&ss_channel_topic, ss_channel_cmds);
 }
 
 void _moddeinit(module_unload_intent_t intent)
 {
-    service_named_unbind_command("statserv", &ss_channel);
+	service_named_unbind_command("statserv", &ss_channel);
 
-    command_delete(&ss_channel_topic, ss_channel_cmds);
+	command_delete(&ss_channel_topic, ss_channel_cmds);
 
-    mowgli_patricia_destroy(ss_channel_cmds, NULL, NULL);
+	mowgli_patricia_destroy(ss_channel_cmds, NULL, NULL);
 }
 
-static void ss_cmd_channel(sourceinfo_t *si, int parc, char *parv[])
+static void ss_cmd_channel(sourceinfo_t * si, int parc, char *parv[])
 {
-    command_t *c;
-    char *cmd = parv[0];
+	command_t *c;
+	char *cmd = parv[0];
 
-    if (!cmd)
-    {
-        command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "CHANNEL");
-        command_fail(si, fault_needmoreparams, _("Syntax: CHANNEL [TOPIC|USERS] [parameters]"));
-        return;
-    }
+	if (!cmd)
+	{
+		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "CHANNEL");
+		command_fail(si, fault_needmoreparams,
+			     _("Syntax: CHANNEL [TOPIC|USERS] [parameters]"));
+		return;
+	}
 
 	c = command_find(ss_channel_cmds, cmd);
 	if (c == NULL)
 	{
-		command_fail(si, fault_badparams, _("Invalid command. Use \2/%s%s help\2 for a command listing."), (ircd->uses_rcommand == false) ? "msg " : "", si->service->disp);
+		command_fail(si, fault_badparams,
+			     _("Invalid command. Use \2/%s%s help\2 for a command listing."),
+			     (ircd->uses_rcommand == false) ? "msg " : "", si->service->disp);
 		return;
 	}
 
-    command_exec(si->service, si, c, parc - 1, parv + 1);
+	command_exec(si->service, si, c, parc - 1, parv + 1);
 }
 
-static void ss_cmd_channel_topic(sourceinfo_t *si, int parc, char *parv[])
+static void ss_cmd_channel_topic(sourceinfo_t * si, int parc, char *parv[])
 {
-    char *chan = parv[0];
-    channel_t *c;
+	char *chan = parv[0];
+	channel_t *c;
 
-    if (!chan)
-    {
-        command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "CHANNEL TOPIC");
-        command_fail(si, fault_needmoreparams, _("Syntax: CHANNEL TOPIC <#channel>"));
-        return;
-    }
+	if (!chan)
+	{
+		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "CHANNEL TOPIC");
+		command_fail(si, fault_needmoreparams, _("Syntax: CHANNEL TOPIC <#channel>"));
+		return;
+	}
 
-    if (c->modes & CMODE_SEC)
-    {
-        if (!has_priv(si, PRIV_CHAN_AUSPEX))
-        {
-            command_fail(si, fault_noprivs, _("You are not authorised to perform this action."));
-            return;
-        }
-    }
+	if (c->modes & CMODE_SEC)
+	{
+		if (!has_priv(si, PRIV_CHAN_AUSPEX))
+		{
+			command_fail(si, fault_noprivs,
+				     _("You are not authorised to perform this action."));
+			return;
+		}
+	}
 
-    if (!(c = channel_find(chan)))
-    {
-        command_fail(si, fault_nosuch_target, _("The channel \2%s\2 does not exist."), chan);
-        return;
-    }
+	if (!(c = channel_find(chan)))
+	{
+		command_fail(si, fault_nosuch_target, _("The channel \2%s\2 does not exist."),
+			     chan);
+		return;
+	}
 
-    if (c->topic)
-        command_success_nodata(si, _("Topic for %s set by %s: %s"), c->name, c->topic_setter, c->topic);
-    else
-        command_success_nodata(si, _("No topic set for %s"), c->name);
+	if (c->topic)
+		command_success_nodata(si, _("Topic for %s set by %s: %s"), c->name,
+				       c->topic_setter, c->topic);
+	else
+		command_success_nodata(si, _("No topic set for %s"), c->name);
 }
-
