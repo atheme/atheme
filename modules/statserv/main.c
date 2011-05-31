b69616f451c8136aa80e7155bf537e6aabbf143f
Alyx <alyx@sporksmoo.net>
2011-05-31 12:49:05 -0700

    this does not need to be repeated twice.

--- modules/statserv/main.c
+++ modules/statserv/main.c
@@ -17,7 +17,7 @@ service_t *statsvs;
 static void ss_cmd_help(sourceinfo_t * si, int parc, char *parv[]);
 
 command_t ss_help =
-	{ "HELP", N_(N_("Displays contextual help information.")), AC_NONE, 2, ss_cmd_help, {.path = "help"}
+	{ "HELP", N_("Displays contextual help information."), AC_NONE, 2, ss_cmd_help, {.path = "help"}
 };
 
 void _modinit(module_t * m)
