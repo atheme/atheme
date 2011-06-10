37a49101777655441d34e8b75b112b33e5dce41f
William Pitcock <nenolod@dereferenced.org>
2011-06-09 21:05:52 -0500

    rpgserv/search: new module

---
+++ modules/rpgserv/search.c
@@ -0,0 +1,83 @@
+/* search.c - rpgserv SEARCH command
+ */
+
+#include "atheme.h"
+
+DECLARE_MODULE_V1
+(
+	"rpgserv/search", false, _modinit, _moddeinit,
+	PACKAGE_STRING,
+	"Atheme Development Group <http://www.atheme.org>"
+);
+
+static void rs_cmd_search(sourceinfo_t *si, int parc, char *parv[]);
+
+command_t rs_search = { "SEARCH", N_("Search for games based on specific criteria."),
+                      AC_NONE, 20, rs_cmd_search, { .path = "rpgserv/search" } };
+
+static void rs_cmd_search(sourceinfo_t *si, int parc, char *parv[])
+{
+	mowgli_patricia_iteration_state_t state;
+	mychan_t *mc;
+	unsigned int listed = 0;
+	char *desc;
+
+	MOWGLI_PATRICIA_FOREACH(mc, &state, mclist)
+	{
+		unsigned int i, j;
+		metadata_t *md;
+
+		static const char *mdkeys[] = {
+			"private:rpgserv:genre", "private:rpgserv:period", "private:rpgserv:ruleset",
+			"private:rpgserv:rating", "private:rpgserv:system"
+		};
+
+		if (!metadata_find(mc, "private:rpgserv:enabled"))
+			continue;
+
+		for (i = 0; i < ARRAY_SIZE(mdkeys); i++)
+		{
+			metadata_t *md;
+
+			md = metadata_find(mc, mdkeys[i]);
+			if (md == NULL)
+				continue;
+
+			for (j = 0; j < parc; j++)
+			{
+				if (strcasestr(md->value, parv[j]) != NULL)
+					goto __matched;
+			}
+		}
+
+		continue;
+
+__matched:				
+		listed++;
+
+		command_success_nodata(si, _("Channel \2%s\2:"), mc->name);
+		md = metadata_find(mc, "private:rpgserv:genre");
+		command_success_nodata(si, _("Genre: %s"), md ? md->value : "<none>");
+		md = metadata_find(mc, "private:rpgserv:period");
+		command_success_nodata(si, _("Period: %s"), md ? md->value : "<none>");
+		md = metadata_find(mc, "private:rpgserv:ruleset");
+		command_success_nodata(si, _("Ruleset: %s"), md ? md->value : "<none>");
+		md = metadata_find(mc, "private:rpgserv:rating");
+		command_success_nodata(si, _("Rating: %s"), md ? md->value : "<none>");
+		md = metadata_find(mc, "private:rpgserv:system");
+		command_success_nodata(si, _("System: %s"), md ? md->value : "<none>");
+	}
+
+	command_success_nodata(si, _("\2%d\2 channels met your criteria."), listed);
+	command_success_nodata(si, _("For more information on a specific channel, use \2/msg %s INFO <channel>\2."), si->service->disp);
+}
+
+void _modinit(module_t *m)
+{
+	service_named_bind_command("rpgserv", &rs_search);
+}
+
+void _moddeinit(module_unload_intent_t intent)
+{
+	service_named_unbind_command("rpgserv", &rs_search);
+}
