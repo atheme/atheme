480a5a134111fa33c36d787400ed289c0dd608cc
William Pitcock <nenolod@dereferenced.org>
2011-06-23 12:36:48 -0500

    autogen: do not regenerate config files for libmowgli as it is supposed to be immutable anyway

--- autogen.sh
+++ autogen.sh
@@ -82,10 +82,4 @@ run_or_die $ACLOCAL -I m4
 run_or_die $AUTOHEADER
 run_or_die $AUTOCONF
 
-cd $TOP_DIR/libmowgli
-
-run_or_die $ACLOCAL -I m4
-run_or_die $AUTOHEADER
-run_or_die $AUTOCONF
-
 cd $LAST_DIR
