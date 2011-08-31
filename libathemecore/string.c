85cdf3a76e2f47e1936e125cf914337df035ddeb
William Pitcock <nenolod@dereferenced.org>
2011-08-30 23:42:16 -0500

    libathemecore: remove strlcpy()/strlcat() stubs

--- libathemecore/string.c
+++ libathemecore/string.c
@@ -23,39 +23,6 @@
 
 #include "atheme.h"
 
-#ifndef HAVE_STRLCAT
-/* These functions are taken from Linux. */
-size_t strlcat(char *dest, const char *src, size_t count)
-{
-        size_t dsize = strlen(dest);
-        size_t len = strlen(src);
-        size_t res = dsize + len;
-
-        dest += dsize;
-        count -= dsize;
-        if (len >= count)
-                len = count - 1;
-        memcpy(dest, src, len);
-        dest[len] = 0;
-        return res;
-}
-#endif
-
-#ifndef HAVE_STRLCPY
-size_t strlcpy(char *dest, const char *src, size_t size)
-{
-        size_t ret = strlen(src);
-
-        if (size)
-        {
-                size_t len = (ret >= size) ? size - 1 : ret;
-                memcpy(dest, src, len);
-                dest[len] = '\0';
-        }
-        return ret;
-}
-#endif
-
 /* removes unwanted chars from a line */
 void strip(char *line)
 {
