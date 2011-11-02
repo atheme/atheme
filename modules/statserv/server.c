16998bc80a0ab78e1df32f111240b8c5aff684b6
Alyx <alyx@malkier.net>
2011-11-02 21:23:38 +0000

    Added a magical SERVER INFO command.

--- modules/statserv/server.c
+++ modules/statserv/server.c
@@ -12,17 +12,21 @@ DECLARE_MODULE_V1("statserv/server", false, _modinit, _moddeinit,
 		  PACKAGE_STRING, "Alexandria Wolcott <alyx@sporksmoo.net>");
 
 static void ss_cmd_server(sourceinfo_t * si, int parc, char *parv[]);
+static void ss_cmd_server_info(sourceinfo_t * si, int parc, char *parv[]);
 static void ss_cmd_server_list(sourceinfo_t * si, int parc, char *parv[]);
 static void ss_cmd_server_count(sourceinfo_t * si, int parc, char *parv[]);
 
 command_t ss_server =
-	{ "SERVER", N_("Obtain information about servers on the network."), AC_NONE, 2, ss_cmd_server, {.path = "statserv/server"} };
+	{ "SERVER", N_("Obtain information about servers on the network."), AC_NONE, 3, ss_cmd_server, {.path = "statserv/server"} };
 
 command_t ss_server_list =
 	{ "LIST", N_("Obtain a list of servers."), AC_NONE, 1, ss_cmd_server_list, {.path = ""} };
 
 command_t ss_server_count =
 	{ "COUNT", N_("Count the amount of servers connected to the network."), AC_NONE, 1, ss_cmd_server_count, {.path = ""} };
+
+command_t ss_server_info = 
+    { "INFO", N_("Obtain information about a specified server."), AC_NONE, 2, ss_cmd_server_info, {.path = ""} };
 
 mowgli_patricia_t *ss_server_cmds;
 
@@ -32,6 +36,7 @@ void _modinit(module_t * m)
 	ss_server_cmds = mowgli_patricia_create(strcasecanon);
 	command_add(&ss_server_list, ss_server_cmds);
 	command_add(&ss_server_count, ss_server_cmds);
+    command_add(&ss_server_info, ss_server_cmds);
 }
 
 void _moddeinit(module_unload_intent_t intent)
@@ -39,6 +44,7 @@ void _moddeinit(module_unload_intent_t intent)
 	service_named_unbind_command("statserv", &ss_server);
 	command_delete(&ss_server_list, ss_server_cmds);
 	command_delete(&ss_server_count, ss_server_cmds);
+    command_delete(&ss_server_info, ss_server_cmds);
 	mowgli_patricia_destroy(ss_server_cmds, NULL, NULL);
 }
 
@@ -50,7 +56,8 @@ static void ss_cmd_server(sourceinfo_t * si, int parc, char *parv[])
 	if (!cmd)
 	{
 		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "SERVER");
-		command_fail(si, fault_needmoreparams, _("Syntax: SERVER [LIST|COUNT] [parameters]"));
+		command_fail(si, fault_needmoreparams, 
+                _("Syntax: SERVER [INFO|LIST|COUNT] [parameters]"));
 		return;
 	}
 
@@ -76,10 +83,47 @@ static void ss_cmd_server_list(sourceinfo_t * si, int parc, char *parv[])
 		i++;
         if ((!(s->flags & SF_HIDE)) || (has_priv(si, PRIV_SERVER_AUSPEX)))
         	command_success_nodata(si, _("%d: %s [%s]"), i, s->name, s->desc);
-
 	}
 	command_success_nodata(si, _("End of server list."));
 }
+
+static void ss_cmd_server_info(sourceinfo_t * si, int parc, char *parv[])
+{
+    server_t *s;
+    char *name = parv[0];
+
+    if (!name)
+    {
+        command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "SERVER INFO");
+        command_fail(si, fault_needmoreparams, _("Syntax: SERVER INFO <server>"));
+        return;
+    }
+
+    if (!(s = mowgli_patricia_retrieve(servlist, name)))
+    {
+        command_fail(si, fault_nosuch_target, _("Server \2%s\2 does not exist."), name);
+        return;
+    }
+
+    if ((s->flags & SF_HIDE) && (!(has_priv(si, PRIV_SERVER_AUSPEX))))
+    {
+        command_fail(si, fault_nosuch_target, _("Server \2%s\2 does not exist."), name);
+        return;
+    }
+
+    command_success_nodata(si, _("Information for server %s:"), s->name);
+    command_success_nodata(si, _("Server description: %s"), s->desc);
+    command_success_nodata(si, _("Current users: %u (%u invisible)"), s->users, s->invis);
+    command_success_nodata(si, _("Online operators: %u"), s->opers);
+    if (has_priv(si, PRIV_SERVER_AUSPEX))
+    {
+        if (s->uplink->name)
+            command_success_nodata(si, _("Server uplink: %s"), s->uplink->name);
+        command_success_nodata(si, _("Servers linked from %s: %u"), name, (unsigned int)s->children.count);
+    }
+    command_success_nodata(si, _("End of server info."));
+}
+
 
 static void ss_cmd_server_count(sourceinfo_t * si, int parc, char *parv[])
 {
