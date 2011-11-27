6eb3d1b0d8ba82d59bc847fcfe6bd26e28b1e24c
William Pitcock <nenolod@dereferenced.org>
2011-11-26 23:55:17 -0600

    b64test: Test return values.

--- src/base64/main.c
+++ src/base64/main.c
@@ -7,8 +7,9 @@ int main(int argc, const char *argv)
 	char b64pristine[] = "ChrisTest\0ChrisTest\0pmzjgulxayebcpbwqqdh";
 	int rc;
 
-	base64_decode(b64, b64out, BUFSIZE);
+	rc = base64_decode(b64, b64out, BUFSIZE);
 
+	printf("decode: %d sz: %zu\n", rc, sizeof(b64pristine));
 	printf("b64pri: %s:%s:%s\n", b64pristine, b64pristine + 10, b64pristine + 20);
 	printf("b64out: %s:%s:%s\n", b64out, b64out + 10, b64out + 20);
 
