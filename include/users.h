atheme/atheme

commit c14ab567552c0053cb4c5d184956d0a1a7a5d19f

libathemecore: move user_t static buffers to strshare framework
This has the benefit of reducing the memory footprint of the user_t object somewhat.
The amount of memory reduction is very dependent on the amount of string duplication,
which can actually change from user to user.

include/users.h
@@ -13,14 +13,14 @@ struct user_
 {
 	object_t parent;
 
-	char nick[NICKLEN];
-	char user[USERLEN];
-	char host[HOSTLEN]; /* Real host */
-	char gecos[GECOSLEN];
-	char chost[HOSTLEN]; /* Cloaked host */
-	char vhost[HOSTLEN]; /* Visible host */
-	char uid[IDLEN]; /* Used for TS6, P10, IRCNet ircd. */
-	char ip[HOSTIPLEN];
+	char *nick;
+	char *user;
+	char *host; /* Real host */
+	char *gecos;
+	char *chost; /* Cloaked host */
+	char *vhost; /* Visible host */
+	char *uid; /* Used for TS6, P10, IRCNet ircd. */
+	char *ip;
 
 	mowgli_list_t channels;
 

libathemecore/users.c
@@ -155,19 +155,19 @@ user_t *user_add(const char *nick, const char *user, const char *host,
 
 	if (uid != NULL)
 	{
-		mowgli_strlcpy(u->uid, uid, IDLEN);
+		u->uid = strshare_get(uid);
 		mowgli_patricia_add(uidlist, u->uid, u);
 	}
 
-	mowgli_strlcpy(u->nick, nick, NICKLEN);
-	mowgli_strlcpy(u->user, user, USERLEN);
-	mowgli_strlcpy(u->host, host, HOSTLEN);
-	mowgli_strlcpy(u->gecos, gecos, GECOSLEN);
-	mowgli_strlcpy(u->chost, vhost ? vhost : host, HOSTLEN);
-	mowgli_strlcpy(u->vhost, vhost ? vhost : host, HOSTLEN);
+	u->nick = strshare_get(nick);
+	u->user = strshare_get(user);
+	u->host = strshare_get(host);
+	u->gecos = strshare_get(gecos);
+	u->chost = strshare_get(vhost ? vhost : host);
+	u->vhost = strshare_get(vhost ? vhost : host);
 
 	if (ip && strcmp(ip, "0") && strcmp(ip, "0.0.0.0") && strcmp(ip, "255.255.255.255"))
-		mowgli_strlcpy(u->ip, ip, HOSTIPLEN);
+		u->ip = strshare_get(ip);
 
 	u->server = server;
 	u->server->users++;
@@ -267,6 +267,15 @@ void user_delete(user_t *u, const char *comment)
 		u->myuser = NULL;
 	}
 
+	strshare_unref(u->uid);
+	strshare_unref(u->nick);
+	strshare_unref(u->user);
+	strshare_unref(u->host);
+	strshare_unref(u->gecos);
+	strshare_unref(u->vhost);
+	strshare_unref(u->chost);
+	strshare_unref(u->ip);
+
 	mowgli_heap_free(user_heap, u);
 
 	cnt.user--;
@@ -358,7 +367,8 @@ void user_changeuid(user_t *u, const char *uid)
 	if (*u->uid)
 		mowgli_patricia_delete(uidlist, u->uid);
 
-	mowgli_strlcpy(u->uid, uid ? uid : "", IDLEN);
+	strshare_unref(u->uid);
+	u->uid = strshare_get(uid);
 
 	if (*u->uid)
 		mowgli_patricia_add(uidlist, u->uid, u);
@@ -479,7 +489,9 @@ bool user_changenick(user_t *u, const char *nick, time_t ts)
 		mn->lastseen = CURRTIME;
 	mowgli_patricia_delete(userlist, u->nick);
 
-	mowgli_strlcpy(u->nick, nick, NICKLEN);
+	strshare_unref(u->nick);
+	u->nick = strshare_get(nick);
+
 	u->ts = ts;
 
 	mowgli_patricia_add(userlist, u->nick, u);

commit 5f64c859cba285f4207a08cc4f02dc450a5b600d

libathemecore: Fix NULL dereference when talking to IRCds without UID support.
Fixes regression caused by c14ab567552c0053cb4c5d184956d0a1a7a5d19f.

include/users.h
@@ -55,7 +55,7 @@ struct user_
 #define UF_WASENFORCED 0x00002000 /* this user was FNCed once already */
 #define UF_DEAF        0x00004000 /* user does not receive channel msgs */
 
-#define CLIENT_NAME(user)	((user)->uid[0] ? (user)->uid : (user)->nick)
+#define CLIENT_NAME(user)	((user)->uid != NULL ? (user)->uid : (user)->nick)
 
 typedef struct {
 	user_t *u;		/* User in question. Write NULL here if you delete the user. */

libathemecore/services.c
@@ -245,9 +245,9 @@ void services_init(void)
 
 	MOWGLI_PATRICIA_FOREACH(svs, &state, services_name)
 	{
-		if (ircd->uses_uid && svs->me->uid[0] == '\0')
+		if (ircd->uses_uid && svs->me->uid == NULL)
 			user_changeuid(svs->me, uid_get());
-		else if (!ircd->uses_uid && svs->me->uid[0] != '\0')
+		else if (!ircd->uses_uid && svs->me->uid != NULL)
 			user_changeuid(svs->me, NULL);
 		if (!ircd->uses_uid)
 			kill_id_sts(NULL, svs->nick, "Attempt to use service nick");
@@ -311,7 +311,7 @@ void reintroduce_user(user_t *u)
 	/* Reintroduce with a new UID.  This avoids problems distinguishing
 	 * commands targeted at the old and new user.
 	 */
-	if (*u->uid)
+	if (u->uid != NULL)
 	{
 		user_changeuid(u, uid_get());
 	}

libathemecore/users.c
@@ -134,7 +134,7 @@ user_t *user_add(const char *nick, const char *user, const char *host,
 		{
 			wallops("Server %s is introducing nick %s which already exists on %s",
 					server->name, nick, u2->server->name);
-			if (uid != NULL && *u2->uid != '\0')
+			if (uid != NULL && u2->uid != NULL)
 			{
 				kill_id_sts(NULL, uid, "Ghost detected via nick collision (new)");
 				kill_id_sts(NULL, u2->uid, "Ghost detected via nick collision (old)");
@@ -244,7 +244,7 @@ void user_delete(user_t *u, const char *comment)
 
 	mowgli_patricia_delete(userlist, u->nick);
 
-	if (*u->uid)
+	if (u->uid != NULL)
 		mowgli_patricia_delete(uidlist, u->uid);
 
 	mowgli_node_delete(&u->snode, &u->server->userlist);
@@ -364,13 +364,13 @@ void user_changeuid(user_t *u, const char *uid)
 {
 	return_if_fail(u != NULL);
 
-	if (*u->uid)
+	if (u->uid != NULL)
 		mowgli_patricia_delete(uidlist, u->uid);
 
 	strshare_unref(u->uid);
 	u->uid = strshare_get(uid);
 
-	if (*u->uid)
+	if (u->uid != NULL)
 		mowgli_patricia_add(uidlist, u->uid, u);
 }
 
@@ -420,7 +420,7 @@ bool user_changenick(user_t *u, const char *nick, time_t ts)
 		slog(LG_INFO, "user_changenick(): nick collision on %s", nick);
 		if (u2->server == me.me)
 		{
-			if (*u->uid != '\0')
+			if (u->uid != NULL)
 			{
 				/* If the changing client has a UID, our
 				 * client will have a UID too and the
@@ -466,7 +466,7 @@ bool user_changenick(user_t *u, const char *nick, time_t ts)
 			wallops("Server %s is sending nick change from %s to %s which already exists on %s",
 					u->server->name, u->nick, nick,
 					u2->server->name);
-			if (*u->uid != '\0' && *u2->uid != '\0')
+			if (u->uid != NULL && u2->uid != NULL)
 			{
 				kill_id_sts(NULL, u->uid, "Ghost detected via nick change collision (new)");
 				kill_id_sts(NULL, u2->uid, "Ghost detected via nick change collision (old)");