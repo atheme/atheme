f4975467942ec121f66324d346902ce420c905ec
Jilles Tjoelker <jilles@stack.nl>
2011-12-04 21:48:48 +0100

    Make contrib/pythonxmlrpc.py work again.

--- contrib/pythonxmlrpc.py
+++ contrib/pythonxmlrpc.py
@@ -5,4 +5,4 @@ import xmlrpclib
 
 atheme = xmlrpclib.ServerProxy('http://127.0.0.1:8080/xmlrpc')
 
-print atheme.atheme.command('', '', '::1', 'ALIS', 'LIST', '*')
+print atheme.atheme.command('*', '*', '::1', 'ALIS', 'LIST', '*')
