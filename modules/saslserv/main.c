a28145e9217e9d42b9a1e107e8c163f09074b69e
William Pitcock <nenolod@dereferenced.org>
2012-01-09 22:53:36 -0600

    saslserv: with PUIDs, find_session() may be passed NULL and session objects may not have a UID yet when they are reclaimed

--- modules/saslserv/main.c
+++ modules/saslserv/main.c
@@ -111,10 +111,13 @@ sasl_session_t *find_session(char *uid)
 	sasl_session_t *p;
 	mowgli_node_t *n;
 
+	if (uid == NULL)
+		return NULL;
+
 	MOWGLI_ITER_FOREACH(n, sessions.head)
 	{
 		p = n->data;
-		if(!strcmp(p->uid, uid))
+		if(p->uid != NULL && !strcmp(p->uid, uid))
 			return p;
 	}
 
