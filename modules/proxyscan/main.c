2f4a6931d60cdeeb0fa290560f707b9b34115dd3
William Pitcock <nenolod@dereferenced.org>
2012-01-15 19:59:54 -0600

    proxyscan: new module (plumbing only)

---
+++ modules/proxyscan/main.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2012 William Pitcock <nenolod@dereferenced.org>.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "atheme.h"
+
+DECLARE_MODULE_V1("proxyscan/main", false, _modinit, _moddeinit, PACKAGE_VERSION, "William Pitcock <nenolod@dereferenced.org>");
+
+static service_t *proxyscan = NULL;
+
+void _modinit(module_t *m)
+{
+	proxyscan = service_add("proxyscan", NULL);
+}
+
+void _moddeinit(module_unload_intent_t intent)
+{
+	service_delete(proxyscan);
+}
