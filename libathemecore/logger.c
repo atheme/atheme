cb6112681897149d176e138009336cd2dd18c7ac
William Pitcock <nenolod@dereferenced.org>
2012-02-15 11:56:30 -0600

    logger: use service_t.botonly to filter out what channels get joined to a logchannel

--- libathemecore/logger.c
+++ libathemecore/logger.c
@@ -81,11 +81,9 @@ static void logfile_join_service(service_t *svs)
 
 	return_if_fail(svs != NULL && svs->me != NULL);
 
-#ifdef NOTYET
 	/* no botserv bots */
-	if (svs->conf_table == NULL)
+	if (svs->botonly)
 		return;
-#endif
 
 	MOWGLI_ITER_FOREACH(n, log_files.head)
 	{
