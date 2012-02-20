15609080d68643af2827d6bd8230f68190f4ca6a
William Pitcock <nenolod@dereferenced.org>
2012-02-19 22:05:07 -0600

    doc/WINDOWS: we don't need libgcc anymore cause msys is gone

--- mingw-build.sh
+++ mingw-build.sh
@@ -2,16 +2,12 @@
 
 echo "Temp image will be staged to $1"
 
-./configure --prefix=. --disable-nls --enable-contrib
+./configure --prefix=/c/atheme --disable-nls --enable-contrib
 make -j16
 make install DESTDIR=$1
 
-# move dependencies around
-mv $1/lib/* $1/bin
-rm -rf $1/lib
-
-# we need libregex from msys
-mv /mingw/msys/1.0/lib/libregex.dll $1/bin
+# stage
+cp /mingw/bin/libgnurx-0.dll $1/c/atheme/bin
 
 # done!
 echo "Image built at $1"
