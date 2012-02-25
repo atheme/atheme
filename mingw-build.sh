3e2343ba55f95b4339e5ce46accde54e8a6b9c5d
William Pitcock <nenolod@dereferenced.org>
2012-02-25 14:02:01 -0600

    mingw-build: --disable-nls no longer needed

--- mingw-build.sh
+++ mingw-build.sh
@@ -2,12 +2,14 @@
 
 echo "Temp image will be staged to $1"
 
-./configure --prefix=/c/atheme --disable-nls --enable-contrib
+./configure --prefix=/c/atheme --enable-contrib
 make -j16
 make install DESTDIR=$1
 
 # stage
 cp /mingw/bin/libgnurx-0.dll $1/c/atheme/bin
+cp /mingw/bin/libeay32.dll $1/c/atheme/bin
+cp /mingw/bin/libintl-8.dll $1/c/atheme/bin
 
 # done!
 echo "Image built at $1"
