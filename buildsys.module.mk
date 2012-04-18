df690163465103c493aa73be7ef688af6827ecd3
William Pitcock <nenolod@dereferenced.org>
2012-04-18 15:49:05 +0000

    Update buildsys for make V=1 feature.

--- buildsys.module.mk
+++ buildsys.module.mk
@@ -8,13 +8,24 @@ PLUGIN=${SRCS:.c=$(PLUGIN_SUFFIX)}
 install: $(PLUGIN)
 
 .c$(PLUGIN_SUFFIX):
-	${COMPILE_STATUS}
-	if ${CC} -MMD -MP ${CFLAGS} ${PLUGIN_CFLAGS} ${CPPFLAGS} ${PLUGIN_LDFLAGS} ${LDFLAGS} -o $@ $< ${LIBS}; then \
+	COMPILER=${CC}; \
+	COMPILER_FLAGS="-MMD -MP ${CFLAGS} ${PLUGIN_CFLAGS} ${CPPFLAGS} ${PLUGIN_LDFLAGS} ${LDFLAGS} -o $@ $< ${LIBS}"; \
+	${COMPILE_STATUS}; \
+	if $${COMPILER} $${COMPILER_FLAGS}; then \
 		${COMPILE_OK}; \
 	else \
 		${COMPILE_FAILED}; \
 	fi
 
+ifndef V
+
 COMPILE_OK = printf "\033[K\033[0;32mSuccessfully compiled \033[1;32m$<\033[0;32m as \033[1;32m$@\033[0;32m.\033[0m\n"
+
+else
+
+COMPILE_STATUS = printf "CompileModule: $$COMPILER $$COMPILER_FLAGS\n"
+COMPILE_OK = true
+
+endif
 
 -include *.d
