a76a2f89a600dcb08898449188688905f148e6de
William Pitcock <nenolod@dereferenced.org>
2012-04-19 18:41:08 +0000

    buildsys: avoid using shell even more (only used for linking now)

--- buildsys.module.mk
+++ buildsys.module.mk
@@ -1,5 +1,4 @@
 # Additional extensions for building single-file modules.
-
 .SUFFIXES: $(PLUGIN_SUFFIX)
 
 plugindir = ${MODDIR}/modules/$(MODULE)
@@ -8,22 +7,8 @@ PLUGIN=${SRCS:.c=$(PLUGIN_SUFFIX)}
 all: $(PLUGIN)
 install: $(PLUGIN)
 
+phase_cmd_cc_plugin = CompileModule
+
 .c$(PLUGIN_SUFFIX):
-	COMPILER=${CC}; \
-	COMPILER_FLAGS="${PLUGIN_CFLAGS} ${CFLAGS} ${CPPFLAGS} ${PLUGIN_LDFLAGS} ${LDFLAGS} -o $@ $< ${LIBS}"; \
-	${COMPILE_STATUS}; \
-	$${COMPILER} $${COMPILER_FLAGS}
-
-ifndef V
-
-COMPILE_STATUS = printf "CompileModule: $@\n"
-COMPILE_OK = true
-
-else
-
-COMPILE_STATUS = printf "CompileModule: $$COMPILER $$COMPILER_FLAGS\n"
-COMPILE_OK = true
-
-endif
-
--include *.d
+	$(call echo-cmd,cmd_cc_plugin)
+	$(cmd_cc_plugin)
