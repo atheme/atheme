41775fd630b94f61dffab29b1157c3e464d6f12d
William Pitcock <nenolod@dereferenced.org>
2012-10-04 04:54:03 +0000

    abirev: stringref gives us API level 71, ABI revision 0.

--- include/abirev.h
+++ include/abirev.h
@@ -16,7 +16,7 @@
  * digits and set the rest to 0 (e.g. 330000). Otherwise, increment
  * the lower digits.
  */
-#define CURRENT_ABI_REVISION 700018
+#define CURRENT_ABI_REVISION 710000
 
 #endif
 
