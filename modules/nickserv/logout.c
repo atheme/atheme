5fd7386b18033dbce8535089c2b4a43ef7908f33
Jilles Tjoelker <jilles@stack.nl>
2012-12-20 21:47:42 +0100

    Revert "nickserv/logout: remove external logout"
    I will fix it in the next commit.
    
    This reverts commit 1aaa9e8f1d0b0b67b36c2a6318c71beaa7f39194.

--- modules/nickserv/logout.c
+++ modules/nickserv/logout.c
@@ -31,24 +31,68 @@ void _moddeinit(module_unload_intent_t intent)
 
 static void ns_cmd_logout(sourceinfo_t *si, int parc, char *parv[])
 {
+	user_t *u = si->su;
 	mowgli_node_t *n, *tn;
 	mynick_t *mn;
+	char *user = parv[0];
+	char *pass = parv[1];
 
-	if (!si->smu)
+	if ((!si->smu) && (!user || !pass))
 	{
 		command_fail(si, fault_nochange, _("You are not logged in."));
 		return;
 	}
 
+	if (user)
+	{
+		u = user_find(user);
+
+		if (!u)
+		{
+			command_fail(si, fault_nosuch_target, _("\2%s\2 is not online."), user);
+			return;
+		}
+
+		if (!u->myuser)
+		{
+			command_fail(si, fault_nosuch_target, _("\2%s\2 is not logged in."), user);
+			return;
+		}
+
+		if (u->myuser == si->smu || (pass != NULL && verify_password(u->myuser, pass)))
+			notice(nicksvs.nick, u->nick, "You were logged out by \2%s\2.", si->su->nick);
+		else
+		{
+			command_fail(si, fault_authfail, _("Authentication failed. Invalid password for \2%s\2."), entity(u->myuser)->name);
+			bad_password(si, u->myuser);
+			return;
+		}
+	}
+	else if (si->su == NULL)
+	{
+		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "LOGOUT");
+		command_fail(si, fault_needmoreparams, _("Syntax: LOGOUT <target> <password>"));
+		return;
+	}
+
+
+	if (is_soper(si->smu))
+		logcommand(si, CMDLOG_ADMIN, "DESOPER: \2%s\2 as \2%s\2", si->su->nick, entity(si->smu)->name);
+
+	if (si->su != u)
+	{
+		logcommand(si, CMDLOG_LOGIN, "LOGOUT: \2%s\2", u->nick);
+		command_success_nodata(si, _("\2%s\2 has been logged out."), si->su->nick);
+	}
+	else
+	{
+		logcommand(si, CMDLOG_LOGIN, "LOGOUT");
+		command_success_nodata(si, _("You have been logged out."));
+	}
+
 	si->smu->lastlogin = CURRTIME;
 	if (si->su != NULL)
 	{
-		if (is_soper(si->smu))
-			logcommand(si, CMDLOG_ADMIN, "DESOPER: \2%s\2 as \2%s\2", si->su->nick, entity(si->smu)->name);
-
-		logcommand(si, CMDLOG_LOGIN, "LOGOUT");
-		command_success_nodata(si, _("You have been logged out."));
-
 		mn = mynick_find(si->su->nick);
 		if (mn != NULL && mn->owner == si->smu)
 			mn->lastseen = CURRTIME;
