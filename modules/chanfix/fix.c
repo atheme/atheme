e3facd96f034b1b85c49b73e00099254bfe14858
Rix Rixov <rix@veselba.net>
2013-02-28 22:11:12 +0200

    chanfix/fix: Fixed typo

--- modules/chanfix/fix.c
+++ modules/chanfix/fix.c
@@ -600,7 +600,7 @@ static void chanfix_cmd_help(sourceinfo_t *si, int parc, char *parv[])
 	if (!command)
 	{
 		command_success_nodata(si, _("***** \2%s Help\2 *****"), si->service->nick);
-		command_success_nodata(si, _("\2%s\2 allows for simple channel operator managment."), si->service->nick);
+		command_success_nodata(si, _("\2%s\2 allows for simple channel operator management."), si->service->nick);
 		command_success_nodata(si, " ");
 		command_success_nodata(si, _("For more information on a command, type:"));
 		command_success_nodata(si, "\2/%s%s help <command>\2", (ircd->uses_rcommand == false) ? "msg " : "", si->service->disp);
