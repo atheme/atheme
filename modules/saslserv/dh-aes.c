57720fe73b2b8f5ba19e06e2459a7c2f7f4ad0bd
Elizabeth Myers <elizabeth@interlinked.me>
2013-04-16 02:05:07 -0500

    Documentation tweak

--- modules/saslserv/dh-aes.c
+++ modules/saslserv/dh-aes.c
@@ -12,7 +12,9 @@
  * 3) CBC mode is used with AES. The IV is sent with the key by the client.
  *
  * Why this over ECDSA-NIST256p-CHALLENGE? Not all langs have good support for
- * ECDSA. DH and AES support, on the other hand, are relatively ubiqutious.
+ * ECDSA. DH and AES support, on the other hand, are relatively ubiqutious. It
+ * may also be usable in environments where the convenience of a password
+ * outweighs the benefits of using challenge-response.
  *
  * Padding structure (sizes in big-endian/network byte order):
  * Server sends: <size p><p><size g><g><size pubkey><pubkey>
