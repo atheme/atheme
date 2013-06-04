28cb1d6790efec7f5e04abc6a206535f0e4ed548
William Pitcock <nenolod@dereferenced.org>
2013-06-04 13:20:23 +0000

    protocol/inspircd: only check protocol version if available

--- modules/protocol/inspircd.c
+++ modules/protocol/inspircd.c
@@ -1454,6 +1454,9 @@ static void m_capab(sourceinfo_t *si, int parc, char *parv[])
 		has_shun = false;
 		has_svstopic_topiclock = false;
 		has_protocol = 0;
+
+		if (parc > 1)
+			has_protocol = atoi(parv[1]);
 	}
 	else if (strcasecmp(parv[0], "CAPABILITIES") == 0 && parc > 1)
 	{
@@ -1553,7 +1556,7 @@ static void m_capab(sourceinfo_t *si, int parc, char *parv[])
 			slog(LG_INFO, "m_capab(): you didn't load m_svshold into inspircd. nickname enforcers will not work.");
 		}
 
-		if (has_protocol < PROTOCOL_12BETA)
+		if (has_protocol && (has_protocol < PROTOCOL_12BETA))
 		{
 			slog(LG_ERROR, "m_capab(): remote protocol version too old (%d). you may need another protocol module or a newer inspircd. exiting.", has_protocol);
 			exit(EXIT_FAILURE);
