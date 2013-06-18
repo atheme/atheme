atheme/atheme

commit 1b29a70114ecf8fabd217ccf698808bcb53b385f

Require opers to use the FORCE option to view private channel flags

modules/chanserv/flags.c
@@ -94,18 +94,20 @@ static const char *get_template_name(mychan_t *mc, unsigned int level)
 	return iter.res;
 }
 
-static void do_list(sourceinfo_t *si, mychan_t *mc)
+static void do_list(sourceinfo_t *si, mychan_t *mc, bool operoverride)
 {
 	chanacs_t *ca;
 	mowgli_node_t *n;
-	bool operoverride = false;
 	const char *str1, *str2;
 	unsigned int i = 1;
 
-	if (!chanacs_source_has_flag(mc, si, CA_ACLVIEW))
+	if (!chanacs_source_has_flag(mc, si, CA_ACLVIEW) && !operoverride)
 	{
 		if (has_priv(si, PRIV_CHAN_AUSPEX))
-			operoverride = true;
+		{
+			command_fail(si, fault_noprivs, _("This channel's flags are private. Use the FORCE option to view them."));
+			return;
+		}
 		else
 		{
 			command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
@@ -175,7 +177,12 @@ static void cs_cmd_flags(sourceinfo_t *si, int parc, char *parv[])
 
 	if (!target)
 	{
-		do_list(si, mc);
+		do_list(si, mc, false);
+		return;
+	}
+	else if (!strcasecmp(target, "FORCE"))
+	{
+		do_list(si, mc, true);
 		return;
 	}
 
@@ -208,7 +215,7 @@ static void cs_cmd_flags(sourceinfo_t *si, int parc, char *parv[])
 	 */
 	else if (!strcasecmp(target, "LIST") && myentity_find_ext(target) == NULL)
 	{
-		do_list(si, mc);
+		do_list(si, mc, false);
 		free(target);
 
 		return;

help/default/cservice/flags
@@ -7,7 +7,11 @@ supports nicknames, groups and hostmasks as targets.
 When only the channel argument is given, a listing of
 permissions granted to users will be displayed.
 
-Syntax: FLAGS <#channel>
+Syntax: FLAGS <#channel> [FORCE]
+
+The FORCE option allows you to view the flag list regardless
+of whether you have the +A flag. This option is restricted
+to IRC Operators.
 
 Otherwise, an access entry is modified. A modification may be
 specified by a template name (changes the access to the

commit 5f323f61cf1d4ed81123f21b7ded639d257a0a72

chanserv/flags: Fix permissions issue with FORCE
1b29a70 introduced a new requirement that opers use FORCE when listing
channel flags if they wish to view flags they don't have access to, but
did not include a check to ensure only opers with chan:auspex can
specify FORCE and have it work.

modules/chanserv/flags.c
@@ -180,7 +180,7 @@ static void cs_cmd_flags(sourceinfo_t *si, int parc, char *parv[])
 		do_list(si, mc, false);
 		return;
 	}
-	else if (!strcasecmp(target, "FORCE"))
+	else if (!strcasecmp(target, "FORCE") && has_priv(si, PRIV_CHAN_AUSPEX))
 	{
 		do_list(si, mc, true);
 		return;