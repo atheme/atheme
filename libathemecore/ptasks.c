54b1d377e6053e85c2e3b1199f0d71701f6a20c9
Stephen Bennett <spb@exherbo.org>
2013-06-25 19:30:11 +0000

    Make flood klines use ip address instead of hostname

--- libathemecore/ptasks.c
+++ libathemecore/ptasks.c
@@ -921,7 +921,7 @@ int floodcheck(user_t *u, user_t *t)
 				kline_t *k;
 
 				/* kline them the third time */
-				k = kline_add("*", u->host, "ten minute ban - flooding services", 600, chansvs.nick);
+				k = kline_add("*", u->ip, "ten minute ban - flooding services", 600, chansvs.nick);
 
 				slog(LG_INFO, "FLOOD:KLINE: \2%s\2", u->nick);
 
