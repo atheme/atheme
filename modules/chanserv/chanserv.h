bd88435989edf9f187d38f4ca1c0632565bc1a28
Alex Iadicicco <alex@ajitek.net>
2013-11-15 17:24:31 -0700

    chanserv: Remove now-unneeded invert_purpose function

--- modules/chanserv/chanserv.h
+++ modules/chanserv/chanserv.h
@@ -13,31 +13,6 @@ static inline unsigned int custom_founder_check(void)
 		return CA_INITIAL & ca_all;
 }
 
-static inline bool invert_purpose(sourceinfo_t *si, int parc, char *chan, char **nick, char neg, cs_cmd_proto func)
-{
-	char *negargar[2];
-
-	if ((*nick)[0] != '+' && (*nick)[0] != '-')
-		return false;
-
-	if ((*nick)[1] == '+' || (*nick)[1] == '-' || (*nick)[1] == '\0')
-	{
-		command_fail(si, fault_badparams, _("Not a valid action: \2%s\2"), *nick);
-		return true;
-	}
-
-	if ((*nick)[0] == (neg != '+' ? '-' : '+'))
-	{
-		negargar[0] = chan;
-		negargar[1] = *nick + 1;
-		(*func)(si, parc, negargar);
-		return true;
-	}
-	else if((*nick)[0] == (neg == '+' ? '-' : '+'))
-		(*nick)++;
-	return false;
-}
-
 struct prefix_action
 {
 	bool en; /* en for "enable" */
