5fa150c3e4d6bcfdfc2cbd9651bc41ef910430f2
Alex Iadicicco <alex@ajitek.net>
2014-01-08 20:01:40 -0700

    api/http: Make some stylistic changes
       o  No semicolons after blocks
    
       o  No spaces at end of lines
    
       o  Indentation with tabs
    
       o  Declaration of HTTP function pointers as static

--- modules/api/http.h
+++ modules/api/http.h
@@ -3,21 +3,21 @@
 
 #include "http_common.h"
 
-http_client_t * (*http_client_new)(void);
-void (*http_parse_headers)(http_client_t *container, const char *http_response);
-int (*http_parse_url)(http_client_t *container, const char *url);
-void (*http_add_param)(http_client_t *http, const char *key, const char *value);
+static http_client_t * (*http_client_new)(void);
+static void (*http_parse_headers)(http_client_t *container, const char *http_response);
+static int (*http_parse_url)(http_client_t *container, const char *url);
+static void (*http_add_param)(http_client_t *http, const char *key, const char *value);
 //int (*http_get)(const char *url, http_cb_t cb, void *userdata);
-int (*http_get)(http_client_t *container, void *userdata, http_cb_t cb);
+static int (*http_get)(http_client_t *container, void *userdata, http_cb_t cb);
 
 static inline void use_http_symbols(module_t *m)
 {
-    MODULE_TRY_REQUEST_DEPENDENCY(m, "api/http");
-    MODULE_TRY_REQUEST_SYMBOL(m, http_client_new, "api/http", "http_client_new");
-    MODULE_TRY_REQUEST_SYMBOL(m, http_parse_headers, "api/http", "http_parse_headers");
-    MODULE_TRY_REQUEST_SYMBOL(m, http_parse_url, "api/http", "http_parse_url");
-    MODULE_TRY_REQUEST_SYMBOL(m, http_add_param, "api/http", "http_add_param");
-    MODULE_TRY_REQUEST_SYMBOL(m, http_get, "api/http","http_get");
+	MODULE_TRY_REQUEST_DEPENDENCY(m, "api/http");
+	MODULE_TRY_REQUEST_SYMBOL(m, http_client_new, "api/http", "http_client_new");
+	MODULE_TRY_REQUEST_SYMBOL(m, http_parse_headers, "api/http", "http_parse_headers");
+	MODULE_TRY_REQUEST_SYMBOL(m, http_parse_url, "api/http", "http_parse_url");
+	MODULE_TRY_REQUEST_SYMBOL(m, http_add_param, "api/http", "http_add_param");
+	MODULE_TRY_REQUEST_SYMBOL(m, http_get, "api/http","http_get");
 }
 
 #endif /* ATHEME_HTTP_H */
--- modules/api/http_common.h
+++ modules/api/http_common.h
@@ -9,24 +9,24 @@ typedef void (*http_cb_t)(http_client_t *http, void *userdata);
 
 struct http_client_
 {
-    char *url;     
-    char *uri;
-    char *protocol;
-    char *domain;
-    char *query_string;
-    int port;
-    int http_code;
+	char *url;
+	char *uri;
+	char *protocol;
+	char *domain;
+	char *query_string;
+	int port;
+	int http_code;
 
-    int error;
-    char *error_str;
+	int error;
+	char *error_str;
 
-    size_t data_len;
-    char *data;
-    mowgli_patricia_t *headers;
+	size_t data_len;
+	char *data;
+	mowgli_patricia_t *headers;
 
-    int fd;
-    http_cb_t callback;
-    void *userdata;
+	int fd;
+	http_cb_t callback;
+	void *userdata;
 };
 
 #endif /* ATHEME_HTTP_COMMON_H */
