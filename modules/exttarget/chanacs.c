491df599f4d19a1d1bee0222cb1ebff3e4e9b5ba
Alex Iadicicco <alex@ajitek.net>
2014-02-03 14:00:53 -0700

    exttarget/chanacs: Prevent recursion

--- modules/exttarget/chanacs.c
+++ modules/exttarget/chanacs.c
@@ -22,6 +22,7 @@ static mowgli_patricia_t **exttarget_tree = NULL;
 typedef struct {
 	myentity_t parent;
 	stringref channel;
+	int checking;
 } chanacs_exttarget_t;
 
 static chanacs_t *chanacs_ext_match_user(chanacs_t *ca, user_t *u)
@@ -32,10 +33,16 @@ static chanacs_t *chanacs_ext_match_user(chanacs_t *ca, user_t *u)
 
 	ent = (chanacs_exttarget_t *) ca->entity;
 
+	if (ent->checking > 4) /* arbitrary recursion limit? */
+		return NULL;
+
 	if (!(mc = mychan_find(ent->channel)))
 		return NULL;
 
+	ent->checking++;
 	flags = chanacs_user_flags(mc, u);
+	ent->checking--;
+
 	if (flags & CA_AKICK)
 		return NULL;
 
@@ -96,6 +103,7 @@ static myentity_t *chanacs_validate_f(const char *param)
 
 	ext = mowgli_heap_alloc(chanacs_ext_heap);
 	ext->channel = strshare_get(param);
+	ext->checking = 0;
 
 	/* name the entity... $chanacs:param */
 #define NAMEPREFIX "$chanacs:"
