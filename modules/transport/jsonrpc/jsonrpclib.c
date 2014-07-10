1630960dea69073e533dee41f95d18e9b6feea19
Errietta Kostala <errietta@errietta.me>
2014-07-10 15:57:34 +0100

    Fix intending

--- modules/transport/jsonrpc/jsonrpclib.c
+++ modules/transport/jsonrpc/jsonrpclib.c
@@ -38,8 +38,6 @@ void jsonrpc_process(char *buffer, void *userdata)
 		return;
 	}
 
-
-
 	mowgli_json_t *method = mowgli_patricia_retrieve(obj, "method");
 	mowgli_json_t *params = mowgli_patricia_retrieve(obj, "params");
 	mowgli_json_t *id = mowgli_patricia_retrieve(obj, "id");
@@ -68,7 +66,7 @@ void jsonrpc_process(char *buffer, void *userdata)
 
 	jsonrpc_method_t call_method = get_json_method(method_str);
 
-    MOWGLI_LIST_FOREACH(n, params_list->head)
+	MOWGLI_LIST_FOREACH(n, params_list->head)
 	{
 		param = n->data;
 
@@ -154,74 +152,74 @@ char *jsonrpc_normalizeBuffer(const char *buf)
 	{
 		switch (buf[i])
 		{
-			  /* ctrl char */
-		  case 1:
-			  break;
-			  /* Bold ctrl char */
-		  case 2:
-			  break;
-			  /* Color ctrl char */
-		  case 3:
-			  /* If the next character is a digit, its also removed */
-			  if (isdigit(buf[i + 1]))
-			  {
-				  i++;
+			/* ctrl char */
+			case 1:
+				break;
+				/* Bold ctrl char */
+			case 2:
+				break;
+				/* Color ctrl char */
+			case 3:
+				/* If the next character is a digit, its also removed */
+				if (isdigit(buf[i + 1]))
+				{
+					i++;
 
-				  /* not the best way to remove colors
-				   * which are two digit but no worse then
-				   * how the Unreal does with +S - TSL
-				   */
-				  if (isdigit(buf[i + 1]))
-				  {
-					  i++;
-				  }
+					/* not the best way to remove colors
+					 * which are two digit but no worse then
+					 * how the Unreal does with +S - TSL
+					 */
+					if (isdigit(buf[i + 1]))
+					{
+						i++;
+					}
 
-				  /* Check for background color code
-				   * and remove it as well
-				   */
-				  if (buf[i + 1] == ',')
-				  {
-					  i++;
+					/* Check for background color code
+					 * and remove it as well
+					 */
+					if (buf[i + 1] == ',')
+					{
+						i++;
 
-					  if (isdigit(buf[i + 1]))
-					  {
-						  i++;
-					  }
-					  /* not the best way to remove colors
-					   * which are two digit but no worse then
-					   * how the Unreal does with +S - TSL
-					   */
-					  if (isdigit(buf[i + 1]))
-					  {
-						  i++;
-					  }
-				  }
-			  }
+						if (isdigit(buf[i + 1]))
+						{
+							i++;
+						}
+						/* not the best way to remove colors
+						 * which are two digit but no worse then
+						 * how the Unreal does with +S - TSL
+						 */
+						if (isdigit(buf[i + 1]))
+						{
+							i++;
+						}
+					}
+				}
 
-			  break;
-			  /* tabs char */
-		  case 9:
-			  break;
-			  /* line feed char */
-		  case 10:
-			  break;
-			  /* carrage returns char */
-		  case 13:
-			  break;
-			  /* Reverse ctrl char */
-		  case 22:
-			  break;
-			  /* Underline ctrl char */
-		  case 31:
-			  break;
-			  /* A valid char gets copied into the new buffer */
-		  default:
-			  /* All valid <32 characters are handled above. */
-			  if (buf[i] > 31)
-			  {
-				newbuf[j] = buf[i];
-				j++;
-			  }
+				break;
+				/* tabs char */
+			case 9:
+				break;
+				/* line feed char */
+			case 10:
+				break;
+				/* carrage returns char */
+			case 13:
+				break;
+				/* Reverse ctrl char */
+			case 22:
+				break;
+				/* Underline ctrl char */
+			case 31:
+				break;
+				/* A valid char gets copied into the new buffer */
+			default:
+				/* All valid <32 characters are handled above. */
+				if (buf[i] > 31)
+				{
+					newbuf[j] = buf[i];
+					j++;
+				}
 		}
 	}
 
