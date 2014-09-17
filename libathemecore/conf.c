atheme/atheme

commit 25a1f71c44cca3d222977ee07c639ebcc5353016

libathemecore/conf: warn about spaces in passwords
Closes #358

libathemecore/conf.c
@@ -370,6 +370,11 @@ static int c_uplink(mowgli_config_file_entry_t *ce)
 		}
 	}
 
+	if (strchr(send_password, ' '))
+		conf_report_warning(ce, "send_password for uplink %s is invalid (has spaces); continuing anyway", name);
+	if (strchr(receive_password, ' '))
+		conf_report_warning(ce, "receive_password for uplink %s is invalid (has spaces); continuing anyway", name);
+
 	uplink_add(name, host, send_password, receive_password, vhost, port);
 	return 0;
 }

commit f4b6ff9ccb7cc2e60eb54372f98f76466f0544b6

libathemecore/conf: Fix crash when receive_pass or send_pass does not exist, introduced in 25a1f71c44
CID: 1238965 and 1238967

libathemecore/conf.c
@@ -370,9 +370,9 @@ static int c_uplink(mowgli_config_file_entry_t *ce)
 		}
 	}
 
-	if (strchr(send_password, ' '))
+	if ((send_password) && (strchr(send_password, ' ')))
 		conf_report_warning(ce, "send_password for uplink %s is invalid (has spaces); continuing anyway", name);
-	if (strchr(receive_password, ' '))
+	if ((receive_password) && (strchr(receive_password, ' ')))
 		conf_report_warning(ce, "receive_password for uplink %s is invalid (has spaces); continuing anyway", name);
 
 	uplink_add(name, host, send_password, receive_password, vhost, port);