4b6a8faa00381765161b0ffe4a8a3d78614ce9ee
Janik Kleinhoff <janik@kleinhoff.de>
2014-10-24 07:33:00 +0200

    chanserv/info: viewing PRIVATE info needs +A
    The rationale is that access to private information about a channel
    requires the +A flag; PRIVATE and PUBACL allow making adjustments to
    what is considered private. With PRIVATE set, most info requires +A.
    
    However, on channels with PUBACL set, we display the founder and
    successor to everyone as they could just check the ACL themselves
    anyway.

--- modules/chanserv/info.c
+++ modules/chanserv/info.c
@@ -40,7 +40,7 @@ static void cs_cmd_info(sourceinfo_t *si, int parc, char *parv[])
 	metadata_t *md;
 	mowgli_patricia_iteration_state_t state;
 	hook_channel_req_t req;
-	bool hide_info;
+	bool hide_info, hide_acl;
 
 	if (!name)
 	{
@@ -69,19 +69,21 @@ static void cs_cmd_info(sourceinfo_t *si, int parc, char *parv[])
 	}
 
 	hide_info = use_channel_private && mc->flags & MC_PRIVATE &&
-		!(mc->flags & MC_PUBACL) && !chanacs_source_has_flag(mc, si, CA_ACLVIEW) &&
+		!chanacs_source_has_flag(mc, si, CA_ACLVIEW) &&
 		!has_priv(si, PRIV_CHAN_AUSPEX);
+	hide_acl = !chanacs_source_has_flag(mc, si, CA_ACLVIEW) &&
+		!has_priv(si, PRIV_CHAN_AUSPEX) &&
+		!(mc->flags & MC_PUBACL);
 
 	tm = *localtime(&mc->registered);
 	strftime(strfbuf, sizeof strfbuf, TIME_FORMAT, &tm);
 
 	command_success_nodata(si, _("Information on \2%s\2:"), mc->name);
 
-	if (!hide_info)
+	if (!(hide_info && hide_acl))
 		command_success_nodata(si, _("Founder    : %s"), mychan_founder_names(mc));
 
-	if ((!(mc->flags & MC_PUBACL) && chanacs_source_has_flag(mc, si, CA_ACLVIEW)) ||
-		has_priv(si, PRIV_CHAN_AUSPEX))
+	if (!hide_acl)
 	{
 		mu = mychan_pick_successor(mc);
 		if (mu != NULL)
