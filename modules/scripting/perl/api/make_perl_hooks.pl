5badf2bf989b78591d75a5e568773b2d7fe258b2
Shockk <shokku.ra@gmail.com>
2015-02-11 17:06:23 +0000

    scripting/perl: Fix lack of perl_hooks.c ordering
    This patch resolves the contents of perl_hooks.c being different between
    identical compilations due to Perl hashes being looped over in an
    intentionally unique order each execution.

--- modules/scripting/perl/api/make_perl_hooks.pl
+++ modules/scripting/perl/api/make_perl_hooks.pl
@@ -204,7 +204,7 @@ EOF
 # Write the hook-data marshalling functions.
 #
 
-foreach my $arg_type (keys %arg_types) {
+foreach my $arg_type (sort keys %arg_types) {
 	next if ($arg_type eq 'void');
 
 	if (defined $perl_api_types{$arg_type}) {
@@ -231,7 +231,8 @@ EOF
 
 		my @members;
 
-		while (my ($member, $definition) = each %$struct) {
+		foreach my $member (sort keys %$struct) {
+			my $definition = $struct->{$member};
 			my ($type, $pretty_name, $rw);
 
 			if (ref $definition eq 'ARRAY') {
@@ -301,7 +302,8 @@ EOF
 # Now write the actual hook handlers.
 #
 
-while (my ($hookname, $arg_type) = each %hooks) {
+foreach my $hookname (sort keys %hooks) {
+	my $arg_type = $hooks{$hookname};
 	next if grep { $_ eq $arg_type } @special_types;
 	print $outfile <<"EOF";
 static void perl_hook_$hookname ($arg_type * data)
@@ -346,7 +348,7 @@ print $outfile <<EOF;
 void enable_perl_hook_handler(const char *hookname)
 {
 EOF
-foreach my $hookname (keys %hooks) {
+foreach my $hookname (sort keys %hooks) {
 	print $outfile <<"EOF";
 	if (0 == strcmp(hookname, "$hookname")) {
 		hook_add_$hookname(perl_hook_$hookname);
@@ -363,7 +365,7 @@ print $outfile <<EOF;
 void disable_perl_hook_handler(const char *hookname)
 {
 EOF
-foreach my $hookname (keys %hooks) {
+foreach my $hookname (sort keys %hooks) {
 	print $outfile <<"EOF";
 	if (0 == strcmp(hookname, "$hookname")) {
 		hook_del_$hookname(perl_hook_$hookname);
