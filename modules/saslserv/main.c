c41014b7a1e79320e82cf3f9c83dc1a1b90dd5ff
Mantas Mikulėnas <grawity@gmail.com>
2015-03-04 00:20:09 +0200

    saslserv: clean up the 'if' mess in sasl_input()
    (cherry picked from commit 5e5d08c5eb4d446594422e14159c20d3089b46c4)

--- modules/saslserv/main.c
+++ modules/saslserv/main.c
@@ -250,62 +250,64 @@ static void sasl_input(sasl_message_t *smsg)
 	char *tmpbuf;
 	int tmplen;
 
-	/* Abort packets, or maybe some other kind of (D)one */
-	if(smsg->mode == 'D')
+	switch(smsg->mode)
 	{
-		destroy_session(p);
-		return;
-	}
-
-	if(smsg->mode == 'H')
-	{
+	case 'H':
+		/* (H)ost information */
 		p->host = sstrdup(smsg->buf);
 		p->ip   = sstrdup(smsg->ext);
 		return;
-	}
 
-	if(smsg->mode != 'S' && smsg->mode != 'C')
-		return;
-
-	if(smsg->mode == 'S' && smsg->ext != NULL &&
-			!strcmp(smsg->buf, "EXTERNAL"))
-	{
-		free(p->certfp);
-		p->certfp = sstrdup(smsg->ext);
-	}
-
-	if(p->buf == NULL)
-	{
-		p->buf = (char *)malloc(len + 1);
-		p->p = p->buf;
-		p->len = len;
-	}
-	else
-	{
-		if(p->len + len + 1 > 8192) /* This is a little much... */
+	case 'S':
+		/* (S)tart authentication */
+		if(smsg->mode == 'S' && smsg->ext != NULL && !strcmp(smsg->buf, "EXTERNAL"))
 		{
-			sasl_sts(p->uid, 'D', "F");
-			destroy_session(p);
-			return;
+			free(p->certfp);
+			p->certfp = sstrdup(smsg->ext);
+		}
+		/* fallthrough to 'C' */
+
+	case 'C':
+		/* (C)lient data */
+		if(p->buf == NULL)
+		{
+			p->buf = (char *)malloc(len + 1);
+			p->p = p->buf;
+			p->len = len;
+		}
+		else
+		{
+			if(p->len + len + 1 > 8192) /* This is a little much... */
+			{
+				sasl_sts(p->uid, 'D', "F");
+				destroy_session(p);
+				return;
+			}
+
+			p->buf = (char *)realloc(p->buf, p->len + len + 1);
+			p->p = p->buf + p->len;
+			p->len += len;
 		}
 
-		p->buf = (char *)realloc(p->buf, p->len + len + 1);
-		p->p = p->buf + p->len;
-		p->len += len;
-	}
+		memcpy(p->p, smsg->buf, len);
 
-	memcpy(p->p, smsg->buf, len);
+		/* Messages not exactly 400 bytes are the end of a packet. */
+		if(len < 400)
+		{
+			p->buf[p->len] = '\0';
+			tmpbuf = p->buf;
+			tmplen = p->len;
+			p->buf = p->p = NULL;
+			p->len = 0;
+			sasl_packet(p, tmpbuf, tmplen);
+			free(tmpbuf);
+		}
+		return;
 
-	/* Messages not exactly 400 bytes are the end of a packet. */
-	if(len < 400)
-	{
-		p->buf[p->len] = '\0';
-		tmpbuf = p->buf;
-		tmplen = p->len;
-		p->buf = p->p = NULL;
-		p->len = 0;
-		sasl_packet(p, tmpbuf, tmplen);
-		free(tmpbuf);
+	case 'D':
+		/* (D)one -- when we receive it, means client abort */
+		destroy_session(p);
+		return;
 	}
 }
 
