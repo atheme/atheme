a07c5280a3b34ffcc399117597dd39c3706bfa33
Janik Kleinhoff <ilbelkyr@shalture.org>
2016-07-17 03:58:56 +0000

    Fix various typos

--- include/phandler.h
+++ include/phandler.h
@@ -165,10 +165,10 @@ E void (*qline_sts)(const char *server, const char *mask, long duration, const c
  * if the ircd requires unqlines to be sent from users, use opersvs */
 E void (*unqline_sts)(const char *server, const char *mask);
 /* add a dline (sometimes called zline) on the servers matching mask
- * if the ircd requires zlines to be sent from users, use opersvs */
+ * if the ircd requires dlines to be sent from users, use opersvs */
 E void (*dline_sts)(const char *server, const char *host, long duration, const char *reason);
 /* remove a dline (sometimes called zline) on the servers matching the given mask
- * if the ircd requires unqlines to be sent from users, use opersvs */
+ * if the ircd requires undlines to be sent from users, use opersvs */
 E void (*undline_sts)(const char *server, const char *host);
 /* make the given service set a topic on a channel
  * setter and ts should be used if the ircd supports topics to be set
