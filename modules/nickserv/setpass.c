e5ce76c8e6c20f12445b35e23e0ae54eb97eb1e0
Max Teufel <max@teufelsnetz.com>
2016-09-24 16:03:43 +0200

    nickserv/{setpass,sendpass_user}: fix a typo breaking the build

--- modules/nickserv/setpass.c
+++ modules/nickserv/setpass.c
@@ -65,7 +65,7 @@ static void ns_cmd_setpass(sourceinfo_t *si, int parc, char *parv[])
 		return;
 	}
 
-	if (si->mu == mu)
+	if (si->smu == mu)
 	{
 		command_fail(si, fault_already_authed, _("You are logged in and can change your password using the SET PASSWORD command."));
 		return;
