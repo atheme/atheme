2c48ea00143f524918d42311cf650c7dcbc45feb
Aaron Jones <aaronmdjones@gmail.com>
2017-10-09 00:09:22 +0000

    modules/nickserv/sendpass_user: don't assume crypt_string() will always succeed

--- modules/nickserv/sendpass_user.c
+++ modules/nickserv/sendpass_user.c
@@ -130,14 +130,23 @@ static void ns_cmd_sendpass(sourceinfo_t *si, int parc, char *parv[])
 	}
 
 	key = random_string(12);
+
+	const char *const hash = crypt_string(key, NULL);
+
+	if (!hash)
+	{
+		command_fail(si, fault_internalerror, _("Hash generation for password change key failed."));
+		free(key);
+		return;
+	}
 	if (sendemail(si->su != NULL ? si->su : si->service->me, mu, EMAIL_SETPASS, mu->email, key))
 	{
 		if (ismarked)
 			wallops("%s sent the password for the \2MARKED\2 account %s.", get_oper_name(si), entity(mu)->name);
 		logcommand(si, CMDLOG_ADMIN, "SENDPASS: \2%s\2 (change key)", name);
-		metadata_add(mu, "private:setpass:key", crypt_string(key, gen_salt()));
 		metadata_add(mu, "private:sendpass:sender", get_oper_name(si));
 		metadata_add(mu, "private:sendpass:timestamp", number_to_string(time(NULL)));
+		metadata_add(mu, "private:setpass:key", hash);
 		command_success_nodata(si, _("The password change key for \2%s\2 has been sent to the corresponding email address."), entity(mu)->name);
 	}
 	else
