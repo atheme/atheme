a6c1b6a7603b2b5ae2645f3ad2c88d124997b6d9
Aaron Jones <aaronmdjones@gmail.com>
2017-10-22 17:28:35 +0000

    modules/crypto/pbkdf2v2: move SCRAM key derivation to function

--- modules/crypto/pbkdf2v2.c
+++ modules/crypto/pbkdf2v2.c
@@ -219,6 +219,32 @@ parsed:
 	return true;
 }
 
+static bool
+atheme_pbkdf2v2_scram_derive(const struct pbkdf2v2_parameters *const parsed,
+                             unsigned char csk[EVP_MAX_MD_SIZE],
+                             unsigned char chk[EVP_MAX_MD_SIZE])
+{
+	unsigned char cck[EVP_MAX_MD_SIZE];
+
+	if (csk && ! HMAC(parsed->md, parsed->cdg, (int) parsed->dl, ServerKeyStr, sizeof ServerKeyStr, csk, NULL))
+	{
+		(void) slog(LG_ERROR, "%s: HMAC() failed for csk", __func__);
+		return false;
+	}
+	if (chk && ! HMAC(parsed->md, parsed->cdg, (int) parsed->dl, ClientKeyStr, sizeof ClientKeyStr, cck, NULL))
+	{
+		(void) slog(LG_ERROR, "%s: HMAC() failed for cck", __func__);
+		return false;
+	}
+	if (chk && EVP_Digest(cck, parsed->dl, chk, NULL, parsed->md, NULL) != 1)
+	{
+		(void) slog(LG_ERROR, "%s: EVP_Digest(cck) failed for chk", __func__);
+		return false;
+	}
+
+	return true;
+}
+
 static const char *
 atheme_pbkdf2v2_salt(void)
 {
@@ -259,26 +285,14 @@ atheme_pbkdf2v2_crypt(const char *const restrict password, const char *const res
 	if (parsed.scram)
 	{
 		unsigned char csk[EVP_MAX_MD_SIZE];
-		unsigned char cck[EVP_MAX_MD_SIZE];
 		unsigned char chk[EVP_MAX_MD_SIZE];
 		char csk64[EVP_MAX_MD_SIZE * 3];
 		char chk64[EVP_MAX_MD_SIZE * 3];
 
-		if (! HMAC(parsed.md, parsed.cdg, (int) parsed.dl, ServerKeyStr, sizeof ServerKeyStr, csk, NULL))
-		{
-			(void) slog(LG_ERROR, "%s: HMAC() failed for csk", __func__);
-			return NULL;
-		}
-		if (! HMAC(parsed.md, parsed.cdg, (int) parsed.dl, ClientKeyStr, sizeof ClientKeyStr, cck, NULL))
-		{
-			(void) slog(LG_ERROR, "%s: HMAC() failed for cck", __func__);
-			return NULL;
-		}
-		if (EVP_Digest(cck, parsed.dl, chk, NULL, parsed.md, NULL) != 1)
-		{
-			(void) slog(LG_ERROR, "%s: EVP_Digest(cck) failed for chk", __func__);
-			return NULL;
-		}
+		if (! atheme_pbkdf2v2_scram_derive(&parsed, csk, chk))
+			// This function logs messages on failure
+			return false;
+
 		if (base64_encode((const char *) csk, parsed.dl, csk64, sizeof csk64) == (size_t) -1)
 		{
 			(void) slog(LG_ERROR, "%s: base64_encode() failed for csk", __func__);
@@ -327,11 +341,10 @@ atheme_pbkdf2v2_verify(const char *const restrict password, const char *const re
 	{
 		unsigned char csk[EVP_MAX_MD_SIZE];
 
-		if (! HMAC(parsed.md, parsed.cdg, (int) parsed.dl, ServerKeyStr, sizeof ServerKeyStr, csk, NULL))
-		{
-			(void) slog(LG_ERROR, "%s: HMAC() failed for csk", __func__);
+		if (! atheme_pbkdf2v2_scram_derive(&parsed, csk, NULL))
+			// This function logs messages on failure
 			return false;
-		}
+
 		if (memcmp(parsed.ssk, csk, parsed.dl) != 0)
 		{
 			(void) slog(LG_DEBUG, "%s: memcmp(3) mismatch on ssk (invalid password?)", __func__);
