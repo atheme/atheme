72900dc017e4c9b1292f01f7294fc603ff6c155f
Aaron Jones <aaronmdjones@gmail.com>
2017-12-13 03:55:00 +0000

    modules/saslserv/ecdsa-nist256p-challenge: general code quality cleanups
    - Use appropriate qualifiers and attributes on function parameters
    - Avoid enum and struct typedefs that end in _t as these are reserved
    - Move variables to where they are used
    - Use arc4random_buf() not RAND_pseudo_bytes(3ssl) for challenge
    - Check return value of o2i_ECPublicKey(3ssl)
    - Cast signature length parameter on ECDSA_verify(3ssl) correctly
    - Use libathemecore/memory instead of mowgli allocator
    - Set p->mechdata to NULL after freeing it

--- modules/saslserv/ecdsa-nist256p-challenge.c
+++ modules/saslserv/ecdsa-nist256p-challenge.c
@@ -7,107 +7,110 @@
 
 #include "atheme.h"
 
-#if defined HAVE_OPENSSL && defined HAVE_OPENSSL_EC_H
+#if defined(HAVE_OPENSSL) && defined(HAVE_OPENSSL_EC_H)
 
 #include <openssl/ec.h>
 #include <openssl/ecdsa.h>
 #include <openssl/evp.h>
-#include <openssl/rand.h>
 #include <openssl/sha.h>
 
 #define CHALLENGE_LENGTH	SHA256_DIGEST_LENGTH
 #define CURVE_IDENTIFIER	NID_X9_62_prime256v1
 
-typedef enum {
-	ECDSA_ST_INIT = 0,
-	ECDSA_ST_ACCNAME,
-	ECDSA_ST_RESPONSE,
-	ECDSA_ST_COUNT,
-} ecdsa_step_t;
+enum ecdsa_step
+{
+	ECDSA_ST_ACCNAME    = 0,
+	ECDSA_ST_RESPONSE   = 1,
+	ECDSA_ST_COUNT      = 2,
+};
 
-typedef struct {
-	ecdsa_step_t step;
-	EC_KEY *pubkey;
-	unsigned char challenge[CHALLENGE_LENGTH];
-} ecdsa_session_t;
+struct ecdsa_session
+{
+	EC_KEY          *pubkey;
+	unsigned char    challenge[CHALLENGE_LENGTH];
+	enum ecdsa_step  step;
+};
 
 static const sasl_core_functions_t *sasl_core_functions = NULL;
 
 static int
-mech_start(sasl_session_t *p, char **out, size_t *out_len)
+mech_start(sasl_session_t *const restrict p, char __attribute__((unused)) **const restrict out,
+           size_t __attribute__((unused)) *const restrict out_len)
 {
-	ecdsa_session_t *s = mowgli_alloc(sizeof(ecdsa_session_t));
-	p->mechdata = s;
+	struct ecdsa_session *const s = smalloc(sizeof *s);
 
+	p->mechdata = s;
 	s->pubkey = EC_KEY_new_by_curve_name(CURVE_IDENTIFIER);
 	s->step = ECDSA_ST_ACCNAME;
 
-	EC_KEY_set_conv_form(s->pubkey, POINT_CONVERSION_COMPRESSED);
+	(void) EC_KEY_set_conv_form(s->pubkey, POINT_CONVERSION_COMPRESSED);
 
 	return ASASL_MORE;
 }
 
 static int
-mech_step_accname(sasl_session_t *p, char *message, size_t len, char **out, size_t *out_len)
+mech_step_accname(sasl_session_t *const restrict p, char *const restrict message, const size_t len,
+                  char **const restrict out, size_t *const restrict out_len)
 {
-	ecdsa_session_t *s = p->mechdata;
-	myuser_t *mu;
-	char *end;
-	unsigned char pubkey_raw[BUFSIZE];
-	const unsigned char *pubkey_raw_p;
-	metadata_t *md;
-	size_t ret;
+	struct ecdsa_session *const s = p->mechdata;
 
-	memset(pubkey_raw, '\0', sizeof pubkey_raw);
+	if (! (message && len))
+		return ASASL_FAIL;
 
-	end = memchr(message, '\0', len);
-	if (end == NULL)
+	const char *const end = memchr(message, 0x00, len);
+	if (! end)
+	{
 		p->username = sstrndup(message, len);
+	}
 	else
 	{
-		p->username = sstrndup(message, end-message);
-		p->authzid = sstrndup(end+1, len-1-(end-message));
+		const size_t authcid_length = (size_t) (end - message);
+		const size_t authzid_length = len - 1 - authcid_length;
+
+		p->username = sstrndup(message, authcid_length);
+		p->authzid = sstrndup(end + 1, authzid_length);
 	}
 
-	mu = myuser_find_by_nick(p->username);
+	myuser_t *const mu = myuser_find_by_nick(p->username);
 	if (mu == NULL)
 		return ASASL_FAIL;
 
-	md = metadata_find(mu, "private:pubkey");
-	if (md == NULL)
-	{
-		md = metadata_find(mu, "pubkey");
-		if (md == NULL)
-			return ASASL_FAIL;
-	}
+	metadata_t *md;
+	if (! (md = metadata_find(mu, "private:pubkey")) && ! (md = metadata_find(mu, "pubkey")))
+		return ASASL_FAIL;
 
-	ret = base64_decode(md->value, pubkey_raw, BUFSIZE);
+	unsigned char pubkey_raw[0x1000];
+	(void) memset(pubkey_raw, 0x00, sizeof pubkey_raw);
+
+	const size_t ret = base64_decode(md->value, pubkey_raw, sizeof pubkey_raw);
 	if (ret == (size_t) -1)
 		return ASASL_FAIL;
 
-	pubkey_raw_p = pubkey_raw;
-	o2i_ECPublicKey(&s->pubkey, &pubkey_raw_p, (long) ret);
+	const unsigned char *pubkey_raw_p = pubkey_raw;
+	if (! o2i_ECPublicKey(&s->pubkey, &pubkey_raw_p, (long) ret))
+		return ASASL_FAIL;
 
-#ifndef DEBUG_STATIC_CHALLENGE_VECTOR
-	RAND_pseudo_bytes(s->challenge, CHALLENGE_LENGTH);
-#else
-	memset(s->challenge, 'A', CHALLENGE_LENGTH);
-#endif
-
-	*out = smalloc(400);
-	memcpy(*out, s->challenge, CHALLENGE_LENGTH);
+	*out = smalloc(CHALLENGE_LENGTH);
 	*out_len = CHALLENGE_LENGTH;
+
+	(void) arc4random_buf(s->challenge, CHALLENGE_LENGTH);
+	(void) memcpy(*out, s->challenge, CHALLENGE_LENGTH);
 
 	s->step = ECDSA_ST_RESPONSE;
 	return ASASL_MORE;
 }
 
 static int
-mech_step_response(sasl_session_t *p, char *message, size_t len, char **out, size_t *out_len)
+mech_step_response(sasl_session_t *const restrict p, char *const restrict message, const size_t len,
+                   char __attribute__((unused)) **const restrict out,
+                   size_t __attribute__((unused)) *const restrict out_len)
 {
-	ecdsa_session_t *s = p->mechdata;
+	struct ecdsa_session *const s = p->mechdata;
 
-	if (ECDSA_verify(0, s->challenge, CHALLENGE_LENGTH, (const unsigned char *)message, len, s->pubkey) != 1)
+	if (! (message && len))
+		return ASASL_FAIL;
+
+	if (ECDSA_verify(0, s->challenge, CHALLENGE_LENGTH, (unsigned char *) message, (int) len, s->pubkey) != 1)
 		return ASASL_FAIL;
 
 	return ASASL_DONE;
@@ -116,13 +119,15 @@ mech_step_response(sasl_session_t *p, char *message, size_t len, char **out, siz
 typedef int (*mech_stepfn_t)(sasl_session_t *p, char *message, size_t len, char **out, size_t *out_len);
 
 static int
-mech_step(sasl_session_t *p, char *message, size_t len, char **out, size_t *out_len)
+mech_step(sasl_session_t *const restrict p, char *const restrict message, const size_t len,
+          char **const restrict out, size_t *const restrict out_len)
 {
 	static mech_stepfn_t mech_steps[ECDSA_ST_COUNT] = {
 		[ECDSA_ST_ACCNAME] = &mech_step_accname,
 		[ECDSA_ST_RESPONSE] = &mech_step_response,
 	};
-	ecdsa_session_t *s = p->mechdata;
+
+	struct ecdsa_session *const s = p->mechdata;
 
 	if (mech_steps[s->step] != NULL)
 		return mech_steps[s->step](p, message, len, out, out_len);
@@ -131,14 +136,16 @@ mech_step(sasl_session_t *p, char *message, size_t len, char **out, size_t *out_
 }
 
 static void
-mech_finish(sasl_session_t *p)
+mech_finish(sasl_session_t *const restrict p)
 {
-	ecdsa_session_t *s = p->mechdata;
+	struct ecdsa_session *const s = p->mechdata;
 
-	if (s->pubkey != NULL)
-		EC_KEY_free(s->pubkey);
+	if (s->pubkey)
+		(void) EC_KEY_free(s->pubkey);
 
-	mowgli_free(s);
+	(void) free(s);
+
+	p->mechdata = NULL;
 }
 
 static sasl_mechanism_t mech = {
@@ -159,7 +166,7 @@ mod_init(module_t *const restrict m)
 }
 
 static void
-mod_deinit(const module_unload_intent_t intent)
+mod_deinit(const module_unload_intent_t __attribute__((unused)) intent)
 {
 	(void) sasl_core_functions->mech_unregister(&mech);
 }
