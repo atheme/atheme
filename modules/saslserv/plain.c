d8614744120ec5a74679b3d940400b3f316eae0c
Aaron Jones <aaronmdjones@gmail.com>
2017-12-13 03:55:00 +0000

    modules/saslserv/plain: general code quality cleanups
    - Use appropriate qualifiers and attributes on function parameters
    - Ensure we don't blow up if the input data isn't NULL-terminated
    - Use pointer arithmetic to extract client-provided data

--- modules/saslserv/plain.c
+++ modules/saslserv/plain.c
@@ -10,59 +10,49 @@
 static const sasl_core_functions_t *sasl_core_functions = NULL;
 
 static int
-mech_step(sasl_session_t *p, char *message, size_t len, char **out, size_t *out_len)
+mech_step(sasl_session_t *const restrict p, char *const restrict message, const size_t len,
+          char __attribute__((unused)) **const restrict out,
+          size_t __attribute__((unused)) *const restrict out_len)
 {
-	char authz[256];
-	char authc[256];
-	char pass[256];
-	myuser_t *mu;
-	char *end;
-
-	/* Copy the authzid */
-	end = memchr(message, '\0', len);
-	if (end == NULL)
-		return ASASL_FAIL;
-	if (end - message > 255)
-		return ASASL_FAIL;
-	len -= end - message + 1;
-	if (len <= 0)
-		return ASASL_FAIL;
-	memcpy(authz, message, end - message + 1);
-	message = end + 1;
-
-	/* Copy the authcid */
-	end = memchr(message, '\0', len);
-	if (end == NULL)
-		return ASASL_FAIL;
-	if (end - message > 255)
-		return ASASL_FAIL;
-	len -= end - message + 1;
-	if (len <= 0)
-		return ASASL_FAIL;
-	memcpy(authc, message, end - message + 1);
-	message = end + 1;
-
-	/* Copy the password */
-	end = memchr(message, '\0', len);
-	if (end == NULL)
-		end = message + len;
-	if (end - message > 255)
-		return ASASL_FAIL;
-	memcpy(pass, message, end - message);
-	pass[end - message] = '\0';
-
-	/* Done dissecting, now check. */
-	if(!(mu = myuser_find_by_nick(authc)))
+	if (! (message && len))
 		return ASASL_FAIL;
 
-	/* Return ASASL_FAIL before p->username is set,
-	   to prevent triggering bad_password(). */
+	char data[768];
+	if (len >= sizeof data)
+		return ASASL_FAIL;
+
+	(void) memset(data, 0x00, sizeof data);
+	(void) memcpy(data, message, len);
+
+	const char *ptr = data;
+	const char *const end = data + len;
+
+	const char *const authzid = ptr;
+	if (! *ptr || (ptr += strlen(ptr) + 1) >= end)
+		return ASASL_FAIL;
+
+	const char *const authcid = ptr;
+	if (! *ptr || (ptr += strlen(ptr) + 1) >= end)
+		return ASASL_FAIL;
+
+	const char *const secret = ptr;
+	if (! *secret)
+		return ASASL_FAIL;
+
+	myuser_t *const mu = myuser_find_by_nick(authcid);
+	if (! mu)
+		return ASASL_FAIL;
+
 	if (mu->flags & MU_NOPASSWORD)
 		return ASASL_FAIL;
 
-	p->username = sstrdup(authc);
-	p->authzid = sstrdup(authz);
-	return verify_password(mu, pass) ? ASASL_DONE : ASASL_FAIL;
+	p->username = sstrdup(authcid);
+	p->authzid = sstrdup(authzid);
+
+	if (! verify_password(mu, secret))
+		return ASASL_FAIL;
+
+	return ASASL_DONE;
 }
 
 static sasl_mechanism_t mech = {
@@ -82,7 +72,7 @@ mod_init(module_t *const restrict m)
 }
 
 static void
-mod_deinit(const module_unload_intent_t intent)
+mod_deinit(const module_unload_intent_t __attribute__((unused)) intent)
 {
 	(void) sasl_core_functions->mech_unregister(&mech);
 }
