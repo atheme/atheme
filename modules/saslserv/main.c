1d68dc2572c8b82e59716247d5261f8a39a44331
Aaron Jones <aaronmdjones@gmail.com>
2017-12-13 03:55:00 +0000

    modules/saslserv/main: sasl_newuser: cleanup

--- modules/saslserv/main.c
+++ modules/saslserv/main.c
@@ -704,42 +704,36 @@ login_user(sasl_session_t *p)
 static void
 sasl_newuser(hook_user_nick_t *data)
 {
-	user_t *u = data->u;
-	sasl_mechanism_t *mptr;
-	sasl_session_t *p;
-	myuser_t *mu;
-
 	/* If the user has been killed, don't do anything. */
-	if (!u)
+	user_t *const u = data->u;
+	if (! u)
 		return;
 
-	p = find_session(u->uid);
-
 	/* Not concerned unless it's a SASL login. */
-	if(p == NULL)
+	sasl_session_t *const p = find_session(u->uid);
+	if (! p)
 		return;
 
 	/* We will log it ourselves, if needed */
 	p->flags &= ~ASASL_NEED_LOG;
 
 	/* Find the account */
-	mu = p->authzid ? myuser_find_by_nick(p->authzid) : NULL;
-	if (mu == NULL)
+	myuser_t *const mu = p->authzid ? myuser_find_by_nick(p->authzid) : NULL;
+	if (! mu)
 	{
-		notice(saslsvs->nick, u->nick, "Account %s dropped, login cancelled",
-		       p->authzid ? p->authzid : "??");
-		destroy_session(p);
+		(void) notice(saslsvs->nick, u->nick, "Account %s dropped, login cancelled",
+		                                      p->authzid ? p->authzid : "??");
+		(void) destroy_session(p);
+
 		/* We'll remove their ircd login in handle_burstlogin() */
 		return;
 	}
 
-	mptr = p->mechptr;
+	sasl_mechanism_t *const mptr = p->mechptr;
 
-	destroy_session(p);
-
-	myuser_login(saslsvs, u, mu, false);
-
-	logcommand_user(saslsvs, u, CMDLOG_LOGIN, "LOGIN (%s)", mptr->name);
+	(void) destroy_session(p);
+	(void) myuser_login(saslsvs, u, mu, false);
+	(void) logcommand_user(saslsvs, u, CMDLOG_LOGIN, "LOGIN (%s)", mptr->name);
 }
 
 /* This function is run approximately once every 30 seconds.
