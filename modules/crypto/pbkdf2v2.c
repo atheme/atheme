atheme/atheme

commit 317796ead0ea5f34d4a9546423f98a0f6b558bca

Password Crypto API: Several significant improvements
There were 4 concerns with the codebase in its current state:

When a password is verified against a password hash by a password
crypto module, and the verification fails, libathemecore would go on
to try the next module(s) even if the password hash was not produced
by those modules. This is inefficient.

When a password crypto module provides a 'verify' function,
libathemecore/auth.c and libathemecore/crypto.c will only use the
'crypt' function to encrypt new passwords or re-encrypt existing ones,
but it will first call the 'salt' function to generate a salt string,
only to immediately give it back to the 'crypt' function so that it
can parse the parameters from that string. This is inefficient.

When a password is verified against a password hash by a password
crypto module, and that module provides a 'recrypt' function to
indicate to libathemecore/auth.c that the password should be re-
encrypted, the 'recrypt' function has to be invoked and parse the
parameters again (which 'crypt' or 'verify' already did). This is
inefficient.

Existing callers of crypt_string() now pass NULL as the salt argument,
rendering that argument completely useless. In addition, crypt_string
only tried the first encryption-capable crypto module, and if that
failed, gave up entirely. This is undesirable. It is better to fall
back to the next module than to not encrypt a password.

These defects are resolved as follows:

The 'salt' funcptr has been removed from the crypto modules. Those
modules are now expected to generate their own parameters on-the-fly
if their 'crypt' function is passed a NULL argument as the salt. This
avoids encoding parameters into a string only to immediately scan the
parameters from that string.

The 'recrypt' funcptr has been removed from the crypto modules. Those
modules now indicate whether the password is to be re-encrypted by
setting a flag in their 'verify' function. This avoids parsing twice.

modules/crypto/pbkdf2v2.c
@@ -57,41 +57,8 @@ atheme_pbkdf2v2_salt_is_b64(const unsigned int prf)
 }
 
 static bool
-atheme_pbkdf2v2_parse_dbentry(struct pbkdf2v2_dbentry *const restrict dbe, const char *const restrict parameters,
-                              const bool need_digest_components)
+atheme_pbkdf2v2_determine_params(struct pbkdf2v2_dbentry *const restrict dbe)
 {
-	char sdg64[0x1000];
-	char ssk64[0x1000];
-	char shk64[0x1000];
-
-	(void) memset(dbe, 0x00, sizeof *dbe);
-
-	if (need_digest_components)
-	{
-		if (sscanf(parameters, PBKDF2_FS_LOADHASH, &dbe->a, &dbe->c, dbe->salt64, ssk64, shk64) == 5)
-		{
-			(void) slog(LG_DEBUG, "%s: matched PBKDF2_FS_LOADHASH (SCRAM-SHA)", __func__);
-			goto parsed;
-		}
-		if (sscanf(parameters, PBKDF2_FN_LOADHASH, &dbe->a, &dbe->c, dbe->salt64, sdg64) == 4)
-		{
-			(void) slog(LG_DEBUG, "%s: matched PBKDF2_FN_LOADHASH (HMAC-SHA)", __func__);
-			goto parsed;
-		}
-	}
-	else
-	{
-		if (sscanf(parameters, PBKDF2_FN_LOADSALT, &dbe->a, &dbe->c, dbe->salt64) == 3)
-		{
-			(void) slog(LG_DEBUG, "%s: matched PBKDF2_FN_LOADSALT (Encrypting)", __func__);
-			goto parsed;
-		}
-	}
-
-	return false;
-
-parsed:
-
 	switch (dbe->a)
 	{
 		case PBKDF2_PRF_SCRAM_SHA1:
@@ -129,16 +96,38 @@ parsed:
 			return false;
 	}
 
-	dbe->salt_was_b64 = atheme_pbkdf2v2_salt_is_b64(dbe->a);
-
 	if (! dbe->md)
 	{
 		(void) slog(LG_ERROR, "%s: dbe->md is NULL", __func__);
 		return false;
 	}
 
-	if (need_digest_components && dbe->scram)
+	return true;
+}
+
+static bool
+atheme_pbkdf2v2_parse_dbentry(struct pbkdf2v2_dbentry *const restrict dbe, const char *const restrict parameters)
+{
+	char sdg64[0x1000];
+	char ssk64[0x1000];
+	char shk64[0x1000];
+
+	(void) memset(dbe, 0x00, sizeof *dbe);
+
+	if (sscanf(parameters, PBKDF2_FS_LOADHASH, &dbe->a, &dbe->c, dbe->salt64, ssk64, shk64) == 5)
 	{
+		(void) slog(LG_DEBUG, "%s: matched PBKDF2_FS_LOADHASH (SCRAM-SHA)", __func__);
+
+		if (! atheme_pbkdf2v2_determine_params(dbe))
+			// This function logs messages on failure
+			return false;
+
+		if (! dbe->scram)
+		{
+			(void) slog(LG_ERROR, "%s: is not a SCRAM-SHA PRF (BUG)", __func__);
+			return false;
+		}
+
 		if (base64_decode(ssk64, dbe->ssk, sizeof dbe->ssk) != dbe->dl)
 		{
 			(void) slog(LG_ERROR, "%s: base64_decode('%s') for ssk failed", __func__, ssk64);
@@ -149,25 +138,41 @@ parsed:
 			(void) slog(LG_ERROR, "%s: base64_decode('%s') for shk failed", __func__, shk64);
 			return false;
 		}
+
+#ifndef HAVE_LIBIDN
+		(void) slog(LG_INFO, "%s: encountered SCRAM format hash, but GNU libidn is unavailable", __func__);
+		(void) slog(LG_INFO, "%s: user logins may fail if they have exotic password characters", __func__);
+#endif /* !HAVE_LIBIDN */
+
+		return true;
 	}
-	else if (need_digest_components)
+
+	(void) memset(dbe, 0x00, sizeof *dbe);
+
+	if (sscanf(parameters, PBKDF2_FN_LOADHASH, &dbe->a, &dbe->c, dbe->salt64, sdg64) == 4)
 	{
+		(void) slog(LG_DEBUG, "%s: matched PBKDF2_FN_LOADHASH (HMAC-SHA)", __func__);
+
+		if (! atheme_pbkdf2v2_determine_params(dbe))
+			// This function logs messages on failure
+			return false;
+
+		if (dbe->scram)
+		{
+			(void) slog(LG_ERROR, "%s: is a SCRAM-SHA PRF (BUG)", __func__);
+			return false;
+		}
+
 		if (base64_decode(sdg64, dbe->sdg, sizeof dbe->sdg) != dbe->dl)
 		{
 			(void) slog(LG_ERROR, "%s: base64_decode('%s') for sdg failed", __func__, sdg64);
 			return false;
 		}
+
+		return true;
 	}
 
-#ifndef HAVE_LIBIDN
-	if (dbe->scram)
-	{
-		(void) slog(LG_INFO, "%s: encountered SCRAM format hash, but GNU libidn is unavailable", __func__);
-		(void) slog(LG_INFO, "%s: user logins may fail if they have exotic password characters", __func__);
-	}
-#endif /* !HAVE_LIBIDN */
-
-	return true;
+	return false;
 }
 
 static inline bool
@@ -212,6 +217,39 @@ atheme_pbkdf2v2_scram_derive(const struct pbkdf2v2_dbentry *const dbe, const uns
 	return true;
 }
 
+static bool
+atheme_pbkdf2v2_recrypt(const struct pbkdf2v2_dbentry *const restrict dbe)
+{
+	if (dbe->a != pbkdf2v2_digest)
+	{
+		(void) slog(LG_DEBUG, "%s: prf (%u) != default (%u)", __func__, dbe->a, pbkdf2v2_digest);
+		return true;
+	}
+
+	if (dbe->c != pbkdf2v2_rounds)
+	{
+		(void) slog(LG_DEBUG, "%s: rounds (%u) != default (%u)", __func__, dbe->c, pbkdf2v2_rounds);
+		return true;
+	}
+
+	if (atheme_pbkdf2v2_salt_is_b64(dbe->a))
+	{
+		if (dbe->sl != pbkdf2v2_saltsz)
+		{
+			(void) slog(LG_DEBUG, "%s: salt length (%zu) != default (%u)", __func__, dbe->sl,
+			                      pbkdf2v2_saltsz);
+			return true;
+		}
+	}
+	else
+	{
+		(void) slog(LG_DEBUG, "%s: salt wasn't base64-encoded", __func__);
+		return true;
+	}
+
+	return false;
+}
+
 #ifdef HAVE_LIBIDN
 
 /* **********************************************************************************************
@@ -231,11 +269,13 @@ const char *atheme_pbkdf2v2_scram_normalize(const char *restrict);
 bool
 atheme_pbkdf2v2_scram_dbextract(const char *const restrict parameters, struct pbkdf2v2_dbentry *const restrict dbe)
 {
-	if (! atheme_pbkdf2v2_parse_dbentry(dbe, parameters, true))
+	if (! atheme_pbkdf2v2_parse_dbentry(dbe, parameters))
 	{
 		(void) slog(LG_DEBUG, "%s: could not extract necessary information from database", __func__);
 		return false;
 	}
+
+	const bool salt_was_b64 = atheme_pbkdf2v2_salt_is_b64(dbe->a);
 
 	// Ensure that the SCRAM-SHA module knows which one of 2 possible algorithms we're using
 	switch (dbe->a)
@@ -260,7 +300,7 @@ atheme_pbkdf2v2_scram_dbextract(const char *const restrict parameters, struct pb
 	}
 
 	// Ensure that the SCRAM-SHA module has a base64-encoded salt if it wasn't already so
-	if (! dbe->salt_was_b64)
+	if (! salt_was_b64)
 	{
 		dbe->sl = strlen(dbe->salt64);
 
@@ -319,50 +359,10 @@ static const atheme_pbkdf2v2_scram_normalize_fn __attribute__((unused)) nm_fn_pt
 #endif /* HAVE_LIBIDN */
 
 static bool
-atheme_pbkdf2v2_compute(const char *restrict password, const char *const restrict parameters,
-                        struct pbkdf2v2_dbentry *const restrict dbe, const bool verifying)
+atheme_pbkdf2v2_compute(const char *restrict password, struct pbkdf2v2_dbentry *const restrict dbe)
 {
-	if (! atheme_pbkdf2v2_parse_dbentry(dbe, parameters, verifying))
-	{
-		if (verifying)
-			(void) slog(LG_DEBUG, "%s: sscanf(3) was unsuccessful", __func__);
-		else
-			(void) slog(LG_ERROR, "%s: sscanf(3) was unsuccessful (BUG)", __func__);
-
-		return false;
-	}
-
-	if (dbe->salt_was_b64)
-	{
-		if ((dbe->sl = base64_decode(dbe->salt64, dbe->salt, sizeof dbe->salt)) == (size_t) -1)
-		{
-			(void) slog(LG_ERROR, "%s: base64_decode('%s') for salt failed", __func__, dbe->salt64);
-			return false;
-		}
-
-		if (! atheme_pbkdf2v2_parameters_sane(dbe))
-			// This function logs messages on failure
-			return false;
-	}
-	else
-	{
-		dbe->sl = strlen(dbe->salt64);
-
-		if (! atheme_pbkdf2v2_parameters_sane(dbe))
-			// This function logs messages on failure
-			return false;
-
-		(void) memcpy(dbe->salt, dbe->salt64, dbe->sl);
-
-		if (base64_encode(dbe->salt, dbe->sl, dbe->salt64, sizeof dbe->salt64) == (size_t) -1)
-		{
-			(void) slog(LG_ERROR, "%s: base64_encode() failed for salt", __func__);
-			return false;
-		}
-	}
-
 #ifdef HAVE_LIBIDN
-	if (dbe->scram && ((password = atheme_pbkdf2v2_scram_normalize(password)) == NULL))
+	if (dbe->scram && ! (password = atheme_pbkdf2v2_scram_normalize(password)))
 		// This function logs messages on failure
 		return false;
 #endif /* HAVE_LIBIDN */
@@ -371,7 +371,7 @@ atheme_pbkdf2v2_compute(const char *restrict password, const char *const restric
 
 	if (! pl)
 	{
-		(void) slog(LG_ERROR, "%s: password length == 0", __func__);
+		(void) slog(LG_DEBUG, "%s: password length == 0", __func__);
 		return false;
 	}
 
@@ -387,37 +387,27 @@ atheme_pbkdf2v2_compute(const char *restrict password, const char *const restric
 }
 
 static const char *
-atheme_pbkdf2v2_salt(void)
+atheme_pbkdf2v2_crypt(const char *const restrict password,
+                      const char __attribute__((unused)) *const restrict parameters)
 {
-	unsigned char salt[PBKDF2_SALTLEN_MAX];
+	struct pbkdf2v2_dbentry dbe;
+	(void) memset(&dbe, 0x00, sizeof dbe);
+	(void) arc4random_buf(dbe.salt, (size_t) pbkdf2v2_saltsz);
 
-	(void) arc4random_buf(salt, (size_t) pbkdf2v2_saltsz);
-
-	char salt64[PBKDF2_SALTLEN_MAX * 3];
-
-	if (base64_encode(salt, (size_t) pbkdf2v2_saltsz, salt64, sizeof salt64) == (size_t) -1)
+	if (base64_encode(dbe.salt, (size_t) pbkdf2v2_saltsz, dbe.salt64, sizeof dbe.salt64) == (size_t) -1)
 	{
 		(void) slog(LG_ERROR, "%s: base64_encode() failed (BUG)", __func__);
 		return NULL;
 	}
 
-	static char res[PASSLEN];
+	dbe.a = pbkdf2v2_digest;
+	dbe.c = pbkdf2v2_rounds;
 
-	if (snprintf(res, PASSLEN, PBKDF2_FN_SAVESALT, pbkdf2v2_digest, pbkdf2v2_rounds, salt64) >= PASSLEN)
-	{
-		(void) slog(LG_ERROR, "%s: snprintf(3) would have overflowed result buffer (BUG)", __func__);
+	if (! atheme_pbkdf2v2_determine_params(&dbe))
+		// This function logs messages on failure
 		return NULL;
-	}
 
-	return res;
-}
-
-static const char *
-atheme_pbkdf2v2_crypt(const char *const restrict password, const char *const restrict parameters)
-{
-	struct pbkdf2v2_dbentry dbe;
-
-	if (! atheme_pbkdf2v2_compute(password, parameters, &dbe, false))
+	if (! atheme_pbkdf2v2_compute(password, &dbe))
 		// This function logs messages on failure
 		return NULL;
 
@@ -470,11 +460,43 @@ atheme_pbkdf2v2_crypt(const char *const restrict password, const char *const res
 }
 
 static bool
-atheme_pbkdf2v2_verify(const char *const restrict password, const char *const restrict parameters)
+atheme_pbkdf2v2_verify(const char *const restrict password, const char *const restrict parameters,
+                       unsigned int *const restrict flags)
 {
 	struct pbkdf2v2_dbentry dbe;
 
-	if (! atheme_pbkdf2v2_compute(password, parameters, &dbe, true))
+	if (! atheme_pbkdf2v2_parse_dbentry(&dbe, parameters))
+	{
+		(void) slog(LG_DEBUG, "%s: sscanf(3) was unsuccessful", __func__);
+		return false;
+	}
+
+	if (atheme_pbkdf2v2_salt_is_b64(dbe.a))
+	{
+		if ((dbe.sl = base64_decode(dbe.salt64, dbe.salt, sizeof dbe.salt)) == (size_t) -1)
+		{
+			(void) slog(LG_ERROR, "%s: base64_decode('%s') for salt failed", __func__, dbe.salt64);
+			return false;
+		}
+
+		if (! atheme_pbkdf2v2_parameters_sane(&dbe))
+			// This function logs messages on failure
+			return false;
+	}
+	else
+	{
+		dbe.sl = strlen(dbe.salt64);
+
+		if (! atheme_pbkdf2v2_parameters_sane(&dbe))
+			// This function logs messages on failure
+			return false;
+
+		(void) memcpy(dbe.salt, dbe.salt64, dbe.sl);
+	}
+
+	*flags |= PWVERIFY_FLAG_MYMODULE;
+
+	if (! atheme_pbkdf2v2_compute(password, &dbe))
 		// This function logs messages on failure
 		return false;
 
@@ -501,56 +523,10 @@ atheme_pbkdf2v2_verify(const char *const restrict password, const char *const re
 		}
 	}
 
+	if (atheme_pbkdf2v2_recrypt(&dbe))
+		*flags |= PWVERIFY_FLAG_RECRYPT;
+
 	return true;
-}
-
-static bool
-atheme_pbkdf2v2_recrypt(const char *const restrict parameters)
-{
-	unsigned int prf;
-	unsigned int c;
-	char salt64[0x1000];
-
-	if (sscanf(parameters, PBKDF2_FN_LOADSALT, &prf, &c, salt64) != 3)
-	{
-		(void) slog(LG_ERROR, "%s: sscanf(3) was unsuccessful (BUG)", __func__);
-		return false;
-	}
-	if (prf != pbkdf2v2_digest)
-	{
-		(void) slog(LG_DEBUG, "%s: prf (%u) != default (%u)", __func__, prf, pbkdf2v2_digest);
-		return true;
-	}
-	if (c != pbkdf2v2_rounds)
-	{
-		(void) slog(LG_DEBUG, "%s: rounds (%u) != default (%u)", __func__, c, pbkdf2v2_rounds);
-		return true;
-	}
-
-	if (atheme_pbkdf2v2_salt_is_b64(prf))
-	{
-		unsigned char salt[PBKDF2_SALTLEN_MAX];
-
-		const size_t sl = base64_decode(salt64, salt, sizeof salt);
-
-		if (sl == (size_t) -1)
-		{
-			(void) slog(LG_ERROR, "%s: base64_decode('%s') for salt failed", __func__, salt64);
-			return false;
-		}
-		if (sl != pbkdf2v2_saltsz)
-		{
-			(void) slog(LG_DEBUG, "%s: salt length (%zu) != default (%u)", __func__, sl, pbkdf2v2_saltsz);
-			return true;
-		}
-	}
-	else
-	{
-		(void) slog(LG_DEBUG, "%s: salt wasn't base64-encoded", __func__);
-		return true;
-	}
-
-	return false;
 }
 
 static int
@@ -590,10 +566,8 @@ c_ci_pbkdf2v2_digest(mowgli_config_file_entry_t *const restrict ce)
 static crypt_impl_t crypto_pbkdf2v2_impl = {
 
 	.id         = "pbkdf2v2",
-	.salt       = &atheme_pbkdf2v2_salt,
 	.crypt      = &atheme_pbkdf2v2_crypt,
 	.verify     = &atheme_pbkdf2v2_verify,
-	.recrypt    = &atheme_pbkdf2v2_recrypt,
 };
 
 static mowgli_list_t pbkdf2v2_conf_table;

doc/CRYPTO-API
@@ -1,0 +1,51 @@
+Password Crypto API Documentation
+---------------------------------
+
+This document serves as a reference for how to write password crypto modules
+(the ones usually located under modules/crypto/).
+
+When creating a password hash, modules are expected to encode all of their
+parameters (module name, salt, digest, any other pertinent information) into
+a string and return that to libathemecore.
+
+This string should contain everything necessary to:
+
+1) Verify a password hash string was produced by this module
+2) Verify a password against a password hash string
+3) Determine if the password needs re-encrypting (for example, digest
+   algorithm upgrade).
+
+There are 2 functions a module can provide: 'crypt' and 'verify'. How they
+behave depends on whether you provide one, or the other, or both:
+
+- If you provide 'crypt' but not 'verify', then 'crypt' is expected to be
+  able to both encrypt new passwords (this will be the case when the second
+  parameter is NULL) and compute digests of passwords against pre-existing
+  information (the password hash string, given as the second argument). It
+  should return a password hash string, which, if the supplied password was
+  correct (in the second case), should compare string-equal to its original
+  output.
+
+- If you provide 'verify' but not 'crypt', then 'verify' only has to process
+  the password hash string (second argument) and verify the password against
+  it. It should return true if the password successfully verifies, and false
+  otherwise, or upon encountering errors. The module will not be used to
+  encrypt new passwords.
+
+- If you provide 'crypt' and 'verify', then 'crypt' will only be called to
+  encrypt new passwords; its second parameter will always be NULL, and its
+  output does not need to be string-equal to its original output (this allows
+  you to change the format of the password hash string over time).
+
+When 'verify' is being executed and you are certain that the password hash
+string (second parameter) was indeed produced by this module, you should set
+the PWVERIFY_FLAG_MYMODULE flag (third parameter). This will prevent Atheme
+from continuing to try other modules in turn if your verification function
+returns false. If you detect that the password should be re-encrypted (for
+example, if there is a better digest algorithm available in a newer version
+of the module), you should set the PWVERIFY_FLAG_RECRYPT flag. Note that this
+flag has no effect if your module does not provide a 'crypt' function.
+
+For an actual example of all of this, please see modules/crypto/argon2d,
+which provides both functions, and modules/crypto/rawmd5, which provides only
+a 'verify' function.

include/crypto.h
@@ -3,31 +3,32 @@
  * Rights to this code are as documented under doc/LICENSE.
  *
  * Cryptographic module support.
- *
  */
 
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
+#define PWVERIFY_FLAG_NONE      0x0000U // Initial state
+#define PWVERIFY_FLAG_MYMODULE  0x0001U // This password hash was from 'this' crypto module
+#define PWVERIFY_FLAG_RECRYPT   0x0002U // This password needs re-encrypting
+
 typedef struct {
 
 	const char *id;
-	const char *(*salt)(void);
 	const char *(*crypt)(const char *password, const char *parameters);
-	bool (*verify)(const char *password, const char *parameters);
-	bool (*recrypt)(const char *parameters);
+	bool (*verify)(const char *password, const char *parameters, unsigned int *flags);
 
 	mowgli_node_t node;
+
 } crypt_impl_t;
 
-E const crypt_impl_t *crypt_get_default_provider(void);
 E void crypt_register(crypt_impl_t *impl);
 E void crypt_unregister(crypt_impl_t *impl);
 
-E const char *gen_salt(void);
+E const crypt_impl_t *crypt_get_default_provider(void);
+E const crypt_impl_t *crypt_verify_password(const char *password, const char *parameters, unsigned int *flags);
+E const char *crypt_password(const char *password);
 E const char *crypt_string(const char *password, const char *parameters);
-
-E const crypt_impl_t *crypt_verify_password(const char *password, const char *parameters);
 
 #endif
 

include/pbkdf2v2.h
@@ -63,7 +63,6 @@ struct pbkdf2v2_dbentry
 	unsigned int     a;                                 // PRF ID (one of the macros above)
 	unsigned int     c;                                 // PBKDF2 Iteration Count
 	bool             scram;                             // Whether to use HMAC-SHA or SCRAM-SHA
-	bool             salt_was_b64;                      // Whether the salt was base64-encoded
 };
 
 static const unsigned char ServerKeyStr[] = {

libathemecore/auth.c
@@ -33,7 +33,7 @@ set_password(myuser_t *const restrict mu, const char *const restrict password)
 		return;
 
 	/* if we can, try to crypt it */
-	const char *const hash = crypt_string(password, NULL);
+	const char *const hash = crypt_password(password);
 
 	if (hash)
 	{
@@ -53,7 +53,7 @@ set_password(myuser_t *const restrict mu, const char *const restrict password)
 bool
 verify_password(myuser_t *const restrict mu, const char *const restrict password)
 {
-	if (mu == NULL || password == NULL)
+	if (! mu || ! password)
 		return false;
 
 	if (auth_module_loaded && auth_user_custom)
@@ -62,29 +62,29 @@ verify_password(myuser_t *const restrict mu, const char *const restrict password
 	if (mu->flags & MU_CRYPTPASS)
 	{
 		const crypt_impl_t *ci, *ci_default;
-		const char *new_salt, *new_hash;
+		unsigned int verify_flags = PWVERIFY_FLAG_NONE;
 
-		if ((ci = crypt_verify_password(password, mu->pass)) == NULL)
+		if (! (ci = crypt_verify_password(password, mu->pass, &verify_flags)))
 			// Verification failure
 			return false;
 
-		if ((ci_default = crypt_get_default_provider()) == NULL)
+		if (! (ci_default = crypt_get_default_provider()) || ! ci_default->crypt)
 			// Verification succeeded but we don't have a module that can create new password hashes
 			return true;
 
 		if (ci != ci_default)
 			(void) slog(LG_INFO, "%s: transitioning from crypt scheme '%s' to '%s' for account '%s'",
 				             __func__, ci->id, ci_default->id, entity(mu)->name);
-		else if (ci->recrypt != NULL && ci->recrypt(mu->pass))
+		else if (verify_flags & PWVERIFY_FLAG_RECRYPT)
 			(void) slog(LG_INFO, "%s: re-encrypting password for account '%s'",
 			                     __func__, entity(mu)->name);
 		else
 			// Verification succeeded and re-encrypting not required, nothing more to do
 			return true;
 
-		if ((new_salt = ci_default->salt()) == NULL)
-			(void) slog(LG_ERROR, "%s: salt generation failed", __func__);
-		else if ((new_hash = ci_default->crypt(password, new_salt)) == NULL)
+		const char *new_hash;
+
+		if (! (new_hash = ci_default->crypt(password, NULL)))
 			(void) slog(LG_ERROR, "%s: hash generation failed", __func__);
 		else
 			(void) mowgli_strlcpy(mu->pass, new_hash, PASSLEN);

libathemecore/crypto.c
@@ -25,53 +25,6 @@
 
 static mowgli_list_t crypt_impl_list = { NULL, NULL, 0 };
 
-const crypt_impl_t *
-crypt_get_default_provider(void)
-{
-	mowgli_node_t *n;
-
-	if (!MOWGLI_LIST_LENGTH(&crypt_impl_list))
-		return NULL;
-
-	MOWGLI_ITER_FOREACH(n, crypt_impl_list.head)
-	{
-		const crypt_impl_t *const ci = n->data;
-
-		if (ci->salt != NULL && ci->crypt != NULL)
-			return ci;
-	}
-
-	return NULL;
-}
-
-const char *
-gen_salt(void)
-{
-	const crypt_impl_t *const ci = crypt_get_default_provider();
-
-	if (!ci)
-		return NULL;
-
-	return ci->salt();
-}
-
-const char *
-crypt_string(const char *const restrict password, const char *restrict parameters)
-{
-	const crypt_impl_t *const ci = crypt_get_default_provider();
-
-	if (!ci)
-		return NULL;
-
-	if (!parameters || !*parameters)
-		parameters = ci->salt();
-
-	if (!parameters)
-		return NULL;
-
-	return ci->crypt(password, parameters);
-}
-
 static void
 crypt_log_modchg(const char *const restrict caller, const char *const restrict which,
                  const crypt_impl_t *const restrict impl)
@@ -90,56 +43,137 @@ crypt_log_modchg(const char *const restrict caller, const char *const restrict w
 void
 crypt_register(crypt_impl_t *const restrict impl)
 {
-	return_if_fail(impl != NULL);
-	return_if_fail(impl->id != NULL);
+	if (! impl || ! impl->id)
+		return;
 
-	if ((impl->salt != NULL && impl->crypt != NULL) || impl->verify != NULL)
+	if (impl->crypt || impl->verify)
 	{
-		mowgli_node_add(impl, &impl->node, &crypt_impl_list);
+		(void) mowgli_node_add(impl, &impl->node, &crypt_impl_list);
 		(void) crypt_log_modchg(__func__, "registered", impl);
 	}
 	else
-		(void) slog(LG_ERROR, "%s: crypto provider '%s' provides neither salt/crypt nor verify methods",
+		(void) slog(LG_ERROR, "%s: crypto provider '%s' provides neither crypt nor verify methods (BUG)",
 		                      __func__, impl->id);
 }
 
 void
 crypt_unregister(crypt_impl_t *const restrict impl)
 {
-	return_if_fail(impl != NULL);
+	if (! impl || ! impl->id)
+		return;
 
-	if ((impl->salt != NULL && impl->crypt != NULL) || impl->verify != NULL)
+	if (impl->crypt || impl->verify)
 	{
-		mowgli_node_delete(&impl->node, &crypt_impl_list);
+		(void) mowgli_node_delete(&impl->node, &crypt_impl_list);
 		(void) crypt_log_modchg(__func__, "unregistered", impl);
 	}
 }
 
 const crypt_impl_t *
-crypt_verify_password(const char *const restrict password, const char *const restrict parameters)
+crypt_get_default_provider(void)
 {
 	mowgli_node_t *n;
-
-	if (!MOWGLI_LIST_LENGTH(&crypt_impl_list))
-		return NULL;
 
 	MOWGLI_ITER_FOREACH(n, crypt_impl_list.head)
 	{
 		const crypt_impl_t *const ci = n->data;
 
-		if (ci->verify != NULL)
+		if (ci->crypt)
+			return ci;
+	}
+
+	return NULL;
+}
+
+const crypt_impl_t *
+crypt_verify_password(const char *const restrict password, const char *const restrict parameters,
+                      unsigned int *const restrict flags)
+{
+	mowgli_node_t *n;
+
+	MOWGLI_ITER_FOREACH(n, crypt_impl_list.head)
+	{
+		const crypt_impl_t *const ci = n->data;
+
+		if (ci->verify)
 		{
-			if (ci->verify(password, parameters))
+			unsigned int myflags = PWVERIFY_FLAG_NONE;
+
+			if (ci->verify(password, parameters, &myflags))
+			{
+				if (flags)
+					*flags = myflags;
+
 				return ci;
+			}
+
+			/* If password verification failed and the password hash was produced
+			 * by the module we just tried, there's no point continuing to test it
+			 * against the other modules. This saves some CPU time.
+			 */
+			if (myflags & PWVERIFY_FLAG_MYMODULE)
+				return NULL;
+
+			continue;
 		}
-		else if (ci->crypt != NULL)
+
+		if (ci->crypt)
 		{
 			const char *const result = ci->crypt(password, parameters);
 
-			if (result != NULL && strcmp(result, parameters) == 0)
+			if (result && strcmp(result, parameters) == 0)
 				return ci;
+
+			continue;
 		}
 	}
 
 	return NULL;
+}
+
+const char *
+crypt_password(const char *const restrict password)
+{
+	bool encryption_capable_module = false;
+
+	mowgli_node_t *n;
+
+	MOWGLI_ITER_FOREACH(n, crypt_impl_list.head)
+	{
+		const crypt_impl_t *const ci = n->data;
+
+		if (! ci->crypt)
+		{
+			(void) slog(LG_DEBUG, "%s: skipping incapable provider '%s'", __func__, ci->id);
+			continue;
+		}
+
+		encryption_capable_module = true;
+
+		const char *const result = ci->crypt(password, NULL);
+
+		if (! result)
+		{
+			(void) slog(LG_ERROR, "%s: ci->crypt() failed for provider '%s'", __func__, ci->id);
+			continue;
+		}
+
+		(void) slog(LG_DEBUG, "%s: encrypted password with provider '%s'", __func__, ci->id);
+		return result;
+	}
+
+	if (encryption_capable_module)
+		(void) slog(LG_ERROR, "%s: all encryption-capable crypto modules failed", __func__);
+	else
+		(void) slog(LG_ERROR, "%s: you have no encryption-capable crypto module", __func__);
+
+	return NULL;
+}
+
+const char *
+crypt_string(const char *const restrict password, const char __attribute__((unused)) *const restrict parameters)
+{
+	(void) slog(LG_DEBUG, "%s: this is provided for compatibility only; use crypt_password()", __func__);
+
+	return crypt_password(password);
 }

commit 48df21ecf5599f1af4a1d6ab5f49fcd8a0f8c0f5

modules/crypto/pbkdf2v2: fix computation of new digests
When we're encrypting a new password, we generate a random salt for it,
but we neglected to set the salt length before running the computation.

This results in encrypting the password with no salt.

When it comes time to verify it, the salt is decoded and its length is
set, which results in computation of a different digest.

This regression was introduced in commit 317796ead0ea5f34d4a9.

modules/crypto/pbkdf2v2.c
@@ -437,6 +437,7 @@ atheme_pbkdf2v2_crypt(const char *const restrict password,
 		return NULL;
 	}
 
+	dbe.sl = (size_t) pbkdf2v2_saltsz;
 	dbe.a = pbkdf2v2_digest;
 	dbe.c = pbkdf2v2_rounds;
 