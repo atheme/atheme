e6844c6de9ec31a1674ac8e049e3f54862552a54
Aaron Jones <aaronmdjones@gmail.com>
2018-02-07 01:12:10 +0000

    modules/chanserv/drop: various cleanups
    - Move variables to where they are used (or remove unused ones)
    - Use consistent variable for channel name (user-given argument
      before we've found its registration, registered name after)
    - Remove redundant 'return' statement from the end of functions
    - Use C99 designated initialisers for command structures
    - Make command structures static (scope-limited to this file)
      Avoids compiler warnings about no previous extern declarations
    - Add attributes to mark/silence unused variables
    - More const-correctness
    - Put function names on their own line
      Makes grepping for '^foo' easy

--- modules/chanserv/drop.c
+++ modules/chanserv/drop.c
@@ -7,82 +7,63 @@
 
 #include "atheme.h"
 
-static void cs_cmd_drop(sourceinfo_t *si, int parc, char *parv[]);
-static void cs_cmd_fdrop(sourceinfo_t *si, int parc, char *parv[]);
-
-command_t cs_drop = { "DROP", N_("Drops a channel registration."),
-                        AC_NONE, 2, cs_cmd_drop, { .path = "cservice/drop" } };
-command_t cs_fdrop = { "FDROP", N_("Forces dropping of a channel registration."),
-                        PRIV_CHAN_ADMIN, 1, cs_cmd_fdrop, { .path = "cservice/fdrop" } };
-
 static void
-mod_init(module_t *const restrict m)
+cmd_cs_drop_func(sourceinfo_t *const restrict si, const int __attribute__((unused)) parc, char *parv[])
 {
-        service_named_bind_command("chanserv", &cs_drop);
-        service_named_bind_command("chanserv", &cs_fdrop);
-}
+	const char *const name = parv[0];
+	const char *const key = parv[1];
 
-static void
-mod_deinit(const module_unload_intent_t intent)
-{
-	service_named_unbind_command("chanserv", &cs_drop);
-	service_named_unbind_command("chanserv", &cs_fdrop);
-}
-
-static void cs_cmd_drop(sourceinfo_t *si, int parc, char *parv[])
-{
-	mychan_t *mc;
-	char *name = parv[0];
-	char *key = parv[1];
-	char fullcmd[512];
-	char key0[80], key1[80];
-
-	if (!name)
+	if (! name)
 	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "DROP");
-		command_fail(si, fault_needmoreparams, _("Syntax: DROP <#channel>"));
+		(void) command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "DROP");
+		(void) command_fail(si, fault_needmoreparams, _("Syntax: DROP <#channel>"));
 		return;
 	}
 
 	if (*name != '#')
 	{
-		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "DROP");
-		command_fail(si, fault_badparams, _("Syntax: DROP <#channel>"));
+		(void) command_fail(si, fault_badparams, STR_INVALID_PARAMS, "DROP");
+		(void) command_fail(si, fault_badparams, _("Syntax: DROP <#channel>"));
 		return;
 	}
 
-	if (!(mc = mychan_find(name)))
+	mychan_t *mc;
+
+	if (! (mc = mychan_find(name)))
 	{
-		command_fail(si, fault_nosuch_target, _("Channel \2%s\2 is not registered."), name);
+		(void) command_fail(si, fault_nosuch_target, _("Channel \2%s\2 is not registered."), name);
 		return;
 	}
 
-	if (si->c != NULL)
+	if (si->c)
 	{
-		command_fail(si, fault_noprivs, _("For security reasons, you may not drop a channel registration with a fantasy command."));
+		(void) command_fail(si, fault_noprivs, _("For security reasons, you may not drop a channel "
+		                                         "registration with a fantasy command."));
 		return;
 	}
 
-	if (!is_founder(mc, entity(si->smu)))
+	if (! is_founder(mc, entity(si->smu)))
 	{
-		command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
+		(void) command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
 		return;
 	}
 
 	if (metadata_find(mc, "private:close:closer"))
 	{
-		logcommand(si, CMDLOG_REGISTER, "DROP: \2%s\2 failed to drop (closed)", mc->name);
-		command_fail(si, fault_noprivs, _("The channel \2%s\2 is closed; it cannot be dropped."), mc->name);
+		(void) logcommand(si, CMDLOG_REGISTER, "DROP: \2%s\2 failed to drop (closed)", mc->name);
+		(void) command_fail(si, fault_noprivs, _("The channel \2%s\2 is closed; it cannot be dropped."),
+		                    mc->name);
 		return;
 	}
 
 	if (mc->flags & MC_HOLD)
 	{
-		command_fail(si, fault_noprivs, _("The channel \2%s\2 is held; it cannot be dropped."), mc->name);
+		(void) command_fail(si, fault_noprivs, _("The channel \2%s\2 is held; it cannot be dropped."),
+		                    mc->name);
 		return;
 	}
 
-	if (si->su != NULL)
+	if (si->su)
 	{
 		const char *const challenge = create_weak_challenge(si, mc->name);
 
@@ -94,6 +75,8 @@ static void cs_cmd_drop(sourceinfo_t *si, int parc, char *parv[])
 
 		if (! key)
 		{
+			char fullcmd[BUFSIZE];
+
 			(void) snprintf(fullcmd, sizeof fullcmd, "/%s%s DROP %s %s", (ircd->uses_rcommand == false) ?
 			                "msg " : "", chansvs.me->disp, mc->name, challenge);
 
@@ -110,62 +93,112 @@ static void cs_cmd_drop(sourceinfo_t *si, int parc, char *parv[])
 		}
 	}
 
-	logcommand(si, CMDLOG_REGISTER, "DROP: \2%s\2", mc->name);
+	(void) logcommand(si, CMDLOG_REGISTER, "DROP: \2%s\2", mc->name);
 
-	hook_call_channel_drop(mc);
-	if (mc->chan != NULL && !(mc->chan->flags & CHAN_LOG))
-		part(mc->name, chansvs.nick);
-	object_unref(mc);
-	command_success_nodata(si, _("The channel \2%s\2 has been dropped."), name);
-	return;
+	(void) hook_call_channel_drop(mc);
+
+	if (mc->chan && ! (mc->chan->flags & CHAN_LOG))
+		(void) part(mc->name, chansvs.nick);
+
+	(void) command_success_nodata(si, _("The channel \2%s\2 has been dropped."), mc->name);
+	(void) object_unref(mc);
 }
 
-static void cs_cmd_fdrop(sourceinfo_t *si, int parc, char *parv[])
+static void
+cmd_cs_fdrop_func(sourceinfo_t *const restrict si, const int __attribute__((unused)) parc, char *parv[])
 {
-	mychan_t *mc;
-	char *name = parv[0];
+	const char *const name = parv[0];
 
-	if (!name)
+	if (! name)
 	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "FDROP");
-		command_fail(si, fault_needmoreparams, _("Syntax: FDROP <#channel>"));
+		(void) command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "FDROP");
+		(void) command_fail(si, fault_needmoreparams, _("Syntax: FDROP <#channel>"));
 		return;
 	}
 
 	if (*name != '#')
 	{
-		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "FDROP");
-		command_fail(si, fault_badparams, _("Syntax: FDROP <#channel>"));
+		(void) command_fail(si, fault_badparams, STR_INVALID_PARAMS, "FDROP");
+		(void) command_fail(si, fault_badparams, _("Syntax: FDROP <#channel>"));
 		return;
 	}
 
-	if (!(mc = mychan_find(name)))
+	mychan_t *mc;
+
+	if (! (mc = mychan_find(name)))
 	{
-		command_fail(si, fault_nosuch_target, _("Channel \2%s\2 is not registered."), name);
+		(void) command_fail(si, fault_nosuch_target, _("Channel \2%s\2 is not registered."), name);
 		return;
 	}
 
-	if (si->c != NULL)
+	if (si->c)
 	{
-		command_fail(si, fault_noprivs, _("For security reasons, you may not drop a channel registration with a fantasy command."));
+		(void) command_fail(si, fault_noprivs, _("For security reasons, you may not drop a channel "
+		                                         "registration with a fantasy command."));
 		return;
 	}
 
 	if (mc->flags & MC_HOLD)
 	{
-		command_fail(si, fault_noprivs, _("The channel \2%s\2 is held; it cannot be dropped."), mc->name);
+		(void) command_fail(si, fault_noprivs, _("The channel \2%s\2 is held; it cannot be dropped."),
+		                    mc->name);
 		return;
 	}
 
-	logcommand(si, CMDLOG_ADMIN | LG_REGISTER, "FDROP: \2%s\2", mc->name);
-	wallops("%s dropped the channel \2%s\2", get_oper_name(si), name);
+	(void) logcommand(si, CMDLOG_ADMIN | LG_REGISTER, "FDROP: \2%s\2", mc->name);
+	(void) wallops("%s dropped the channel \2%s\2", get_oper_name(si), mc->name);
 
-	hook_call_channel_drop(mc);
-	if (mc->chan != NULL && !(mc->chan->flags & CHAN_LOG))
-		part(mc->name, chansvs.nick);
-	object_unref(mc);
-	command_success_nodata(si, _("The channel \2%s\2 has been dropped."), name);
-	return;
+	(void) hook_call_channel_drop(mc);
+
+	if (mc->chan && ! (mc->chan->flags & CHAN_LOG))
+		(void) part(mc->name, chansvs.nick);
+
+	(void) command_success_nodata(si, _("The channel \2%s\2 has been dropped."), mc->name);
+	(void) object_unref(mc);
+}
+
+static command_t cmd_cs_drop = {
+
+	.name           = "DROP",
+	.desc           = N_("Drops a channel registration."),
+	.access         = AC_NONE,
+	.maxparc        = 2,
+	.cmd            = &cmd_cs_drop_func,
+
+	.help           = {
+
+		.path   = "cservice/drop",
+		.func   = NULL,
+	},
+};
+
+static command_t cmd_cs_fdrop = {
+
+	.name           = "FDROP",
+	.desc           = N_("Forces dropping of a channel registration."),
+	.access         = PRIV_CHAN_ADMIN,
+	.maxparc        = 1,
+	.cmd            = &cmd_cs_fdrop_func,
+
+	.help           = {
+
+		.path   = "cservice/fdrop",
+		.func   = NULL,
+	},
+};
+
+static void
+mod_init(module_t __attribute__((unused)) *const restrict m)
+{
+	(void) service_named_bind_command("chanserv", &cmd_cs_drop);
+	(void) service_named_bind_command("chanserv", &cmd_cs_fdrop);
+}
+
+static void
+mod_deinit(const module_unload_intent_t __attribute__((unused)) intent)
+{
+	(void) service_named_unbind_command("chanserv", &cmd_cs_drop);
+	(void) service_named_unbind_command("chanserv", &cmd_cs_fdrop);
 }
 
 SIMPLE_DECLARE_MODULE_V1("chanserv/drop", MODULE_UNLOAD_CAPABILITY_OK)
