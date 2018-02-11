720a07725991819f47489c2c9c30f5d6434725ee
Aaron Jones <aaronmdjones@gmail.com>
2018-02-11 07:27:31 +0000

    modules/crypto/: add SHA2-256 and SHA2-512 modules for crypt(3)
    cf. https://github.com/atheme/atheme/issues/603

---
+++ modules/crypto/crypt3-sha2-512.c
@@ -0,0 +1,146 @@
+/*
+ * Copyright (C) 2018 Atheme Development Group
+ * Rights to this code are as documented in doc/LICENSE.
+ *
+ * POSIX-style SHA2-512 crypt(3) wrapper.
+ */
+
+#include "crypt3.h"
+
+#ifdef HAVE_CRYPT
+
+static bool __attribute__((warn_unused_result))
+atheme_crypt3_sha2_512_selftest(void)
+{
+	static const char password[] = CRYPT3_MODULE_TEST_PASSWORD;
+	static const char parameters[] = CRYPT3_MODULE_TEST_VECTOR_SHA2_512;
+
+	const char *const result = atheme_crypt3_wrapper(password, parameters, __func__);
+
+	if (! result)
+		return false;
+
+	if (strcmp(result, parameters) != 0)
+	{
+		(void) slog(LG_ERROR, "%s: crypt(3) returned an incorrect result", __func__);
+		(void) slog(LG_ERROR, "%s: expected '%s', got '%s'", __func__, parameters, result);
+		return false;
+	}
+
+	return true;
+}
+
+static const char *
+atheme_crypt3_sha2_512_crypt(const char *const restrict password,
+                             const char __attribute__((unused)) *const restrict parameters)
+{
+	static const char saltchars[CRYPT3_SHA2_SALTCHARS_LENGTH] = CRYPT3_SHA2_SALTCHARS;
+
+	unsigned char rawsalt[CRYPT3_SHA2_SALTLENGTH];
+	char salt[sizeof rawsalt + 1];
+	char parv[PASSLEN];
+
+	(void) arc4random_buf(rawsalt, sizeof rawsalt);
+	(void) memset(salt, 0x00, sizeof salt);
+
+	for (size_t i = 0; i < sizeof rawsalt; i++)
+		salt[i] = saltchars[rawsalt[i] % sizeof saltchars];
+
+	if (snprintf(parv, PASSLEN, CRYPT3_SAVESALT_FORMAT_SHA2_512, salt) >= PASSLEN)
+	{
+		(void) slog(LG_ERROR, "%s: snprintf(3) would have overflowed parameters buffer (BUG)", __func__);
+		return NULL;
+	}
+
+	(void) slog(LG_DEBUG, CRYPT3_MODULE_WARNING, __func__);
+
+	return atheme_crypt3_wrapper(password, parv, __func__);
+}
+
+static bool
+atheme_crypt3_sha2_512_verify(const char *const restrict password, const char *const restrict parameters,
+                              unsigned int *const restrict flags)
+{
+	char hash[BUFSIZE];
+
+	if (sscanf(parameters, CRYPT3_LOADHASH_FORMAT_SHA2_512, hash) != 1)
+	{
+		if (sscanf(parameters, CRYPT3_LOADHASH_FORMAT_SHA2_512_EXT, hash) != 1)
+		{
+			(void) slog(LG_DEBUG, "%s: sscanf(3) was unsuccessful", __func__);
+			return false;
+		}
+	}
+	if (strlen(hash) != CRYPT3_LOADHASH_LENGTH_SHA2_512)
+	{
+		(void) slog(LG_DEBUG, "%s: digest not %u characters long", __func__, CRYPT3_LOADHASH_LENGTH_SHA2_512);
+		return false;
+	}
+
+	*flags |= PWVERIFY_FLAG_MYMODULE;
+
+	(void) slog(LG_DEBUG, CRYPT3_MODULE_WARNING, __func__);
+
+	const char *const result = atheme_crypt3_wrapper(password, parameters, __func__);
+
+	if (! result)
+		return false;
+
+	if (strcmp(parameters, result) != 0)
+	{
+		(void) slog(LG_DEBUG, "%s: strcmp(3) mismatch, invalid password?", __func__);
+		(void) slog(LG_DEBUG, "%s: expected '%s', got '%s'", __func__, parameters, result);
+		return false;
+	}
+
+	return true;
+}
+
+static crypt_impl_t crypto_crypt3_impl = {
+
+	.id        = "crypt3-sha2-512",
+	.crypt     = &atheme_crypt3_sha2_512_crypt,
+	.verify    = &atheme_crypt3_sha2_512_verify,
+};
+
+static void
+mod_init(module_t *const restrict m)
+{
+	if (! atheme_crypt3_sha2_512_selftest())
+	{
+		(void) slog(LG_ERROR, "%s: self-test failed, does this platform support this algorithm?", m->name);
+
+		m->mflags = MODTYPE_FAIL;
+		return;
+	}
+
+	(void) slog(LG_INFO, CRYPT3_MODULE_WARNING, m->name);
+
+	(void) crypt_register(&crypto_crypt3_impl);
+}
+
+static void
+mod_deinit(const module_unload_intent_t __attribute__((unused)) intent)
+{
+	(void) crypt_unregister(&crypto_crypt3_impl);
+}
+
+#else /* HAVE_CRYPT */
+
+static void
+mod_init(module_t *const restrict m)
+{
+	(void) slog(LG_ERROR, "%s: crypt(3) is not available on this platform", m->name);
+
+	m->mflags = MODTYPE_FAIL;
+}
+
+static void
+mod_deinit(const module_unload_intent_t __attribute__((unused)) intent)
+{
+	/* Nothing To Do */
+}
+
+#endif /* !HAVE_CRYPT */
+
+SIMPLE_DECLARE_MODULE_V1("crypto/crypt3-sha2-512", MODULE_UNLOAD_CAPABILITY_OK)
--- modules/crypto/crypt3.h
+++ modules/crypto/crypt3.h
@@ -15,9 +15,22 @@
 
 #define CRYPT3_LOADHASH_FORMAT_DES              "%[" CRYPT3_B64_CHARS_RANGE "]"
 #define CRYPT3_LOADHASH_FORMAT_MD5              "$1$%*[" CRYPT3_B64_CHARS_RANGE "]$%[" CRYPT3_B64_CHARS_RANGE "]"
+#define CRYPT3_LOADHASH_FORMAT_SHA2_256         "$5$%*[" CRYPT3_B64_CHARS_RANGE "]$%[" CRYPT3_B64_CHARS_RANGE "]"
+#define CRYPT3_LOADHASH_FORMAT_SHA2_512         "$6$%*[" CRYPT3_B64_CHARS_RANGE "]$%[" CRYPT3_B64_CHARS_RANGE "]"
+#define CRYPT3_LOADHASH_FORMAT_SHA2_256_EXT     "$5$rounds=%*u$%*[" CRYPT3_B64_CHARS_RANGE "]$%[" CRYPT3_B64_CHARS_RANGE "]"
+#define CRYPT3_LOADHASH_FORMAT_SHA2_512_EXT     "$6$rounds=%*u$%*[" CRYPT3_B64_CHARS_RANGE "]$%[" CRYPT3_B64_CHARS_RANGE "]"
+
+#define CRYPT3_SAVESALT_FORMAT_SHA2_256         "$5$%s$"
+#define CRYPT3_SAVESALT_FORMAT_SHA2_512         "$6$%s$"
 
 #define CRYPT3_LOADHASH_LENGTH_DES              0x0DU
 #define CRYPT3_LOADHASH_LENGTH_MD5              0x16U
+#define CRYPT3_LOADHASH_LENGTH_SHA2_256         0x2BU
+#define CRYPT3_LOADHASH_LENGTH_SHA2_512         0x56U
+
+#define CRYPT3_SHA2_SALTLENGTH                  0x10U
+#define CRYPT3_SHA2_SALTCHARS                   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+#define CRYPT3_SHA2_SALTCHARS_LENGTH            0x3EU
 
 #define CRYPT3_MODULE_TEST_PASSWORD             "YnqBeyzDmb4IHyXsGbasMBbmNiPM1E8G" \
                                                 "SAOsIwKhozJC0bZnHsaZBbT4x47iq4J7" \
@@ -31,6 +44,9 @@
 
 #define CRYPT3_MODULE_TEST_VECTOR_DES           "JCaMNN9g7BnPQ"
 #define CRYPT3_MODULE_TEST_VECTOR_MD5           "$1$xFIg9A$mXXbp3eKvgkOlChjAd2Eq1"
+#define CRYPT3_MODULE_TEST_VECTOR_SHA2_256      "$5$vishj8N7EZ05xejG$xz/ipkwwSKMJUEjF5gT.E7UWHQh9KI9ld0ornIVG0S1"
+#define CRYPT3_MODULE_TEST_VECTOR_SHA2_512      "$6$vishj8N7EZ05xejG$yjsXj1aO1Vh.ZuhLrAPG1ch8NA.2HaaND" \
+                                                "im5hixDtnNLr7i2c0fO7kM7ZGGkk0VBpqtaRzmnD7ob60m6JREb2/"
 
 #define CRYPT3_MODULE_WARNING                   "%s: this module relies upon platform-specific behaviour and " \
                                                 "may stop working if you migrate services to another machine!"
