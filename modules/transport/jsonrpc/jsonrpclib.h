bfc749537d7d667349fd857e0c99bf818f3ac4a8
Aaron Jones <aaronmdjones@gmail.com>
2018-02-24 23:56:31 +0000

    modules/transport/jsonrpc/jsonrpclib.h: remove unnecessary extern qualifier from function prototypes
    This qualifier is only needed on variables (both simple variables and
    function pointer variables).

--- modules/transport/jsonrpc/jsonrpclib.h
+++ modules/transport/jsonrpc/jsonrpclib.h
@@ -17,15 +17,15 @@ struct jsonrpc_sourceinfo
     char *id;
 };
 
-extern char *jsonrpc_normalizeBuffer(const char *buf);
+char *jsonrpc_normalizeBuffer(const char *buf);
 
-extern jsonrpc_method_fn get_json_method(const char *method_name);
+jsonrpc_method_fn get_json_method(const char *method_name);
 
-extern void jsonrpc_process(char *buffer, void *userdata);
-extern void jsonrpc_register_method(const char *method_name, bool (*method)(void *conn, mowgli_list_t *params, char *id));
-extern void jsonrpc_unregister_method(const char *method_name);
-extern void jsonrpc_send_data(void *conn, char *str);
-extern void jsonrpc_success_string(void *conn, const char *str, const char *id);
-extern void jsonrpc_failure_string(void *conn, int code, const char *str, const char *id);
+void jsonrpc_process(char *buffer, void *userdata);
+void jsonrpc_register_method(const char *method_name, bool (*method)(void *conn, mowgli_list_t *params, char *id));
+void jsonrpc_unregister_method(const char *method_name);
+void jsonrpc_send_data(void *conn, char *str);
+void jsonrpc_success_string(void *conn, const char *str, const char *id);
+void jsonrpc_failure_string(void *conn, int code, const char *str, const char *id);
 
 #endif
