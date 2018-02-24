ee944a1f71388780c2d68258adeac598edc03adf
Aaron Jones <aaronmdjones@gmail.com>
2018-02-24 23:54:19 +0000

    modules/groupserv/main/groupserv_main.h: remove unnecessary extern qualifier from function prototypes
    This qualifier is only needed on variables (both simple variables and
    function pointer variables).

--- modules/groupserv/main/groupserv_main.h
+++ modules/groupserv/main/groupserv_main.h
@@ -10,33 +10,33 @@
 
 extern struct groupserv_config gs_config;
 
-extern void mygroups_init(void);
-extern void mygroups_deinit(void);
-extern struct mygroup *mygroup_add(const char *name);
-extern struct mygroup *mygroup_add_id(const char *id, const char *name);
-extern struct mygroup *mygroup_find(const char *name);
+void mygroups_init(void);
+void mygroups_deinit(void);
+struct mygroup *mygroup_add(const char *name);
+struct mygroup *mygroup_add_id(const char *id, const char *name);
+struct mygroup *mygroup_find(const char *name);
 
-extern struct groupacs *groupacs_add(struct mygroup *mg, struct myentity *mt, unsigned int flags);
-extern struct groupacs *groupacs_find(struct mygroup *mg, struct myentity *mt, unsigned int flags, bool allow_recurse);
-extern void groupacs_delete(struct mygroup *mg, struct myentity *mt);
+struct groupacs *groupacs_add(struct mygroup *mg, struct myentity *mt, unsigned int flags);
+struct groupacs *groupacs_find(struct mygroup *mg, struct myentity *mt, unsigned int flags, bool allow_recurse);
+void groupacs_delete(struct mygroup *mg, struct myentity *mt);
 
-extern bool groupacs_sourceinfo_has_flag(struct mygroup *mg, struct sourceinfo *si, unsigned int flag);
+bool groupacs_sourceinfo_has_flag(struct mygroup *mg, struct sourceinfo *si, unsigned int flag);
 
-extern void gs_db_init(void);
-extern void gs_db_deinit(void);
+void gs_db_init(void);
+void gs_db_deinit(void);
 
-extern void gs_hooks_init(void);
-extern void gs_hooks_deinit(void);
+void gs_hooks_init(void);
+void gs_hooks_deinit(void);
 
-extern void mygroup_set_chanacs_validator(struct myentity *mt);
-extern unsigned int mygroup_count_flag(struct mygroup *mg, unsigned int flag);
-extern unsigned int gs_flags_parser(char *flagstring, bool allow_minus, unsigned int flags);
-extern void remove_group_chanacs(struct mygroup *mg);
+void mygroup_set_chanacs_validator(struct myentity *mt);
+unsigned int mygroup_count_flag(struct mygroup *mg, unsigned int flag);
+unsigned int gs_flags_parser(char *flagstring, bool allow_minus, unsigned int flags);
+void remove_group_chanacs(struct mygroup *mg);
 
-extern mowgli_list_t *myentity_get_membership_list(struct myentity *mt);
-extern unsigned int myentity_count_group_flag(struct myentity *mt, unsigned int flagset);
+mowgli_list_t *myentity_get_membership_list(struct myentity *mt);
+unsigned int myentity_count_group_flag(struct myentity *mt, unsigned int flagset);
 
-extern const char *mygroup_founder_names(struct mygroup *mg);
+const char *mygroup_founder_names(struct mygroup *mg);
 
 /* services plumbing */
 extern struct service *groupsvs;
