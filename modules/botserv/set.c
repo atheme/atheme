82af6a968a534d965b6916bda8b83b1428db9f8c
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:05:53 +0000

    modules/botserv/set.c: put function names on their own line

--- modules/botserv/set.c
+++ modules/botserv/set.c
@@ -25,6 +25,7 @@ mod_init(struct module *const restrict m)
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
+
 }
 
 SIMPLE_DECLARE_MODULE_V1("botserv/set", MODULE_UNLOAD_CAPABILITY_OK)
