f0a40b8e143233e1c1d827bfb21e91f2d1e8fe4d
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:05:55 +0000

    modules/chanserv/getkey.c: put function names on their own line

--- modules/chanserv/getkey.c
+++ modules/chanserv/getkey.c
@@ -24,7 +24,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("chanserv", &cs_getkey);
 }
 
-static void cs_cmd_getkey(struct sourceinfo *si, int parc, char *parv[])
+static void
+cs_cmd_getkey(struct sourceinfo *si, int parc, char *parv[])
 {
 	char *chan = parv[0];
 	struct mychan *mc;
