315eba667d3c31d2ed7b84853794681311c718f1
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:14 +0000

    modules/protocol/ratbox.c: put function names on their own line

--- modules/protocol/ratbox.c
+++ modules/protocol/ratbox.c
@@ -93,6 +93,7 @@ mod_init(struct module *const restrict m)
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
+
 }
 
 SIMPLE_DECLARE_MODULE_V1("protocol/ratbox", MODULE_UNLOAD_CAPABILITY_NEVER)
