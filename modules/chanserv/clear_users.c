64caf5bed874bc3602a1ab888f0277cc75d72b06
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:05:54 +0000

    modules/chanserv/clear_users.c: put function names on their own line

--- modules/chanserv/clear_users.c
+++ modules/chanserv/clear_users.c
@@ -28,7 +28,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	command_delete(&cs_clear_users, *cs_clear_cmds);
 }
 
-static void cs_cmd_clear_users(struct sourceinfo *si, int parc, char *parv[])
+static void
+cs_cmd_clear_users(struct sourceinfo *si, int parc, char *parv[])
 {
 	char fullreason[200];
 	struct channel *c;
