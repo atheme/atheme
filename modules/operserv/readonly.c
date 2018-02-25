7c264493efec812a9cf7c76f0fd4377d764bb3a8
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:12 +0000

    modules/operserv/readonly.c: put function names on their own line

--- modules/operserv/readonly.c
+++ modules/operserv/readonly.c
@@ -24,7 +24,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("operserv", &os_readonly);
 }
 
-static void os_cmd_readonly(struct sourceinfo *si, int parc, char *parv[])
+static void
+os_cmd_readonly(struct sourceinfo *si, int parc, char *parv[])
 {
 	struct service *svs;
 	char *action = parv[0];
