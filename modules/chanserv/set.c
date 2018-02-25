5d6ecc3dc2f5420f3037cb08c5260888ede2768f
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:05:56 +0000

    modules/chanserv/set.c: put function names on their own line

--- modules/chanserv/set.c
+++ modules/chanserv/set.c
@@ -33,6 +33,7 @@ mod_init(struct module *const restrict m)
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
+
 }
 
 SIMPLE_DECLARE_MODULE_V1("chanserv/set", MODULE_UNLOAD_CAPABILITY_OK)
