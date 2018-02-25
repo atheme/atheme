22be89fabd3270b019a7cca1b0bc7b4adb85602a
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:02 +0000

    modules/groupserv/set_open.c: put function names on their own line

--- modules/groupserv/set_open.c
+++ modules/groupserv/set_open.c
@@ -12,7 +12,8 @@ static void gs_cmd_set_open(struct sourceinfo *si, int parc, char *parv[]);
 
 struct command gs_set_open = { "OPEN", N_("Sets the group as open for anyone to join."), AC_AUTHENTICATED, 2, gs_cmd_set_open, { .path = "groupserv/set_open" } };
 
-static void gs_cmd_set_open(struct sourceinfo *si, int parc, char *parv[])
+static void
+gs_cmd_set_open(struct sourceinfo *si, int parc, char *parv[])
 {
 	struct mygroup *mg;
 
