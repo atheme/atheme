62afd8df9105da791cb68aac8c2d3c0acf560f0e
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:05:55 +0000

    modules/chanserv/count.c: put function names on their own line

--- modules/chanserv/count.c
+++ modules/chanserv/count.c
@@ -25,7 +25,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("chanserv", &cs_count);
 }
 
-static void cs_cmd_count(struct sourceinfo *si, int parc, char *parv[])
+static void
+cs_cmd_count(struct sourceinfo *si, int parc, char *parv[])
 {
 	char *chan = parv[0];
 	struct chanacs *ca;
