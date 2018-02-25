8f89717a9c3cec8dbf4d648ebc293318a331fd76
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:05:54 +0000

    modules/chanserv/clear_akicks.c: put function names on their own line

--- modules/chanserv/clear_akicks.c
+++ modules/chanserv/clear_akicks.c
@@ -27,7 +27,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	command_delete(&cs_clear_akicks, *cs_clear_cmds);
 }
 
-static void cs_cmd_clear_akicks(struct sourceinfo *si, int parc, char *parv[])
+static void
+cs_cmd_clear_akicks(struct sourceinfo *si, int parc, char *parv[])
 {
 	struct mychan *mc;
 	mowgli_node_t *n, *tn;
