97afb4d5613f048cb020692c9d7463ea62597b38
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:13 +0000

    modules/operserv/uptime.c: put function names on their own line

--- modules/operserv/uptime.c
+++ modules/operserv/uptime.c
@@ -23,7 +23,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("operserv", &os_uptime);
 }
 
-static void os_cmd_uptime(struct sourceinfo *si, int parc, char *parv[])
+static void
+os_cmd_uptime(struct sourceinfo *si, int parc, char *parv[])
 {
 	logcommand(si, CMDLOG_GET, "UPTIME");
 
