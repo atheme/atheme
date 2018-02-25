1ce7a45751e9cca4305dea1036f429ca9391e1a5
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:10 +0000

    modules/nickserv/verify.c: put function names on their own line

--- modules/nickserv/verify.c
+++ modules/nickserv/verify.c
@@ -27,7 +27,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("nickserv", &ns_fverify);
 }
 
-static void ns_cmd_verify(struct sourceinfo *si, int parc, char *parv[])
+static void
+ns_cmd_verify(struct sourceinfo *si, int parc, char *parv[])
 {
 	struct myuser *mu;
 	struct metadata *md;
@@ -147,7 +148,8 @@ static void ns_cmd_verify(struct sourceinfo *si, int parc, char *parv[])
 	}
 }
 
-static void ns_cmd_fverify(struct sourceinfo *si, int parc, char *parv[])
+static void
+ns_cmd_fverify(struct sourceinfo *si, int parc, char *parv[])
 {
 	struct myuser *mu;
 	struct metadata *md;
