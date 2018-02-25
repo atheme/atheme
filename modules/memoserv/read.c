e95cb21b012e27109a30bb9d57c49d16265a8fff
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:05 +0000

    modules/memoserv/read.c: put function names on their own line

--- modules/memoserv/read.c
+++ modules/memoserv/read.c
@@ -26,7 +26,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("memoserv", &ms_read);
 }
 
-static void ms_cmd_read(struct sourceinfo *si, int parc, char *parv[])
+static void
+ms_cmd_read(struct sourceinfo *si, int parc, char *parv[])
 {
 	/* Misc structs etc */
 	struct myuser *tmu;
