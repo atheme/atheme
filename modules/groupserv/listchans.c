e3e33ced0727f2f231556fc472196b82cf1a9f24
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:01 +0000

    modules/groupserv/listchans.c: put function names on their own line

--- modules/groupserv/listchans.c
+++ modules/groupserv/listchans.c
@@ -25,7 +25,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("groupserv", &gs_listchans);
 }
 
-static void gs_cmd_listchans(struct sourceinfo *si, int parc, char *parv[])
+static void
+gs_cmd_listchans(struct sourceinfo *si, int parc, char *parv[])
 {
 	struct mygroup *mg;
 	mowgli_node_t *n;
