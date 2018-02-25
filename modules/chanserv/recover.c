f5d66f8b842ead0895dd3be10b3830840f3aba05
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:05:56 +0000

    modules/chanserv/recover.c: put function names on their own line

--- modules/chanserv/recover.c
+++ modules/chanserv/recover.c
@@ -24,7 +24,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("chanserv", &cs_recover);
 }
 
-static void cs_cmd_recover(struct sourceinfo *si, int parc, char *parv[])
+static void
+cs_cmd_recover(struct sourceinfo *si, int parc, char *parv[])
 {
 	struct chanuser *cu, *origin_cu = NULL;
 	struct mychan *mc;
