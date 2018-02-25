00b56472e1671e74cc7fcd90de3d3840053a95e7
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:11 +0000

    modules/operserv/modlist.c: put function names on their own line

--- modules/operserv/modlist.c
+++ modules/operserv/modlist.c
@@ -23,7 +23,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("operserv", &os_modlist);
 }
 
-static void os_cmd_modlist(struct sourceinfo *si, int parc, char *parv[])
+static void
+os_cmd_modlist(struct sourceinfo *si, int parc, char *parv[])
 {
 	mowgli_node_t *n;
 	unsigned int i = 0;
