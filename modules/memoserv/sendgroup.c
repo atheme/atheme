ee8c51754fdd2018bcf5ab71f2edf524867ed18b
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:05 +0000

    modules/memoserv/sendgroup.c: put function names on their own line

--- modules/memoserv/sendgroup.c
+++ modules/memoserv/sendgroup.c
@@ -27,7 +27,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("memoserv", &ms_sendgroup);
 }
 
-static void ms_cmd_sendgroup(struct sourceinfo *si, int parc, char *parv[])
+static void
+ms_cmd_sendgroup(struct sourceinfo *si, int parc, char *parv[])
 {
 	/* misc structs etc */
 	struct myuser *tmu;
