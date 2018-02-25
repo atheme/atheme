7d3db2007586f70c3fedfd4f242d470af1bb0a30
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:12 +0000

    modules/operserv/restart.c: put function names on their own line

--- modules/operserv/restart.c
+++ modules/operserv/restart.c
@@ -23,7 +23,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("operserv", &os_restart);
 }
 
-static void os_cmd_restart(struct sourceinfo *si, int parc, char *parv[])
+static void
+os_cmd_restart(struct sourceinfo *si, int parc, char *parv[])
 {
 	logcommand(si, CMDLOG_ADMIN, "RESTART");
 	wallops("Restarting by request of \2%s\2.", get_oper_name(si));
