94f3a4a2ad8fdd34e6cbc8c86c48c8b46050568b
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:09 +0000

    modules/nickserv/set_password.c: put function names on their own line

--- modules/nickserv/set_password.c
+++ modules/nickserv/set_password.c
@@ -30,7 +30,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 }
 
 /* SET PASSWORD <password> */
-static void ns_cmd_set_password(struct sourceinfo *si, int parc, char *parv[])
+static void
+ns_cmd_set_password(struct sourceinfo *si, int parc, char *parv[])
 {
 	char *password = parv[0];
 
