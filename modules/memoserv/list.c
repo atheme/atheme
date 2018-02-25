832c194a4184954a92b2c3afe0b37107dcde4f53
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:04 +0000

    modules/memoserv/list.c: put function names on their own line

--- modules/memoserv/list.c
+++ modules/memoserv/list.c
@@ -24,7 +24,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("memoserv", &ms_list);
 }
 
-static void ms_cmd_list(struct sourceinfo *si, int parc, char *parv[])
+static void
+ms_cmd_list(struct sourceinfo *si, int parc, char *parv[])
 {
 	/* Misc structs etc */
 	struct mymemo *memo;
