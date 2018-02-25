a663053f5d0eafb1cb810587c86df574ed07ae13
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:05:58 +0000

    modules/chanserv/status.c: put function names on their own line

--- modules/chanserv/status.c
+++ modules/chanserv/status.c
@@ -24,7 +24,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("chanserv", &cs_status);
 }
 
-static void cs_cmd_status(struct sourceinfo *si, int parc, char *parv[])
+static void
+cs_cmd_status(struct sourceinfo *si, int parc, char *parv[])
 {
 	char *chan = parv[0];
 
