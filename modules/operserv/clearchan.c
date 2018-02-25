a44bef99b344cb8312c713e8abd47f088c2885d7
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:10 +0000

    modules/operserv/clearchan.c: put function names on their own line

--- modules/operserv/clearchan.c
+++ modules/operserv/clearchan.c
@@ -29,7 +29,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("operserv", &os_clearchan);
 }
 
-static void os_cmd_clearchan(struct sourceinfo *si, int parc, char *parv[])
+static void
+os_cmd_clearchan(struct sourceinfo *si, int parc, char *parv[])
 {
 	struct chanuser *cu = NULL;
 	mowgli_node_t *n, *tn;
