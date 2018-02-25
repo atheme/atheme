df27f41b62d2e33e725d4f0e42f9e0737ab3f526
Aaron Jones <aaronmdjones@gmail.com>
2018-02-25 05:06:11 +0000

    modules/operserv/modinspect.c: put function names on their own line

--- modules/operserv/modinspect.c
+++ modules/operserv/modinspect.c
@@ -23,7 +23,8 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 	service_named_unbind_command("operserv", &os_modinspect);
 }
 
-static void os_cmd_modinspect(struct sourceinfo *si, int parc, char *parv[])
+static void
+os_cmd_modinspect(struct sourceinfo *si, int parc, char *parv[])
 {
 	char *mname = parv[0];
 	struct module *m;
