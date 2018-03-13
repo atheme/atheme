19e52e137d2351ab2e7639fafc40483746f1ce23
Aaron Jones <aaronmdjones@gmail.com>
2018-03-13 18:17:44 +0000

    modules/chanserv/access.c: remove misuse of comma operator

--- modules/chanserv/access.c
+++ modules/chanserv/access.c
@@ -74,9 +74,15 @@ cs_cmd_access(struct sourceinfo *si, int parc, char *parv[])
 	}
 
 	if (parv[0][0] == '#')
-		chan = parv[0], cmd = parv[1];
+	{
+		chan = parv[0];
+		cmd = parv[1];
+	}
 	else if (parv[1][0] == '#')
-		cmd = parv[0], chan = parv[1];
+	{
+		cmd = parv[0];
+		chan = parv[1];
+	}
 	else
 	{
 		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "ACCESS");
@@ -115,9 +121,15 @@ cs_cmd_role(struct sourceinfo *si, int parc, char *parv[])
 	}
 
 	if (parv[0][0] == '#')
-		chan = parv[0], cmd = parv[1];
+	{
+		chan = parv[0];
+		cmd = parv[1];
+	}
 	else if (parv[1][0] == '#')
-		cmd = parv[0], chan = parv[1];
+	{
+		cmd = parv[0];
+		chan = parv[1];
+	}
 	else
 	{
 		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "ROLE");
