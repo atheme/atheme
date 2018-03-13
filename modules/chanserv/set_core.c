b1e0613e4cf43a092732c6e085d03d01237d58d4
Aaron Jones <aaronmdjones@gmail.com>
2018-03-13 18:17:47 +0000

    modules/chanserv/set_core.c: remove misuse of comma operator

--- modules/chanserv/set_core.c
+++ modules/chanserv/set_core.c
@@ -47,9 +47,15 @@ cs_cmd_set(struct sourceinfo *si, int parc, char *parv[])
 	}
 
 	if (parv[0][0] == '#')
-		chan = parv[0], cmd = parv[1];
+	{
+		chan = parv[0];
+		cmd = parv[1];
+	}
 	else if (parv[1][0] == '#')
-		cmd = parv[0], chan = parv[1];
+	{
+		cmd = parv[0];
+		chan = parv[1];
+	}
 	else
 	{
 		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "SET");
