4660df1334348c575210efa117ca89c58fa65f1b
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:27 +0000

    modules/nickserv/restrict.c: apply commit 507f49117bea9a3f

--- modules/nickserv/restrict.c
+++ modules/nickserv/restrict.c
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2005 William Pitcock
- * Rights to this code are as documented in doc/LICENSE.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
+ *
+ * Copyright (C) 2005-2010 William Pitcock
  *
  * Restrictions for nicknames.
  */
