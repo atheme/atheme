00f0a803b615c20faa75fb09e705736eec1532eb
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:03 +0000

    libathemecore/commandhelp.c: apply commit 507f49117bea9a3f

--- libathemecore/commandhelp.c
+++ libathemecore/commandhelp.c
@@ -1,6 +1,6 @@
 /*
- * atheme-services: A collection of minimalist IRC services
- * commandhelp.c: Help system implementation.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2005-2007 Atheme Project (http://atheme.org/)
  * Copyright (C) 2018 Atheme Development Group (https://atheme.github.io/)
@@ -9,17 +9,8 @@
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
- * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * atheme-services: A collection of minimalist IRC services
+ * commandhelp.c: Help system implementation.
  */
 
 #include "atheme.h"
