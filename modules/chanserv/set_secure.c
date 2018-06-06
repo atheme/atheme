e8f89889dfe711a7dcc3be8760e4b3e70f75ba81
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:14 +0000

    modules/chanserv/set_secure.c: apply commit 507f49117bea9a3f

--- modules/chanserv/set_secure.c
+++ modules/chanserv/set_secure.c
@@ -1,7 +1,9 @@
 /*
- * Copyright (c) 2003-2004 E. Will et al.
- * Copyright (c) 2006-2010 Atheme Development Group
- * Rights to this code are documented in doc/LICENSE.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
+ *
+ * Copyright (C) 2003-2004 E. Will, et al.
+ * Copyright (C) 2006-2010 Atheme Project (http://atheme.org/)
  *
  * This file contains routines to handle the CService SET SECURE command.
  */
