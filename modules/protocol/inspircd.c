22b0b76da28128841af7a59b86fb552977e98b76
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:33 +0000

    modules/protocol/inspircd.c: apply commit 507f49117bea9a3f

--- modules/protocol/inspircd.c
+++ modules/protocol/inspircd.c
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2005-2006 Atheme Development Group
- * Rights to this code are documented in doc/LICENSE.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
+ *
+ * Copyright (C) 2005-2012 Atheme Project (http://atheme.org/)
  *
  * InspIRCd link support
  */
