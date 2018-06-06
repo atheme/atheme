89b79b90e81f47781cd4ac869dfe69e61421ae5b
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:09 +0000

    modules/botserv/botserv.h: apply commit 507f49117bea9a3f

--- modules/botserv/botserv.h
+++ modules/botserv/botserv.h
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2009 Celestin, et al.
- * Rights to this code are as documented in doc/LICENSE.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
+ *
+ * Copyright (C) 2009 Celestin, et al.
  *
  * BotServ common definitions.
  */
