2f0b2e2452c0a1d0852a7ab3c34f7c849ca2bf89
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:24 +0000

    modules/misc/httpd.c: apply commit 507f49117bea9a3f

--- modules/misc/httpd.c
+++ modules/misc/httpd.c
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2005-2007 Jilles Tjoelker et al.
- * Rights to this code are as documented in doc/LICENSE.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
+ *
+ * Copyright (C) 2005-2007 Jilles Tjoelker, et al.
  *
  * Generic HTTPd
  */
