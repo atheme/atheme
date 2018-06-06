ee1cc7a5d7d6d5d6d6cc55c9cde96506178f9e69
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:31:58 +0000

    include/crypto.h: apply commit 507f49117bea9a3f

--- include/crypto.h
+++ include/crypto.h
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2005 Atheme Development Group
- * Rights to this code are as documented under doc/LICENSE.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
+ *
+ * Copyright (C) 2005 Atheme Project (http://atheme.org/)
  *
  * Cryptographic module support.
  */
