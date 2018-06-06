cc738971f534ead4078f4a032d0c1358574fe8a2
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:18 +0000

    modules/gameserv/gameserv_common.h: apply commit 507f49117bea9a3f

--- modules/gameserv/gameserv_common.h
+++ modules/gameserv/gameserv_common.h
@@ -1,8 +1,9 @@
 /*
- * Copyright (c) 2005-2010 William Pitcock <nenolod@atheme.org>.
- * Copyright (c) 2006-2007 Jilles Tjoelker <jilles@stack.nl>.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
- * Rights to this code are documented in doc/LICENSE.
+ * Copyright (C) 2005-2010 William Pitcock <nenolod@atheme.org>
+ * Copyright (C) 2006-2007 Jilles Tjoelker <jilles@stack.nl>
  */
 
 #ifndef ATHEME_MOD_GAMESERV_GAMESERV_COMMON_H
