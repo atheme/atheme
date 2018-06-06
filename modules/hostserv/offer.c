99e5ebd69d9e1120b51ba51eba07064945ad256b
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:23 +0000

    modules/hostserv/offer.c: apply commit 507f49117bea9a3f

--- modules/hostserv/offer.c
+++ modules/hostserv/offer.c
@@ -1,7 +1,9 @@
 /*
- * Copyright (c) 2014-2017 Xtheme Development Group <Xtheme.org>
- * Copyright (c) 2005 William Pitcock <nenolod -at- nenolod.net>
- * Rights to this code are as documented in doc/LICENSE.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
+ *
+ * Copyright (C) 2005-2010 William Pitcock <nenolod -at- nenolod.net>
+ * Copyright (C) 2014-2017 Xtheme Development Group (https://xtheme.org/)
  *
  * Allows opers to offer vhosts to users
  */
