37f86cd8a648c38dfca2b208cafcec7798c37e7a
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:37 +0000

    modules/scripting/perl/api/perl_hooks_extra.h: apply commit 507f49117bea9a3f

--- modules/scripting/perl/api/perl_hooks_extra.h
+++ modules/scripting/perl/api/perl_hooks_extra.h
@@ -1,3 +1,10 @@
+/*
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
+ *
+ * Copyright (C) 2011-2013 Atheme Project (http://atheme.org/)
+ */
+
 #ifndef ATHEME_MOD_SCRIPTING_PERL_API_PERL_HOOKS_EXTRA_H
 #define ATHEME_MOD_SCRIPTING_PERL_API_PERL_HOOKS_EXTRA_H 1
 
