4d63dcd3a125048f1790b4f8bab7c1e6a9e812b8
Aaron Jones <aaronmdjones@gmail.com>
2018-06-06 00:32:27 +0000

    modules/nickserv/return.c: apply commit 507f49117bea9a3f

--- modules/nickserv/return.c
+++ modules/nickserv/return.c
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2005 Alex Lambert
- * Rights to this code are as documented in doc/LICENSE.
+ * SPDX-License-Identifier: ISC
+ * SPDX-URL: https://spdx.org/licenses/ISC.html
+ *
+ * Copyright (C) 2005 Alex Lambert
  *
  * Implements nickserv RETURN.
  */
