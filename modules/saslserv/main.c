atheme/atheme

commit 306312d22261c454766d66a9a8c66ece6f0cc8e0

Memory Allocator: Balance s(c|m|re)alloc() with sfree() & cleanups
If we ever want to make use of a third-party memory allocation library
(e.g. for debugging or security hardening) without interposing calls
to the libc malloc(3) family of functions, we need an sfree() function
of our own.

This commit also tidies up some pointless code such as:

  - if (foo != NULL)
  -     free(foo);

  + sfree(foo);

... as free(NULL) is well-specified to do nothing [1]; and:

  - foo = bar ? sstrdup(bar) : NULL;
  + foo = sstrdup(bar);

... as our sstrdup() wrapper already returns NULL if it is given NULL.

[1] void free(void *ptr);                                   free(3)

    The free() function frees the memory space pointed to  by  ptr,
    which  must  have been returned by a previous call to malloc(),
    calloc(), or realloc().  Otherwise, or if free(ptr) has already
    been called before, undefined behavior occurs.  If ptr is NULL,
    no operation is performed.

modules/saslserv/main.c
@@ -428,7 +428,7 @@ sasl_packet(struct sasl_session *const restrict p, const char *const restrict bu
 			char outbuf[SASL_C2S_MAXLEN + 1];
 			const size_t rs = base64_encode(out, out_len, outbuf, sizeof outbuf);
 
-			(void) free(out);
+			(void) sfree(out);
 
 			if (rs == (size_t) -1)
 			{
@@ -464,7 +464,7 @@ sasl_packet(struct sasl_session *const restrict p, const char *const restrict bu
 		}
 	}
 
-	(void) free(out);
+	(void) sfree(out);
 	return false;
 }
 
@@ -476,7 +476,7 @@ sasl_buf_process(struct sasl_session *const restrict p)
 	if (! sasl_packet(p, p->buf, p->len))
 		return false;
 
-	(void) free(p->buf);
+	(void) sfree(p->buf);
 
 	p->buf = NULL;
 	p->len = 0;
@@ -506,7 +506,7 @@ sasl_input_startauth(const struct sasl_message *const restrict smsg, struct sasl
 			return false;
 		}
 
-		(void) free(p->certfp);
+		(void) sfree(p->certfp);
 
 		p->certfp = sstrdup(smsg->parv[1]);
 		p->tls = true;
@@ -611,12 +611,12 @@ destroy_session(struct sasl_session *const restrict p)
 	if (p->si)
 		(void) atheme_object_unref(p->si);
 
-	(void) free(p->certfp);
-	(void) free(p->host);
-	(void) free(p->buf);
-	(void) free(p->uid);
-	(void) free(p->ip);
-	(void) free(p);
+	(void) sfree(p->certfp);
+	(void) sfree(p->host);
+	(void) sfree(p->buf);
+	(void) sfree(p->uid);
+	(void) sfree(p->ip);
+	(void) sfree(p);
 }
 
 static inline void

modules/alis/main.c
@@ -264,11 +264,8 @@ free_alis(struct alis_query *query)
 {
 	return_if_fail(query != NULL);
 
-	if (query->mask)
-		free(query->mask);
-
-	if (query->topic)
-		free(query->topic);
+	sfree(query->mask);
+	sfree(query->topic);
 }
 
 static void

modules/botserv/main.c
@@ -606,23 +606,23 @@ bs_cmd_change(struct sourceinfo *si, int parc, char *parv[])
 		case 5:
 			if (strlen(parv[4]) <= GECOSLEN)
 			{
-				free(bot->real);
+				sfree(bot->real);
 				bot->real = sstrdup(parv[4]);
 			}
 			else
 				command_fail(si, fault_badparams, _("\2%s\2 is an invalid realname, not changing it"), parv[4]);
 		case 4:
-			free(bot->host);
+			sfree(bot->host);
 			bot->host = sstrdup(parv[3]);
 		case 3:
 			// XXX: we really need an is_valid_user(), but this is close enough. --nenolod
 			if (is_valid_username(parv[2])) {
-				free(bot->user);
+				sfree(bot->user);
 				bot->user = sstrndup(parv[2], USERLEN);
 			} else
 				command_fail(si, fault_badparams, _("\2%s\2 is an invalid username, not changing it."), parv[2]);
 		case 2:
-			free(bot->nick);
+			sfree(bot->nick);
 			bot->nick = sstrdup(parv[1]);
 			break;
 		default:
@@ -695,11 +695,11 @@ bs_cmd_delete(struct sourceinfo *si, int parc, char *parv[])
 
 	mowgli_node_delete(&bot->bnode, &bs_bots);
 	service_delete(bot->me);
-	free(bot->nick);
-	free(bot->user);
-	free(bot->real);
-	free(bot->host);
-	free(bot);
+	sfree(bot->nick);
+	sfree(bot->user);
+	sfree(bot->real);
+	sfree(bot->host);
+	sfree(bot);
 
 	logcommand(si, CMDLOG_ADMIN, "BOT:DEL: \2%s\2", parv[0]);
 	command_success_nodata(si, "Bot \2%s\2 has been deleted.", parv[0]);

modules/chanfix/main.c
@@ -19,7 +19,7 @@ mod_init(struct module ATHEME_VATTR_UNUSED *const restrict m)
 
 	if (rec != NULL)
 	{
-		free(rec);
+		sfree(rec);
 		return;
 	}
 

modules/global/main.c
@@ -64,13 +64,13 @@ gs_cmd_global(struct sourceinfo *si, const int parc, char *parv[])
 			global = (struct global_ *)n->data;
 			mowgli_node_delete(n, &globlist);
 			mowgli_node_free(n);
-			free(global->text);
+			sfree(global->text);
 			mowgli_heap_free(glob_heap, global);
 		}
 
 		mowgli_heap_destroy(glob_heap);
 		glob_heap = NULL;
-		free(sender);
+		sfree(sender);
 		sender = NULL;
 
 		command_success_nodata(si, "The pending message has been deleted.");
@@ -113,13 +113,13 @@ gs_cmd_global(struct sourceinfo *si, const int parc, char *parv[])
 			global = (struct global_ *)n->data;
 			mowgli_node_delete(n, &globlist);
 			mowgli_node_free(n);
-			free(global->text);
+			sfree(global->text);
 			mowgli_heap_free(glob_heap, global);
 		}
 
 		mowgli_heap_destroy(glob_heap);
 		glob_heap = NULL;
-		free(sender);
+		sfree(sender);
 		sender = NULL;
 
 		command_success_nodata(si, "The global notice has been sent.");

modules/groupserv/main/main.c
@@ -43,7 +43,7 @@ mod_init(struct module *const restrict m)
 		groupacs_heap = rec->groupacs_heap;
 
 		mowgli_global_storage_free("atheme.groupserv.main.persist");
-		free(rec);
+		sfree(rec);
 
 		MYENTITY_FOREACH_T(grp, &iter, ENT_GROUP)
 		{

modules/infoserv/main.c
@@ -165,7 +165,7 @@ display_info(hook_user_nick_t *data)
 			notice(infoserv->nick, u->nick, "[\2%s\2] Notice from %s, posted %s:",
 				y, l->nick, dBuf);
 			notice(infoserv->nick, u->nick, "%s", l->story);
-			free(y);
+			sfree(y);
 			count++;
 
 			// only display three latest entries, max.
@@ -212,7 +212,7 @@ display_oper_info(struct user *u)
 			notice(infoserv->nick, u->nick, "[\2%s\2] Notice from %s, posted %s:",
 				y, o->nick, dBuf);
 			notice(infoserv->nick, u->nick, "%s", o->story);
-			free(y);
+			sfree(y);
 			count++;
 
 			// only display three latest entries, max.
@@ -275,7 +275,7 @@ is_cmd_post(struct sourceinfo *si, int parc, char *parv[])
 		msg_global_sts(infoserv->me, "*", buf);
 		command_success_nodata(si, _("The InfoServ message has been sent"));
 		logcommand(si, CMDLOG_ADMIN, "INFO:POST: Importance: \2%s\2, Subject: \2%s\2, Message: \2%s\2", importance, y, story);
-		free(y);
+		sfree(y);
 		return;
 	}
 
@@ -285,7 +285,7 @@ is_cmd_post(struct sourceinfo *si, int parc, char *parv[])
 		notice_global_sts(infoserv->me, "*", buf);
 		command_success_nodata(si, _("The InfoServ message has been sent"));
 		logcommand(si, CMDLOG_ADMIN, "INFO:POST: Importance: \2%s\2, Subject: \2%s\2, Message: \2%s\2", importance, y, story);
-		free(y);
+		sfree(y);
 		return;
 	}
 
@@ -322,7 +322,7 @@ is_cmd_post(struct sourceinfo *si, int parc, char *parv[])
 		notice_global_sts(infoserv->me, "*", buf);
 	}
 
-	free(y);
+	sfree(y);
 
 	return;
 }
@@ -365,9 +365,9 @@ is_cmd_del(struct sourceinfo *si, int parc, char *parv[])
 			mowgli_node_delete(n, &logon_info);
 
 			strshare_unref(l->nick);
-			free(l->subject);
-			free(l->story);
-			free(l);
+			sfree(l->subject);
+			sfree(l->story);
+			sfree(l);
 
 			command_success_nodata(si, _("Deleted entry %d from logon info."), id);
 			return;
@@ -416,9 +416,9 @@ is_cmd_odel(struct sourceinfo *si, int parc, char *parv[])
 			mowgli_node_delete(n, &operlogon_info);
 
 			strshare_unref(o->nick);
-			free(o->subject);
-			free(o->story);
-			free(o);
+			sfree(o->subject);
+			sfree(o->story);
+			sfree(o);
 
 			command_success_nodata(si, _("Deleted entry %d from oper logon info."), id);
 			return;
@@ -450,7 +450,7 @@ is_cmd_list(struct sourceinfo *si, int parc, char *parv[])
 		strftime(dBuf, BUFSIZE, "%H:%M on %m/%d/%Y", &tm);
 		command_success_nodata(si, "%d: [\2%s\2] by \2%s\2 at \2%s\2: \2%s\2",
 			x, y, l->nick, dBuf, l->story);
-		free(y);
+		sfree(y);
 	}
 
 	command_success_nodata(si, _("End of list."));
@@ -479,7 +479,7 @@ is_cmd_olist(struct sourceinfo *si, int parc, char *parv[])
 		strftime(dBuf, BUFSIZE, "%H:%M on %m/%d/%Y", &tm);
 		command_success_nodata(si, "%d: [\2%s\2] by \2%s\2 at \2%s\2: \2%s\2",
 			x, y, o->nick, dBuf, o->story);
-		free(y);
+		sfree(y);
 	}
 
 	command_success_nodata(si, _("End of list."));

modules/nickserv/main.c
@@ -113,7 +113,7 @@ c_ni_emailexempts(mowgli_config_file_entry_t *ce)
 
 	MOWGLI_ITER_FOREACH_SAFE(n, tn, nicksvs.emailexempts.head)
 	{
-		free(n->data);
+		sfree(n->data);
 		mowgli_node_delete(n, &nicksvs.emailexempts);
 		mowgli_node_free(n);
 	}

commit 1f4b32e76de628879a6516982d2fc946be899591

modules/saslserv/main: use correct free(3) function on SASL object
This was identified by Valgrind, and is a regression introduced by
commit 306312d22261c454766d & commit 4f9e5d3c46bc446f00da

modules/saslserv/main.c
@@ -76,7 +76,7 @@ sasl_sourceinfo_recreate(struct sasl_session *const restrict p)
 
 	struct sasl_sourceinfo *const ssi = smalloc(sizeof *ssi);
 
-	(void) atheme_object_init(atheme_object(ssi), "<sasl sourceinfo>", &free);
+	(void) atheme_object_init(atheme_object(ssi), "<sasl sourceinfo>", &sfree);
 
 	ssi->parent.s = p->server;
 	ssi->parent.connection = curr_uplink->conn;