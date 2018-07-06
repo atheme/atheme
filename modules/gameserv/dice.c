5ef3ef0ca53f1aebfac44ac538266e2de4986050
Aaron Jones <aaronmdjones@gmail.com>
2018-07-06 11:49:59 +0000

    modules/gameserv/dice.c: use integers of same sign in comparison
    Fixes 2 compiler diagnostics regarding comparing a signed integer
    to an unsigned integer.

--- modules/gameserv/dice.c
+++ modules/gameserv/dice.c
@@ -541,7 +541,7 @@ command_dice(struct sourceinfo *si, int parc, char *parv[])
 		times = atoi(parv[0]);
 		arg = parv[1];
 
-		if (times > max_rolls)
+		if (times > (int) max_rolls)
 			times = max_rolls;
 	}
 
@@ -580,7 +580,7 @@ command_calc(struct sourceinfo *si, int parc, char *parv[])
 		times = atoi(parv[0]);
 		arg = parv[1];
 
-		if (times > max_rolls)
+		if (times > (int) max_rolls)
 			times = max_rolls;
 	}
 
