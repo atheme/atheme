160756c6081598e6d33aa91b359ba26bc6fa6d04
Aaron Jones <aaronmdjones@gmail.com>
2019-01-09 02:11:32 +0000

    libathemecore/random_fe_internal.c: apply commit 507f49117bea9a3f
    This corrects the license back to BSD-3-Clause which was stripped during
    the merge of the master branch due to its massive rewrite of arc4random.c.

--- libathemecore/random_fe_internal.c
+++ libathemecore/random_fe_internal.c
@@ -1,15 +1,37 @@
 /*
- * SPDX-License-Identifier: ISC
- * SPDX-URL: https://spdx.org/licenses/ISC.html
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-URL: https://spdx.org/licenses/BSD-3-Clause.html
  *
  * Copyright (C) 1996 David Mazieres <dm@uun.org>
  * Copyright (C) 2008 Damien Miller <djm@openbsd.org>
  * Copyright (C) 2013 Markus Friedl <markus@openbsd.org>
- * Copyright (C) 2017 Aaron M. D. Jones <aaronmdjones@gmail.com>
+ * Copyright (C) 2017-2019 Aaron M. D. Jones <aaronmdjones@gmail.com>
  *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 
 #ifndef ATHEME_RANDOM_FRONTEND_C
