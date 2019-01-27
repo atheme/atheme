atheme/atheme

commit 6a7d31696934361b8a98d54bed796cead5336a33

Build System: Enable CFLAGS="-O2" only if --disable-debugging

configure
@@ -2509,6 +2509,12 @@ fi
 if test "x${prefix}" = "xNONE"; then :
   prefix="${ac_default_prefix}"
 fi
+if test -z "${CFLAGS}"; then :
+  CFLAGS=""
+fi
+if test -z "${CPPFLAGS}"; then :
+  CPPFLAGS=""
+fi
 
 
 	# Extract the first word of "tput", so it can be a program name with args.
@@ -10837,6 +10843,47 @@ $as_echo "#define ATHEME_DEBUGGING 1" >>confdefs.h
 			DEBUGGING="Yes"
 			;;
 		no)
+
+		{ $as_echo "$as_me:${as_lineno-$LINENO}: checking for C compiler flag(s) -O2 " >&5
+$as_echo_n "checking for C compiler flag(s) -O2 ... " >&6; }
+
+		CFLAGS_SAVED="${CFLAGS}"
+		CFLAGS="${CFLAGS} -O2"
+
+		cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+
+int
+main ()
+{
+
+  ;
+  return 0;
+}
+
+_ACEOF
+if ac_fn_c_try_compile "$LINENO"; then :
+
+				ATHEME_CC_TEST_CFLAGS_RESULT='yes'
+
+				{ $as_echo "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+$as_echo "yes" >&6; }
+
+else
+
+				ATHEME_CC_TEST_CFLAGS_RESULT='no'
+				CFLAGS="${CFLAGS_SAVED}"
+
+				{ $as_echo "$as_me:${as_lineno-$LINENO}: result: no" >&5
+$as_echo "no" >&6; }
+
+fi
+rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
+
+		unset CFLAGS_SAVED
+
+
 			DEBUGGING="No"
 			;;
 		*)

configure.ac
@@ -30,6 +30,8 @@ AS_IF([test -f "libmowgli-2/Makefile" -a -f "modules/contrib/Makefile"], [
 ])
 
 AS_IF([test "x${prefix}" = "xNONE"], [prefix="${ac_default_prefix}"])
+AS_IF([test -z "${CFLAGS}"], [CFLAGS=""])
+AS_IF([test -z "${CPPFLAGS}"], [CPPFLAGS=""])
 
 dnl Buildsys module stuff
 BUILDSYS_INIT

m4/atheme-featuretest-debugging.m4
@@ -19,6 +19,7 @@ AC_DEFUN([ATHEME_FEATURETEST_DEBUGGING], [
 			DEBUGGING="Yes"
 			;;
 		no)
+			ATHEME_CC_TEST_CFLAGS([-O2])
 			DEBUGGING="No"
 			;;
 		*)

commit b11ef6c244a57f574e9feb7522ad14a57da87974

configure: allow user to override CFLAGS optimisation level
If --disable-debugging was given, CFLAGS="-O2" was forced.
If --enable-debugging was given, CFLAGS="-O0 -g" was forced.

In either case, the user cannot specify their own -Ox via the
environment. Now we only set CFLAGS under --disable-debugging
(the default option) if it was unset when configure was invoked.

We also test for (but don't require) -g under --disable-debugging
if only so that occasional production backtraces may be usable.
They are not anywhere near as useful as under -O0 (e.g. variables
are usually all optimised out, line numbers may be incorrect, ...)
but the file names and function names are sometimes still useful.

Fixes: 6a7d3169 ("Build System: Enable CFLAGS="-O2" only if
                  --disable-debugging")

configure
@@ -2548,11 +2548,16 @@ fi
 if test "x${prefix}" = "xNONE"; then :
   prefix="${ac_default_prefix}"
 fi
-if test -z "${CFLAGS}"; then :
-  CFLAGS=""
-fi
-if test -z "${CPPFLAGS}"; then :
-  CPPFLAGS=""
+
+if test "x${CFLAGS+set}" = "xset"; then :
+
+	CFLAGS_WAS_SET="yes"
+
+else
+
+	CFLAGS_WAS_SET="no"
+	CFLAGS=""
+
 fi
 
 
@@ -12132,7 +12137,6 @@ rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
 
 			if test "${ATHEME_CC_TEST_CFLAGS_RESULT}" = "no"; then :
 
-
 				as_fn_error $? "--enable-debugging requires compiler support for -O0 -g" "$LINENO" 5
 
 fi
@@ -12143,6 +12147,8 @@ $as_echo "#define ATHEME_DEBUGGING 1" >>confdefs.h
 			DEBUGGING="Yes"
 			;;
 		xno)
+			if test "${CFLAGS_WAS_SET}" = "no"; then :
+
 
 		{ $as_echo "$as_me:${as_lineno-$LINENO}: checking for C compiler flag(s) -O2 " >&5
 $as_echo_n "checking for C compiler flag(s) -O2 ... " >&6; }
@@ -12183,6 +12189,50 @@ rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
 
 		unset CFLAGS_SAVED
 
+
+
+		{ $as_echo "$as_me:${as_lineno-$LINENO}: checking for C compiler flag(s) -g " >&5
+$as_echo_n "checking for C compiler flag(s) -g ... " >&6; }
+
+		CFLAGS_SAVED="${CFLAGS}"
+		CFLAGS="${CFLAGS} -g"
+
+		cat confdefs.h - <<_ACEOF >conftest.$ac_ext
+/* end confdefs.h.  */
+
+
+int
+main ()
+{
+
+  ;
+  return 0;
+}
+
+_ACEOF
+if ac_fn_c_try_compile "$LINENO"; then :
+
+				ATHEME_CC_TEST_CFLAGS_RESULT='yes'
+
+				{ $as_echo "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+$as_echo "yes" >&6; }
+
+else
+
+				ATHEME_CC_TEST_CFLAGS_RESULT='no'
+				CFLAGS="${CFLAGS_SAVED}"
+
+				{ $as_echo "$as_me:${as_lineno-$LINENO}: result: no" >&5
+$as_echo "no" >&6; }
+
+fi
+rm -f core conftest.err conftest.$ac_objext conftest.$ac_ext
+
+		unset CFLAGS_SAVED
+
+
+
+fi
 
 			DEBUGGING="No"
 			;;

configure.ac
@@ -52,8 +52,13 @@ AS_IF([test -f "libmowgli-2/Makefile" -a -f "modules/contrib/Makefile"], [
 ])
 
 AS_IF([test "x${prefix}" = "xNONE"], [prefix="${ac_default_prefix}"])
-AS_IF([test -z "${CFLAGS}"], [CFLAGS=""])
-AS_IF([test -z "${CPPFLAGS}"], [CPPFLAGS=""])
+
+AS_IF([test "x${CFLAGS+set}" = "xset"], [
+	CFLAGS_WAS_SET="yes"
+], [
+	CFLAGS_WAS_SET="no"
+	CFLAGS=""
+])
 
 dnl Buildsys module stuff
 BUILDSYS_INIT

m4/atheme-featuretest-debugging.m4
@@ -11,7 +11,6 @@ AC_DEFUN([ATHEME_FEATURETEST_DEBUGGING], [
 			ATHEME_CC_TEST_CFLAGS([-O0 -g])
 
 			AS_IF([test "${ATHEME_CC_TEST_CFLAGS_RESULT}" = "no"], [
-
 				AC_MSG_ERROR([--enable-debugging requires compiler support for -O0 -g])
 			])
 
@@ -19,7 +18,11 @@ AC_DEFUN([ATHEME_FEATURETEST_DEBUGGING], [
 			DEBUGGING="Yes"
 			;;
 		xno)
-			ATHEME_CC_TEST_CFLAGS([-O2])
+			AS_IF([test "${CFLAGS_WAS_SET}" = "no"], [
+				ATHEME_CC_TEST_CFLAGS([-O2])
+				ATHEME_CC_TEST_CFLAGS([-g])
+			])
+
 			DEBUGGING="No"
 			;;
 		*)