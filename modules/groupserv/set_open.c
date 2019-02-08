beb72014f53419b83025c2f83a7913e691ce2838
Aaron Jones <aaronmdjones@gmail.com>
2019-02-08 10:25:00 +0000

    modules/groupserv/set_open: tidy up

--- modules/groupserv/set_open.c
+++ modules/groupserv/set_open.c
@@ -3,8 +3,7 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2005-2010 Atheme Project (http://atheme.org/)
- *
- * This file contains routines to handle the GroupServ HELP command.
+ * Copyright (C) 2018-2019 Atheme Development Group (https://atheme.github.io/)
  */
 
 #include "atheme.h"
@@ -14,74 +13,86 @@ static const struct groupserv_core_symbols *gcsyms = NULL;
 static mowgli_patricia_t **gs_set_cmdtree = NULL;
 
 static void
-gs_cmd_set_open(struct sourceinfo *si, int parc, char *parv[])
+gs_cmd_set_open_func(struct sourceinfo *const restrict si, const int parc, char **const restrict parv)
 {
-	struct mygroup *mg;
-
-	if (!parv[0] || !parv[1])
+	if (parc != 2)
 	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "OPEN");
-		command_fail(si, fault_needmoreparams, _("Syntax: OPEN <!group> <ON|OFF>"));
+		(void) command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "SET OPEN");
+		(void) command_fail(si, fault_needmoreparams, _("Syntax: SET OPEN <!group> <ON|OFF>"));
 		return;
 	}
 
-	if ((mg = gcsyms->mygroup_find(parv[0])) == NULL)
+	const char *const group = parv[0];
+	const char *const param = parv[1];
+
+	if (*group != '!')
 	{
-		command_fail(si, fault_nosuch_target, _("The group \2%s\2 does not exist."), parv[0]);
+		(void) command_fail(si, fault_badparams, STR_INVALID_PARAMS, "SET OPEN");
+		(void) command_fail(si, fault_badparams, _("Syntax: SET OPEN <!group> <ON|OFF>"));
+		return;
+	}
+
+	struct mygroup *mg;
+
+	if (! (mg = gcsyms->mygroup_find(group)))
+	{
+		(void) command_fail(si, fault_nosuch_target, _("The group \2%s\2 does not exist."), group);
 		return;
 	}
 
 	if (! gcsyms->groupacs_sourceinfo_has_flag(mg, si, GA_FOUNDER))
 	{
-		command_fail(si, fault_noprivs, _("You are not authorized to execute this command."));
+		(void) command_fail(si, fault_noprivs, _("You are not authorized to execute this command."));
 		return;
 	}
 
-	if (!strcasecmp(parv[1], "ON"))
+	if (strcasecmp(param, "ON") == 0)
 	{
-		if (!gcsyms->config->enable_open_groups)
+		if (! gcsyms->config->enable_open_groups)
 		{
-			command_fail(si, fault_nochange, _("Setting groups as open has been administratively disabled."));
+			(void) command_fail(si, fault_nochange, _("Setting groups as open has been administratively "
+			                                          "disabled."));
 			return;
 		}
 
 		if (mg->flags & MG_OPEN)
 		{
-			command_fail(si, fault_nochange, _("\2%s\2 is already open to anyone joining."), entity(mg)->name);
+			(void) command_fail(si, fault_nochange, _("\2%s\2 is already open to anyone joining."), group);
 			return;
 		}
 
 		mg->flags |= MG_OPEN;
 
-		logcommand(si, CMDLOG_SET, "OPEN:ON: \2%s\2", entity(mg)->name);
-		command_success_nodata(si, _("\2%s\2 is now open to anyone joining."), entity(mg)->name);
+		(void) logcommand(si, CMDLOG_SET, "SET:OPEN:ON: \2%s\2", group);
+		(void) command_success_nodata(si, _("\2%s\2 is now open to anyone joining."), group);
 	}
-	else if (!strcasecmp(parv[1], "OFF"))
+	else if (strcasecmp(param, "OFF") == 0)
 	{
-		if (!(mg->flags & MG_OPEN))
+		if (! (mg->flags & MG_OPEN))
 		{
-			command_fail(si, fault_nochange, _("\2%s\2 is already not open to anyone joining."), entity(mg)->name);
+			(void) command_fail(si, fault_nochange, _("\2%s\2 is already not open to anyone joining."),
+			                    group);
 			return;
 		}
 
 		mg->flags &= ~MG_OPEN;
 
-		logcommand(si, CMDLOG_SET, "OPEN:OFF: \2%s\2", entity(mg)->name);
-		command_success_nodata(si, _("\2%s\2 is no longer open to anyone joining."), entity(mg)->name);
+		(void) logcommand(si, CMDLOG_SET, "SET:OPEN:OFF: \2%s\2", group);
+		(void) command_success_nodata(si, _("\2%s\2 is no longer open to anyone joining."), group);
 	}
 	else
 	{
-		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "OPEN");
-		command_fail(si, fault_badparams, _("Syntax: OPEN <!group> <ON|OFF>"));
+		(void) command_fail(si, fault_badparams, STR_INVALID_PARAMS, "SET OPEN");
+		(void) command_fail(si, fault_badparams, _("Syntax: SET OPEN <!group> <ON|OFF>"));
 	}
 }
 
-static struct command gs_set_open = {
+static struct command gs_cmd_set_open = {
 	.name           = "OPEN",
 	.desc           = N_("Sets the group as open for anyone to join."),
 	.access         = AC_AUTHENTICATED,
 	.maxparc        = 2,
-	.cmd            = &gs_cmd_set_open,
+	.cmd            = &gs_cmd_set_open_func,
 	.help           = { .path = "groupserv/set_open" },
 };
 
@@ -91,13 +102,13 @@ mod_init(struct module *const restrict m)
 	MODULE_TRY_REQUEST_SYMBOL(m, gcsyms, "groupserv/main", "groupserv_core_symbols");
 	MODULE_TRY_REQUEST_SYMBOL(m, gs_set_cmdtree, "groupserv/set", "gs_set_cmdtree");
 
-	command_add(&gs_set_open, *gs_set_cmdtree);
+	(void) command_add(&gs_cmd_set_open, *gs_set_cmdtree);
 }
 
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
-	command_delete(&gs_set_open, *gs_set_cmdtree);
+	(void) command_delete(&gs_cmd_set_open, *gs_set_cmdtree);
 }
 
 SIMPLE_DECLARE_MODULE_V1("groupserv/set_open", MODULE_UNLOAD_CAPABILITY_OK)
