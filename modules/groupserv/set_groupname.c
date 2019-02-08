7bfe00545c205b30a066ca3d751872f598ba2649
Aaron Jones <aaronmdjones@gmail.com>
2019-02-08 10:25:00 +0000

    modules/groupserv/set_groupname: tidy up

--- modules/groupserv/set_groupname.c
+++ modules/groupserv/set_groupname.c
@@ -3,8 +3,7 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2014 William Pitcock <nenolod -at- nenolod.net>
- *
- * Changes the group name
+ * Copyright (C) 2018-2019 Atheme Development Group (https://atheme.github.io/)
  */
 
 #include "atheme.h"
@@ -14,65 +13,63 @@ static const struct groupserv_core_symbols *gcsyms = NULL;
 static mowgli_patricia_t **gs_set_cmdtree = NULL;
 
 static void
-gs_cmd_set_groupname(struct sourceinfo *si, int parc, char *parv[])
+gs_cmd_set_groupname_func(struct sourceinfo *const restrict si, const int parc, char **const restrict parv)
 {
-	char *oldname, *newname;
-	struct mygroup *mg;
-
 	if (parc != 2)
 	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "GROUPNAME");
-		command_fail(si, fault_needmoreparams, _("Syntax: SET GROUPNAME <!oldname> <!newname>"));
+		(void) command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "SET GROUPNAME");
+		(void) command_fail(si, fault_needmoreparams, _("Syntax: SET GROUPNAME <!oldname> <!newname>"));
 		return;
 	}
 
-	oldname = parv[0];
-	newname = parv[1];
+	const char *const oldname = parv[0];
+	const char *const newname = parv[1];
 
 	if (*oldname != '!' || *newname != '!')
 	{
-		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "GROUPNAME");
-		command_fail(si, fault_badparams, _("Syntax: SET GROUPNAME <!oldname> <!newname>"));
+		(void) command_fail(si, fault_badparams, STR_INVALID_PARAMS, "SET GROUPNAME");
+		(void) command_fail(si, fault_badparams, _("Syntax: SET GROUPNAME <!oldname> <!newname>"));
 		return;
 	}
 
-	mg = gcsyms->mygroup_find(oldname);
-	if (mg == NULL)
+	struct mygroup *mg;
+
+	if (! (mg = gcsyms->mygroup_find(oldname)))
 	{
-		command_fail(si, fault_nosuch_target, _("The group \2%s\2 does not exist."), oldname);
+		(void) command_fail(si, fault_nosuch_target, _("The group \2%s\2 does not exist."), oldname);
 		return;
 	}
 
 	if (! gcsyms->groupacs_sourceinfo_has_flag(mg, si, GA_FOUNDER))
 	{
-		command_fail(si, fault_noprivs, _("You are not authorized to execute this command."));
+		(void) command_fail(si, fault_noprivs, _("You are not authorized to execute this command."));
 		return;
 	}
 
 	if (strcmp(entity(mg)->name, newname) == 0)
 	{
-		command_fail(si, fault_nochange, _("The group name is already set to \2%s\2."), newname);
+		(void) command_fail(si, fault_nochange, _("The group name is already set to \2%s\2."), newname);
 		return;
 	}
 
-	if (gcsyms->mygroup_find(newname) != NULL)
+	if (gcsyms->mygroup_find(newname))
 	{
-		command_fail(si, fault_nochange, _("The group \2%s\2 already exists."), newname);
+		(void) command_fail(si, fault_nochange, _("The group \2%s\2 already exists."), newname);
 		return;
 	}
 
-	gcsyms->mygroup_rename(mg, newname);
+	(void) gcsyms->mygroup_rename(mg, newname);
 
-	logcommand(si, CMDLOG_REGISTER, "SET:GROUPNAME: \2%s\2 to \2%s\2", oldname, newname);
-	command_success_nodata(si, _("The group \2%s\2 has been renamed to \2%s\2."), oldname, newname);
+	(void) logcommand(si, CMDLOG_SET, "SET:GROUPNAME: \2%s\2 to \2%s\2", oldname, newname);
+	(void) command_success_nodata(si, _("The group \2%s\2 has been renamed to \2%s\2."), oldname, newname);
 }
 
-static struct command gs_set_groupname = {
+static struct command gs_cmd_set_groupname = {
 	.name           = "GROUPNAME",
 	.desc           = N_("Changes the group's name."),
-	.access         = AC_NONE,
-	.maxparc        = 1,
-	.cmd            = &gs_cmd_set_groupname,
+	.access         = AC_AUTHENTICATED,
+	.maxparc        = 2,
+	.cmd            = &gs_cmd_set_groupname_func,
 	.help           = { .path = "groupserv/set_groupname" },
 };
 
@@ -82,13 +79,13 @@ mod_init(struct module *const restrict m)
 	MODULE_TRY_REQUEST_SYMBOL(m, gcsyms, "groupserv/main", "groupserv_core_symbols");
 	MODULE_TRY_REQUEST_SYMBOL(m, gs_set_cmdtree, "groupserv/set", "gs_set_cmdtree");
 
-	command_add(&gs_set_groupname, *gs_set_cmdtree);
+	(void) command_add(&gs_cmd_set_groupname, *gs_set_cmdtree);
 }
 
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
-	command_delete(&gs_set_groupname, *gs_set_cmdtree);
+	(void) command_delete(&gs_cmd_set_groupname, *gs_set_cmdtree);
 }
 
 SIMPLE_DECLARE_MODULE_V1("groupserv/set_groupname", MODULE_UNLOAD_CAPABILITY_OK)
