8ada8a000acd0ea749661dba01e0b49f0ac58128
Aaron Jones <aaronmdjones@gmail.com>
2019-02-08 10:25:00 +0000

    modules/groupserv/listchans: tidy up

--- modules/groupserv/listchans.c
+++ modules/groupserv/listchans.c
@@ -3,8 +3,7 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2005-2011 Atheme Project (http://atheme.org/)
- *
- * This file contains code for the GroupServ LISTCHANS function.
+ * Copyright (C) 2018-2019 Atheme Development Group (https://atheme.github.io/)
  */
 
 #include "atheme.h"
@@ -13,79 +12,90 @@
 static const struct groupserv_core_symbols *gcsyms = NULL;
 
 static void
-gs_cmd_listchans(struct sourceinfo *si, int parc, char *parv[])
+gs_cmd_listchans_func(struct sourceinfo *const restrict si, const int parc, char **const restrict parv)
 {
+	if (parc != 1)
+	{
+		(void) command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "LISTCHANS");
+		(void) command_fail(si, fault_needmoreparams, _("Syntax: LISTCHANS <!group>"));
+		return;
+	}
+
+	const char *const group = parv[0];
+
+	if (*group != '!')
+	{
+		(void) command_fail(si, fault_badparams, STR_INVALID_PARAMS, "LISTCHANS");
+		(void) command_fail(si, fault_badparams, _("Syntax: LISTCHANS <!group>"));
+		return;
+	}
+
 	struct mygroup *mg;
-	mowgli_node_t *n;
-	struct chanacs *ca;
+
+	if (! (mg = gcsyms->mygroup_find(group)))
+	{
+		(void) command_fail(si, fault_nosuch_target, _("Group \2%s\2 does not exist."), group);
+		return;
+	}
+
 	bool operoverride = false;
-	unsigned int akicks = 0, i;
-
-	// target
-	char *target = parv[0];
-
-	if (!target)
-	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "LISTCHANS");
-		command_fail(si, fault_needmoreparams, _("Syntax: LISTCHANS <!groupname>"));
-		return;
-	}
-
-	if (!(mg = gcsyms->mygroup_find(target)))
-	{
-		command_fail(si, fault_nosuch_target, _("Group \2%s\2 does not exist."), target);
-		return;
-	}
 
 	if (! gcsyms->groupacs_sourceinfo_has_flag(mg, si, GA_CHANACS))
 	{
-		if (has_priv(si, PRIV_GROUP_AUSPEX))
-			operoverride = true;
-		else
+		if (! has_priv(si, PRIV_GROUP_AUSPEX))
 		{
-			command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
+			(void) command_fail(si, fault_noprivs, _("You are not authorized to perform this operation."));
 			return;
 		}
+
+		operoverride = true;
 	}
 
 	if (operoverride)
-		logcommand(si, CMDLOG_ADMIN, "LISTCHANS: \2%s\2", entity(mg)->name);
+		(void) logcommand(si, CMDLOG_ADMIN, "LISTCHANS: \2%s\2", group);
 	else
-		logcommand(si, CMDLOG_GET, "LISTCHANS: \2%s\2", entity(mg)->name);
+		(void) logcommand(si, CMDLOG_GET, "LISTCHANS: \2%s\2", group);
 
 	if (MOWGLI_LIST_LENGTH(&entity(mg)->chanacs) == 0)
 	{
-		command_success_nodata(si, _("No channel access was found for the group \2%s\2."), entity(mg)->name);
+		(void) command_success_nodata(si, _("No channel access was found for the group \2%s\2."), group);
 		return;
 	}
 
+	mowgli_node_t *n;
+	unsigned int akicks = 0;
+
 	MOWGLI_ITER_FOREACH(n, entity(mg)->chanacs.head)
 	{
-		ca = (struct chanacs *)n->data;
+		const struct chanacs *const ca = n->data;
 
-		// don't tell users they're akicked (flag +b)
+		// Don't tell users they're akicked (flag +b)
 		if (ca->level != CA_AKICK)
-			command_success_nodata(si, _("Access flag(s) %s in %s"), bitmask_to_flags(ca->level), ca->mychan->name);
+			(void) command_success_nodata(si, _("Access flag(s) %s in %s"), bitmask_to_flags(ca->level),
+			                              ca->mychan->name);
 		else
 			akicks++;
 	}
 
-	i = MOWGLI_LIST_LENGTH(&entity(mg)->chanacs) - akicks;
+	const unsigned int count = MOWGLI_LIST_LENGTH(&entity(mg)->chanacs) - akicks;
 
-	if (i == 0)
-		command_success_nodata(si, _("No channel access was found for the group \2%s\2."), entity(mg)->name);
-	else
-		command_success_nodata(si, ngettext(N_("\2%d\2 channel access match for the group \2%s\2"),
-						    N_("\2%d\2 channel access matches for the group \2%s\2"), i),
-						    i, entity(mg)->name);
+	if (! count)
+	{
+		(void) command_success_nodata(si, _("No channel access was found for the group \2%s\2."), group);
+		return;
+	}
+
+	(void) command_success_nodata(si, ngettext(N_("\2%u\2 channel access match for the group \2%s\2"),
+	                                           N_("\2%u\2 channel access matches for the group \2%s\2"), count),
+	                                           count, group);
 }
 
-static struct command gs_listchans = {
+static struct command gs_cmd_listchans = {
 	.name           = "LISTCHANS",
 	.desc           = N_("Lists channels that a group has access to."),
 	.access         = AC_NONE,
 	.maxparc        = 1,
-	.cmd            = &gs_cmd_listchans,
+	.cmd            = &gs_cmd_listchans_func,
 	.help           = { .path = "groupserv/listchans" },
 };
 
@@ -94,13 +104,13 @@ mod_init(struct module *const restrict m)
 {
 	MODULE_TRY_REQUEST_SYMBOL(m, gcsyms, "groupserv/main", "groupserv_core_symbols");
 
-	(void) service_bind_command(*gcsyms->groupsvs, &gs_listchans);
+	(void) service_bind_command(*gcsyms->groupsvs, &gs_cmd_listchans);
 }
 
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
-	(void) service_unbind_command(*gcsyms->groupsvs, &gs_listchans);
+	(void) service_unbind_command(*gcsyms->groupsvs, &gs_cmd_listchans);
 }
 
 SIMPLE_DECLARE_MODULE_V1("groupserv/listchans", MODULE_UNLOAD_CAPABILITY_OK)
