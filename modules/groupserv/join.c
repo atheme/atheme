632ff751e8f78c26fcd3489bcea48a28064fc7b0
Aaron Jones <aaronmdjones@gmail.com>
2019-02-08 10:25:00 +0000

    modules/groupserv/join: tidy up

--- modules/groupserv/join.c
+++ modules/groupserv/join.c
@@ -3,8 +3,7 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2005-2010 Atheme Project (http://atheme.org/)
- *
- * This file contains routines to handle the GroupServ HELP command.
+ * Copyright (C) 2018-2019 Atheme Development Group (https://atheme.github.io/)
  */
 
 #include "atheme.h"
@@ -13,77 +12,80 @@
 static const struct groupserv_core_symbols *gcsyms = NULL;
 
 static void
-gs_cmd_join(struct sourceinfo *si, int parc, char *parv[])
+gs_cmd_join_func(struct sourceinfo *const restrict si, const int parc, char **const restrict parv)
 {
+	if (parc != 1)
+	{
+		(void) command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "JOIN");
+		(void) command_fail(si, fault_needmoreparams, _("Syntax: JOIN <!group>"));
+		return;
+	}
+
+	const char *const group = parv[0];
+
+	if (*group != '!')
+	{
+		(void) command_fail(si, fault_badparams, STR_INVALID_PARAMS, "JOIN");
+		(void) command_fail(si, fault_badparams, _("Syntax: JOIN <!group>"));
+		return;
+	}
+
 	struct mygroup *mg;
-	struct metadata *md, *md2;
-	unsigned int flags = 0;
-	bool invited = false;
 
-	if (!parv[0])
+	if (! (mg = gcsyms->mygroup_find(group)))
 	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "JOIN");
-		command_fail(si, fault_needmoreparams, _("Syntax: JOIN <!groupname>"));
-		return;
-	}
-
-	if (!(mg = gcsyms->mygroup_find(parv[0])))
-	{
-		command_fail(si, fault_alreadyexists, _("Group \2%s\2 does not exist."), parv[0]);
-		return;
-	}
-
-	if ((md2 = metadata_find(si->smu, "private:groupinvite")))
-	{
-		if (!strcasecmp(md2->value, parv[0]))
-			invited = true;
-		else
-			invited = false;
-	}
-
-	if (!(mg->flags & MG_OPEN) && !invited)
-	{
-		command_fail(si, fault_noprivs, _("Group \2%s\2 is not open to anyone joining."), parv[0]);
+		(void) command_fail(si, fault_alreadyexists, _("Group \2%s\2 does not exist."), group);
 		return;
 	}
 
 	if (gcsyms->groupacs_sourceinfo_has_flag(mg, si, GA_BAN))
 	{
-		command_fail(si, fault_noprivs, _("You are not authorized to execute this command."));
+		(void) command_fail(si, fault_noprivs, _("You are not authorized to execute this command."));
 		return;
 	}
-
 	if (gcsyms->groupacs_sourceinfo_has_flag(mg, si, 0))
 	{
-		command_fail(si, fault_nochange, _("You are already a member of group \2%s\2."), parv[0]);
+		(void) command_fail(si, fault_nochange, _("You are already a member of group \2%s\2."), group);
 		return;
 	}
 
-	if (MOWGLI_LIST_LENGTH(&mg->acs) > gcsyms->config->maxgroupacs && (!(mg->flags & MG_ACSNOLIMIT)) && !invited)
-        {
-                command_fail(si, fault_toomany, _("Group \2%s\2 access list is full."), entity(mg)->name);
-                return;
-        }
+	const struct metadata *md;
+	bool invited = false;
+
+	if ((md = metadata_find(si->smu, "private:groupinvite")) && strcasecmp(md->value, group) == 0)
+		invited = true;
+
+	if (! (mg->flags & MG_OPEN) && ! invited)
+	{
+		(void) command_fail(si, fault_noprivs, _("Group \2%s\2 is not open to anyone joining."), group);
+		return;
+	}
+	if (MOWGLI_LIST_LENGTH(&mg->acs) > gcsyms->config->maxgroupacs && ! (mg->flags & MG_ACSNOLIMIT) && ! invited)
+	{
+		(void) command_fail(si, fault_toomany, _("Group \2%s\2 access list is full."), group);
+		return;
+	}
+
+	if (invited)
+		(void) metadata_delete(si->smu, "private:groupinvite");
+
+	unsigned int flags;
 
 	if ((md = metadata_find(mg, "joinflags")))
 		flags = atoi(md->value);
 	else
-		flags = gcsyms->gs_flags_parser(gcsyms->config->join_flags, 0, flags);
+		flags = gcsyms->gs_flags_parser(gcsyms->config->join_flags, true, 0);
 
-	gcsyms->groupacs_add(mg, entity(si->smu), flags);
-
-	if (invited)
-		metadata_delete(si->smu, "private:groupinvite");
-
-	command_success_nodata(si, _("You are now a member of \2%s\2."), entity(mg)->name);
+	(void) gcsyms->groupacs_add(mg, entity(si->smu), flags);
+	(void) command_success_nodata(si, _("You are now a member of \2%s\2."), group);
 }
 
-static struct command gs_join = {
+static struct command gs_cmd_join = {
 	.name           = "JOIN",
 	.desc           = N_("Join a open group."),
 	.access         = AC_AUTHENTICATED,
 	.maxparc        = 2,
-	.cmd            = &gs_cmd_join,
+	.cmd            = &gs_cmd_join_func,
 	.help           = { .path = "groupserv/join" },
 };
 
@@ -92,13 +94,13 @@ mod_init(struct module *const restrict m)
 {
 	MODULE_TRY_REQUEST_SYMBOL(m, gcsyms, "groupserv/main", "groupserv_core_symbols");
 
-	(void) service_bind_command(*gcsyms->groupsvs, &gs_join);
+	(void) service_bind_command(*gcsyms->groupsvs, &gs_cmd_join);
 }
 
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
-	(void) service_unbind_command(*gcsyms->groupsvs, &gs_join);
+	(void) service_unbind_command(*gcsyms->groupsvs, &gs_cmd_join);
 }
 
 SIMPLE_DECLARE_MODULE_V1("groupserv/join", MODULE_UNLOAD_CAPABILITY_OK)
