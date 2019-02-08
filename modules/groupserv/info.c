ae461c28ec4fd3360d468a1d38be4a77a3f29ae2
Aaron Jones <aaronmdjones@gmail.com>
2019-02-08 10:25:00 +0000

    modules/groupserv/info: tidy up

--- modules/groupserv/info.c
+++ modules/groupserv/info.c
@@ -3,8 +3,7 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2005-2010 Atheme Project (http://atheme.org/)
- *
- * This file contains routines to handle the GroupServ HELP command.
+ * Copyright (C) 2018-2019 Atheme Development Group (https://atheme.github.io/)
  */
 
 #include "atheme.h"
@@ -12,96 +11,119 @@
 
 static const struct groupserv_core_symbols *gcsyms = NULL;
 
-static void
-gs_cmd_info(struct sourceinfo *si, int parc, char *parv[])
+static const char *
+gs_info_flags_str(const struct mygroup *const restrict mg)
 {
-	struct mygroup *mg;
-	struct tm tm;
-	char buf[BUFSIZE], strfbuf[BUFSIZE];
-	struct metadata *md;
+	static const struct {
+		const char *    name;
+		unsigned int    flag;
+	} flags_to_name[] = {
+		{ "ACSNOLIMIT", MG_ACSNOLIMIT },
+		{       "OPEN", MG_OPEN       },
+		{     "PUBLIC", MG_PUBLIC     },
+		{ "REGNOLIMIT", MG_REGNOLIMIT },
+	};
 
-	if (!parv[0])
+	static char flagsbuf[BUFSIZE];
+
+	(void) memset(flagsbuf, 0x00, sizeof flagsbuf);
+
+	for (size_t i = 0; i < ((sizeof flags_to_name) / (sizeof flags_to_name[0])); i++)
 	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "INFO");
-		command_fail(si, fault_needmoreparams, _("Syntax: INFO <!groupname>"));
-		return;
+		if (! (mg->flags & flags_to_name[i].flag))
+			continue;
+
+		if (*flagsbuf)
+			(void) mowgli_strlcat(flagsbuf, " ", sizeof flagsbuf);
+
+		(void) mowgli_strlcat(flagsbuf, flags_to_name[i].name, sizeof flagsbuf);
 	}
 
-	if (!(mg = gcsyms->mygroup_find(parv[0])))
-	{
-		command_fail(si, fault_alreadyexists, _("Group \2%s\2 does not exist."), parv[0]);
-		return;
-	}
+	if (! *flagsbuf)
+		(void) mowgli_strlcat(flagsbuf, "NONE", sizeof flagsbuf);
 
-	tm = *localtime(&mg->regtime);
-	strftime(strfbuf, sizeof strfbuf, TIME_FORMAT, &tm);
-
-	command_success_nodata(si, _("Information for \2%s\2:"), parv[0]);
-	command_success_nodata(si, _("Registered  : %s (%s ago)"), strfbuf, time_ago(mg->regtime));
-
-	if (config_options.show_entity_id || has_priv(si, PRIV_GROUP_AUSPEX))
-		command_success_nodata(si, _("Entity ID   : %s"), entity(mg)->id);
-
-	if (mg->flags & MG_PUBLIC || (si->smu != NULL && gcsyms->groupacs_sourceinfo_has_flag(mg, si, 0) && !gcsyms->groupacs_sourceinfo_has_flag(mg, si, GA_BAN)) || has_priv(si, PRIV_GROUP_AUSPEX))
-		command_success_nodata(si, _("Founder     : %s"), gcsyms->mygroup_founder_names(mg));
-
-	if ((md = metadata_find(mg, "description")))
-		command_success_nodata(si, _("Description : %s"), md->value);
-	if ((md = metadata_find(mg, "channel")))
-		command_success_nodata(si, _("Channel     : %s"), md->value);
-	if ((md = metadata_find(mg, "url")))
-		command_success_nodata(si, _("URL         : %s"), md->value);
-	if ((md = metadata_find(mg, "email")))
-		command_success_nodata(si, _("Email       : %s"), md->value);
-
-	*buf = '\0';
-
-	if (mg->flags & MG_REGNOLIMIT)
-		mowgli_strlcat(buf, "REGNOLIMIT", BUFSIZE);
-
-	if (mg->flags & MG_ACSNOLIMIT)
-	{
-		if (*buf)
-			mowgli_strlcat(buf, " ", BUFSIZE);
-
-		mowgli_strlcat(buf, "ACSNOLIMIT", BUFSIZE);
-	}
-
-	if (mg->flags & MG_OPEN)
-	{
-		if (*buf)
-			mowgli_strlcat(buf, " ", BUFSIZE);
-
-		mowgli_strlcat(buf, "OPEN", BUFSIZE);
-	}
-
-	if (mg->flags & MG_PUBLIC)
-	{
-		if (*buf)
-			mowgli_strlcat(buf, " ", BUFSIZE);
-
-		mowgli_strlcat(buf, "PUBLIC", BUFSIZE);
-	}
-
-	if (*buf)
-		command_success_nodata(si, _("Flags       : %s"), buf);
-
-	if ((md = metadata_find(mg, "joinflags")) != NULL)
-	{
-		command_success_nodata(si, _("Join flags  : %s"), gflags_tostr(ga_flags, atoi(md->value)));
-	}
-
-	command_success_nodata(si, _("\2*** End of Info ***\2"));
-
-	logcommand(si, CMDLOG_GET, "INFO: \2%s\2", parv[0]);
+	return flagsbuf;
 }
 
-static struct command gs_info = {
+static void
+gs_cmd_info_func(struct sourceinfo *const restrict si, const int parc, char **const restrict parv)
+{
+	if (parc != 1)
+	{
+		(void) command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "INFO");
+		(void) command_fail(si, fault_needmoreparams, _("Syntax: INFO <!group>"));
+		return;
+	}
+
+	const char *const group = parv[0];
+
+	if (*group != '!')
+	{
+		(void) command_fail(si, fault_badparams, STR_INVALID_PARAMS, "INFO");
+		(void) command_fail(si, fault_badparams, _("Syntax: INFO <!group>"));
+		return;
+	}
+
+	struct mygroup *mg;
+
+	if (! (mg = gcsyms->mygroup_find(group)))
+	{
+		command_fail(si, fault_alreadyexists, _("Group \2%s\2 does not exist."), group);
+		return;
+	}
+
+	char strfbuf[BUFSIZE];
+	struct tm *tm = localtime(&mg->regtime);
+
+	(void) strftime(strfbuf, sizeof strfbuf, TIME_FORMAT, tm);
+
+	(void) command_success_nodata(si, _("Information for \2%s\2:"), group);
+	(void) command_success_nodata(si, _("Registered  : %s (%s ago)"), strfbuf, time_ago(mg->regtime));
+
+	if (config_options.show_entity_id || has_priv(si, PRIV_GROUP_AUSPEX))
+		(void) command_success_nodata(si, _("Entity ID   : %s"), entity(mg)->id);
+
+	if ((mg->flags & MG_PUBLIC) ||
+	    has_priv(si, PRIV_GROUP_AUSPEX) ||
+	    (
+	        si->smu &&
+	        gcsyms->groupacs_sourceinfo_has_flag(mg, si, 0) &&
+	        ! gcsyms->groupacs_sourceinfo_has_flag(mg, si, GA_BAN)
+	    ))
+	{
+		(void) command_success_nodata(si, _("Founder     : %s"), gcsyms->mygroup_founder_names(mg));
+	}
+
+	struct metadata *md;
+	const char *const flagsbuf = gs_info_flags_str(mg);
+
+	if ((md = metadata_find(mg, "channel")))
+		(void) command_success_nodata(si, _("Channel     : %s"), md->value);
+
+	if ((md = metadata_find(mg, "description")))
+		(void) command_success_nodata(si, _("Description : %s"), md->value);
+
+	if ((md = metadata_find(mg, "email")))
+		(void) command_success_nodata(si, _("Email       : %s"), md->value);
+
+	(void) command_success_nodata(si, _("Flags       : %s"), flagsbuf);
+
+	if ((md = metadata_find(mg, "joinflags")))
+		(void) command_success_nodata(si, _("Join Flags  : %s"), gflags_tostr(ga_flags, atoi(md->value)));
+
+	if ((md = metadata_find(mg, "url")))
+		(void) command_success_nodata(si, _("URL         : %s"), md->value);
+
+	(void) command_success_nodata(si, _("\2*** End of Info ***\2"));
+	(void) logcommand(si, CMDLOG_GET, "INFO: \2%s\2", group);
+}
+
+static struct command gs_cmd_info = {
 	.name           = "INFO",
 	.desc           = N_("Displays information about registered groups."),
 	.access         = AC_NONE,
 	.maxparc        = 2,
-	.cmd            = &gs_cmd_info,
+	.cmd            = &gs_cmd_info_func,
 	.help           = { .path = "groupserv/info" },
 };
 
@@ -110,13 +132,13 @@ mod_init(struct module *const restrict m)
 {
 	MODULE_TRY_REQUEST_SYMBOL(m, gcsyms, "groupserv/main", "groupserv_core_symbols");
 
-	(void) service_bind_command(*gcsyms->groupsvs, &gs_info);
+	(void) service_bind_command(*gcsyms->groupsvs, &gs_cmd_info);
 }
 
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
-	(void) service_unbind_command(*gcsyms->groupsvs, &gs_info);
+	(void) service_unbind_command(*gcsyms->groupsvs, &gs_cmd_info);
 }
 
 SIMPLE_DECLARE_MODULE_V1("groupserv/info", MODULE_UNLOAD_CAPABILITY_OK)
