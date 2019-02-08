1c6e7067bdd381bfcd04061ae52bd4c58488f20c
Aaron Jones <aaronmdjones@gmail.com>
2019-02-08 10:25:00 +0000

    modules/groupserv/register: tidy up

--- modules/groupserv/register.c
+++ modules/groupserv/register.c
@@ -3,8 +3,7 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2005-2010 Atheme Project (http://atheme.org/)
- *
- * This file contains routines to handle the GroupServ HELP command.
+ * Copyright (C) 2018-2019 Atheme Development Group (https://atheme.github.io/)
  */
 
 #include "atheme.h"
@@ -13,69 +12,74 @@
 static const struct groupserv_core_symbols *gcsyms = NULL;
 
 static void
-gs_cmd_register(struct sourceinfo *si, int parc, char *parv[])
+gs_cmd_register_func(struct sourceinfo *const restrict si, const int parc, char **const restrict parv)
 {
-	struct mygroup *mg;
-
-	if (!parv[0])
+	if (parc != 1)
 	{
-		command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "REGISTER");
-		command_fail(si, fault_needmoreparams, _("To register a group: REGISTER <!groupname>"));
+		(void) command_fail(si, fault_needmoreparams, STR_INSUFFICIENT_PARAMS, "REGISTER");
+		(void) command_fail(si, fault_needmoreparams, _("Syntax: REGISTER <!group>"));
 		return;
 	}
 
-	if (*parv[0] != '!')
+	const char *const group = parv[0];
+
+	if (*group != '!')
 	{
-		command_fail(si, fault_needmoreparams, STR_INVALID_PARAMS, "REGISTER");
-		command_fail(si, fault_needmoreparams, _("To register a group: REGISTER <!groupname>"));
+		(void) command_fail(si, fault_badparams, STR_INVALID_PARAMS, "REGISTER");
+		(void) command_fail(si, fault_badparams, _("Syntax: REGISTER <!group>"));
+		return;
+	}
+
+	if (strlen(group) > GROUPLEN)
+	{
+		(void) command_fail(si, fault_badparams, _("The group name \2%s\2 is too long."), group);
 		return;
 	}
 
 	if (si->smu->flags & MU_WAITAUTH)
 	{
-		command_fail(si, fault_notverified, _("You need to verify your email address before you may register groups."));
-		return;
-	}
-
-	if (gcsyms->mygroup_find(parv[0]))
-	{
-		command_fail(si, fault_alreadyexists, _("The group \2%s\2 already exists."), parv[0]);
-		return;
-	}
-
-	if (strlen(parv[0]) > GROUPLEN)
-	{
-		command_fail(si, fault_badparams, _("The group name \2%s\2 is invalid."), parv[0]);
-		return;
-	}
-
-	if (gcsyms->myentity_count_group_flag(entity(si->smu), GA_FOUNDER) > gcsyms->config->maxgroups &&
-	    !has_priv(si, PRIV_REG_NOLIMIT))
-	{
-		command_fail(si, fault_toomany, _("You have too many groups registered."));
+		(void) command_fail(si, fault_notverified, _("You need to verify your email address before you "
+		                                             "may register groups."));
 		return;
 	}
 
 	if (metadata_find(si->smu, "private:restrict:setter"))
 	{
-		command_fail(si, fault_noprivs, _("You have been restricted from registering groups by network staff."));
+		(void) command_fail(si, fault_noprivs, _("You have been restricted from registering groups by "
+		                                         "network staff."));
 		return;
 	}
 
-	mg = gcsyms->mygroup_add(parv[0]);
-	gcsyms->groupacs_add(mg, entity(si->smu), GA_ALL | GA_FOUNDER);
-	hook_call_group_register(mg);
+	const unsigned int founders = gcsyms->myentity_count_group_flag(entity(si->smu), GA_FOUNDER);
 
-	logcommand(si, CMDLOG_REGISTER, "REGISTER: \2%s\2", entity(mg)->name);
-	command_success_nodata(si, _("The group \2%s\2 has been registered to \2%s\2."), entity(mg)->name, entity(si->smu)->name);
+	if (founders > gcsyms->config->maxgroups && ! has_priv(si, PRIV_REG_NOLIMIT))
+	{
+		(void) command_fail(si, fault_toomany, _("You have too many groups registered."));
+		return;
+	}
+
+	if (gcsyms->mygroup_find(group))
+	{
+		(void) command_fail(si, fault_alreadyexists, _("The group \2%s\2 already exists."), group);
+		return;
+	}
+
+	struct mygroup *const mg = gcsyms->mygroup_add(group);
+
+	(void) gcsyms->groupacs_add(mg, entity(si->smu), (GA_ALL | GA_FOUNDER));
+	(void) hook_call_group_register(mg);
+
+	(void) logcommand(si, CMDLOG_REGISTER, "REGISTER: \2%s\2", group);
+	(void) command_success_nodata(si, _("The group \2%s\2 has been registered to \2%s\2."), group,
+	                              entity(si->smu)->name);
 }
 
-static struct command gs_register = {
+static struct command gs_cmd_register = {
 	.name           = "REGISTER",
 	.desc           = N_("Registers a group."),
 	.access         = AC_AUTHENTICATED,
 	.maxparc        = 2,
-	.cmd            = &gs_cmd_register,
+	.cmd            = &gs_cmd_register_func,
 	.help           = { .path = "groupserv/register" },
 };
 
@@ -84,13 +88,15 @@ mod_init(struct module *const restrict m)
 {
 	MODULE_TRY_REQUEST_SYMBOL(m, gcsyms, "groupserv/main", "groupserv_core_symbols");
 
-	(void) service_bind_command(*gcsyms->groupsvs, &gs_register);
+	(void) service_bind_command(*gcsyms->groupsvs, &gs_cmd_register);
+
+	(void) hook_add_event("group_register");
 }
 
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
-	(void) service_unbind_command(*gcsyms->groupsvs, &gs_register);
+	(void) service_unbind_command(*gcsyms->groupsvs, &gs_cmd_register);
 }
 
 SIMPLE_DECLARE_MODULE_V1("groupserv/register", MODULE_UNLOAD_CAPABILITY_OK)
