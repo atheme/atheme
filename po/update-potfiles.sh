a65bc8da32e6a9f01e236913e3fcd19bd9c39260
Aaron Jones <aaronmdjones@gmail.com>
2019-03-28 19:38:17 +0000

    Internationalisation cleanups
    - Move enabling of the po subdirectory in Makefile to
      m4/atheme-conditional-compilation.m4
    
    - Use ${USE_NLS} directly instead of setting a USE_GETTEXT variable
    
    - Only add ${LIBINTL} to ${LIBS} if ${USE_NLS} is yes
    
    - Remove unused HELP_LINGUAS variable from libathemecore/Makefile
    
    - Remove unused HELP_LINGUAS variable from src/dbverify/Makefile
    
    - Correct the HELP_LINGUAS variable in src/services/Makefile
    
    - Remove unnecessary files from po/:
    
      - ChangeLog
      - boldquot.sed
      - en@boldquot.header
      - en@quot.header
      - insert-header.sin
      - quot.sed
      - remove-potcdate.sin
    
      These are only used if using a full-blown GNU gettext build
      infrastructure, including automake, which we are not
    
    - Add libathemecore/ to list of translatable files (po/POTFILES.in)
    
    - Add po/Makefile target "update-potfiles" to update POTFILES.in
    
    - Make po/Makefile target "update-pot" depend on the above target
    
    - Correct "Language" variable in po/*.po
    
    - Unset "Language-Team" variable in po/atheme.pot and po/*.po
    
      We are not a member of the Translation Project at this time
    
    - Correct the contents of po/LINGUAS to match po/Makefile:LOCALES
    
    - Update comments in po/Makefile
    
    - Preserve the header on po/atheme.pot when it is regenerated and
      replicate this header to every po/*.po file too

--- po/update-potfiles.sh
+++ po/update-potfiles.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-rm POTFILES*
-echo "# Please don't update this file manually - use ./update-potfiles.sh instead!" > POTFILES.in
+#!/bin/sh
+
+rm -f POTFILES*
+echo "# Please don't update this file manually - use 'make update-potfiles' instead!" > POTFILES.in
 cd ..
-find src/ modules/ \( -name "*.c" -o -name "*.cxx" -o -name "*.cc" -o -name "*.glade" \) -exec grep -lE "translatable|_\(" \{\} \; | sort | uniq >> po/POTFILES.in
+find libathemecore/ modules/ src/ \( -name "*.c" -o -name "*.cxx" -o -name "*.cc" -o -name "*.glade" \) -exec grep -lE "translatable|_\(" \{\} \+ | sort | uniq >> po/POTFILES.in
