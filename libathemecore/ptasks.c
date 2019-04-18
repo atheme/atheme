atheme/atheme

commit 6dac1548feceafc9dd02db9b9a4bd1b4eeb82f32

Build System: Reproducible Builds: Support SOURCE_DATE_EPOCH
More information about this scheme can be found at:
  <https://reproducible-builds.org/specs/source-date-epoch/>

libathemecore/ptasks.c
@@ -31,19 +31,44 @@ handle_info(struct user *u)
 	numeric_sts(me.me, 374, u, ":End of /INFO list");
 }
 
+const char *
+get_build_date(void)
+{
+	const char *build_date = "<unknown>";
+
+#ifdef ATHEME_ENABLE_REPRODUCIBLE_BUILDS
+#  ifdef SOURCE_DATE_EPOCH
+	errno = 0;
+	const char *eptr = NULL;
+	const time_t build_time = (time_t) strtoul(SOURCE_DATE_EPOCH, &eptr, 10);
+	static char datebuf[BUFSIZE];
+
+	if (build_time && ! errno && (! eptr || (eptr && ! *eptr)))
+	{
+		const struct tm *const tm = localtime(&build_time);
+
+		if (tm && strftime(datebuf, sizeof datebuf, "%b %e %Y", tm) != 0)
+			build_date = datebuf;
+	}
+#  endif /* SOURCE_DATE_EPOCH */
+#else /* ATHEME_ENABLE_REPRODUCIBLE_BUILDS */
+#  ifdef __DATE__
+	build_date = __DATE__;
+#  endif
+#endif /* !ATHEME_ENABLE_REPRODUCIBLE_BUILDS */
+
+	return build_date;
+}
+
 int
 get_version_string(char *buf, size_t bufsize)
 {
 	const struct crypt_impl *const ci = crypt_get_default_provider();
 	const char *const ci_id = ci ? ci->id : "<none>";
 
-#ifdef ATHEME_ENABLE_REPRODUCIBLE_BUILDS
-	return snprintf(buf, bufsize, "%s %s. %s %s :%s [%s] [enc:%s]",
-		PACKAGE_TARNAME, PACKAGE_VERSION, me.name, revision, get_conf_opts(), ircd->ircdname, ci_id);
-#else /* ATHEME_ENABLE_REPRODUCIBLE_BUILDS */
 	return snprintf(buf, bufsize, "%s %s. %s %s :%s [%s] [enc:%s] Build Date: %s",
-		PACKAGE_TARNAME, PACKAGE_VERSION, me.name, revision, get_conf_opts(), ircd->ircdname, ci_id, __DATE__);
-#endif /* !ATHEME_ENABLE_REPRODUCIBLE_BUILDS */
+	                              PACKAGE_TARNAME, PACKAGE_VERSION, me.name, revision,
+	                              get_conf_opts(), ircd->ircdname, ci_id, get_build_date());
 }
 
 void

configure
@@ -12980,6 +12980,15 @@ fi
 
 $as_echo "#define ATHEME_ENABLE_REPRODUCIBLE_BUILDS 1" >>confdefs.h
 
+			if test "x${SOURCE_DATE_EPOCH}" != "x"; then :
+
+
+cat >>confdefs.h <<_ACEOF
+#define SOURCE_DATE_EPOCH ${SOURCE_DATE_EPOCH}
+_ACEOF
+
+
+fi
 			;;
 		xno)
 			REPRODUCIBLE_BUILDS="No"

include/pmodule.h
@@ -43,6 +43,7 @@ void pcommand_delete(const char *token);
 struct proto_cmd *pcommand_find(const char *token);
 
 /* ptasks.c */
+const char *get_build_date(void);
 int get_version_string(char *, size_t);
 void handle_version(struct user *);
 void handle_admin(struct user *);

include/sysconf.h.in
@@ -453,6 +453,9 @@
 /* Define to the version of this package. */
 #undef PACKAGE_VERSION
 
+/* Reproducible build timestamp */
+#undef SOURCE_DATE_EPOCH
+
 /* Define to 1 if you have the ANSI C header files. */
 #undef STDC_HEADERS
 

m4/atheme-featuretest-reprobuilds.m4
@@ -10,6 +10,9 @@ AC_DEFUN([ATHEME_FEATURETEST_REPROBUILDS], [
 		xyes)
 			REPRODUCIBLE_BUILDS="Yes"
 			AC_DEFINE([ATHEME_ENABLE_REPRODUCIBLE_BUILDS], [1], [Define to 1 if --enable-reproducible-builds was given to ./configure])
+			AS_IF([test "x${SOURCE_DATE_EPOCH}" != "x"], [
+				AC_DEFINE_UNQUOTED([SOURCE_DATE_EPOCH], [${SOURCE_DATE_EPOCH}], [Reproducible build timestamp])
+			])
 			;;
 		xno)
 			REPRODUCIBLE_BUILDS="No"

modules/operserv/uptime.c
@@ -10,46 +10,43 @@
 #include "atheme.h"
 
 static void
-os_cmd_uptime(struct sourceinfo *si, int parc, char *parv[])
+os_cmd_uptime_func(struct sourceinfo *const restrict si, const int ATHEME_VATTR_UNUSED parc,
+                   char **const restrict parv)
 {
-	logcommand(si, CMDLOG_GET, "UPTIME");
+	(void) logcommand(si, CMDLOG_GET, "UPTIME");
 
-#ifdef ATHEME_ENABLE_REPRODUCIBLE_BUILDS
-	command_success_nodata(si, "%s [%s]", PACKAGE_STRING, revision);
-#else /* ATHEME_ENABLE_REPRODUCIBLE_BUILDS */
-        command_success_nodata(si, "%s [%s] Build Date: %s", PACKAGE_STRING, revision, __DATE__);
-#endif /* !ATHEME_ENABLE_REPRODUCIBLE_BUILDS */
+	(void) command_success_nodata(si, "%s [%s] Build Date: %s", PACKAGE_STRING, revision, get_build_date());
 
-        command_success_nodata(si, _("Services have been up for %s"), timediff(CURRTIME - me.start));
-	command_success_nodata(si, _("Current PID: %d"), getpid());
-        command_success_nodata(si, _("Registered accounts: %d"), cnt.myuser);
+	(void) command_success_nodata(si, _("Services have been up for %s"), timediff(CURRTIME - me.start));
+	(void) command_success_nodata(si, _("Current PID: %lu"), (unsigned long) getpid());
+	(void) command_success_nodata(si, _("Registered accounts: %u"), cnt.myuser);
 
-	if (!nicksvs.no_nick_ownership)
-        	command_success_nodata(si, _("Registered nicknames: %d"), cnt.mynick);
+	if (! nicksvs.no_nick_ownership)
+		(void) command_success_nodata(si, _("Registered nicknames: %u"), cnt.mynick);
 
-        command_success_nodata(si, _("Registered channels: %d"), cnt.mychan);
-        command_success_nodata(si, _("Users currently online: %d"), cnt.user - me.me->users);
+	(void) command_success_nodata(si, _("Registered channels: %u"), cnt.mychan);
+	(void) command_success_nodata(si, _("Users currently online: %u"), (cnt.user - me.me->users));
 }
 
-static struct command os_uptime = {
+static struct command os_cmd_uptime = {
 	.name           = "UPTIME",
 	.desc           = N_("Shows services uptime and the number of registered nicks and channels."),
 	.access         = PRIV_SERVER_AUSPEX,
 	.maxparc        = 1,
-	.cmd            = &os_cmd_uptime,
+	.cmd            = &os_cmd_uptime_func,
 	.help           = { .path = "oservice/uptime" },
 };
 
 static void
 mod_init(struct module ATHEME_VATTR_UNUSED *const restrict m)
 {
-        service_named_bind_command("operserv", &os_uptime);
+	(void) service_named_bind_command("operserv", &os_cmd_uptime);
 }
 
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
-	service_named_unbind_command("operserv", &os_uptime);
+	(void) service_named_unbind_command("operserv", &os_cmd_uptime);
 }
 
 SIMPLE_DECLARE_MODULE_V1("operserv/uptime", MODULE_UNLOAD_CAPABILITY_OK)

commit a0ab669ced98dd048156c8fe81faea4f5de0a747

libathemecore/ptasks: get_build_date(): don't crash when reproducibly-built
The SOURCE_DATE_EPOCH variable is not a string; we should not call
strtoul(3) on it.

Fixes: 6dac1548feceafc9dd02 ("Support SOURCE_DATE_EPOCH")

libathemecore/ptasks.c
@@ -38,18 +38,12 @@ get_build_date(void)
 
 #ifdef ATHEME_ENABLE_REPRODUCIBLE_BUILDS
 #  ifdef SOURCE_DATE_EPOCH
-	errno = 0;
-	const char *eptr = NULL;
-	const time_t build_time = (time_t) strtoul(SOURCE_DATE_EPOCH, &eptr, 10);
+	const time_t build_time = SOURCE_DATE_EPOCH;
+	const struct tm *const tm = localtime(&build_time);
 	static char datebuf[BUFSIZE];
 
-	if (build_time && ! errno && (! eptr || (eptr && ! *eptr)))
-	{
-		const struct tm *const tm = localtime(&build_time);
-
-		if (tm && strftime(datebuf, sizeof datebuf, "%b %e %Y", tm) != 0)
-			build_date = datebuf;
-	}
+	if (tm && strftime(datebuf, sizeof datebuf, "%b %e %Y", tm) != 0)
+		build_date = datebuf;
 #  endif /* SOURCE_DATE_EPOCH */
 #else /* ATHEME_ENABLE_REPRODUCIBLE_BUILDS */
 #  ifdef __DATE__