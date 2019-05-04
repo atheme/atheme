6b8212fe1f0d2b0af27a7178d1e8879c55197123
Aaron Jones <aaronmdjones@gmail.com>
2019-05-04 01:13:15 +0000

    modules/crypto/crypt3-sha2*: delete conf on module deinit
    This was overlooked in commit bbec70cd40a69053acc7.

--- modules/crypto/crypt3-sha2-512.c
+++ modules/crypto/crypt3-sha2-512.c
@@ -140,6 +140,9 @@ mod_init(struct module *const restrict m)
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
+	(void) del_conf_item("ROUNDS", &crypt3_conf_table);
+	(void) del_top_conf("CRYPT3SHA2512");
+
 	(void) crypt_unregister(&crypto_crypt3_impl);
 }
 
