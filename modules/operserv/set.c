atheme/atheme

commit c7a62ee12812019f78f2bd565b499ebba11dc074

modules/: add static qualifier to compilation-unit-private variables
Comments have been added to the appropriate non-private declarations
indicating which modules import them.

This does not cover all modules; it completely ignores modules/groupserv/
because I am going to almost completely rewrite that in the near future
instead (as I did with modules/saslserv/). It also doesn't do much with
the NickServ LIST stuff, I'll look at that later.

Eliminates 240 compiler diagnostics to the effect of:

foo.c: warning: no previous extern declaration for non-static
                variable 'bar' [-Wmissing-variable-declarations]

modules/operserv/set.c
@@ -43,7 +43,7 @@ static struct command os_set_spam = { "SPAM", N_("Changes whether service spams 
 static struct command os_set_nickexpire = { "NICKEXPIRE", N_("Sets when unused nicks and accounts expire."), AC_NONE, 1, os_cmd_set_nickexpire, { .path = "oservice/set_nickexpire" } };
 static struct command os_set_enforceprefix = { "ENFORCEPREFIX", N_("Changes the prefix to use when changing the user's nick on enforcement."), AC_NONE, 1, os_cmd_set_enforceprefix, { .path = "oservice/set_enforceprefix" } };
 
-mowgli_patricia_t *os_set_cmdtree;
+static mowgli_patricia_t *os_set_cmdtree = NULL;
 
 /* HELP SET */
 static void

commit 3f95d994c23149ed887ead338820a94bb23c21c0

modules/operserv/set: make os_set_cmdtree extern
This broke proxyscan/dnsbl which hooks OperServ SET for a
SET DNSBLACTION subcommand.

Fixes: c7a62ee12812019f78f2

modules/operserv/set.c
@@ -3,14 +3,16 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2006-2011 William Pitcock, et al.
- * Copyright (C) 2018 Atheme Development Group (https://atheme.github.io/)
+ * Copyright (C) 2018-2019 Atheme Development Group (https://atheme.github.io/)
  *
  * This file contains routines to handle the OperServ SET command.
  */
 
 #include <atheme.h>
 
-static mowgli_patricia_t *os_set_cmdtree = NULL;
+// Imported by modules/proxyscan/dnsbl
+extern mowgli_patricia_t *os_set_cmdtree;
+mowgli_patricia_t *os_set_cmdtree = NULL;
 
 static void
 os_help_set(struct sourceinfo *const restrict si, const char *const restrict subcmd)