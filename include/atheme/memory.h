e0ebbca31d99bae98e703e8ff4729a3262026023
Aaron Jones <aaronmdjones@gmail.com>
2019-09-04 20:23:46 +0000

    include/atheme/*.h: collapse multiple function declaration attributes
    Absent compiler support for __attribute__((__diagnose_if__(...))), it
    gets replaced with nothing anyway. Therefore, there's no need to gate
    this behind an #ifdef, and they also stack with other attributes and
    with themselves (testing different conditions).
    
    Just put all the attributes on one declaration.
    It looks cleaner anyway.

--- include/atheme/memory.h
+++ include/atheme/memory.h
@@ -3,6 +3,7 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2005 Atheme Project (http://atheme.org/)
+ * Copyright (C) 2019 Atheme Development Group (https://atheme.github.io/)
  *
  * Memory stuff.
  */
@@ -18,46 +19,54 @@
 extern void *(* volatile volatile_memset)(void *, int, size_t);
 #endif /* !HAVE_MEMSET_S && !HAVE_EXPLICIT_BZERO && !HAVE_LIBSODIUM_MEMZERO */
 
-int smemcmp(const void *ptr1, const void *ptr2, size_t len) ATHEME_FATTR_WUR;
-void smemzerofree(void *ptr, size_t len);
-void smemzero(void *ptr, size_t len);
-void sfree(void *ptr);
-void *scalloc(size_t num, size_t len) ATHEME_FATTR_ALLOC_SIZE_PRODUCT(1, 2) ATHEME_FATTR_MALLOC ATHEME_FATTR_RETURNS_NONNULL;
-void *smalloc(size_t len) ATHEME_FATTR_ALLOC_SIZE(1) ATHEME_FATTR_MALLOC ATHEME_FATTR_RETURNS_NONNULL;
-void *srealloc(void *ptr, size_t len) ATHEME_FATTR_ALLOC_SIZE(2) ATHEME_FATTR_WUR;
-char *sstrdup(const char *ptr) ATHEME_FATTR_MALLOC;
-char *sstrndup(const char *ptr, size_t maxlen) ATHEME_FATTR_MALLOC;
-void *smemdup(const void *ptr, size_t len) ATHEME_FATTR_MALLOC;
 
-#ifdef ATHEME_ATTR_HAS_DIAGNOSE_IF
+
 int smemcmp(const void *ptr1, const void *ptr2, size_t len)
-    ATHEME_FATTR_DIAGNOSE_IF(!ptr1, "calling smemcmp() with !ptr1", "error");
-int smemcmp(const void *ptr1, const void *ptr2, size_t len)
-    ATHEME_FATTR_DIAGNOSE_IF(!ptr2, "calling smemcmp() with !ptr2", "error");
-int smemcmp(const void *ptr1, const void *ptr2, size_t len)
+    ATHEME_FATTR_WUR
+    ATHEME_FATTR_DIAGNOSE_IF(!ptr1, "calling smemcmp() with !ptr1", "error")
+    ATHEME_FATTR_DIAGNOSE_IF(!ptr2, "calling smemcmp() with !ptr2", "error")
     ATHEME_FATTR_DIAGNOSE_IF(!len, "calling smemcmp() with !len", "warning");
+
+void smemzerofree(void *ptr, size_t len)
+    ATHEME_FATTR_DIAGNOSE_IF(!ptr, "calling smemzerofree() with !ptr", "warning")
+    ATHEME_FATTR_DIAGNOSE_IF(!len, "calling smemzerofree() with !len", "warning");
+
 void smemzero(void *ptr, size_t len)
-    ATHEME_FATTR_DIAGNOSE_IF(!ptr, "calling smemzero() with !ptr", "warning");
-void smemzero(void *ptr, size_t len)
+    ATHEME_FATTR_DIAGNOSE_IF(!ptr, "calling smemzero() with !ptr", "warning")
     ATHEME_FATTR_DIAGNOSE_IF(!len, "calling smemzero() with !len", "warning");
+
+void sfree(void *ptr);
+
 void *scalloc(size_t num, size_t len)
-    ATHEME_FATTR_DIAGNOSE_IF(!num, "calling scalloc() with !num", "error");
-void *scalloc(size_t num, size_t len)
+    ATHEME_FATTR_ALLOC_SIZE_PRODUCT(1, 2)
+    ATHEME_FATTR_MALLOC
+    ATHEME_FATTR_RETURNS_NONNULL
+    ATHEME_FATTR_DIAGNOSE_IF(!num, "calling scalloc() with !num", "error")
     ATHEME_FATTR_DIAGNOSE_IF(!len, "calling scalloc() with !len", "error");
+
 void *smalloc(size_t len)
+    ATHEME_FATTR_ALLOC_SIZE(1)
+    ATHEME_FATTR_MALLOC
+    ATHEME_FATTR_RETURNS_NONNULL
     ATHEME_FATTR_DIAGNOSE_IF(!len, "calling smalloc() with !len", "error");
+
 void *srealloc(void *ptr, size_t len)
+    ATHEME_FATTR_ALLOC_SIZE(2)
+    ATHEME_FATTR_WUR
     ATHEME_FATTR_DIAGNOSE_IF((!ptr && !len), "calling srealloc() with (!ptr && !len)", "warning");
+
 char *sstrdup(const char *ptr)
+    ATHEME_FATTR_MALLOC
     ATHEME_FATTR_DIAGNOSE_IF(!ptr, "calling sstrdup() with !ptr", "warning");
+
 char *sstrndup(const char *ptr, size_t maxlen)
-    ATHEME_FATTR_DIAGNOSE_IF(!ptr, "calling sstrndup() with !ptr", "warning");
-char *sstrndup(const char *ptr, size_t maxlen)
+    ATHEME_FATTR_MALLOC
+    ATHEME_FATTR_DIAGNOSE_IF(!ptr, "calling sstrndup() with !ptr", "warning")
     ATHEME_FATTR_DIAGNOSE_IF(!maxlen, "calling sstrndup() with !maxlen", "error");
+
 void *smemdup(const void *ptr, size_t len)
-    ATHEME_FATTR_DIAGNOSE_IF(!ptr, "calling smemdup() with !ptr", "warning");
-void *smemdup(const void *ptr, size_t len)
+    ATHEME_FATTR_MALLOC
+    ATHEME_FATTR_DIAGNOSE_IF(!ptr, "calling smemdup() with !ptr", "warning")
     ATHEME_FATTR_DIAGNOSE_IF(!len, "calling smemdup() with !len", "warning");
-#endif /* ATHEME_ATTR_HAS_DIAGNOSE_IF */
 
 #endif /* !ATHEME_INC_MEMORY_H */
--- include/atheme/random.h
+++ include/atheme/random.h
@@ -14,24 +14,19 @@
 #include <atheme/stdheaders.h>
 
 uint32_t atheme_random(void);
-uint32_t atheme_random_uniform(uint32_t bound);
-void atheme_random_buf(void *buf, size_t len);
-void atheme_random_str(char *buf, size_t len);
+
+uint32_t atheme_random_uniform(uint32_t bound)
+    ATHEME_FATTR_DIAGNOSE_IF((bound < 2), "calling atheme_random_uniform() with bound < 2", "error");
+
+void atheme_random_buf(void *buf, size_t len)
+    ATHEME_FATTR_DIAGNOSE_IF(!buf, "calling atheme_random_buf() with !buf", "error")
+    ATHEME_FATTR_DIAGNOSE_IF(!len, "calling atheme_random_buf() with !len", "error");
+
+void atheme_random_str(char *buf, size_t len)
+    ATHEME_FATTR_DIAGNOSE_IF(!buf, "calling atheme_random_str() with !buf", "error")
+    ATHEME_FATTR_DIAGNOSE_IF(!len, "calling atheme_random_str() with !len", "error");
 
 bool libathemecore_random_early_init(void) ATHEME_FATTR_WUR;
 const char *random_get_frontend_info(void);
-
-#ifdef ATHEME_ATTR_HAS_DIAGNOSE_IF
-uint32_t atheme_random_uniform(uint32_t bound)
-    ATHEME_FATTR_DIAGNOSE_IF((bound < 2), "calling atheme_random_uniform() with bound < 2", "error");
-void atheme_random_buf(void *buf, size_t len)
-    ATHEME_FATTR_DIAGNOSE_IF(!buf, "calling atheme_random_buf() with !buf", "error");
-void atheme_random_buf(void *buf, size_t len)
-    ATHEME_FATTR_DIAGNOSE_IF(!len, "calling atheme_random_buf() with !len", "error");
-void atheme_random_str(char *buf, size_t len)
-    ATHEME_FATTR_DIAGNOSE_IF(!buf, "calling atheme_random_str() with !buf", "error");
-void atheme_random_str(char *buf, size_t len)
-    ATHEME_FATTR_DIAGNOSE_IF(!len, "calling atheme_random_str() with !len", "error");
-#endif /* ATHEME_ATTR_HAS_DIAGNOSE_IF */
 
 #endif /* !ATHEME_INC_RANDOM_H */
