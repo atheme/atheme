72f4f499791559f86840635e93424209b248ed03
Aaron Jones <aaronmdjones@gmail.com>
2019-10-06 17:49:17 +0000

    modules/nickserv/pwquality: libpasswdqc: make configurable

--- modules/nickserv/pwquality.c
+++ modules/nickserv/pwquality.c
@@ -25,12 +25,41 @@ static char *cracklib_dict = NULL;
 #endif /* HAVE_CRACKLIB */
 
 #ifdef HAVE_LIBPASSWDQC
+
+#define PASSWDQC_N0_DEF     20
+#define PASSWDQC_N1_DEF     16
+#define PASSWDQC_N2_DEF     16
+#define PASSWDQC_N3_DEF     12
+#define PASSWDQC_N4_DEF     8
+
+#define PASSWDQC_PW_MIN     2
+#define PASSWDQC_PW_DEF     4
+#define PASSWDQC_PW_MAX     8
+
+#define PASSWDQC_PM_MIN     8
+#define PASSWDQC_PM_DEF     PASSLEN
+#define PASSWDQC_PM_MAX     PASSLEN
+
+static unsigned int passwdqc_min_n0 = PASSWDQC_N0_DEF;
+static unsigned int passwdqc_min_n1 = PASSWDQC_N1_DEF;
+static unsigned int passwdqc_min_n2 = PASSWDQC_N2_DEF;
+static unsigned int passwdqc_min_n3 = PASSWDQC_N3_DEF;
+static unsigned int passwdqc_min_n4 = PASSWDQC_N4_DEF;
+static unsigned int passwdqc_words = PASSWDQC_PW_DEF;
+static unsigned int passwdqc_max = PASSWDQC_PM_DEF;
+
 static passwdqc_params_qc_t qc_config = {
-	.min                    = { 32, 24, 11, 8, 7 },
-	.max                    = PASSLEN,
-	.passphrase_words       = 3,
-	.match_length           = 4,
+
+	/* Set in pwquality_config_check() below */
+	.min                = { 0, 0, 0, 0, 0 },
+	.max                = 0,
+	.passphrase_words   = 0,
+
+	/* We don't have access to the user's old password */
+	.match_length       = 0,
+	.similar_deny       = 0,
 };
+
 #endif /* HAVE_LIBPASSWDQC */
 
 static bool pwquality_warn_only = false;
@@ -56,6 +85,42 @@ pwquality_config_check(void)
 	}
 #endif /* HAVE_CRACKLIB */
 
+#ifdef HAVE_LIBPASSWDQC
+	if (passwdqc_min_n0 > passwdqc_max)
+	{
+		(void) slog(LG_ERROR, "Bad config: nickserv::passwdqc_min_n0 is more than nickserv::passwdqc_max");
+		return false;
+	}
+	if (passwdqc_min_n1 > passwdqc_min_n0)
+	{
+		(void) slog(LG_ERROR, "Bad config: nickserv::passwdqc_min_n1 is more than nickserv::passwdqc_min_n0");
+		return false;
+	}
+	if (passwdqc_min_n2 > passwdqc_min_n1)
+	{
+		(void) slog(LG_ERROR, "Bad config: nickserv::passwdqc_min_n2 is more than nickserv::passwdqc_min_n1");
+		return false;
+	}
+	if (passwdqc_min_n3 > passwdqc_min_n2)
+	{
+		(void) slog(LG_ERROR, "Bad config: nickserv::passwdqc_min_n3 is more than nickserv::passwdqc_min_n2");
+		return false;
+	}
+	if (passwdqc_min_n4 > passwdqc_min_n3)
+	{
+		(void) slog(LG_ERROR, "Bad config: nickserv::passwdqc_min_n4 is more than nickserv::passwdqc_min_n3");
+		return false;
+	}
+
+	qc_config.passphrase_words = (int) passwdqc_words;
+	qc_config.min[0] = (int) passwdqc_min_n0;
+	qc_config.min[1] = (int) passwdqc_min_n1;
+	qc_config.min[2] = (int) passwdqc_min_n2;
+	qc_config.min[3] = (int) passwdqc_min_n3;
+	qc_config.min[4] = (int) passwdqc_min_n4;
+	qc_config.max = (int) passwdqc_max;
+#endif /* HAVE_LIBPASSWDQC */
+
 	return true;
 }
 
@@ -67,18 +132,18 @@ pwquality_register_hook(hook_user_register_check_t *const restrict hdata)
 	return_if_fail(hdata->password != NULL);
 
 	bool good_password = true;
-	const char *cl_reason = NULL;
-	const char *qc_reason = NULL;
 
 	if (! pwquality_config_check())
 		return;
 
 #ifdef HAVE_CRACKLIB
+	const char *cl_reason = NULL;
 	if ((cl_reason = FascistCheck(hdata->password, cracklib_dict)))
 		good_password = false;
 #endif /* HAVE_CRACKLIB */
 
 #ifdef HAVE_LIBPASSWDQC
+	const char *qc_reason = NULL;
 	if ((qc_reason = passwdqc_check(&qc_config, hdata->password, NULL, NULL)))
 		good_password = false;
 #endif /* HAVE_LIBPASSWDQC */
@@ -86,11 +151,15 @@ pwquality_register_hook(hook_user_register_check_t *const restrict hdata)
 	if (good_password)
 		return;
 
+#ifdef HAVE_CRACKLIB
 	if (cl_reason)
 		(void) command_fail(hdata->si, fault_badparams, _("Password is insecure [cracklib]: %s"), cl_reason);
+#endif /* HAVE_CRACKLIB */
 
+#ifdef HAVE_LIBPASSWDQC
 	if (qc_reason)
 		(void) command_fail(hdata->si, fault_badparams, _("Password is insecure [passwdqc]: %s"), qc_reason);
+#endif /* HAVE_LIBPASSWDQC */
 
 	if (pwquality_warn_only)
 	{
@@ -114,6 +183,12 @@ pwquality_osinfo_hook(struct sourceinfo *const restrict si)
 
 #ifdef HAVE_CRACKLIB
 	(void) command_success_nodata(si, _("Cracklib dictionary path: %s"), cracklib_dict);
+#endif /* HAVE_CRACKLIB */
+
+#ifdef HAVE_LIBPASSWDQC
+	(void) command_success_nodata(si, _("Configuration for libpasswdqc: min:[%u,%u,%u,%u,%u], max:%u, words:%u"),
+	                                    passwdqc_min_n0, passwdqc_min_n1, passwdqc_min_n2, passwdqc_min_n3,
+	                                    passwdqc_min_n4, passwdqc_max, passwdqc_words);
 #endif /* HAVE_LIBPASSWDQC */
 
 	(void) command_success_nodata(si, _("Registrations will fail with bad passwords: %s"),
@@ -134,6 +209,23 @@ mod_init(struct module ATHEME_VATTR_UNUSED *const restrict m)
 #ifdef HAVE_CRACKLIB
 	(void) add_dupstr_conf_item("CRACKLIB_DICT", &nicksvs.me->conf_table, 0, &cracklib_dict, NULL);
 #endif /* HAVE_CRACKLIB */
+
+#ifdef HAVE_LIBPASSWDQC
+	(void) add_uint_conf_item("PASSWDQC_MIN_N0", &nicksvs.me->conf_table, 0, &passwdqc_min_n0, 0,
+	                          PASSWDQC_PM_MAX, PASSWDQC_N0_DEF);
+	(void) add_uint_conf_item("PASSWDQC_MIN_N1", &nicksvs.me->conf_table, 0, &passwdqc_min_n1, 0,
+	                          PASSWDQC_PM_MAX, PASSWDQC_N1_DEF);
+	(void) add_uint_conf_item("PASSWDQC_MIN_N2", &nicksvs.me->conf_table, 0, &passwdqc_min_n2, 0,
+	                          PASSWDQC_PM_MAX, PASSWDQC_N2_DEF);
+	(void) add_uint_conf_item("PASSWDQC_MIN_N3", &nicksvs.me->conf_table, 0, &passwdqc_min_n3, 0,
+	                          PASSWDQC_PM_MAX, PASSWDQC_N3_DEF);
+	(void) add_uint_conf_item("PASSWDQC_MIN_N4", &nicksvs.me->conf_table, 0, &passwdqc_min_n4, 0,
+	                          PASSWDQC_PM_MAX, PASSWDQC_N4_DEF);
+	(void) add_uint_conf_item("PASSWDQC_MAX", &nicksvs.me->conf_table, 0, &passwdqc_max, PASSWDQC_PM_MIN,
+	                          PASSWDQC_PM_MAX, PASSWDQC_PM_DEF);
+	(void) add_uint_conf_item("PASSWDQC_WORDS", &nicksvs.me->conf_table, 0, &passwdqc_words, PASSWDQC_PW_MIN,
+	                          PASSWDQC_PW_MAX, PASSWDQC_PW_DEF);
+#endif /* HAVE_LIBPASSWDQC */
 }
 
 static void
@@ -147,6 +239,16 @@ mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 #ifdef HAVE_CRACKLIB
 	(void) del_conf_item("CRACKLIB_DICT", &nicksvs.me->conf_table);
 #endif /* HAVE_CRACKLIB */
+
+#ifdef HAVE_LIBPASSWDQC
+	(void) del_conf_item("PASSWDQC_MIN_N0", &nicksvs.me->conf_table);
+	(void) del_conf_item("PASSWDQC_MIN_N1", &nicksvs.me->conf_table);
+	(void) del_conf_item("PASSWDQC_MIN_N2", &nicksvs.me->conf_table);
+	(void) del_conf_item("PASSWDQC_MIN_N3", &nicksvs.me->conf_table);
+	(void) del_conf_item("PASSWDQC_MIN_N4", &nicksvs.me->conf_table);
+	(void) del_conf_item("PASSWDQC_MAX", &nicksvs.me->conf_table);
+	(void) del_conf_item("PASSWDQC_WORDS", &nicksvs.me->conf_table);
+#endif /* HAVE_LIBPASSWDQC */
 }
 
 #else /* HAVE_ANY_PASSWORD_QUALITY_LIBRARY */
