atheme/atheme

commit dc456f962528d1323941eacd516a191d784e6752

libathemecore/base64.c: add error checking for custom alphabets

libathemecore/base64.c
@@ -29,6 +29,47 @@ static const unsigned char inverse_alphabet_default[] = {
 	0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 };
 
+static bool ATHEME_FATTR_WUR
+base64_alphabet_invert(const char alphabet[const restrict static 65],
+                       unsigned char inverse_alphabet[const restrict static 128])
+{
+	// Prefill with invalid character indicator
+	(void) memset(inverse_alphabet, 0xFF, 128);
+
+	for (unsigned char i = 0; i < 65; i++)
+	{
+		if (isspace(alphabet[i]))
+			// Whitespace in alphabet
+			return false;
+
+		if (i < 64 && ! isprint(alphabet[i]))
+			// Non-printable character in alphabet
+			return false;
+
+		if (i == 64 && ! isprint(alphabet[i]) && alphabet[i] != 0x00)
+			// Non-printable non-null padding character
+			return false;
+
+		const unsigned char och = (unsigned char) alphabet[i];
+
+		if (inverse_alphabet[och] != 0xFF)
+			// Duplicated character in alphabet
+			return false;
+
+		// Populate character positions
+		inverse_alphabet[och] = i;
+	}
+
+	if (inverse_alphabet[0x00] != 0xFF && inverse_alphabet[0x00] != 0x40)
+		// Duplicated character in alphabet
+		return false;
+
+	// A null string terminator is always end-of-input
+	inverse_alphabet[0x00] = 0x40;
+
+	return true;
+}
+
 static size_t ATHEME_FATTR_WUR
 base64_encode_run(const void *const restrict in, const size_t in_len, char *const restrict dst, const size_t dst_len,
                   const bool terminate, const char alphabet[const restrict static 65])
@@ -146,16 +187,28 @@ base64_encode_raw(const void *const restrict in, const size_t in_len, char *cons
 }
 
 size_t ATHEME_FATTR_WUR
-base64_encode_table(const void *const restrict in, const size_t in_len, char *const restrict dst, const size_t dst_len,
-                    const char alphabet[const restrict static 65])
+base64_encode_table(const void *const restrict in, const size_t in_len, char *const restrict dst,
+                    const size_t dst_len, const char alphabet[const restrict static 65])
 {
+	unsigned char inverse_alphabet[128];
+
+	if (! base64_alphabet_invert(alphabet, inverse_alphabet))
+		// Duplicated or invalid character in alphabet
+		return BASE64_FAIL;
+
 	return base64_encode_run(in, in_len, dst, dst_len, true, alphabet);
 }
 
 size_t ATHEME_FATTR_WUR
-base64_encode_table_raw(const void *const restrict in, const size_t in_len, char *const restrict dst, const size_t dst_len,
-                        const char alphabet[const restrict static 65])
+base64_encode_table_raw(const void *const restrict in, const size_t in_len, char *const restrict dst,
+                        const size_t dst_len, const char alphabet[const restrict static 65])
 {
+	unsigned char inverse_alphabet[128];
+
+	if (! base64_alphabet_invert(alphabet, inverse_alphabet))
+		// Duplicated or invalid character in alphabet
+		return BASE64_FAIL;
+
 	return base64_encode_run(in, in_len, dst, dst_len, false, alphabet);
 }
 
@@ -286,18 +339,11 @@ size_t ATHEME_FATTR_WUR
 base64_decode_table(const char *const restrict src, void *const restrict out, const size_t out_len,
                     const char alphabet[const restrict static 65])
 {
-	unsigned char inverse_alphabet_computed[128];
+	unsigned char inverse_alphabet[128];
 
-	// Prefill with invalid character indicator
-	(void) memset(inverse_alphabet_computed, 0xFF, sizeof inverse_alphabet_computed);
+	if (! base64_alphabet_invert(alphabet, inverse_alphabet))
+		// Duplicated or invalid character in alphabet
+		return BASE64_FAIL;
 
-	for (unsigned char i = 0; i < 64; i++)
-		// Populate character positions
-		inverse_alphabet_computed[((unsigned char) alphabet[i])] = i;
-
-	// Indicate end-of-input bytes
-	inverse_alphabet_computed[0x00] = 0xFE;
-	inverse_alphabet_computed[((unsigned char) alphabet[64])] = 0xFE;
-
-	return base64_decode_run(src, out, out_len, inverse_alphabet_computed);
+	return base64_decode_run(src, out, out_len, inverse_alphabet);
 }

commit 8926391c9857ff5e5f020d071e65cd6db28c38f3

libathemecore/base64: another minor end-of-input marker tweak
Upon further reflection, undo the last commit and additionally swap
0x40 for 0xFE when storing the end-of-input marker in the custom
alphabet inverter.

The only functional difference between this commit and the last one
is that this commit allows use of 0x40 (ASCII '@') in a custom
alphabet, which should not be excluded purely for an "invent your
own number" reason.

Fixes: dc456f962528

libathemecore/base64.c
@@ -19,10 +19,10 @@ static const char alphabet_default[] = {
 
 static const unsigned char inverse_alphabet_default[] = {
 
-	0x40, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x3E, 0xFF, 0xFF, 0xFF, 0x3F,
-	0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0xFF, 0xFF, 0xFF, 0x40, 0xFF, 0xFF,
+	0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0xFF, 0xFF, 0xFF, 0xFE, 0xFF, 0xFF,
 	0xFF, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E,
 	0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
 	0xFF, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28,
@@ -57,15 +57,15 @@ base64_alphabet_invert(const char alphabet[const restrict static 65],
 			return false;
 
 		// Populate character positions
-		inverse_alphabet[och] = i;
+		inverse_alphabet[och] = (i < 64 ? i : 0xFE);
 	}
 
-	if (inverse_alphabet[0x00] != 0xFF && inverse_alphabet[0x00] != 0x40)
+	if (inverse_alphabet[0x00] != 0xFF && inverse_alphabet[0x00] != 0xFE)
 		// Duplicated character in alphabet
 		return false;
 
 	// A null string terminator is always end-of-input
-	inverse_alphabet[0x00] = 0x40;
+	inverse_alphabet[0x00] = 0xFE;
 
 	return true;
 }
@@ -114,7 +114,7 @@ base64_decode_run(const char *restrict src, void *const restrict out, const size
 
 			och[done] = inverse_alphabet[och[done]];
 
-			if (och[done] == 0x40U)
+			if (och[done] == 0xFEU)
 				// End of input (null terminator / padding character), errors will be handled below
 				break;
 