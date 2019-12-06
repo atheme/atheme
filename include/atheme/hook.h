6702b4df38a1d89aeaed24ccf9d1fdd3adf8c7ed
Aaron Jones <aaronmdjones@gmail.com>
2019-12-06 15:00:10 +0000

    include/atheme/hook.h: s/(database_handle)_t/struct \1/g
    Types with names ending in '_t' are reserved and should not be defined
    by applications.

--- include/atheme/hook.h
+++ include/atheme/hook.h
@@ -15,7 +15,6 @@
 #include <atheme/structures.h>
 
 // Types necessary for the hook system (and/or for Perl scripts)
-typedef struct database_handle database_handle_t;
 typedef struct mychan mychan_t;
 typedef struct myentity myentity_t;
 typedef struct mygroup mygroup_t;
