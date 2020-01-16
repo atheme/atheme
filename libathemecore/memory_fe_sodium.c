f848a96cc3da3d8149b474e3437d8fa16bf824de
Aaron Jones <aaronmdjones@gmail.com>
2020-01-16 09:33:13 +0000

    libathemecore/memory_fe_sodium: several small improvements
    - Correct opening header comment block.
    
    - Document why a list of allocations is needed.
    
    - Use C99-style comments for single-line comments.
    
    - When performing a new memory allocation, and freeing an existing
      allocation, don't iterate the entire list of allocations and change
      the memory protection permissions on everything; only change what
      is necessary.
    
    - When performing a new memory allocation, add the new allocation to
      the head of the allocations list. Statistically speaking, the most
      recently-allocated memory is the memory that is most likely to be
      freed soon. This makes freeing it faster, because it's closer to
      the start of the list that you need to iterate over.
    
    - When freeing the information about an allocation, also free the
      allocation itself. This avoids needing to do so in the 3 places
      that free allocation information, which removes some duplication.
    
    - Remove an unnecessary typecast.
    
    - Liberally comment everything.

--- libathemecore/memory_fe_sodium.c
+++ libathemecore/memory_fe_sodium.c
@@ -3,14 +3,15 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2005-2012 Atheme Project (http://atheme.org/)
- * Copyright (C) 2017-2019 Aaron M. D. Jones <aaronmdjones@gmail.com>
+ * Copyright (C) 2017-2020 Aaron M. D. Jones <aaronmdjones@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
  * atheme-services: A collection of minimalist IRC services
- * memory.c: Memory wrappers
+ * memory_fe_sodium.c: An extremely-paranoid hardened memory allocator
+ *                     built on top of the libsodium cryptographic library
  */
 
 #ifndef ATHEME_MEMORY_FRONTEND_C
@@ -19,6 +20,9 @@
 
 #include <sodium/utils.h>
 
+/* Stores memory allocation information (pointer to allocated memory, amount of memory allocated). This is
+ * entered into a list of memory allocations. This is all necessary to correctly implement srealloc() below.
+ */
 struct sodium_memblock
 {
 	mowgli_node_t   node;
@@ -26,81 +30,78 @@ struct sodium_memblock
 	size_t          len;
 };
 
+// The list of memory allocations
 static mowgli_list_t *sodium_memblocks = NULL;
-
-static inline void
-make_sodium_memblocks_ro(void)
-{
-	mowgli_node_t *n;
-
-	if (sodium_mprotect_readonly(sodium_memblocks) != 0)
-		abort();
-
-	MOWGLI_ITER_FOREACH(n, sodium_memblocks->head)
-	{
-		struct sodium_memblock *const mptr = n->data;
-
-		if (sodium_mprotect_readonly(mptr) != 0)
-			abort();
-	}
-}
-
-static inline void
-make_sodium_memblocks_rw(void)
-{
-	mowgli_node_t *n;
-
-	if (sodium_mprotect_readwrite(sodium_memblocks) != 0)
-		abort();
-
-	MOWGLI_ITER_FOREACH(n, sodium_memblocks->head)
-	{
-		struct sodium_memblock *const mptr = n->data;
-
-		if (sodium_mprotect_readwrite(mptr) != 0)
-			abort();
-	}
-}
 
 static void
 make_sodium_memblock(void *const restrict ptr, const size_t len)
 {
 	if (! ptr || ! len)
-		/* bug in this code */
+		// Bug in this code
 		abort();
 
 	if (! sodium_memblocks)
 	{
+		// Allocate it
 		if (! (sodium_memblocks = sodium_malloc(sizeof *sodium_memblocks)))
-			/* no free memory? */
+			// No free memory? Library failure?
 			abort();
 
+		// Erase it
 		(void) memset(sodium_memblocks, 0x00, sizeof *sodium_memblocks);
+
+		// Test whether making it read-only works
+		if (sodium_mprotect_readonly(sodium_memblocks) != 0)
+			abort();
 	}
 
+	// Create a memory allocation infoblock
 	struct sodium_memblock *const mptr = sodium_malloc(sizeof *mptr);
 
 	if (! mptr)
-		/* no free memory? */
+		// No free memory? Library failure?
 		abort();
 
+	// Store the memory allocation info
 	mptr->ptr = ptr;
 	mptr->len = len;
 
-	(void) make_sodium_memblocks_rw();
-	(void) mowgli_node_add(mptr, &mptr->node, sodium_memblocks);
-	(void) make_sodium_memblocks_ro();
+	// Grab the current head infoblock from the list (if any)
+	const mowgli_node_t *const tn = sodium_memblocks->head;
+
+	// Make the current head infoblock writeable (so its `node.prev` pointer can be modified)
+	if (tn && sodium_mprotect_readwrite(tn->data) != 0)
+		abort();
+
+	// Make the allocations list writeable
+	if (sodium_mprotect_readwrite(sodium_memblocks) != 0)
+		abort();
+
+	// Add the new infoblock to the head of the allocations list
+	(void) mowgli_node_add_head(mptr, &mptr->node, sodium_memblocks);
+
+	// Make the allocations list read-only
+	if (sodium_mprotect_readonly(sodium_memblocks) != 0)
+		abort();
+
+	// Make the previous head infoblock read-only
+	if (tn && sodium_mprotect_readonly(tn->data) != 0)
+		abort();
+
+	// Finally, make the new infoblock read-only
+	if (sodium_mprotect_readonly(mptr) != 0)
+		abort();
 }
 
-static struct sodium_memblock *
+static struct sodium_memblock * ATHEME_FATTR_RETURNS_NONNULL
 find_sodium_memblock(void *const restrict ptr)
 {
 	if (! ptr)
-		/* bug in this code */
+		// Bug in this code
 		abort();
 
 	if (! sodium_memblocks)
-		/* srealloc(ptr!=NULL) or sfree(ptr!=NULL) before any s(c|m|re)alloc() calls */
+		// An srealloc(ptr!=NULL) or sfree(ptr!=NULL) call, before any s(c|m|re)alloc() calls
 		abort();
 
 	mowgli_node_t *n;
@@ -113,48 +114,77 @@ find_sodium_memblock(void *const restrict ptr)
 			return mptr;
 	}
 
-	return NULL;
+	// An srealloc(ptr!=NULL) or free(ptr!=NULL) call for a ptr not allocated by us, or a double-free
+	abort();
 }
 
 static void
 free_sodium_memblock(struct sodium_memblock *const restrict mptr)
 {
 	if (! mptr)
-		/* bug in this code */
+		// Bug in this code
 		abort();
 
 	if (! sodium_memblocks)
-		/* srealloc(ptr!=NULL,len==0) or sfree(ptr!=NULL) before any s(c|m|re)alloc() calls */
+		// An srealloc(ptr!=NULL,len==0) or sfree(ptr!=NULL) call, before any s(c|m|re)alloc() calls
 		abort();
 
-	(void) make_sodium_memblocks_rw();
-	(void) mowgli_node_delete(&mptr->node, sodium_memblocks);
-	(void) make_sodium_memblocks_ro();
+	// Grab the current infoblock's previous and next infoblocks (if any)
+	const mowgli_node_t *const tp = mptr->node.prev;
+	const mowgli_node_t *const tn = mptr->node.next;
 
+	// Make the current infoblock writeable
+	if (sodium_mprotect_readwrite(mptr) != 0)
+		abort();
+
+	// Make the current infoblock's previous infoblock writeable (so its `node.next` pointer can be modified)
+	if (tp && sodium_mprotect_readwrite(tp->data) != 0)
+		abort();
+
+	// Make the current infoblock's next infoblock writeable (so its `node.prev` pointer can be modified)
+	if (tn && sodium_mprotect_readwrite(tn->data) != 0)
+		abort();
+
+	// Make the allocations list writeable
+	if (sodium_mprotect_readwrite(sodium_memblocks) != 0)
+		abort();
+
+	// Remove the current infoblock from the list
+	(void) mowgli_node_delete(&mptr->node, sodium_memblocks);
+
+	// Make the allocations list read-only
+	if (sodium_mprotect_readonly(sodium_memblocks) != 0)
+		abort();
+
+	// Make the current infoblock's previous infoblock read-only
+	if (tp && sodium_mprotect_readonly(tp->data) != 0)
+		abort();
+
+	// Make the current infoblock's next infoblock read-only
+	if (tn && sodium_mprotect_readonly(tn->data) != 0)
+		abort();
+
+	// Finally, free the memory allocation within the current infoblock, and then the current infoblock itself
+	(void) sodium_free(mptr->ptr);
 	(void) sodium_free(mptr);
 }
 
 void
 sfree(void *const restrict ptr)
 {
+	// A free(NULL) call is well-defined to do nothing
 	if (! ptr)
-		/* free(NULL) is well-defined to do nothing */
 		return;
 
 	struct sodium_memblock *const mptr = find_sodium_memblock(ptr);
 
-	if (! mptr)
-		/* sfree() on something that s(c|m|re)alloc() didn't provide or double-free */
-		abort();
-
 	(void) free_sodium_memblock(mptr);
-	(void) sodium_free(ptr);
 }
 
 void
 smemzerofree(void *const restrict ptr, const size_t ATHEME_VATTR_UNUSED len)
 {
-	/* libsodium already does a sodium_memzero() on the pointer given to sodium_free() */
+	// No erasing here: the library does its own sodium_memzero() on the pointer given to sodium_free()
 	(void) sfree(ptr);
 }
 
@@ -162,61 +192,61 @@ void * ATHEME_FATTR_ALLOC_SIZE_PRODUCT(1, 2) ATHEME_FATTR_MALLOC ATHEME_FATTR_RE
 scalloc(const size_t num, const size_t len)
 {
 	if (! num || ! len)
-		/* calloc(x, y) for (x * y) == 0 should return NULL but that would break us */
+		// A calloc(num, len) call where the product is 0 should return NULL, but codebase assumes it doesn't
 		abort();
 
-	if (len >= (size_t) (((size_t) SIZE_MAX) / num))
-		/* (num * len) would overflow */
+	if (len >= (((size_t) SIZE_MAX) / num))
+		// The product of num and len would overflow and result in too small an allocation
 		abort();
 
 	const size_t totalsz = (num * len);
 
+	// Allocate the memory
 	void *const buf = sodium_malloc(totalsz);
 
 	if (! buf)
-		/* no free memory? */
+		// No free memory? Library failure?
 		abort();
 
+	// Store it in the allocations list
 	(void) make_sodium_memblock(buf, totalsz);
 
+	// Erase it before returning it -- codebase assumes smalloc() and scalloc() both return zeroed memory
 	return memset(buf, 0x00, totalsz);
 }
 
 void * ATHEME_FATTR_ALLOC_SIZE(2) ATHEME_FATTR_WUR
 srealloc(void *const restrict ptr, const size_t len)
 {
+	// A realloc(NULL, len) call is equivalent to malloc(len)
 	if (! ptr)
-		/* realloc(NULL, x) == malloc(x) */
 		return smalloc(len);
 
 	struct sodium_memblock *const mptr = find_sodium_memblock(ptr);
 
-	if (! mptr)
-		abort();
-
+	// A realloc(ptr, 0) call is equivalent to free(ptr)
 	if (! len)
 	{
-		/* realloc(ptr, 0) == free(ptr) */
-
 		(void) free_sodium_memblock(mptr);
-		(void) sodium_free(ptr);
-
 		return NULL;
 	}
 
 	if (len == mptr->len)
-		/* realloc() to same size as existing alloc -- do nothing */
+		// Asked to reallocate to the same size -- do nothing
 		return ptr;
 
+	// Perform a new allocation
 	void *const buf = smalloc(len);
 
+	// Copy the data in the existing allocation to it
 	if (len > mptr->len)
 		(void) memcpy(buf, ptr, mptr->len);
 	else
 		(void) memcpy(buf, ptr, len);
 
+	// Free the existing allocation
 	(void) free_sodium_memblock(mptr);
-	(void) sodium_free(ptr);
 
+	// Return the new allocation
 	return buf;
 }
