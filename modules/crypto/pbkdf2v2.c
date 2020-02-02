09563b5d7c607f8311c8b390b17b5f37da3ca404
Aaron Jones <aaronmdjones@gmail.com>
2020-02-02 05:20:00 +0000

    modules/crypto/pbkdf2v2: option parsing: some improvements
    - Give clearer error message for the SCRAM- variants being unavailable
    
    - Accept more digest names differing only by e.g. hyphen or lack of, or
      an implied or explicit version, for broader compatibility
    
    - Only assign to pbkdf2v2_digest once

--- modules/crypto/pbkdf2v2.c
+++ modules/crypto/pbkdf2v2.c
@@ -612,30 +612,64 @@ end:
 static int
 c_ci_pbkdf2v2_digest(mowgli_config_file_entry_t *const restrict ce)
 {
-	pbkdf2v2_digest = PBKDF2_PRF_DEFAULT;
-
 	if (! ce->vardata)
 	{
 		(void) conf_report_warning(ce, "no parameter for configuration option -- using default");
+
+		pbkdf2v2_digest = PBKDF2_PRF_DEFAULT;
 		return 0;
 	}
 
+#ifndef HAVE_LIBIDN
+	if (! strncasecmp(ce->vardata, "SCRAM-", 6))
+	{
+		(void) conf_report_warning(ce, "SCRAM mode is unavailable (GNU libidn is missing) -- using default");
+
+		pbkdf2v2_digest = PBKDF2_PRF_DEFAULT;
+		return 0;
+	}
+#endif
+
+	// Most of these are aliases, for compatibility. Having them around is harmless.   -- amdj
+
 	if (! strcasecmp(ce->vardata, "SHA1"))
 		pbkdf2v2_digest = PBKDF2_PRF_HMAC_SHA1_S64;
+	else if (! strcasecmp(ce->vardata, "SHA-1"))
+		pbkdf2v2_digest = PBKDF2_PRF_HMAC_SHA1_S64;
+	else if (! strcasecmp(ce->vardata, "SHA2-256"))
+		pbkdf2v2_digest = PBKDF2_PRF_HMAC_SHA2_256_S64;
+	else if (! strcasecmp(ce->vardata, "SHA-256"))
+		pbkdf2v2_digest = PBKDF2_PRF_HMAC_SHA2_256_S64;
 	else if (! strcasecmp(ce->vardata, "SHA256"))
 		pbkdf2v2_digest = PBKDF2_PRF_HMAC_SHA2_256_S64;
+	else if (! strcasecmp(ce->vardata, "SHA2-512"))
+		pbkdf2v2_digest = PBKDF2_PRF_HMAC_SHA2_512_S64;
+	else if (! strcasecmp(ce->vardata, "SHA-512"))
+		pbkdf2v2_digest = PBKDF2_PRF_HMAC_SHA2_512_S64;
 	else if (! strcasecmp(ce->vardata, "SHA512"))
 		pbkdf2v2_digest = PBKDF2_PRF_HMAC_SHA2_512_S64;
-#ifdef HAVE_LIBIDN
+	else if (! strcasecmp(ce->vardata, "SCRAM-SHA1"))
+		pbkdf2v2_digest = PBKDF2_PRF_SCRAM_SHA1_S64;
 	else if (! strcasecmp(ce->vardata, "SCRAM-SHA-1"))
 		pbkdf2v2_digest = PBKDF2_PRF_SCRAM_SHA1_S64;
+	else if (! strcasecmp(ce->vardata, "SCRAM-SHA2-256"))
+		pbkdf2v2_digest = PBKDF2_PRF_SCRAM_SHA2_256_S64;
 	else if (! strcasecmp(ce->vardata, "SCRAM-SHA-256"))
 		pbkdf2v2_digest = PBKDF2_PRF_SCRAM_SHA2_256_S64;
+	else if (! strcasecmp(ce->vardata, "SCRAM-SHA256"))
+		pbkdf2v2_digest = PBKDF2_PRF_SCRAM_SHA2_256_S64;
+	else if (! strcasecmp(ce->vardata, "SCRAM-SHA2-512"))
+		pbkdf2v2_digest = PBKDF2_PRF_SCRAM_SHA2_512_S64;
 	else if (! strcasecmp(ce->vardata, "SCRAM-SHA-512"))
 		pbkdf2v2_digest = PBKDF2_PRF_SCRAM_SHA2_512_S64;
-#endif /* HAVE_LIBIDN */
+	else if (! strcasecmp(ce->vardata, "SCRAM-SHA512"))
+		pbkdf2v2_digest = PBKDF2_PRF_SCRAM_SHA2_512_S64;
 	else
+	{
 		(void) conf_report_warning(ce, "invalid parameter for configuration option -- using default");
+
+		pbkdf2v2_digest = PBKDF2_PRF_DEFAULT;
+	}
 
 	return 0;
 }
