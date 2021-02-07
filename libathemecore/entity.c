atheme/atheme

commit 416602a02a2cb2c3c7efcd55aa52d3b40638541a

Give entities a UID, and make them persistent for accounts and groups

libathemecore/entity.c
@@ -5,14 +5,65 @@
 #include "atheme.h"
 
 static mowgli_patricia_t *entities;
+static char last_entity_uid[IDLEN];
 
 void init_entities(void)
 {
 	entities = mowgli_patricia_create(irccasecanon);
+	memset(last_entity_uid, 'A', sizeof last_entity_uid);
+}
+
+void myentity_set_last_uid(const char *last_id)
+{
+	strlcpy(last_entity_uid, last_id, sizeof last_entity_uid);
+	last_entity_uid[IDLEN-1] = '\0';
+}
+
+const char *myentity_get_last_uid(void)
+{
+	return last_entity_uid;
+}
+
+const char *myentity_alloc_uid(void)
+{
+	int i;
+
+	for(i = 8; i > 3; i--)
+	{
+		if(last_entity_uid[i] == 'Z')
+		{
+			last_entity_uid[i] = '0';
+			return last_entity_uid;
+		}
+		else if(last_entity_uid[i] != '9')
+		{
+			last_entity_uid[i]++;
+			return last_entity_uid;
+		}
+		else
+			last_entity_uid[i] = 'A';
+	}
+
+	/* if this next if() triggers, we're fucked. */
+	if(last_entity_uid[3] == 'Z')
+	{
+		last_entity_uid[i] = 'A';
+		slog(LG_ERROR, "Out of entity UIDs!");
+		wallops("Out of entity UIDs. This is a Bad Thing.");
+		wallops("You should probably do something about this.");
+	}
+	else
+		last_entity_uid[3]++;
+
+	return last_entity_uid;
 }
 
 void myentity_put(myentity_t *mt)
 {
+	/* If the entity doesn't have an ID yet, allocate one */
+	if (mt->id[0] == '\0')
+		strlcpy(mt->id, myentity_alloc_uid(), sizeof mt->id);
+
 	mowgli_patricia_add(entities, mt->name, mt);
 }
 

include/abirev.h
@@ -16,7 +16,7 @@
  * digits and set the rest to 0 (e.g. 330000). Otherwise, increment
  * the lower digits.
  */
-#define CURRENT_ABI_REVISION 700004
+#define CURRENT_ABI_REVISION 700005
 
 #endif
 

include/account.h
@@ -378,6 +378,7 @@ E mowgli_patricia_t *mclist;
 E void init_accounts(void);
 
 E myuser_t *myuser_add(const char *name, const char *pass, const char *email, unsigned int flags);
+E myuser_t *myuser_add_id(const char *id, const char *name, const char *pass, const char *email, unsigned int flags);
 E void myuser_delete(myuser_t *mu);
 //inline myuser_t *myuser_find(const char *name);
 E void myuser_rename(myuser_t *mu, const char *name);

include/entity.h
@@ -16,6 +16,7 @@ typedef struct {
 	object_t parent;
 	myentity_type_t type;
 	char name[NICKLEN * 4];
+	char id[IDLEN];
 
 	mowgli_list_t chanacs;
 	void *chanacs_validate;	/* vtable for validating chanacs entries */
@@ -28,6 +29,10 @@ typedef struct {
 #define isgroup(x)	(x != NULL && entity(x)->type == ENT_GROUP)
 
 void init_entities(void);
+void myentity_set_last_uid(const char *last_uid);
+const char *myentity_get_last_uid(void);
+const char *myentity_alloc_uid(void);
+
 void myentity_put(myentity_t *me);
 void myentity_del(myentity_t *me);
 myentity_t *myentity_find(const char *name);

libathemecore/account.c
@@ -99,7 +99,19 @@ void init_accounts(void)
  *      - if nicksvs.no_nick_ownership is not enabled, the caller is
  *        responsible for adding a nick with the same name
  */
+
 myuser_t *myuser_add(const char *name, const char *pass, const char *email, unsigned int flags)
+{
+	return myuser_add_id(NULL, name, pass, email, flags);
+}
+
+/*
+ * myuser_add_id(const char *id, const char *name, const char *pass,
+ * const char *email, unsigned int flags)
+ *
+ * Like myuser_add, but lets you specify the new entity's UID.
+ */
+myuser_t *myuser_add_id(const char *id, const char *name, const char *pass, const char *email, unsigned int flags)
 {
 	myuser_t *mu;
 	soper_t *soper;
@@ -115,6 +127,10 @@ myuser_t *myuser_add(const char *name, const char *pass, const char *email, unsi
 	entity(mu)->type = ENT_USER;
 	strlcpy(entity(mu)->name, name, NICKLEN);
 	mu->email = sstrdup(email);
+	if (id)
+		strlcpy(entity(mu)->id, id, sizeof(entity(mu)->name));
+	else
+		entity(mu)->id[0] = '\0';
 
 	mu->registered = CURRTIME;
 	mu->flags = flags;

libathemecore/logger.c
@@ -646,15 +646,21 @@ void logcommand_user(service_t *svs, user_t *source, int level, const char *fmt,
 {
 	va_list args;
 	char lbuf[BUFSIZE];
+	char accountbuf[NICKLEN * 5]; /* entity name len is NICKLEN * 4, plus another for the ID */
 	bool showaccount;
 
 	va_start(args, fmt);
 	vsnprintf(lbuf, BUFSIZE, fmt, args);
 	va_end(args);
 
+	accountbuf[0] = '\0';
+	if (source->myuser != NULL)
+		snprintf(accountbuf, sizeof accountbuf, "%s/%s",
+				entity(source->myuser)->name, entity(source->myuser)->id);
+
 	slog_ext(LOG_NONINTERACTIVE, level, "%s %s:%s!%s@%s[%s] %s",
 			svs != NULL ? svs->nick : me.name,
-			source->myuser != NULL ? entity(source->myuser)->name : "",
+			accountbuf,
 			source->nick, source->user, source->host,
 			source->ip[0] != '\0' ? source->ip : source->host,
 			lbuf);

modules/backend/opensex.c
@@ -63,7 +63,11 @@ opensex_db_save(database_handle_t *db)
 
 	/* write the database version */
 	db_start_row(db, "DBV");
-	db_write_int(db, 9);
+	db_write_int(db, 10);
+	db_commit_row(db);
+
+	db_start_row(db, "LUID");
+	db_write_word(db, myentity_get_last_uid());
 	db_commit_row(db);
 
 	db_start_row(db, "CF");
@@ -82,6 +86,7 @@ opensex_db_save(database_handle_t *db)
 		 */
 		char *flags = gflags_tostr(mu_flags, MOWGLI_LIST_LENGTH(&mu->logins) ? mu->flags & ~MU_NOBURSTLOGIN : mu->flags);
 		db_start_row(db, "MU");
+		db_write_word(db, entity(mu)->id);
 		db_write_word(db, entity(mu)->name);
 		db_write_word(db, mu->pass);
 		db_write_word(db, mu->email);
@@ -389,6 +394,11 @@ static void opensex_h_dbv(database_handle_t *db, const char *type)
 	slog(LG_INFO, "opensex: data format version is %d.", rs->dbv);
 }
 
+static void opensex_h_luid(database_handle_t *db, const char *type)
+{
+	myentity_set_last_uid(db_sread_word(db));
+}
+
 static void opensex_h_cf(database_handle_t *db, const char *type)
 {
 	unsigned int their_ca_all;
@@ -408,12 +418,18 @@ static void opensex_h_cf(database_handle_t *db, const char *type)
 static void opensex_h_mu(database_handle_t *db, const char *type)
 {
 	opensex_t *rs = (opensex_t *)db->priv;
-	const char *name = db_sread_word(db);
+	const char *uid = NULL;
+	const char *name;
 	const char *pass, *email, *language;
 	time_t reg, login;
 	const char *sflags;
 	unsigned int flags = 0;
 	myuser_t *mu;
+
+	if (rs->dbv >= 10)
+		uid = db_sread_word(db);
+
+	name = db_sread_word(db);
 
 	if (myuser_find(name))
 	{
@@ -435,7 +451,7 @@ static void opensex_h_mu(database_handle_t *db, const char *type)
 	language = db_read_word(db);
 
 
-	mu = myuser_add(name, pass, email, flags);
+	mu = myuser_add_id(uid, name, pass, email, flags);
 	mu->registered = reg;
 	mu->lastlogin = login;
 	if (language)
@@ -1225,6 +1241,7 @@ void _modinit(module_t *m)
 	db_save = &opensex_db_write;
 
 	db_register_type_handler("DBV", opensex_h_dbv);
+	db_register_type_handler("LUID", opensex_h_luid);
 	db_register_type_handler("CF", opensex_h_cf);
 	db_register_type_handler("MU", opensex_h_mu);
 	db_register_type_handler("ME", opensex_h_me);

modules/groupserv/info.c
@@ -46,6 +46,9 @@ static void gs_cmd_info(sourceinfo_t *si, int parc, char *parv[])
 	command_success_nodata(si, _("Information for \2%s\2:"), parv[0]);
 	command_success_nodata(si, _("Registered  : %s (%s ago)"), strfbuf, time_ago(mg->regtime));
 
+	if (has_priv(si, PRIV_GROUP_AUSPEX))
+		command_success_nodata(si, _("Entity ID   : %s"), entity(mg)->id);
+
 	if (mg->flags & MG_PUBLIC || (si->smu != NULL && groupacs_sourceinfo_has_flag(mg, si, 0) && !groupacs_sourceinfo_has_flag(mg, si, GA_BAN)) || has_priv(si, PRIV_GROUP_AUSPEX))
 		command_success_nodata(si, _("Founder     : %s"), mygroup_founder_names(mg));
 

commit a89d6a7e5d7e9d060956e23cb2dbf678ed291797

libathemecore/entity.c: ensure entity ID buffer has a null terminator
Caught by Clang's AddressSanitizer.

Fixes: 416602a02a2cb2c3c7ef ("Give entities a UID, and make them persistent
    for accounts and groups")

libathemecore/entity.c
@@ -21,7 +21,9 @@ init_entities(void)
 {
 	entities = mowgli_patricia_create(irccasecanon);
 	entities_by_id = mowgli_patricia_create(noopcanon);
-	memset(last_entity_uid, 'A', sizeof last_entity_uid);
+
+	memset(last_entity_uid, 0x00, sizeof last_entity_uid);
+	memset(last_entity_uid, 'A', IDLEN);
 }
 
 void