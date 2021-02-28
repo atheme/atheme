f16c9b181ca1a69ca289573dd10d563ea04bbaf2
Aaron Jones <me@aaronmdjones.net>
2021-02-28 02:15:43 +0000

    Build System: Detect user error w/ downloading sourcecode
    A frequent complaint we receive is that serno.h is missing. This turns
    out to be users downloading release tarballs from GitHub, which does
    not include the .git directory, thus making the target for serno.h
    fail.
    
    Detect whether .git/ exists; reference GIT-Access.txt then. Otherwise,
    this must be a release tarball, so see if it has a pre-supplied serno.h
    and error if not (this could only be caused by downloading a GitHub
    source code link instead of an asset/release tarball).
    
    Also tidy up mkserno.sh a bit. It still can't use git-describe(1) on
    this branch (lack of recent tags), but will be made to do so on release
    branches.
    
    Thanks to ilbelkyr for the idea (pointing out that the sourcecode links
    on GitHub don't include the .git/ directory, making it possible to
    distinguish between that and git-clone(1)).

--- include/atheme/mkserno.sh
+++ include/atheme/mkserno.sh
@@ -3,10 +3,11 @@
 # SPDX-License-Identifier: ISC
 # SPDX-URL: https://spdx.org/licenses/ISC.html
 #
-# Copyright (C) 2018 Atheme Development Group (https://atheme.github.io/)
+# Copyright (C) 2018-2021 Atheme Development Group (https://atheme.github.io/)
 #
 # atheme-services: A collection of minimalist IRC services
-# mkserno.sh: Stores git commit hash into serno.h
+# mkserno.sh: Stores git revision information into serno.h
+#   (for use when building with a git(1) source tree)
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -18,12 +19,19 @@ REVH_NEW="$(git rev-parse --short=20 HEAD)"
 
 if [ "x${REVH_NEW}" = "x" ]
 then
-	REVH_NEW="<unknown>"
-else
-	if [ -n "$(git diff --stat)" -o -n "$(git diff --cached --stat)" ]
-	then
-		REVH_NEW="${REVH_NEW}-dirty"
-	fi
+	echo " " >&2
+	echo "Could not determine git revision; is git(1) installed?" >&2
+	echo " " >&2
+	echo "If you are not expecting this message, perhaps you" >&2
+	echo "downloaded the wrong sourcecode tarball. Please see" >&2
+	echo "the GIT-Access.txt file for instructions." >&2
+	echo " " >&2
+	exit 1
+fi
+
+if [ -n "$(git diff --stat)" -o -n "$(git diff --cached --stat)" ]
+then
+	REVH_NEW="${REVH_NEW}-dirty"
 fi
 
 REVH_NEW="\"${REVH_NEW}\""
@@ -34,13 +42,17 @@ then
 
 	if [ "x${REVH_NEW}" = "x${REVH_CUR}" ]
 	then
+		# If we would write the same information, avoid touching the
+		# file altogether, and thus avoid causing a rebuild of the
+		# entire source repository next time make is run (due to an
+		# updated mtime in combination with dependency tracking).
 		exit 0
 	fi
 fi
 
 echo "Generate: ${SERNO_FILE}"
 
-cat <<- EOF > "${SERNO_FILE}"
+cat <<-EOF > "${SERNO_FILE}"
 
 #ifndef ATHEME_INC_SERNO_H
 #define ATHEME_INC_SERNO_H 1
