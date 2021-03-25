atheme/atheme

commit 642759fc2907efe5e0ba442403b158d53d0fd14d

libathemecore/connection.c: use connection->name to store addrs & ports
Also obtain IP addresses where the sockets are created, instead of in
connection_add(). This is a better approach than that taken by commit
1bb7e1e587306239ca87, and restores the ability to perform non-blocking
connections.

Finally, since connection_add() creates the mowgli.pollable object for
the fd, don't bother testing if the fd is valid. Add an assertion for a
valid fd being passed, because an invalid fd will only result in
nothing working anyway. Likewise assert that the connection name is
valid, and that at least one I/O handler was supplied.

libathemecore/connection.c
@@ -26,17 +26,21 @@ mowgli_list_t connection_list;
 
 static bool
 connection_addr_satostr(const char *const restrict func, const struct sockaddr *const restrict sa,
-                        char dst[const restrict static INET6_ADDRSTRLEN])
+                        char dst[const restrict static CONNECTION_ADDRSTRLEN])
 {
+	char addr[INET6_ADDRSTRLEN];
+	unsigned int port;
 	const void *src;
 
 	switch (sa->sa_family)
 	{
 		case AF_INET:
+			port = (unsigned int) ntohs(((struct sockaddr_in *) sa)->sin_port);
 			src = &((struct sockaddr_in *) sa)->sin_addr;
 			break;
 
 		case AF_INET6:
+			port = (unsigned int) ntohs(((struct sockaddr_in6 *) sa)->sin6_port);
 			src = &((struct sockaddr_in6 *) sa)->sin6_addr;
 			break;
 
@@ -44,15 +48,14 @@ connection_addr_satostr(const char *const restrict func, const struct sockaddr *
 			(void) slog(LG_ERROR, "%s: unknown address family", func);
 			return false;
 	}
-
-	(void) memset(dst, 0x00, INET6_ADDRSTRLEN);
-
-	if (! inet_ntop(sa->sa_family, src, dst, INET6_ADDRSTRLEN))
+	if (! inet_ntop(sa->sa_family, src, addr, sizeof addr))
 	{
 		(void) slog(LG_ERROR, "%s: inet_ntop(3): %s", func, strerror(ioerrno()));
 		return false;
 	}
 
+	(void) memset(dst, 0x00, CONNECTION_ADDRSTRLEN);
+	(void) snprintf(dst, CONNECTION_ADDRSTRLEN, "[%s]:%u", addr, port);
 	return true;
 }
 
@@ -124,6 +127,10 @@ struct connection * ATHEME_FATTR_MALLOC
 connection_add(const char *const restrict name, const int fd, const unsigned int flags,
                const connection_evhandler read_handler, const connection_evhandler write_handler)
 {
+	return_null_if_fail(fd >= 0);
+	return_null_if_fail(name != NULL);
+	return_null_if_fail(read_handler != NULL || write_handler != NULL);
+
 	struct connection *cptr;
 
 	if ((cptr = connection_find(fd)))
@@ -135,25 +142,8 @@ connection_add(const char *const restrict name, const int fd, const unsigned int
 
 	(void) slog(LG_DEBUG, "%s: adding connection %d ('%s')", MOWGLI_FUNC_NAME, fd, name);
 
-	if (fd > -1)
-	{
-		struct sockaddr_storage saddr;
-		socklen_t saddrlen = sizeof saddr;
-		(void) memset(&saddr, 0x00, sizeof saddr);
-
-		if (getpeername(fd, (struct sockaddr *) &saddr, &saddrlen) < 0)
-		{
-			(void) slog(LG_ERROR, "%s: '%s': getpeername(2): %s",
-			                      MOWGLI_FUNC_NAME, name, strerror(ioerrno()));
-			return NULL;
-		}
-
-		if (! connection_addr_satostr(MOWGLI_FUNC_NAME, (struct sockaddr *) &saddr, cptr->hbuf))
-			return NULL;
-
-		if (! socket_setnonblocking(fd))
-			return NULL;
-	}
+	if (! socket_setnonblocking(fd))
+		return NULL;
 
 	cptr                = smalloc(sizeof *cptr);
 	cptr->fd            = fd;
@@ -356,7 +346,23 @@ connection_open_tcp(const char *const restrict host, const char *const restrict 
 	if (fd > claro_state.maxfd)
 		claro_state.maxfd = fd;
 
+	if (! socket_setnonblocking(fd))
+	{
+		(void) freeaddrinfo(host_addr);
+		(void) close(fd);
+		return NULL;
+	}
+
+	char vhost_hbuf[CONNECTION_ADDRSTRLEN];
+	char host_hbuf[CONNECTION_ADDRSTRLEN];
 	char name[BUFSIZE];
+
+	if (! connection_addr_satostr(MOWGLI_FUNC_NAME, host_addr->ai_addr, host_hbuf))
+	{
+		(void) freeaddrinfo(host_addr);
+		(void) close(fd);
+		return NULL;
+	}
 
 	if (vhost)
 	{
@@ -421,19 +427,28 @@ connection_open_tcp(const char *const restrict host, const char *const restrict 
 		}
 
 		(void) freeaddrinfo(vhost_addr);
-		(void) snprintf(name, sizeof name, "TCP: '%s' -> '%s'", vhost, host);
+
+		if (! connection_addr_satostr(MOWGLI_FUNC_NAME, host_addr->ai_addr, host_hbuf))
+		{
+			(void) freeaddrinfo(host_addr);
+			(void) close(fd);
+			return NULL;
+		}
+
+		(void) snprintf(name, sizeof name, "%s -> %s", vhost_hbuf, host_hbuf);
 	}
 	else
-		(void) snprintf(name, sizeof name, "TCP: * -> '%s'", host);
+		(void) snprintf(name, sizeof name, "[::]:0 -> %s", host_hbuf);
 
 	if (connect(fd, host_addr->ai_addr, host_addr->ai_addrlen) == -1 && ! connection_ignore_errno(ioerrno()))
 	{
 		if (vhost)
-			(void) slog(LG_ERROR, "%s: unable to connect from '%s' to '%s': connect(2): %s",
-			                      MOWGLI_FUNC_NAME, vhost, host, strerror(ioerrno()));
+			(void) slog(LG_ERROR, "%s: unable to connect from '%s' (%s) to '%s' (%s): connect(2): %s",
+			                      MOWGLI_FUNC_NAME, vhost, vhost_hbuf, host, host_hbuf,
+			                      strerror(ioerrno()));
 		else
-			(void) slog(LG_ERROR, "%s: unable to connect to '%s': connect(2): %s",
-			                      MOWGLI_FUNC_NAME, host, strerror(ioerrno()));
+			(void) slog(LG_ERROR, "%s: unable to connect to '%s' (%s): connect(2): %s",
+			                      MOWGLI_FUNC_NAME, host, host_hbuf, strerror(ioerrno()));
 
 		(void) freeaddrinfo(host_addr);
 		(void) close(fd);
@@ -497,6 +512,15 @@ connection_open_listener_tcp(const char *const restrict host, const unsigned int
 	if (fd > claro_state.maxfd)
 		claro_state.maxfd = fd;
 
+	char hbuf[CONNECTION_ADDRSTRLEN];
+
+	if (! connection_addr_satostr(MOWGLI_FUNC_NAME, addr->ai_addr, hbuf))
+	{
+		(void) freeaddrinfo(addr);
+		(void) close(fd);
+		return NULL;
+	}
+
 #if defined(SOL_SOCKET) && defined(SO_REUSEADDR)
 	const unsigned int optval = 1;
 	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval) == -1)
@@ -532,8 +556,7 @@ connection_open_listener_tcp(const char *const restrict host, const unsigned int
 	}
 
 	char name[BUFSIZE];
-
-	(void) snprintf(name, sizeof name, "TCP: listening on [%s]:%u", host, port);
+	(void) snprintf(name, sizeof name, "%s", hbuf);
 
 	return connection_add(name, fd, CF_LISTENING, read_handler, NULL);
 }
@@ -542,8 +565,9 @@ struct connection *
 connection_accept_tcp(struct connection *const restrict cptr, const connection_evhandler read_handler,
                       const connection_evhandler write_handler)
 {
-	const int fd = accept(cptr->fd, NULL, NULL);
-
+	struct sockaddr_storage peer_ss;
+	socklen_t peer_ss_len = sizeof peer_ss;
+	const int fd = accept(cptr->fd, (struct sockaddr *) &peer_ss, &peer_ss_len);
 	if (fd == -1)
 	{
 		(void) slog(LG_ERROR, "%s: accept(2): %s", MOWGLI_FUNC_NAME, strerror(ioerrno()));
@@ -554,8 +578,30 @@ connection_accept_tcp(struct connection *const restrict cptr, const connection_e
 	if (fd > claro_state.maxfd)
 		claro_state.maxfd = fd;
 
+	struct sockaddr_storage sock_ss;
+	socklen_t sock_ss_len = sizeof sock_ss;
+	if (getsockname(fd, (struct sockaddr *) &sock_ss, &sock_ss_len) == -1)
+	{
+		(void) slog(LG_ERROR, "%s: getsockname(2): %s", MOWGLI_FUNC_NAME, strerror(ioerrno()));
+		(void) close(fd);
+		return NULL;
+	}
+
+	char peer_hbuf[CONNECTION_ADDRSTRLEN];
+	char sock_hbuf[CONNECTION_ADDRSTRLEN];
+	if (! connection_addr_satostr(MOWGLI_FUNC_NAME, (struct sockaddr *) &peer_ss, peer_hbuf))
+	{
+		(void) close(fd);
+		return NULL;
+	}
+	if (! connection_addr_satostr(MOWGLI_FUNC_NAME, (struct sockaddr *) &sock_ss, sock_hbuf))
+	{
+		(void) close(fd);
+		return NULL;
+	}
+
 	char name[BUFSIZE];
-	(void) mowgli_strlcpy(name, "incoming connection", sizeof name);
+	(void) snprintf(name, sizeof name, "%s <- %s", sock_hbuf, peer_hbuf);
 
 	struct connection *const newptr = connection_add(name, fd, CF_CONNECTED, read_handler, write_handler);
 

include/atheme/connection.h
@@ -21,6 +21,9 @@
 #  define ioerrno()             WSAGetLastError()
 #endif
 
+// Enough space for brackets and a port; [INET6_ADDRSTRLEN]:12345
+#define CONNECTION_ADDRSTRLEN   (INET6_ADDRSTRLEN + 8)
+
 #define CF_UPLINK               0x00000001U
 #define CF_DCCOUT               0x00000002U
 #define CF_DCCIN                0x00000004U
@@ -63,8 +66,7 @@ struct connection
 	time_t                          last_recv;
 	unsigned int                    flags;
 	int                             fd;
-	char                            hbuf[BUFSIZE + 1];
-	char                            name[HOSTLEN + 1];
+	char                            name[BUFSIZE];
 };
 
 struct connection *connection_add(const char *, int, unsigned int, connection_evhandler, connection_evhandler)

libathemecore/logger.c
@@ -724,7 +724,7 @@ format_external(const char *type, struct connection *source, const char *sourced
 		snprintf(buf, sizeof buf, "%s:%s(%s)[%s]",
 			mu != NULL ? entity(mu)->name : "",
 			type,
-			source != NULL ? source->hbuf : "<noconn>",
+			source != NULL ? source->name : "<noconn>",
 			sourcedesc != NULL ? sourcedesc : "<unknown>");
 	else
 		snprintf(buf, sizeof buf, "<%s>%s",

modules/misc/httpd.c
@@ -218,7 +218,7 @@ httpd_recvqhandler(struct connection *cptr)
 		return;
 	if (CF_IS_NONEWLINE(cptr))
 	{
-		slog(LG_INFO, "httpd_recvqhandler(): throwing out fd %d (%s) for excessive line length", cptr->fd, cptr->hbuf);
+		slog(LG_INFO, "httpd_recvqhandler(): throwing out fd %d (%s) for excessive line length", cptr->fd, cptr->name);
 		send_error(cptr, 400, "Bad request", true);
 		sendq_add_eof(cptr);
 		return;
@@ -304,7 +304,7 @@ httpd_recvqhandler(struct connection *cptr)
 			close(in);
 			if (count1 > 0)
 			{
-				slog(LG_INFO, "httpd_recvqhandler(): disconnecting fd %d (%s), read failed on %s", cptr->fd, cptr->hbuf, hd->filename);
+				slog(LG_INFO, "httpd_recvqhandler(): disconnecting fd %d (%s), read failed on %s", cptr->fd, cptr->name, hd->filename);
 				cptr->flags |= CF_DEAD;
 			}
 			else
@@ -352,7 +352,7 @@ httpd_closehandler(struct connection *cptr)
 {
 	struct httpddata *hd;
 
-	slog(LG_DEBUG, "httpd_closehandler(): fd %d (%s) closed", cptr->fd, cptr->hbuf);
+	slog(LG_DEBUG, "httpd_closehandler(): fd %d (%s) closed", cptr->fd, cptr->name);
 	hd = cptr->userdata;
 	if (hd != NULL)
 	{
@@ -368,7 +368,7 @@ do_listen(struct connection *cptr)
 	struct connection *newptr;
 
 	newptr = connection_accept_tcp(cptr, recvq_put, NULL);
-	slog(LG_DEBUG, "do_listen(): accepted httpd from %s fd %d", newptr->hbuf, newptr->fd);
+	slog(LG_DEBUG, "do_listen(): accepted fd %d (%s)", newptr->fd, newptr->name);
 
 	struct httpddata *const hd = smalloc(sizeof *hd);
 	hd->connection_close = false;

commit 4dc030252fa65aaf257c276e2148688c4bf26b15

libathemecore/connection.c: fix cut/paste mistake
Fixes: 642759fc2907efe5e0ba ("libathemecore/connection.c: use
                              connection->name to store addrs & ports")

libathemecore/connection.c
@@ -426,7 +426,7 @@ connection_open_tcp(const char *const restrict host, const char *const restrict 
 
 		(void) freeaddrinfo(vhost_addr);
 
-		if (! connection_addr_satostr(MOWGLI_FUNC_NAME, host_addr->ai_addr, host_hbuf))
+		if (! connection_addr_satostr(MOWGLI_FUNC_NAME, vhost_addr->ai_addr, vhost_hbuf))
 		{
 			(void) freeaddrinfo(host_addr);
 			(void) close(fd);