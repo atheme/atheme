atheme/atheme

commit e2fd6f2a8959fb89acb295d6e29a642a1a4e73b1

modules/operserv/modmanager: account for an entire list of non-reloadables
When recursing a given module's reverse dependencies, it is
possible that we run into a permanent module that is depended
upon by a semi-permanent (reload-only) module. This will result
in the reload of that module being skipped (as there is a
permanent module in its reverse dependency chain), but the
mod_recurse_revdeps() function will still set the r_dep pointer
to the semi-permanent one.

We then test if r_dep is set to perform a database save (in case
reloading of the reload-only module fails). However, if our
entire list of modules to reload has recursive non-reloadability
status, we will be saving the database for nothing, as we then
go on to ignore every module in the list.

Exit early if this is the case, avoiding a pointless wallops and
database save.

modules/operserv/modmanager.c
@@ -401,6 +401,7 @@ os_cmd_modreload_func(struct sourceinfo *const restrict si, const int parc, char
 
 	const char *n_dep = NULL;
 	const char *r_dep = NULL;
+	bool can_reload_any = false;
 	enum module_unload_capability ucap[parc];
 	mowgli_list_t *const deplist = mowgli_list_create();
 
@@ -439,7 +440,13 @@ os_cmd_modreload_func(struct sourceinfo *const restrict si, const int parc, char
 		(void) mod_recurse_revdeps(mptr, deplist, NULL, NULL);
 
 		(void) logcommand(si, CMDLOG_ADMIN, "MODRELOAD: \2%s\2", parv[i]);
+
+		can_reload_any = true;
 	}
+
+	// If we are not able to perform any reloads, just give up now
+	if (! can_reload_any)
+		return;
 
 	/* Sort the list of dependencies so that we reload them in the order they're needed. We can't do this below
 	 * because the modules will be unloaded at that point, which is also why we need to iterate parv[] twice...

commit fc081d76793e3f3f87e37c9680e54bd4e0e08d3c

modules/operserv/modmanager: fix relatively harmless memory leak
If passed a list consisting entirely of non-reloadable modules, we would
leak approximately up to 24 bytes of memory each time. Discovered manually
during reading the function.

Fixes: e2fd6f2a8959fb89acb2

modules/operserv/modmanager.c
@@ -446,7 +446,10 @@ os_cmd_modreload_func(struct sourceinfo *const restrict si, const int parc, char
 
 	// If we are not able to perform any reloads, just give up now
 	if (! can_reload_any)
+	{
+		(void) free_revdep_list(deplist);
 		return;
+	}
 
 	/* Sort the list of dependencies so that we reload them in the order they're needed. We can't do this below
 	 * because the modules will be unloaded at that point, which is also why we need to iterate parv[] twice...