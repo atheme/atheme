1274be8a43ffbf5f0bca19cbc346991640109cf6
Aaron Jones <me@aaronmdjones.net>
2022-02-12 20:35:48 +0000

    libathemecore/table: top-to-bottom renovations
    - Remove somewhat-pointless name argument from table_new().
    
      This was never printed anywhere, and could only be seen by
      doing an object introspection with STATS.
    
    - Remove this object's "object-likeness". I can see no valid
      reason why this should have to behave like any other object.
    
    - Rename some structures and functions to more closely match
      what people typically call them.
    
    The above changes are not expected to break anything, as the
    table rendering API has gone unused ever since it was written.
    An exception is the contrib module os_tabletest, which exists
    to exercise this code, and will be updated shortly.
    
    - When rendering, verify that a table has at least one row,
      and verify that all of the table's rows have the same number
      of columns each. Also verify that the first row's columns
      all have names.
    
    - Allow callers to detect if a column has been added to a row
      successfully.
    
    - Allow callers to detect if a table has been rendered
      successfully.
    
    - Reduce the amount of dynamic memory allocation and
      fragmentation when creating new tables, rows, and columns,
      by using a mowgli heap for the allocations.
    
    - Further reduce the amount of dynamic memory allocation and
      fragmentation when adding new rows and columns to tables, by
      using an intrusive list node in the structure we're already
      allocating, instead of allocating a new node as well.
    
    - Further reduce the amount of dynamic memory allocation and
      fragmentation when adding a new column to a table, by taking
      a copy of the column's name only when adding the column to
      the first row of the table.
    
    - Now that we require a C99 compiler, use VLAs (a C99 feature)
      to remove all dynamic memory allocation from the table
      rendering process.
    
    - Move calculation of widths into an array rather than storing
      them in the column members, removing the need for the "width"
      member of "struct atheme_table_column" and its associated
      comment that only the first row will have a value for it.
    
    - Remove unused "id" member from "struct atheme_table_row".
    
    - Enforce a minimum column width of 4 characters, to enable
      people to more easily visually separate them, and read their
      values if they were to be packed very close together.
    
    - Comment the table routines so that other people can follow
      along more easily, including explaining various magic numbers.

--- include/atheme/table.h
+++ include/atheme/table.h
@@ -3,6 +3,7 @@
  * SPDX-URL: https://spdx.org/licenses/ISC.html
  *
  * Copyright (C) 2007 William Pitcock <nenolod -at- sacredspiral.co.uk>
+ * Copyright (C) 2022 Atheme Development Group (https://atheme.github.io/)
  *
  * Table rendering class.
  */
@@ -11,46 +12,33 @@
 #define ATHEME_INC_TABLE_H 1
 
 #include <atheme/attributes.h>
-#include <atheme/object.h>
 #include <atheme/stdheaders.h>
 
 struct atheme_table
 {
-	struct atheme_object    parent;
 	mowgli_list_t           rows;
 };
 
 struct atheme_table_row
 {
-	int             id;
-	mowgli_list_t   cells;
+	mowgli_node_t           node;
+	mowgli_list_t           columns;
+	struct atheme_table *   table;
 };
 
-struct atheme_table_cell
+struct atheme_table_column
 {
-	int     width;  // only if first row
-	char *  name;
-	char *  value;
+	mowgli_node_t           node;
+	char *                  name;   // NULL except in first row
+	char *                  value;
 };
 
-/*
- * Creates a new table object. Use atheme_object_unref() to destroy it.
- */
-struct atheme_table *table_new(const char *fmt, ...) ATHEME_FATTR_MALLOC ATHEME_FATTR_PRINTF(1, 2);
+struct atheme_table *table_create(void) ATHEME_FATTR_MALLOC;
+struct atheme_table_row *table_add_row(struct atheme_table *) ATHEME_FATTR_MALLOC_UNCHECKED;
+struct atheme_table_column *table_add_column(struct atheme_table_row *, const char *, const char *)
+    ATHEME_FATTR_MALLOC_UNCHECKED;
 
-/*
- * Renders a table, each line going to callback().
- */
-void table_render(struct atheme_table *t, void (*callback)(const char *line, void *data), void *data);
-
-/*
- * Associates a value with a row.
- */
-void table_cell_associate(struct atheme_table_row *r, const char *name, const char *value);
-
-/*
- * Associates a row with a table.
- */
-struct atheme_table_row *table_row_new(struct atheme_table *t) ATHEME_FATTR_MALLOC;
+bool table_render(struct atheme_table *, void (*)(const char *, void *), void *);
+void table_destroy(struct atheme_table *);
 
 #endif /* !ATHEME_INC_TABLE_H */
