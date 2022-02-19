atheme/atheme

commit b427d04059d4d4f7b39685e7054da6cdd63b71e8

libathemecore/atheme.c: allow periodic db save strategy to be adjusted
Also document a situation where it is desirable to do so.

libathemecore/atheme.c
@@ -329,7 +329,11 @@ static void
 db_save_periodic(void *unused)
 {
 	slog(LG_DEBUG, "db_save_periodic(): initiating periodic database write");
-	db_save(unused, DB_SAVE_BG_REGULAR);
+
+	if (config_options.db_save_blocking)
+		db_save(unused, DB_SAVE_BLOCKING);
+	else
+		db_save(unused, DB_SAVE_BG_REGULAR);
 }
 
 int

dist/atheme.conf.example
@@ -2602,9 +2602,23 @@ general {
 
 	/* commit_interval (minutes)
 	 *
-	 * The time between database writes.
+	 * The time between periodic database writes.
 	 */
 	commit_interval = 5;
+
+	/* (*) db_save_blocking
+	 *
+	 * Whether to always use a blocking database save (even in the
+	 * periodic db save timer; see commit_interval above).
+	 *
+	 * Useful if you are running services with LeakSanitizer enabled, as
+	 * services does not free its resources before exiting. A regular db
+	 * save forks to the background, writes the database, and exits, which
+	 * will pollute the console with pointless memory leak messages.
+	 *
+	 * Not recommended if you are not running services in the console.
+	 */
+	#db_save_blocking;
 
 	/* (*) operstring
 	 *

include/atheme/global.h
@@ -71,6 +71,7 @@ struct ConfOption
 	unsigned int    vhost_change;           // days in which a user must wait between vhost changes
 	unsigned int    clone_time;             // default expire for clone exemptions
 	unsigned int    commit_interval;        // interval between commits
+	bool            db_save_blocking;       // whether to always use a blocking database commit
 	bool            silent;                 // stop sending WALLOPS?
 	bool            join_chans;             // join registered channels?
 	bool            leave_chans;            // leave channels when empty?

libathemecore/conf.c
@@ -293,6 +293,7 @@ init_newconf(void)
 	add_bool_conf_item("KLINE_VERIFIED_IDENT", &conf_gi_table, 0, &config_options.kline_verified_ident, false);
 	add_duration_conf_item("CLONE_TIME", &conf_gi_table, 0, &config_options.clone_time, "m", 0);
 	add_duration_conf_item("COMMIT_INTERVAL", &conf_gi_table, 0, &config_options.commit_interval, "m", 300);
+	add_bool_conf_item("DB_SAVE_BLOCKING", &conf_gi_table, 0, &config_options.db_save_blocking, false);
 	add_dupstr_conf_item("OPERSTRING", &conf_gi_table, 0, &config_options.operstring, "is an IRC Operator");
 	add_dupstr_conf_item("SERVICESTRING", &conf_gi_table, 0, &config_options.servicestring, "is a Network Service");
 

commit 43fc85fbe5a305649f92cba8041824f79934bf96

modules/backend/corestorage: respect general::db_save_blocking
We're not just called from the periodic commit timer, but also
on rehash, which includes some modreloads too (when reloading a
module that has configuration options).

We should always use a blocking save regardless of what caused
the save to happen, rather than just test in the periodic commit
timer callback.

This makes running under Valgrind much more amenable.

Fixes: b427d04059d4d4f7b396

libathemecore/atheme.c
@@ -381,10 +381,7 @@ db_save_periodic(void *unused)
 {
 	slog(LG_DEBUG, "db_save_periodic(): initiating periodic database write");
 
-	if (config_options.db_save_blocking)
-		db_save(unused, DB_SAVE_BLOCKING);
-	else
-		db_save(unused, DB_SAVE_BG_REGULAR);
+	db_save(unused, DB_SAVE_BG_REGULAR);
 }
 
 int

modules/backend/corestorage.c
@@ -1088,7 +1088,6 @@ corestorage_db_write(void *filename, enum db_save_strategy strategy)
 #ifndef HAVE_FORK
 	corestorage_db_write_blocking(filename);
 #else
-
 	if (child_pid && strategy == DB_SAVE_BG_REGULAR)
 	{
 		slog(LG_DEBUG, "db_save(): previous save unfinished, skipping save");
@@ -1104,6 +1103,9 @@ corestorage_db_write(void *filename, enum db_save_strategy strategy)
 			waitpid(child_pid, NULL, 0);
 		}
 	}
+
+	if (config_options.db_save_blocking)
+		strategy = DB_SAVE_BLOCKING;
 
 	if (strategy == DB_SAVE_BLOCKING)
 	{