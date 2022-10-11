353162f2f1ad222c262f63dc9dd0a35d0fa40c6f
jesopo <github@lolnerd.net>
2022-10-11 23:19:06 +0000

    switch from weight & max to burst & replenish

--- modules/misc/login_throttling.c
+++ modules/misc/login_throttling.c
@@ -7,19 +7,19 @@
 
 #include <atheme.h>
 
-#define LT_WEIGHT_MIN   0U
-#define LT_WEIGHT_MAX   300U
-#define LT_WEIGHT_DEF   1U
+#define LT_BURST_MIN   0U
+#define LT_BURST_MAX   300U
+#define LT_BURST_DEF   1U
 
-#define LT_MAX_MIN      1U
-#define LT_MAX_MAX      1800U
-#define LT_MAX_DEF      2U
+#define LT_REPLENISH_MIN      0.1f
+#define LT_REPLENISH_MAX      1800.0f
+#define LT_REPLENISH_DEF      2.0f
 
 #define MAX_ADDR_LEN    56U // strlen("2001:0db8:0000:0000:0000:0000:0000:0001%abcdefghabcdefgh")
 
 struct lt_bucket
 {
-	time_t          timestamp;
+	double          timestamp;
 	char            key[MAX_ADDR_LEN + 1 + IDLEN + 1];
 };
 
@@ -27,16 +27,16 @@ static mowgli_list_t lt_config_table;
 static mowgli_patricia_t *lt_buckets = NULL;
 static mowgli_eventloop_timer_t *lt_expire_timer = NULL;
 
-static unsigned int lt_address_account_weight = 0U;
-static unsigned int lt_address_account_max = 0U;
-static unsigned int lt_address_weight = 0U;
-static unsigned int lt_address_max = 0U;
+static unsigned int lt_address_account_burst = 0U;
+static float lt_address_account_replenish = 0.0f;
+static unsigned int lt_address_burst = 0U;
+static float lt_address_replenish = 0.0f;
 
 static inline bool
 lt_deny_common(const time_t currts, const char *const restrict key,
-               const unsigned int vweight, const unsigned int vmax)
+               const unsigned int vburst, const float vreplenish)
 {
-	if (! vweight)
+	if (! vburst)
 		return false;
 
 	struct lt_bucket *bucket = mowgli_patricia_retrieve(lt_buckets, key);
@@ -47,29 +47,27 @@ lt_deny_common(const time_t currts, const char *const restrict key,
 
 		(void) mowgli_strlcpy(bucket->key, key, sizeof bucket->key);
 		(void) mowgli_patricia_add(lt_buckets, bucket->key, bucket);
-
-		return false;
 	}
 
-	const time_t vcap = (currts + vmax);
+	double currts_d = currts;
 
-	if (bucket->timestamp < currts)
-		bucket->timestamp = currts;
+	if (bucket->timestamp < currts_d)
+		bucket->timestamp = currts_d;
 
-	if ((bucket->timestamp += vweight) <= vcap)
+	if ((bucket->timestamp + vreplenish) > (currts_d + (vreplenish * vburst)))
+		return true;
+	else
+	{
+		bucket->timestamp += vreplenish;
 		return false;
-
-	if (bucket->timestamp > vcap)
-		bucket->timestamp = vcap;
-
-	return true;
+	}
 }
 
 static bool
 lt_deny_iplogin(const time_t currts, const char *const restrict ipaddr,
                 struct myuser ATHEME_VATTR_UNUSED *const restrict mu)
 {
-	return lt_deny_common(currts, ipaddr, lt_address_weight, lt_address_max);
+	return lt_deny_common(currts, ipaddr, lt_address_burst, lt_address_replenish);
 }
 
 static bool
@@ -80,7 +78,7 @@ lt_deny_ipacctlogin(const time_t currts, const char *const restrict ipaddr,
 
 	(void) snprintf(key, sizeof key, "%s/%s", ipaddr, entity(mu)->id);
 
-	return lt_deny_common(currts, key, lt_address_account_weight, lt_address_account_max);
+	return lt_deny_common(currts, key, lt_address_account_burst, lt_address_account_replenish);
 }
 
 static void
@@ -161,11 +159,11 @@ lt_expire_timer_cb(void ATHEME_VATTR_UNUSED *const restrict unused)
 	mowgli_patricia_iteration_state_t state;
 	struct lt_bucket *bucket;
 
-	const time_t expirets = (time(NULL) - SECONDS_PER_HOUR);
+	const time_t currts = time(NULL);
 
 	MOWGLI_PATRICIA_FOREACH(bucket, &state, lt_buckets)
 	{
-		if (bucket->timestamp > expirets)
+		if (bucket->timestamp > currts)
 			continue;
 
 		(void) mowgli_patricia_delete(lt_buckets, bucket->key);
@@ -207,23 +205,23 @@ mod_init(struct module *const restrict m)
 	(void) hook_add_user_can_login(&lt_user_can_login_hook);
 
 	(void) add_subblock_top_conf("throttle", &lt_config_table);
-	(void) add_uint_conf_item("address_account_weight", &lt_config_table, 0, &lt_address_account_weight,
-	                          LT_WEIGHT_MIN, LT_WEIGHT_MAX, LT_WEIGHT_DEF);
-	(void) add_uint_conf_item("address_account_max", &lt_config_table, 0, &lt_address_account_max,
-	                          LT_MAX_MIN, LT_MAX_MAX, LT_MAX_DEF);
-	(void) add_uint_conf_item("address_weight", &lt_config_table, 0, &lt_address_weight,
-	                          LT_WEIGHT_MIN, LT_WEIGHT_MAX, LT_WEIGHT_DEF);
-	(void) add_uint_conf_item("address_max", &lt_config_table, 0, &lt_address_max,
-	                          LT_MAX_MIN, LT_MAX_MAX, LT_MAX_DEF);
+	(void) add_uint_conf_item("address_account_burst", &lt_config_table, 0, &lt_address_account_burst,
+	                          LT_BURST_MIN, LT_BURST_MAX, LT_BURST_DEF);
+	(void) add_float_conf_item("address_account_replenish", &lt_config_table, 0, &lt_address_account_replenish,
+	                          LT_REPLENISH_MIN, LT_REPLENISH_MAX, LT_REPLENISH_DEF);
+	(void) add_uint_conf_item("address_burst", &lt_config_table, 0, &lt_address_burst,
+	                          LT_BURST_MIN, LT_BURST_MAX, LT_BURST_DEF);
+	(void) add_float_conf_item("address_replenish", &lt_config_table, 0, &lt_address_replenish,
+	                          LT_REPLENISH_MIN, LT_REPLENISH_MAX, LT_REPLENISH_DEF);
 }
 
 static void
 mod_deinit(const enum module_unload_intent ATHEME_VATTR_UNUSED intent)
 {
-	(void) del_conf_item("address_account_weight", &lt_config_table);
-	(void) del_conf_item("address_account_max", &lt_config_table);
-	(void) del_conf_item("address_weight", &lt_config_table);
-	(void) del_conf_item("address_max", &lt_config_table);
+	(void) del_conf_item("address_account_burst", &lt_config_table);
+	(void) del_conf_item("address_account_replenish", &lt_config_table);
+	(void) del_conf_item("address_burst", &lt_config_table);
+	(void) del_conf_item("address_replenish", &lt_config_table);
 	(void) del_top_conf("throttle");
 
 	(void) hook_del_operserv_info(&lt_operserv_info_hook);
